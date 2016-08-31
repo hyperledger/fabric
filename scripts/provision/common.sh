5ee82b99275b9d8c452b9d1bda71f792c06bc1cb
Gregory Haskins <gregory.haskins@gmail.com>
2016-08-31 11:11:50 -0400

    Revert "Change APT to use mirror protocol"
    This reverts commit d6d9d28d6fd2df26204ac34e4fd4625844e79fc5.
    
    We are seeing intermittent/context specific failures that seem
    to affect certain users.  The theory is that there might be
    inconsistencies in the mirror CDN network.  Lets try walking
    this back and see if things improve.
    
    Change-Id: I4d9a840cc67644803dfbeb70568fa18c8486514c
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- scripts/provision/common.sh
+++ scripts/provision/common.sh
@@ -2,16 +2,6 @@
 
 # Add any logic that is common to both the peer and docker environments here
 
-# Use the mirror protocol for apt so that geographically close mirrors are automatically chosen
-source /etc/lsb-release
-
-cat <<EOF > /etc/apt/sources.list
-deb mirror://mirrors.ubuntu.com/mirrors.txt $DISTRIB_CODENAME main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt $DISTRIB_CODENAME-updates main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt $DISTRIB_CODENAME-backports main restricted universe multiverse
-deb mirror://mirrors.ubuntu.com/mirrors.txt $DISTRIB_CODENAME-security main restricted universe multiverse
-EOF
-
 apt-get update -qq
 
 # Used by CHAINTOOL
