93ccc88a244d214762aa6e0d8ac9ff0ca19f42e5
mastersingh24 <gari.r.singh@gmail.com>
2016-09-04 10:17:41 -0400

    Temporary fix for failing CI for master branch
    TestHTTPExecuteDeployTransaction is failing on master
    because it is trying to go get
    http://github.com/hyperledger/fabric-test-resources/examples/chaincode/go/chaincode_example01
    which implements new interfaces from the convergence branch.
    This patch points the test at a different repo for now and demonstrates usage of
    gopkg.in to attempt to overcome limitations of go versioning
    
    Fixes FAB-305
    
    Change-Id: I55113b36a7cd6c505e7dc2503a31d91fa11b8c5b
    Signed-off-by: Gari Singh <gari.r.singh@gmail.com>

--- core/chaincode/exectransaction_test.go
+++ core/chaincode/exectransaction_test.go
@@ -376,7 +376,7 @@ func TestHTTPExecuteDeployTransaction(t *testing.T) {
 	// itself or it won't be downloaded because it will be found
 	// in GOPATH, which would defeat the test
 	testDBWrapper.CleanDB(t)
-	executeDeployTransaction(t, "http://github.com/hyperledger/fabric-test-resources/examples/chaincode/go/chaincode_example01")
+	executeDeployTransaction(t, "http://gopkg.in/mastersingh24/fabric-test-resources.v0")
 }
 
 // Check the correctness of the final state after transaction execution.
