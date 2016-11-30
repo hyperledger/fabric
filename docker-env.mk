c242f619358cc3f74be290c8560dc7c31e727cae
Gregory Haskins <gregory.haskins@gmail.com>
2016-11-30 14:25:24 -0500

    [BUILD] Make docker-image building more reliable
    We need to create a tighter coupling between the build
    system and the resulting docker images to prevent
    make from over-optimizing steps away.
    
    Fixes FAB-1145
    
    Change-Id: I50c7cdf98e173c88ff6722d6c28aeade7395428e
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

--- docker-env.mk
+++ docker-env.mk
@@ -56,5 +56,32 @@ BASE_DOCKER_TAG=$(ARCH)-$(BASEIMAGE_RELEASE)
 DOCKER_GO_LDFLAGS += $(GO_LDFLAGS)
 DOCKER_GO_LDFLAGS += -linkmode external -extldflags '-static -lpthread'
 
+#
+# What is a .dummy file?
+#
+# Make is designed to work with files.  It uses the presence (or lack thereof)
+# and timestamps of files when deciding if a given target needs to be rebuilt.
+# Docker containers throw a wrench into the works because the output of docker
+# builds do not translate into standard files that makefile rules can evaluate.
+# Therefore, we have to fake it.  We do this by constructioning our rules such
+# as
+#       my-docker-target/.dummy:
+#              docker build ...
+#              touch $@
+#
+# If the docker-build succeeds, the touch operation creates/updates the .dummy
+# file.  If it fails, the touch command never runs.  This means the .dummy
+# file follows relatively 1:1 with the underlying container.
+#
+# This isn't perfect, however.  For instance, someone could delete a docker
+# container using docker-rmi outside of the build, and make would be fooled
+# into thinking the dependency is statisfied when it really isn't.  This is
+# our closest approximation we can come up with.
+#
+# As an aside, also note that we incorporate the version number in the .dummy
+# file to differentiate different tags to fix FAB-1145
+#
+DUMMY = .dummy-$(DOCKER_TAG)
+
 
 
