02b780226263ebfb7470d9623a316c434309b8b2
grapebaba <281165273@qq.com>
2017-01-09 11:29:18 +0800

    [FAB-1540]Extract HashFilesInDir and IsCodeExist
    https://jira.hyperledger.org/browse/FAB-1540
    
    This commit extract several util functions and
    Fix a bug in previous java hashfilesindir function
    which actually doesn't fill the file content.
    
    Change-Id: I2e7cba473bb1ed43fc03df93f94f14a46b487d09
    Signed-off-by: grapebaba <281165273@qq.com>

--- core/chaincode/platforms/util/utils_test.go
+++ core/chaincode/platforms/util/utils_test.go
@@ -1,20 +1,4 @@
-/*
-Copyright IBM Corp. 2016 All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-package golang
+package util
 
 import (
 	"bytes"
@@ -33,7 +17,7 @@ func TestHashContentChange(t *testing.T) {
 
 	b2 := []byte("To be, or not to be- that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a sea of troubles, And by opposing end them. To die- to sleep- No more; and by a sleep to say we end The heartache, and the thousand natural shocks That flesh is heir to. 'Tis a consummation Devoutly to be wish'd.")
 
-	h1 := computeHash(b2, hash)
+	h1 := ComputeHash(b2, hash)
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	randIndex := (int(r.Uint32())) % len(b2)
@@ -53,12 +37,11 @@ func TestHashContentChange(t *testing.T) {
 	b2[randIndex] = randByte
 
 	//this is the core hash func under test
-	h2 := computeHash(b2, hash)
+	h2 := ComputeHash(b2, hash)
 
 	//the two hashes should be different
 	if bytes.Compare(h1, h2) == 0 {
-		t.Fail()
-		t.Logf("Hash expected to be different but is same")
+		t.Error("Hash expected to be different but is same")
 	}
 }
 
@@ -69,19 +52,18 @@ func TestHashLenChange(t *testing.T) {
 
 	b2 := []byte("To be, or not to be-")
 
-	h1 := computeHash(b2, hash)
+	h1 := ComputeHash(b2, hash)
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	randIndex := (int(r.Uint32())) % len(b2)
 
 	b2 = b2[0:randIndex]
 
-	h2 := computeHash(b2, hash)
+	h2 := ComputeHash(b2, hash)
 
 	//hash should be different
 	if bytes.Compare(h1, h2) == 0 {
-		t.Fail()
-		t.Logf("Hash expected to be different but is same")
+		t.Error("Hash expected to be different but is same")
 	}
 }
 
@@ -102,7 +84,7 @@ func TestHashOrderChange(t *testing.T) {
 	h1 := hash
 
 	for _, l := range b2 {
-		h1 = computeHash(l, h1)
+		h1 = ComputeHash(l, h1)
 	}
 
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -125,13 +107,12 @@ func TestHashOrderChange(t *testing.T) {
 
 	h2 := hash
 	for _, l := range b2 {
-		h2 = computeHash(l, hash)
+		h2 = ComputeHash(l, hash)
 	}
 
 	//hash should be different
 	if bytes.Compare(h1, h2) == 0 {
-		t.Fail()
-		t.Logf("Hash expected to be different but is same")
+		t.Error("Hash expected to be different but is same")
 	}
 }
 
@@ -140,20 +121,53 @@ func TestHashOverFiles(t *testing.T) {
 	b := []byte("firstcontent")
 	hash := util.ComputeCryptoHash(b)
 
-	hash, err := hashFilesInDir(".", "hashtestfiles", hash, nil)
+	hash, err := HashFilesInDir(".", "hashtestfiles1", hash, nil)
 
 	if err != nil {
 		t.Fail()
 		t.Logf("error : %s", err)
 	}
 
-	//as long as no files under "hashtestfiles" are changed, hash should always compute to the following
+	//as long as no files under "hashtestfiles1" are changed, hash should always compute to the following
 	expectedHash := "a4fe18bebf3d7e1c030c042903bdda9019b33829d03d9b95ab1edc8957be70dee6d786ab27b207210d29b5d9f88456ff753b8da5c244458cdcca6eb3c28a17ce"
 
 	computedHash := hex.EncodeToString(hash[:])
 
 	if expectedHash != computedHash {
-		t.Fail()
-		t.Logf("Hash expected to be unchanged")
+		t.Error("Hash expected to be unchanged")
+	}
+}
+
+func TestHashDiffDir(t *testing.T) {
+	b := []byte("firstcontent")
+	hash := util.ComputeCryptoHash(b)
+
+	hash1, err := HashFilesInDir(".", "hashtestfiles1", hash, nil)
+	if err != nil {
+		t.Errorf("Error getting code %s", err)
+	}
+	hash2, err := HashFilesInDir(".", "hashtestfiles2", hash, nil)
+	if err != nil {
+		t.Errorf("Error getting code %s", err)
+	}
+	if bytes.Compare(hash1, hash2) == 0 {
+		t.Error("Hash should be different for 2 different remote repos")
+	}
+
+}
+func TestHashSameDir(t *testing.T) {
+	b := []byte("firstcontent")
+	hash := util.ComputeCryptoHash(b)
+
+	hash1, err := HashFilesInDir(".", "hashtestfiles1", hash, nil)
+	if err != nil {
+		t.Errorf("Error getting code %s", err)
+	}
+	hash2, err := HashFilesInDir(".", "hashtestfiles1", hash, nil)
+	if err != nil {
+		t.Errorf("Error getting code %s", err)
+	}
+	if bytes.Compare(hash1, hash2) != 0 {
+		t.Error("Hash should be same across multiple downloads")
 	}
 }
