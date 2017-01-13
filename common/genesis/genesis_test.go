c288c76ce9bf990ef1919230bf858504b112f745
Jason Yellick <jyellick@us.ibm.com>
2017-01-13 11:16:54 -0500

    Fix copyright in CR series
    This CR fixes the copyright date on some new files introduced in the
    following changesets.
    
    https://gerrit.hyperledger.org/r/#/c/3887/
    https://jira.hyperledger.org/browse/FAB-1613
    
    https://gerrit.hyperledger.org/r/#/c/3895/
    https://jira.hyperledger.org/browse/FAB-1614
    
    https://gerrit.hyperledger.org/r/#/c/3897/
    https://jira.hyperledger.org/browse/FAB-1615
    
    https://gerrit.hyperledger.org/r/#/c/3899/
    https://jira.hyperledger.org/browse/FAB-1617
    https://jira.hyperledger.org/browse/FAB-1619
    
    Change-Id: Ibb32ec10051ed996939d66fc167ef9bce69042c7
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- common/genesis/genesis_test.go
+++ common/genesis/genesis_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright IBM Corp. 2016 All Rights Reserved.
+Copyright IBM Corp. 2017 All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
