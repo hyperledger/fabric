2c2a6dee2efbb07e1bb2bbe64ae4cacacf2ed94a
Alessandro Sorniotti <ale.linux@sopit.net>
2017-01-16 18:35:40 +0100

    [FAB-1320] - MSP config handler
    This change set introduces an implementation of the configtx.Handler that
    handles the configuration of MSP managers.
    
    Change-Id: I921132f89dd216ddcca32b13cee1862b634ba7ce
    Signed-off-by: Alessandro Sorniotti <ale.linux@sopit.net>

--- core/peer/msp/config.go
+++ core/peer/msp/config.go
@@ -1,6 +1,25 @@
+/*
+Copyright IBM Corp. 2017 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package mspmgmt
 
 import (
+	"fmt"
+
+	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric/common/util"
 	"github.com/hyperledger/fabric/msp"
 	"github.com/hyperledger/fabric/protos/common"
@@ -59,4 +78,63 @@ func GetMSPManagerFromBlock(cid string, b *common.Block) (msp.MSPManager, error)
 	}
 
 	return mgr, nil
+}
+
+// MSPConfigHandler
+type MSPConfigHandler struct {
+	config       []*mspprotos.MSPConfig
+	proposedMgr  msp.MSPManager
+	committedMgr msp.MSPManager
+}
+
+// BeginConfig called when a config proposal is begun
+func (bh *MSPConfigHandler) BeginConfig() {
+	if bh.config != nil || bh.proposedMgr != nil {
+		panic("Programming error, called BeginConfig while a proposal was in process")
+	}
+	bh.config = make([]*mspprotos.MSPConfig, 0)
+}
+
+// RollbackConfig called when a config proposal is abandoned
+func (bh *MSPConfigHandler) RollbackConfig() {
+	bh.config = nil
+	bh.proposedMgr = nil
+}
+
+// CommitConfig called when a config proposal is committed
+func (bh *MSPConfigHandler) CommitConfig() {
+	if bh.config == nil {
+		panic("Programming error, called CommitConfig with no proposal in process")
+	}
+
+	bh.committedMgr = bh.proposedMgr
+	bh.config = nil
+	bh.proposedMgr = nil
+}
+
+// ProposeConfig called when config is added to a proposal
+func (bh *MSPConfigHandler) ProposeConfig(configItem *common.ConfigurationItem) error {
+	// we expect MSP type of config items
+	if configItem.Key != msputils.MSPKey {
+		return fmt.Errorf("Expected config item key %s, got %s", msputils.MSPKey, configItem.Key)
+	}
+
+	mspconfig := &mspprotos.MSPConfig{}
+	err := proto.Unmarshal(configItem.Value, mspconfig)
+	if err != nil {
+		return fmt.Errorf("Error unmarshalling msp config item, err %s", err)
+	}
+
+	bh.config = append(bh.config, []*mspprotos.MSPConfig{mspconfig}...)
+	// the only way to make sure that I have a
+	// workable config is to toss the proposed
+	// manager, create a new one, call setup on
+	// it and return whatever error setup gives me
+	bh.proposedMgr = msp.NewMSPManager()
+	return bh.proposedMgr.Setup(bh.config)
+}
+
+// GetMSPManager returns the currently committed MSP manager
+func (bh *MSPConfigHandler) GetMSPManager() msp.MSPManager {
+	return bh.committedMgr
 }
