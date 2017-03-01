fd47bea6c029d2fac4981dcdb1abc89f4a3937c8
Kostas Christidis <kostas@christidis.io>
2017-03-01 14:49:55 -0500

    [FAB-2578] Move utility functions to proper file
    https://jira.hyperledger.org/browse/FAB-2578
    
    `CreateNextBlock` and `GetBlock` are utility functions and as such, they
    belong to `util.go`.
    
    Change-Id: I74567bdbb508f8d8be00defdfee06e303b3f6379
    Signed-off-by: Kostas Christidis <kostas@christidis.io>

--- orderer/ledger/ordererledger.go
+++ orderer/ledger/ordererledger.go
@@ -19,8 +19,6 @@ package ordererledger
 import (
 	cb "github.com/hyperledger/fabric/protos/common"
 	ab "github.com/hyperledger/fabric/protos/orderer"
-
-	"github.com/golang/protobuf/proto"
 )
 
 // Factory retrieves or creates new ledgers by chainID
@@ -61,55 +59,4 @@ type Writer interface {
 type ReadWriter interface {
 	Reader
 	Writer
-}
-
-// CreateNextBlock provides a utility way to construct the next block from contents and metadata for a given ledger
-// XXX this will need to be modified to accept marshaled envelopes to accomodate non-deterministic marshaling
-func CreateNextBlock(rl Reader, messages []*cb.Envelope) *cb.Block {
-	var nextBlockNumber uint64
-	var previousBlockHash []byte
-
-	if rl.Height() > 0 {
-		it, _ := rl.Iterator(&ab.SeekPosition{Type: &ab.SeekPosition_Newest{&ab.SeekNewest{}}})
-		<-it.ReadyChan() // Should never block, but just in case
-		block, status := it.Next()
-		if status != cb.Status_SUCCESS {
-			panic("Error seeking to newest block for chain with non-zero height")
-		}
-		nextBlockNumber = block.Header.Number + 1
-		previousBlockHash = block.Header.Hash()
-	}
-
-	data := &cb.BlockData{
-		Data: make([][]byte, len(messages)),
-	}
-
-	var err error
-	for i, msg := range messages {
-		data.Data[i], err = proto.Marshal(msg)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	block := cb.NewBlock(nextBlockNumber, previousBlockHash)
-	block.Header.DataHash = data.Hash()
-	block.Data = data
-
-	return block
-}
-
-// GetBlock is a utility method for retrieving a single block
-func GetBlock(rl Reader, index uint64) *cb.Block {
-	i, _ := rl.Iterator(&ab.SeekPosition{Type: &ab.SeekPosition_Specified{Specified: &ab.SeekSpecified{Number: index}}})
-	select {
-	case <-i.ReadyChan():
-		block, status := i.Next()
-		if status != cb.Status_SUCCESS {
-			return nil
-		}
-		return block
-	default:
-		return nil
-	}
 }
