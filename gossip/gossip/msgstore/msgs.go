aa841354af027b3a92eb7b2dcdcc287641909496
Gennady Laventman <gennady@il.ibm.com>
2017-03-14 17:04:12 +0200

    [FAB-2778] Msg store update
    Update message store to automatically expire messages.
    
    Includes new go routine and expiration callback functions
    
    Change-Id: I7c4ec11c7392015e65de5d0553bd1e4c8aca3e5c
    Signed-off-by: Gennady Laventman <gennady@il.ibm.com>

--- gossip/gossip/msgstore/msgs.go
+++ gossip/gossip/msgstore/msgs.go
@@ -19,8 +19,12 @@ package msgstore
 import (
 	"sync"
 
+	"time"
+
 	"github.com/hyperledger/fabric/gossip/common"
 )
+
+var noopLock = func() {}
 
 // invalidationTrigger is invoked on each message that was invalidated because of a message addition
 // i.e: if add(0), add(1) was called one after the other, and the store has only {1} after the sequence of invocations
@@ -30,7 +34,49 @@ type invalidationTrigger func(message interface{})
 // NewMessageStore returns a new MessageStore with the message replacing
 // policy and invalidation trigger passed.
 func NewMessageStore(pol common.MessageReplacingPolicy, trigger invalidationTrigger) MessageStore {
-	return &messageStoreImpl{pol: pol, lock: &sync.RWMutex{}, messages: make([]*msg, 0), invTrigger: trigger}
+	return newMsgStore(pol, trigger)
+}
+
+// NewMessageStoreExpirable returns a new MessageStore with the message replacing
+// policy and invalidation trigger passed. It supports old message expiration after msgTTL, during expiration first external
+// lock taken, expiration callback invoked and external lock released. Callback and external lock can be nil.
+func NewMessageStoreExpirable(pol common.MessageReplacingPolicy, trigger invalidationTrigger, msgTTL time.Duration, externalLock func(), externalUnlock func(), externalExpire func(interface{})) MessageStore {
+	store := newMsgStore(pol, trigger)
+
+	store.expirable = true
+	store.msgTTL = msgTTL
+
+	if externalLock != nil {
+		store.externalLock = externalLock
+	}
+
+	if externalUnlock != nil {
+		store.externalUnlock = externalUnlock
+	}
+
+	if externalExpire != nil {
+		store.expireMsgCallback = externalExpire
+	}
+
+	go store.expirationRoutine()
+	return store
+}
+
+func newMsgStore(pol common.MessageReplacingPolicy, trigger invalidationTrigger) *messageStoreImpl {
+	return &messageStoreImpl{
+		pol:        pol,
+		messages:   make([]*msg, 0),
+		invTrigger: trigger,
+
+		expirable:         false,
+		externalLock:      noopLock,
+		externalUnlock:    noopLock,
+		expireMsgCallback: func(m interface{}) {},
+		expiredCount:      0,
+
+		doneCh: make(chan struct{}),
+	}
+
 }
 
 // MessageStore adds messages to an internal buffer.
@@ -44,22 +90,42 @@ type MessageStore interface {
 	// returns true or false whether the message was added to the store
 	Add(msg interface{}) bool
 
+	// Checks if message is valid for insertion to store
+	// returns true or false whether the message can be added to the store
+	CheckValid(msg interface{}) bool
+
 	// size returns the amount of messages in the store
 	Size() int
 
 	// get returns all messages in the store
 	Get() []interface{}
+
+	// Stop all associated go routines
+	Stop()
 }
 
 type messageStoreImpl struct {
 	pol        common.MessageReplacingPolicy
-	lock       *sync.RWMutex
+	lock       sync.RWMutex
 	messages   []*msg
 	invTrigger invalidationTrigger
+
+	expirable    bool
+	msgTTL       time.Duration
+	expiredCount int
+
+	externalLock      func()
+	externalUnlock    func()
+	expireMsgCallback func(msg interface{})
+
+	doneCh   chan struct{}
+	stopOnce sync.Once
 }
 
 type msg struct {
-	data interface{}
+	data    interface{}
+	created time.Time
+	expired bool
 }
 
 // add adds a message to the store
@@ -78,13 +144,23 @@ func (s *messageStoreImpl) Add(message interface{}) bool {
 			s.messages = append(s.messages[:i], s.messages[i+1:]...)
 			n--
 			i--
-			break
-		default:
-			break
 		}
 	}
 
-	s.messages = append(s.messages, &msg{data: message})
+	s.messages = append(s.messages, &msg{data: message, created: time.Now()})
+	return true
+}
+
+// Checks if message is valid for insertion to store
+func (s *messageStoreImpl) CheckValid(message interface{}) bool {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+
+	for _, m := range s.messages {
+		if s.pol(message, m.data) == common.MessageInvalidated {
+			return false
+		}
+	}
 	return true
 }
 
@@ -92,18 +168,84 @@ func (s *messageStoreImpl) Add(message interface{}) bool {
 func (s *messageStoreImpl) Size() int {
 	s.lock.RLock()
 	defer s.lock.RUnlock()
-	return len(s.messages)
+	return len(s.messages) - s.expiredCount
 }
 
 // get returns all messages in the store
 func (s *messageStoreImpl) Get() []interface{} {
+	res := make([]interface{}, 0)
+
 	s.lock.RLock()
 	defer s.lock.RUnlock()
 
-	n := len(s.messages)
-	res := make([]interface{}, n)
-	for i := 0; i < n; i++ {
-		res[i] = s.messages[i].data
+	for _, msg := range s.messages {
+		if !msg.expired {
+			res = append(res, msg.data)
+		}
 	}
 	return res
+}
+
+func (s *messageStoreImpl) expireMessages() {
+	s.externalLock()
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	defer s.externalUnlock()
+
+	n := len(s.messages)
+	for i := 0; i < n; i++ {
+		m := s.messages[i]
+		if !m.expired {
+			if time.Since(m.created) > s.msgTTL {
+				m.expired = true
+				s.expireMsgCallback(m.data)
+				s.expiredCount++
+			}
+		} else {
+			if time.Since(m.created) > (s.msgTTL * 2) {
+				s.messages = append(s.messages[:i], s.messages[i+1:]...)
+				n--
+				i--
+				s.expiredCount--
+			}
+
+		}
+	}
+}
+
+func (s *messageStoreImpl) needToExpire() bool {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	for _, msg := range s.messages {
+		if !msg.expired && time.Since(msg.created) > s.msgTTL {
+			return true
+		} else if time.Since(msg.created) > (s.msgTTL * 2) {
+			return true
+		}
+	}
+	return false
+}
+
+func (s *messageStoreImpl) expirationRoutine() {
+	for {
+		select {
+		case <-s.doneCh:
+			return
+		case <-time.After(s.expirationCheckInterval()):
+			if s.needToExpire() {
+				s.expireMessages()
+			}
+		}
+	}
+}
+
+func (s *messageStoreImpl) Stop() {
+	stopFunc := func() {
+		close(s.doneCh)
+	}
+	s.stopOnce.Do(stopFunc)
+}
+
+func (s *messageStoreImpl) expirationCheckInterval() time.Duration {
+	return s.msgTTL / 100
 }
