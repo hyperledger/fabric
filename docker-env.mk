5b59e0652f9edf5bd12a6ff7fd2e9991495190fe
Nick Murray <nick@passkit.com>
2017-03-19 23:09:30 +0800

    [FAB-2829]-Build fails if Docker flags contain spaces
    There are cases where the make script setting DOCKER_BUILD_FLAGS and
    DOCKER_RUN_FLAGS that are used to set DBUILD and DRUN variables
    generate values containing whitespace.
    
    https://jira.hyperledger.org/browse/FAB-2829
    
    - Enclosed variables to prevent spaces breaking docker build and docker run commands.
    - Tested build on OSX (10.12.3), Debian 8.7 and Windows 10
    
    Change-Id: Ie353c6561dc1abecc05201205ef46b43608af6ac
    Signed-off-by: Nick Murray <nick@passkit.com>

--- docker-env.mk
+++ docker-env.mk
@@ -23,28 +23,28 @@ endif
 endif
 
 ifneq ($(http_proxy),)
-DOCKER_BUILD_FLAGS+=--build-arg http_proxy=$(http_proxy)
-DOCKER_RUN_FLAGS+=-e http_proxy=$(http_proxy)
+DOCKER_BUILD_FLAGS+=--build-arg 'http_proxy=$(http_proxy)'
+DOCKER_RUN_FLAGS+=-e 'http_proxy=$(http_proxy)'
 endif
 ifneq ($(https_proxy),)
-DOCKER_BUILD_FLAGS+=--build-arg https_proxy=$(https_proxy)
-DOCKER_RUN_FLAGS+=-e https_proxy=$(https_proxy)
+DOCKER_BUILD_FLAGS+=--build-arg 'https_proxy=$(https_proxy)'
+DOCKER_RUN_FLAGS+=-e 'https_proxy=$(https_proxy)'
 endif
 ifneq ($(HTTP_PROXY),)
-DOCKER_BUILD_FLAGS+=--build-arg HTTP_PROXY=$(HTTP_PROXY)
-DOCKER_RUN_FLAGS+=-e HTTP_PROXY=$(HTTP_PROXY)
+DOCKER_BUILD_FLAGS+=--build-arg 'HTTP_PROXY=$(HTTP_PROXY)'
+DOCKER_RUN_FLAGS+=-e 'HTTP_PROXY=$(HTTP_PROXY)'
 endif
 ifneq ($(HTTPS_PROXY),)
-DOCKER_BUILD_FLAGS+=--build-arg HTTPS_PROXY=$(HTTPS_PROXY)
-DOCKER_RUN_FLAGS+=-e HTTPS_PROXY=$(HTTPS_PROXY)
+DOCKER_BUILD_FLAGS+=--build-arg 'HTTPS_PROXY=$(HTTPS_PROXY)'
+DOCKER_RUN_FLAGS+=-e 'HTTPS_PROXY=$(HTTPS_PROXY)'
 endif
 ifneq ($(no_proxy),)
-DOCKER_BUILD_FLAGS+=--build-arg no_proxy=$(no_proxy)
-DOCKER_RUN_FLAGS+=-e no_proxy=$(no_proxy)
+DOCKER_BUILD_FLAGS+=--build-arg 'no_proxy=$(no_proxy)'
+DOCKER_RUN_FLAGS+=-e 'no_proxy=$(no_proxy)'
 endif
 ifneq ($(NO_PROXY),)
-DOCKER_BUILD_FLAGS+=--build-arg NO_PROXY=$(NO_PROXY)
-DOCKER_RUN_FLAGS+=-e NO_PROXY=$(NO_PROXY)
+DOCKER_BUILD_FLAGS+=--build-arg 'NO_PROXY=$(NO_PROXY)'
+DOCKER_RUN_FLAGS+=-e 'NO_PROXY=$(NO_PROXY)'
 endif
 
 DRUN = docker run -i --rm $(DOCKER_RUN_FLAGS) \
