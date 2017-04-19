2ec150d4da380d492303866fea2f42cc9a6df649
Gregory Haskins <gregory.haskins@gmail.com>
2017-04-19 17:14:00 -0400

    [FAB-3259] Make cryptogen output more flexible
    This patch enhances the cryptogen tool by providing a more
    flexible configuration and output operation, based on a
    YAML config file and golang templates.
    
    Summary of changes:
    -------------------------
    
    *) Tool now has two subcommands
       *) "generate" - This takes the role of the previous top-level
                       command.  It gets rid of almost all of the
                       previous command-line switches and replaces them
                       with:
    
           -output: specify the output directory.  defaults to
                    crypto-config
           -config: specify the input configuration file.  defaults to
                    a configuration similar to before (1 orderer, 2 peer
                    orgs, 1 peer/org
    
       *) "showtemplate" - Prints the built-in default template to
                           standard out, suitble for saving/editing
                           for future "generate" sessions.
    
    *) CommonName outputs are now much more DNS friendly.  For example: we
       now use "peer3.org1.com" in favor of "peerOrg1Peer3".  Users also gain
       total control over the generation of the CommonNames with various
       templating facilities. See "cryptogen showtemplate" for more details.
    
    Change-Id: I5968a794d4469ada8d3b90e112bdfe93e77c9661
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- common/tools/cryptogen/main.go
+++ common/tools/cryptogen/main.go
@@ -16,139 +16,338 @@ limitations under the License.
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
-	"strings"
+	"text/template"
+
+	"gopkg.in/yaml.v2"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+
+	"bytes"
+	"io/ioutil"
 
 	"github.com/hyperledger/fabric/common/tools/cryptogen/ca"
 	"github.com/hyperledger/fabric/common/tools/cryptogen/msp"
 )
 
 const (
-	peerOrgBaseName    = "peerOrg"
-	peerBaseName       = "Peer"
-	userBaseName       = "User"
-	adminBaseName      = "Admin"
-	ordererOrgBaseName = "ordererOrg"
-	ordererBaseName    = "Orderer"
-	numOrdererOrgs     = 1
+	userBaseName            = "User"
+	adminBaseName           = "Admin"
+	defaultHostnameTemplate = "{{.Prefix}}{{.Index}}"
+	defaultCNTemplate       = "{{.Hostname}}.{{.Domain}}"
 )
+
+type HostnameData struct {
+	Prefix string
+	Index  int
+	Domain string
+}
+
+type CommonNameData struct {
+	Hostname string
+	Domain   string
+}
+
+type NodeTemplate struct {
+	Count    int    `yaml:"Count"`
+	Start    int    `yaml:"Start"`
+	Hostname string `yaml:"Hostname"`
+}
+
+type NodeSpec struct {
+	Hostname     string   `yaml:"Hostname"`
+	AltHostnames []string `yaml:"AltHostnames"`
+	CommonName   string   `yaml:"CommonName"`
+}
+
+type UsersSpec struct {
+	Count int `yaml:"Count"`
+}
+
+type OrgSpec struct {
+	Name     string       `yaml:"Name"`
+	Domain   string       `yaml:"Domain"`
+	Template NodeTemplate `yaml:"Template"`
+	Specs    []NodeSpec   `yaml:"Specs"`
+	Users    UsersSpec    `yaml:"Users"`
+}
+
+type Config struct {
+	OrdererOrgs []OrgSpec `yaml:"OrdererOrgs"`
+	PeerOrgs    []OrgSpec `yaml:"PeerOrgs"`
+}
+
+var defaultConfig = `
+# ---------------------------------------------------------------------------
+# "OrdererOrgs" - Definition of organizations managing orderer nodes
+# ---------------------------------------------------------------------------
+OrdererOrgs:
+  # ---------------------------------------------------------------------------
+  # Orderer
+  # ---------------------------------------------------------------------------
+  - Name: Orderer
+    Domain: example.com
+
+    # ---------------------------------------------------------------------------
+    # "Specs" - See PeerOrgs below for complete description
+    # ---------------------------------------------------------------------------
+    Specs:
+      - Hostname: orderer
+
+# ---------------------------------------------------------------------------
+# "PeerOrgs" - Definition of organizations managing peer nodes
+# ---------------------------------------------------------------------------
+PeerOrgs:
+  # ---------------------------------------------------------------------------
+  # Org1
+  # ---------------------------------------------------------------------------
+  - Name: Org1
+    Domain: org1.example.com
+
+    # ---------------------------------------------------------------------------
+    # "Specs"
+    # ---------------------------------------------------------------------------
+    # Uncomment this section to enable the explicit definition of hosts in your
+    # configuration.  Most users will want to use Template, below
+    #
+    # Specs is an array of Spec entries.  Each Spec entry consists of two fields:
+    #   - Hostname:   (Required) The desired hostname, sans the domain.
+    #   - CommonName: (Optional) Specifies the template or explicit override for
+    #                 the CN.  By default, this is the template:
+    #
+    #                              "{{.Hostname}}.{{.Domain}}"
+    #
+    #                 which obtains its values from the Spec.Hostname and
+    #                 Org.Domain, respectively.
+    # ---------------------------------------------------------------------------
+    # Specs:
+    #   - Hostname: foo # implicitly "foo.org1.example.com"
+    #     CommonName: foo27.org5.example.com # overrides Hostname-based FQDN set above
+    #   - Hostname: bar
+    #   - Hostname: baz
+
+    # ---------------------------------------------------------------------------
+    # "Template"
+    # ---------------------------------------------------------------------------
+    # Allows for the definition of 1 or more hosts that are created sequentially
+    # from a template. By default, this looks like "peer%d" from 0 to Count-1.
+    # You may override the number of nodes (Count), the starting index (Start)
+    # or the template used to construct the name (Hostname).
+    #
+    # Note: Template and Specs are not mutually exclusive.  You may define both
+    # sections and the aggregate nodes will be created for you.  Take care with
+    # name collisions
+    # ---------------------------------------------------------------------------
+    Template:
+      Count: 1
+      # Start: 5
+      # Hostname: {{.Prefix}}{{.Index}} # default
+
+    # ---------------------------------------------------------------------------
+    # "Users"
+    # ---------------------------------------------------------------------------
+    # Count: The number of user accounts _in addition_ to Admin
+    # ---------------------------------------------------------------------------
+    Users:
+      Count: 1
+
+  # ---------------------------------------------------------------------------
+  # Org2: See "Org1" for full specification
+  # ---------------------------------------------------------------------------
+  - Name: Org2
+    Domain: org2.example.com
+    Template:
+      Count: 1
+    Users:
+      Count: 1
+`
 
 //command line flags
 var (
-	numPeerOrgs = flag.Int("peerOrgs", 2,
-		"number of unique organizations with peers, used if peerOrgNames is not specified")
-	peerOrgNames = flag.String("peerOrgNames", "",
-		"comma-separated list of peer organization names")
-	numPeers = flag.Int("peersPerOrg", 1,
-		"number of peers per organization")
-	numPeerOrgUsers = flag.Int("peerOrgUsers", 1,
-		"number of users per peer organization")
+	app = kingpin.New("cryptogen", "Utility for generating Hyperledger Fabric key material")
 
-	numOrderers = flag.Int("ordererNodes", 1,
-		"number of ordering service nodes per organization")
-	ordererOrgName = flag.String("ordererOrgName", "",
-		"orderer organization name")
+	gen        = app.Command("generate", "Generate key material")
+	outputDir  = gen.Flag("output", "The output directory in which to place artifacts").Default("crypto-config").String()
+	configFile = gen.Flag("config", "The configuration template to use").File()
 
-	baseDir = flag.String("baseDir", ".",
-		"directory in which to place artifacts")
+	showtemplate = app.Command("showtemplate", "Show the default configuration template")
 )
 
 func main() {
+	kingpin.Version("0.0.1")
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
 
-	flag.Parse()
+	// "generate" command
+	case gen.FullCommand():
+		generate()
 
-	if flag.NFlag() == 0 {
-		fmt.Println("\nYou must specify at least one parameter")
-		flag.Usage()
+	// "showtemplate" command
+	case showtemplate.FullCommand():
+		fmt.Print(defaultConfig)
+		os.Exit(0)
+	}
+
+}
+
+func getConfig() (*Config, error) {
+	var configData string
+
+	if *configFile != nil {
+		data, err := ioutil.ReadAll(*configFile)
+		if err != nil {
+			return nil, fmt.Errorf("Error reading configuration: %s", err)
+		}
+
+		configData = string(data)
+	} else {
+		configData = defaultConfig
+	}
+
+	config := &Config{}
+	err := yaml.Unmarshal([]byte(configData), &config)
+	if err != nil {
+		return nil, fmt.Errorf("Error Unmarshaling YAML: %s", err)
+	}
+
+	return config, nil
+}
+
+func generate() {
+
+	config, err := getConfig()
+	if err != nil {
+		fmt.Printf("Error reading config: %s", err)
+		os.Exit(-1)
+	}
+
+	for _, orgSpec := range config.PeerOrgs {
+		err = generateNodeSpec(&orgSpec, "peer")
+		if err != nil {
+			fmt.Printf("Error processing peer configuration: %s", err)
+			os.Exit(-1)
+		}
+		generatePeerOrg(*outputDir, orgSpec)
+	}
+
+	for _, orgSpec := range config.OrdererOrgs {
+		generateNodeSpec(&orgSpec, "orderer")
+		if err != nil {
+			fmt.Printf("Error processing orderer configuration: %s", err)
+			os.Exit(-1)
+		}
+		generateOrdererOrg(*outputDir, orgSpec)
+	}
+}
+
+func parseTemplate(input, defaultInput string, data interface{}) (string, error) {
+
+	// Use the default if the input is an empty string
+	if len(input) == 0 {
+		input = defaultInput
+	}
+
+	t, err := template.New("parse").Parse(input)
+	if err != nil {
+		return "", fmt.Errorf("Error parsing template: %s", err)
+	}
+
+	output := new(bytes.Buffer)
+	err = t.Execute(output, data)
+	if err != nil {
+		return "", fmt.Errorf("Error executing template: %s", err)
+	}
+
+	return output.String(), nil
+}
+
+func generateNodeSpec(orgSpec *OrgSpec, prefix string) error {
+	// First process all of our templated nodes
+	for i := 0; i < orgSpec.Template.Count; i++ {
+		data := HostnameData{
+			Prefix: prefix,
+			Index:  i + orgSpec.Template.Start,
+			Domain: orgSpec.Domain,
+		}
+
+		hostname, err := parseTemplate(orgSpec.Template.Hostname, defaultHostnameTemplate, data)
+		if err != nil {
+			return err
+		}
+
+		spec := NodeSpec{Hostname: hostname}
+		orgSpec.Specs = append(orgSpec.Specs, spec)
+	}
+
+	// And finally touch up all specs to add the domain
+	for idx, spec := range orgSpec.Specs {
+		data := CommonNameData{
+			Hostname: spec.Hostname,
+			Domain:   orgSpec.Domain,
+		}
+
+		finalCN, err := parseTemplate(spec.CommonName, defaultCNTemplate, data)
+		if err != nil {
+			return err
+		}
+
+		orgSpec.Specs[idx].CommonName = finalCN
+	}
+
+	return nil
+}
+
+func generatePeerOrg(baseDir string, orgSpec OrgSpec) {
+
+	orgName := orgSpec.Domain
+
+	fmt.Println(orgName)
+	// generate CA
+	orgDir := filepath.Join(baseDir, "peerOrganizations", orgName)
+	caDir := filepath.Join(orgDir, "ca")
+	mspDir := filepath.Join(orgDir, "msp")
+	peersDir := filepath.Join(orgDir, "peers")
+	usersDir := filepath.Join(orgDir, "users")
+	adminCertsDir := filepath.Join(mspDir, "admincerts")
+	rootCA, err := ca.NewCA(caDir, orgName)
+	if err != nil {
+		fmt.Printf("Error generating CA for org %s:\n%v\n", orgName, err)
+		os.Exit(1)
+	}
+	err = msp.GenerateVerifyingMSP(mspDir, rootCA)
+	if err != nil {
+		fmt.Printf("Error generating MSP for org %s:\n%v\n", orgName, err)
 		os.Exit(1)
 	}
 
-	genDir := filepath.Join(*baseDir, "crypto-config")
-	if *peerOrgNames != "" {
-		generatePeerOrgs(genDir, strings.Split(*peerOrgNames, ","))
-	} else if *numPeerOrgs > 0 {
-		fmt.Printf("Generating %d peer organization(s) each with %d peer(s) ...\n",
-			*numPeerOrgs, *numPeers)
-		generatePeerOrgs(genDir, getOrgNames(peerOrgBaseName, *numPeerOrgs))
+	peerNames := []string{}
+	for _, spec := range orgSpec.Specs {
+		peerNames = append(peerNames, spec.CommonName)
 	}
+	generateNodes(peersDir, peerNames, rootCA)
 
-	if *ordererOrgName != "" {
-		fmt.Printf("Generating orderer organization %s with %d ordering node(s) ...\n",
-			*ordererOrgName, *numOrderers)
-		generateOrdererOrg(genDir, *ordererOrgName)
-	} else if numOrdererOrgs > 0 {
-		fmt.Printf("Generating %d orderer organization(s) and %d ordering node(s) ...\n",
-			numOrdererOrgs, *numOrderers)
-		generateOrdererOrg(genDir, fmt.Sprintf("%s1", ordererOrgBaseName))
+	// TODO: add ability to specify usernames
+	usernames := []string{}
+	for j := 1; j <= orgSpec.Users.Count; j++ {
+		usernames = append(usernames, fmt.Sprintf("%s%d@%s",
+			userBaseName, j, orgName))
 	}
+	// add an admin user
+	adminUserName := fmt.Sprintf("%s@%s",
+		adminBaseName, orgName)
 
-}
+	usernames = append(usernames, adminUserName)
+	generateNodes(usersDir, usernames, rootCA)
 
-func generatePeerOrgs(baseDir string, orgNames []string) {
-
-	for _, orgName := range orgNames {
-		fmt.Println(orgName)
-		// generate CA
-		orgDir := filepath.Join(baseDir, "peerOrganizations", orgName)
-		caDir := filepath.Join(orgDir, "ca")
-		mspDir := filepath.Join(orgDir, "msp")
-		peersDir := filepath.Join(orgDir, "peers")
-		usersDir := filepath.Join(orgDir, "users")
-		adminCertsDir := filepath.Join(mspDir, "admincerts")
-		rootCA, err := ca.NewCA(caDir, orgName)
-		if err != nil {
-			fmt.Printf("Error generating CA for org %s:\n%v\n", orgName, err)
-			os.Exit(1)
-		}
-		err = msp.GenerateVerifyingMSP(mspDir, rootCA)
-		if err != nil {
-			fmt.Printf("Error generating MSP for org %s:\n%v\n", orgName, err)
-			os.Exit(1)
-		}
-
-		// TODO: add ability to specify peer names
-		// for name just use default base name
-		peerNames := []string{}
-		for i := 1; i <= *numPeers; i++ {
-			peerNames = append(peerNames, fmt.Sprintf("%s%s%d",
-				orgName, peerBaseName, i))
-		}
-		generateNodes(peersDir, peerNames, rootCA)
-
-		// TODO: add ability to specify usernames
-		usernames := []string{}
-		for j := 1; j <= *numPeerOrgUsers; j++ {
-			usernames = append(usernames, fmt.Sprintf("%s%s%d",
-				orgName, userBaseName, j))
-		}
-		// add an admin user
-		usernames = append(usernames, fmt.Sprintf("%s%s",
-			orgName, adminBaseName))
-		generateNodes(usersDir, usernames, rootCA)
-
-		// copy the admin cert to the org's MSP admincerts
-		adminUserName := fmt.Sprintf("%s%s",
-			orgName, adminBaseName)
-		err = copyAdminCert(usersDir, adminCertsDir, adminUserName)
-		if err != nil {
-			fmt.Printf("Error copying admin cert for org %s:\n%v\n",
-				orgName, err)
-			os.Exit(1)
-		}
-
+	// copy the admin cert to the org's MSP admincerts
+	err = copyAdminCert(usersDir, adminCertsDir, adminUserName)
+	if err != nil {
+		fmt.Printf("Error copying admin cert for org %s:\n%v\n",
+			orgName, err)
+		os.Exit(1)
 	}
-}
-
-func getOrgNames(baseName string, count int) []string {
-	orgNames := []string{}
-	for i := 1; i <= count; i++ {
-		orgNames = append(orgNames, fmt.Sprintf("%s%d", baseName, i))
-	}
-	return orgNames
 }
 
 func copyAdminCert(usersDir, adminCertsDir, adminUserName string) error {
@@ -185,7 +384,9 @@ func generateNodes(baseDir string, nodeNames []string, rootCA *ca.CA) {
 
 }
 
-func generateOrdererOrg(baseDir, orgName string) {
+func generateOrdererOrg(baseDir string, orgSpec OrgSpec) {
+
+	orgName := orgSpec.Domain
 
 	// generate CA
 	orgDir := filepath.Join(baseDir, "ordererOrganizations", orgName)
@@ -208,22 +409,21 @@ func generateOrdererOrg(baseDir, orgName string) {
 	// TODO: add ability to specify orderer names
 	// for name just use default base name
 	ordererNames := []string{}
-	for i := 1; i <= *numOrderers; i++ {
-		ordererNames = append(ordererNames, fmt.Sprintf("%s%s%d",
-			orgName, ordererBaseName, i))
+	for _, spec := range orgSpec.Specs {
+		ordererNames = append(ordererNames, spec.CommonName)
 	}
 	generateNodes(orderersDir, ordererNames, rootCA)
+
+	adminUserName := fmt.Sprintf("%s@%s",
+		adminBaseName, orgName)
 
 	// generate an admin for the orderer org
 	usernames := []string{}
 	// add an admin user
-	usernames = append(usernames, fmt.Sprintf("%s%s",
-		orgName, adminBaseName))
+	usernames = append(usernames, adminUserName)
 	generateNodes(usersDir, usernames, rootCA)
 
 	// copy the admin cert to the org's MSP admincerts
-	adminUserName := fmt.Sprintf("%s%s",
-		orgName, adminBaseName)
 	err = copyAdminCert(usersDir, adminCertsDir, adminUserName)
 	if err != nil {
 		fmt.Printf("Error copying admin cert for org %s:\n%v\n",
