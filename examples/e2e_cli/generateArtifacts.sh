9937c3612b0e12903a5110ad5c63e6334c468e28
ratnakar <asara.ratnakar@gmail.com>
2017-05-03 01:01:36 -0400

    [FAB-3604] e2e_cli sample improvements
    Covers the following :
    1. Use cryptogen tool to generate org certificates for each run.
    
    2. Make a generic docker-compose base file
       a. A docker-compose-cli.yaml extends the base and includes a cli
          container to run e2e test
       b. A docker-compose-e2e.yaml extends the base and includes
          fabric-ca container.(can be used for SDK or other tests)
    
    3. A timer flag in docker-compose CLI container to stop killing CLI
       container
    
    NOTE: A docker-compose-e2e-template.yaml also included. This is required
          to generate docker-compose-e2e.yaml, as we need to replace the
          private key dyamically.
    
    USAGE:
    	./network_setup.sh <up|down|restart> [channel] [CLI timeout in sec]
    
    Ex:
    	./network_setup.sh up mychannel 10
    	This starts CLI with channelname mychannel, CLI timeout set to 10s
    
    	./network_setup.sh down
    	Shutdown the network and clear all the artifacts.
    
    	./network_setup.sh restart xyz 10
    	restarts the network.
    
    Change-Id: I9bc80c0ad0e4dc259383038e7f69e3239a9409f3
    Signed-off-by: ratnakar <asara.ratnakar@gmail.com>

---
+++ examples/e2e_cli/generateArtifacts.sh
@@ -0,0 +1,95 @@
+#!/bin/bash +x
+
+#set -e
+
+: ${CHANNEL_NAME:="mychannel"}
+CHANNEL_NAME=$1
+echo $CHANNEL_NAME
+
+export FABRIC_ROOT=$PWD/../..
+export FABRIC_CFG_PATH=$PWD
+echo
+
+OS_ARCH=$(echo "$(uname -s)-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
+
+## Using docker-compose template replace private key file names with constants
+function replacePrivateKey () {
+	ARCH=`uname -s | grep Darwin`
+	if [ "$ARCH" == "Darwin" ]; then
+		OPTS="-it"
+	else
+		OPTS="-i"
+	fi
+
+	cp docker-compose-e2e-template.yaml docker-compose-e2e.yaml
+
+        CURRENT_DIR=$PWD
+        cd crypto-config/peerOrganizations/org1.example.com/ca/
+        PRIV_KEY=$(ls *_sk)
+        cd $CURRENT_DIR
+        sed $OPTS "s/CA1_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+        cd crypto-config/peerOrganizations/org2.example.com/ca/
+        PRIV_KEY=$(ls *_sk)
+        cd $CURRENT_DIR
+        sed $OPTS "s/CA2_PRIVATE_KEY/${PRIV_KEY}/g" docker-compose-e2e.yaml
+}
+
+## Generates Org certs using cryptogen tool
+function generateCerts (){
+	CRYPTOGEN=$FABRIC_ROOT/release/$OS_ARCH/bin/cryptogen
+
+	if [ -f "$CRYPTOGEN" ]; then
+            echo "Using cryptogen -> $CRYPTOGEN"
+	else
+	    echo "Building cryptogen"
+	    make -C $FABRIC_ROOT release-all
+	fi
+
+	echo
+	echo "##########################################################"
+	echo "##### Generate certificates using cryptogen tool #########"
+	echo "##########################################################"
+	$CRYPTOGEN generate --config=./crypto-config.yaml
+	echo
+}
+
+## Generate orderer genesis block , channel configuration transaction and anchor peer update transactions
+function generateChannelArtifacts() {
+
+	CONFIGTXGEN=$FABRIC_ROOT/release/$OS_ARCH/bin/configtxgen
+	if [ -f "$CONFIGTXGEN" ]; then
+            echo "Using configtxgen -> $CONFIGTXGEN"
+	else
+	    echo "Building configtxgen"
+	    make -C $FABRIC_ROOT release-all
+	fi
+
+	echo "##########################################################"
+	echo "#########  Generating Orderer Genesis block ##############"
+	echo "##########################################################"
+	$CONFIGTXGEN -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/orderer.genesis.block
+
+	echo
+	echo "#################################################################"
+	echo "### Generating channel configuration transaction 'channel.tx' ###"
+	echo "#################################################################"
+	$CONFIGTXGEN -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org1MSP   ##########"
+	echo "#################################################################"
+	$CONFIGTXGEN -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org2MSP   ##########"
+	echo "#################################################################"
+	$CONFIGTXGEN -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+	echo
+}
+
+generateCerts
+replacePrivateKey
+generateChannelArtifacts
+
