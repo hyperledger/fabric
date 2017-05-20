e176cf92abd855a6212738acab326a650ae6afa8
Nick Gaski <ngaski@us.ibm.com>
2017-05-20 13:45:49 -0400

    [FAB-3925] - Fix Bootstrap script
    Remove duplicate listing of docker images
    Rebase on top of Arnaud's window fix
    Point to release folder now
    Bash script was pointing to an empty directory
     Need reviews ASAP!
    Good on mac; need linux and windows test
    [ci-skip]
    
    Change-Id: I64a8e774708f27116cf68ee47d12c3b02a50b646
    Signed-off-by: Nick Gaski <ngaski@us.ibm.com>

--- examples/e2e_cli/bootstrap.sh
+++ examples/e2e_cli/bootstrap.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-export ARCH=$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(go env GOARCH)
+export ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
 
-curl https://nexus.hyperledger.org/content/repositories/logs/sandbox/fabric-binary/${ARCH}-1.0.0-alpha2.tar.gz | tar xz
+curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/fabric-binary/${ARCH}-1.0.0-alpha2/fabric-binary-${ARCH}-1.0.0-alpha2.tar.gz | tar xz
 cd release/${ARCH}
 sh download-dockerimages.sh -c $(uname -m)-1.0.0-alpha2 -f $(uname -m)-1.0.0-alpha2
-docker images
-
-
