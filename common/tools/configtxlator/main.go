b5934259c61bdbd636125531f2ca645f0149e9d4
Gari Singh <gari.r.singh@gmail.com>
2017-06-06 05:48:54 -0400

    [FAB-4352] Add version cmd to configtxlator
    Adds a version cmd to configtxlator.
    Modified configxtxlator to use kingpin
    as the CLI library as that's what is
    used in cryptogen as well.
    
    Version info is added to the binary
    using both
    make configtxlator
    make release[-all]
    
    (Note: this fixes the issue with
    make cryptogen not adding the version
    as well)
    
    Change-Id: I6bc52f12a09b4ea60dcab2693205cf151fb1b681
    Signed-off-by: Gari Singh <gari.r.singh@gmail.com>

--- common/tools/configtxlator/main.go
+++ common/tools/configtxlator/main.go
@@ -17,25 +17,50 @@ limitations under the License.
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/hyperledger/fabric/common/tools/configtxlator/metadata"
 	"github.com/hyperledger/fabric/common/tools/configtxlator/rest"
-
 	"github.com/op/go-logging"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var logger = logging.MustGetLogger("configtxlator")
 
+// command line flags
+var (
+	app = kingpin.New("configtxlator", "Utility for generating Hyperledger Fabric channel configurations")
+
+	start    = app.Command("start", "Start the configtxlator REST server")
+	hostname = start.Flag("hostname", "The hostname or IP on which the REST server will listen").Default("0.0.0.0").String()
+	port     = start.Flag("port", "The port on which the REST server will listen").Default("7059").Int()
+
+	version = app.Command("version", "Show version information")
+)
+
 func main() {
-	var serverPort int
+	kingpin.Version("0.0.1")
+	switch kingpin.MustParse(app.Parse(os.Args[1:])) {
+	// "start" command
+	case start.FullCommand():
+		startServer(fmt.Sprintf("%s:%d", *hostname, *port))
 
-	flag.IntVar(&serverPort, "serverPort", 7059, "Specify the port for the REST server to listen on.")
-	flag.Parse()
+	// "version" command
+	case version.FullCommand():
+		printVersion()
+	}
 
-	logger.Infof("Serving HTTP requests on port: %d", serverPort)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", serverPort), rest.NewRouter())
+}
 
-	logger.Fatal("Error runing http server:", err)
+func startServer(address string) {
+	logger.Infof("Serving HTTP requests on %s", address)
+	err := http.ListenAndServe(address, rest.NewRouter())
+
+	app.Fatalf("Error starting server:[%s]\n", err)
+}
+
+func printVersion() {
+	fmt.Println(metadata.GetVersionInfo())
 }
