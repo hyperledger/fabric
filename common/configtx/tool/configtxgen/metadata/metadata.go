6f876ad5bd148bbdc8d753afea1009d73629e55b
Nao Nishijima <nao.nishijima@hal.hitachi.com>
2017-06-06 10:26:31 -0700

    [FAB-4350] Add version cmd to configtxgen
    Adds a version cmd to configtxgen.
    Version info is added to the binary using both
    - make configtxgen
    - make release[-all]
    
    Change-Id: Ifc881edc7dc4f0802d67403fb06eaec732ef355a
    Signed-off-by: Nao Nishijima <nao.nishijima@hal.hitachi.com>

---
+++ common/configtx/tool/configtxgen/metadata/metadata.go
@@ -0,0 +1,38 @@
+/*
+Copyright 2017 Hitachi America
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metadata
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// Package version
+var Version string
+
+// Program name
+const ProgramName = "configtxgen"
+
+func GetVersionInfo() string {
+	if Version == "" {
+		Version = "development build"
+	}
+
+	return fmt.Sprintf("%s:\n Version: %s\n Go version: %s\n OS/Arch: %s",
+		ProgramName, Version, runtime.Version(),
+		fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH))
+}
