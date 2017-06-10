248fc59d61bc7fcedcb02dc6badad4b6ae23440b
Jay Guo <guojiannan1101@gmail.com>
2017-06-10 23:11:23 +0800

    [FAB-4349] Improve UT coverage of orderer/solo
    Change-Id: I45ae124305f720e552a47403c518f46623bd194e
    Signed-off-by: Jay Guo <guojiannan1101@gmail.com>

--- orderer/solo/consensus_test.go
+++ orderer/solo/consensus_test.go
@@ -26,6 +26,7 @@ import (
 	cb "github.com/hyperledger/fabric/protos/common"
 
 	logging "github.com/op/go-logging"
+	"github.com/stretchr/testify/assert"
 )
 
 func init() {
@@ -54,7 +55,8 @@ func goWithWait(target func()) *waitableGo {
 	return wg
 }
 
-func TestEmptyBatch(t *testing.T) {
+// This test checks that if consenter is halted before a timer fires, nothing is actually written.
+func TestHaltBeforeTimeout(t *testing.T) {
 	batchTimeout, _ := time.ParseDuration("1ms")
 	support := &mockmultichain.ConsenterSupport{
 		Blocks:          make(chan *cb.Block),
@@ -72,6 +74,45 @@ func TestEmptyBatch(t *testing.T) {
 	case <-support.Blocks:
 		t.Fatalf("Expected no invocations of Append")
 	case <-wg.done:
+	}
+}
+
+func TestStart(t *testing.T) {
+	batchTimeout, _ := time.ParseDuration("1ms")
+	support := &mockmultichain.ConsenterSupport{
+		Blocks:          make(chan *cb.Block),
+		BlockCutterVal:  mockblockcutter.NewReceiver(),
+		SharedConfigVal: &mockconfig.Orderer{BatchTimeoutVal: batchTimeout},
+	}
+	close(support.BlockCutterVal.Block)
+	bs, _ := New().HandleChain(support, nil)
+	bs.Start()
+	defer bs.Halt()
+
+	support.BlockCutterVal.CutNext = true
+	bs.Enqueue(testMessage)
+	select {
+	case <-support.Blocks:
+	case <-bs.Errored():
+		t.Fatalf("Expected not to exit")
+	}
+}
+
+func TestEnqueueAfterHalt(t *testing.T) {
+	batchTimeout, _ := time.ParseDuration("1ms")
+	support := &mockmultichain.ConsenterSupport{
+		Blocks:          make(chan *cb.Block),
+		BlockCutterVal:  mockblockcutter.NewReceiver(),
+		SharedConfigVal: &mockconfig.Orderer{BatchTimeoutVal: batchTimeout},
+	}
+	defer close(support.BlockCutterVal.Block)
+	bs := newChain(support)
+	bs.Halt()
+	assert.False(t, bs.Enqueue(testMessage), "Enqueue should not be accepted after halt")
+	select {
+	case <-bs.Errored():
+	default:
+		t.Fatalf("Expected Errored to be closed by halt")
 	}
 }
 
@@ -186,5 +227,37 @@ func TestConfigStyleMultiBatch(t *testing.T) {
 	case <-time.After(time.Second):
 		t.Fatalf("Should have exited")
 	case <-wg.done:
+	}
+}
+
+// This test checks that solo consenter could recover from an erroneous situation
+// where empty batch is cut
+func TestRecoverFromError(t *testing.T) {
+	batchTimeout, _ := time.ParseDuration("1ms")
+	support := &mockmultichain.ConsenterSupport{
+		Blocks:          make(chan *cb.Block),
+		BlockCutterVal:  mockblockcutter.NewReceiver(),
+		SharedConfigVal: &mockconfig.Orderer{BatchTimeoutVal: batchTimeout},
+	}
+	defer close(support.BlockCutterVal.Block)
+	bs := newChain(support)
+	_ = goWithWait(bs.main)
+	defer bs.Halt()
+
+	syncQueueMessage(testMessage, bs, support.BlockCutterVal)
+	support.BlockCutterVal.CurBatch = nil
+
+	select {
+	case <-support.Blocks:
+		t.Fatalf("Expected no invocations of Append")
+	case <-time.After(2 * time.Millisecond):
+	}
+
+	support.BlockCutterVal.CutNext = true
+	syncQueueMessage(testMessage, bs, support.BlockCutterVal)
+	select {
+	case <-support.Blocks:
+	case <-time.After(time.Second):
+		t.Fatalf("Expected block to be cut")
 	}
 }
