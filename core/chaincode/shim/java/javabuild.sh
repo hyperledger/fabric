a18f02cc30984510c9d57e4425436896a16ca62b
Jun Nemoto <jun.nemoto.ff@hitachi.com>
2017-07-05 08:46:39 +0000

    [FAB-5179] Fix proxy auth for javaenv build
    Proxy authentication for javaenv build does not work due to the wrong
    name of the parameter. This patch fixes it.
    
    Change-Id: Ie75a46333f7565c5b7ee61951fe3149f4898f42c
    Signed-off-by: Jun Nemoto <jun.nemoto.ff@hitachi.com>

--- core/chaincode/shim/java/javabuild.sh
+++ core/chaincode/shim/java/javabuild.sh
@@ -30,7 +30,7 @@ pattern='(https?://)?((([^:\/]+)(:([^\/]*))?@)?([^:\/?]+)(:([0-9]+))?)'
 if [ -n "$HTTPPROXY" ]; then
 	if [[ "$HTTPPROXY" =~ $pattern ]]; then
 		[ -n "${BASH_REMATCH[4]}" ] && JAVA_OPTS="$JAVA_OPTS -Dhttp.proxyUser=${BASH_REMATCH[4]}"
-		[ -n "${BASH_REMATCH[6]}" ] && JAVA_OPTS="$JAVA_OPTS -Dhttp.proxyPass=${BASH_REMATCH[6]}"
+		[ -n "${BASH_REMATCH[6]}" ] && JAVA_OPTS="$JAVA_OPTS -Dhttp.proxyPassword=${BASH_REMATCH[6]}"
 		[ -n "${BASH_REMATCH[7]}" ] && JAVA_OPTS="$JAVA_OPTS -Dhttp.proxyHost=${BASH_REMATCH[7]}"
 		[ -n "${BASH_REMATCH[9]}" ] && JAVA_OPTS="$JAVA_OPTS -Dhttp.proxyPort=${BASH_REMATCH[9]}"
 	fi
@@ -38,7 +38,7 @@ fi
 if [ -n "$HTTPSPROXY" ]; then
 	if [[ "$HTTPSPROXY" =~ $pattern ]]; then
 		[ -n "${BASH_REMATCH[4]}" ] && JAVA_OPTS="$JAVA_OPTS -Dhttps.proxyUser=${BASH_REMATCH[4]}"
-		[ -n "${BASH_REMATCH[6]}" ] && JAVA_OPTS="$JAVA_OPTS -Dhttps.proxyPass=${BASH_REMATCH[6]}"
+		[ -n "${BASH_REMATCH[6]}" ] && JAVA_OPTS="$JAVA_OPTS -Dhttps.proxyPassword=${BASH_REMATCH[6]}"
 		[ -n "${BASH_REMATCH[7]}" ] && JAVA_OPTS="$JAVA_OPTS -Dhttps.proxyHost=${BASH_REMATCH[7]}"
 		[ -n "${BASH_REMATCH[9]}" ] && JAVA_OPTS="$JAVA_OPTS -Dhttps.proxyPort=${BASH_REMATCH[9]}"
 	fi
