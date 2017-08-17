16c40e59550e0d8950671613376067e55cfc8e4b
Volodymyr Paprotski <vpaprots@ca.ibm.com>
2017-08-16 20:49:29 -0400

    [FAB-5804] BCCSP yaml parsing in peer weakly-typed
    Fix BCCSP yaml parsing to be weakly-typed. Found this when
    BCCSP:
      PKCS11:
         SoftwareVerify: true #<< would not parse bool
    
    Change-Id: I14644000ecf59fabe9782c550cca8af61315f126
    Signed-off-by: Volodymyr Paprotski <vpaprots@ca.ibm.com>
    Signed-off-by: Gari Singh <gari.r.singh@gmail.com>

--- common/viperutil/config_test.go
+++ common/viperutil/config_test.go
@@ -1,17 +1,7 @@
 /*
-Copyright IBM Corp. 2016 All Rights Reserved.
+Copyright IBM Corp. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-                 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+SPDX-License-Identifier: Apache-2.0
 */
 
 package viperutil
@@ -439,7 +429,8 @@ func TestStringFromFileEnv(t *testing.T) {
 
 func TestEnhancedExactUnmarshalKey(t *testing.T) {
 	type Nested struct {
-		Key string
+		Key     string
+		BoolVar bool
 	}
 
 	type nestedKey struct {
@@ -450,7 +441,8 @@ func TestEnhancedExactUnmarshalKey(t *testing.T) {
 		"Top:\n" +
 		"  Nested:\n" +
 		"    Nested:\n" +
-		"      Key: BAD\n"
+		"      Key: BAD\n" +
+		"      BoolVar: true\n"
 
 	envVar := "VIPERUTIL_TOP_NESTED_NESTED_KEY"
 	envVal := "GOOD"
@@ -475,6 +467,10 @@ func TestEnhancedExactUnmarshalKey(t *testing.T) {
 
 	if uconf.Nested.Key != envVal {
 		t.Fatalf(`Expected: "%s", Actual: "%s"`, envVal, uconf.Nested.Key)
+	}
+
+	if uconf.Nested.BoolVar != true {
+		t.Fatalf(`Expected: "%t", Actual: "%t"`, true, uconf.Nested.BoolVar)
 	}
 
 }
--- common/viperutil/config_util.go
+++ common/viperutil/config_util.go
@@ -1,17 +1,7 @@
 /*
-Copyright IBM Corp. 2016 All Rights Reserved.
+Copyright IBM Corp. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-                 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+SPDX-License-Identifier: Apache-2.0
 */
 
 package viperutil
@@ -321,5 +311,17 @@ func EnhancedExactUnmarshalKey(baseKey string, output interface{}) error {
 	leafKeys := getKeysRecursively("", viper.Get, m)
 
 	logger.Debugf("%+v", leafKeys)
-	return mapstructure.Decode(leafKeys[baseKey], output)
+
+	config := &mapstructure.DecoderConfig{
+		Metadata:         nil,
+		Result:           output,
+		WeaklyTypedInput: true,
+	}
+
+	decoder, err := mapstructure.NewDecoder(config)
+	if err != nil {
+		return err
+	}
+
+	return decoder.Decode(leafKeys[baseKey])
 }
