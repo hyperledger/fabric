6a6770c4d926bf32828ad37cc7083e31bd468229
yacovm <yacovm@il.ibm.com>
2017-09-03 00:13:54 +0300

    [FAB-6019] Implement private data puller
    This commit implements an object that pulls private data
    on demand according to a given specification that includes
    details like:
    - Collection name
    - TxID
    - Channel
    
    Change-Id: I15244736628a8cd91f77eb80a4e8083267d9dcbd
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- protos/gossip/extensions.go
+++ protos/gossip/extensions.go
@@ -1,17 +1,7 @@
 /*
-Copyright IBM Corp. 2016 All Rights Reserved.
+Copyright IBM Corp. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+SPDX-License-Identifier: Apache-2.0
 */
 
 package gossip
@@ -23,6 +13,7 @@ import (
 	"fmt"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric/common/util"
 	"github.com/hyperledger/fabric/gossip/api"
 	"github.com/hyperledger/fabric/gossip/common"
 )
@@ -535,6 +526,8 @@ func (m *SignedGossipMessage) String() string {
 			gMsg = m.GetMemRes().toString()
 		} else if m.IsStateInfoSnapshot() {
 			gMsg = m.GetStateSnapshot().toString()
+		} else if m.GetPrivateRes() != nil {
+			gMsg = m.GetPrivateRes().ToString()
 		} else {
 			gMsg = m.GossipMessage.String()
 			isSimpleMsg = true
@@ -559,6 +552,24 @@ func (dd *DataDigest) FormattedDigests() []string {
 		return digestsToHex(dd.Digests)
 	}
 	return dd.Digests
+}
+
+// Hash returns the SHA256 representation of the PvtDataDigest's bytes
+func (dig *PvtDataDigest) Hash() (string, error) {
+	b, err := proto.Marshal(dig)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(util.ComputeSHA256(b)), nil
+}
+
+// ToString returns a string representation of this RemotePvtDataResponse
+func (res *RemotePvtDataResponse) ToString() string {
+	a := make([]string, len(res.Elements))
+	for i, el := range res.Elements {
+		a[i] = fmt.Sprintf("%s with %d elements", el.Digest.String(), len(el.Payload))
+	}
+	return fmt.Sprintf("%v", a)
 }
 
 func digestsToHex(digests []string) []string {
