7cbeb628252ef6c745e2c4398c7db4ddfbc1e018
Boliang Chen <cblsjtu@gmail.com>
2017-09-14 10:39:16 +0800

    [FAB-6156] Remove chaincode id comments
    Remove out-of-date comments of chaincode id in
    go chaincode examples.
    
    Change-Id: I784d73e1fa7a0a0b7ef45d2e075e3d95e5b2c1c3
    Signed-off-by: Boliang Chen <cblsjtu@gmail.com>

--- examples/chaincode/go/eventsender/eventsender.go
+++ examples/chaincode/go/eventsender/eventsender.go
@@ -16,12 +16,6 @@ limitations under the License.
 
 package main
 
-//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
-//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
-//to be modified as well with the new ID of chaincode_example02.
-//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
-//hard-coding.
-
 import (
 	"fmt"
 	"strconv"
