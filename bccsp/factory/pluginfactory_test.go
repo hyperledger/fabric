01c50efbf2d0831c86e234d371ab21f9656b03c6
Gari Singh <gari.r.singh@gmail.com>
2017-09-16 17:53:46 -0400

    [FAB-6189] Add support for bccsp plugins
    There are currently two types of BCCSP
    providers:
    - SW
    - PKCS11
    
    This CR addes a new "PLUGIN" type support
    BCCSP providers implemented as Go plugins.
    This is useful for providers who might
    not be able to use PKCS11 as well as for
    providers who want to use different
    cryptographic algorithms that are not
    supported via PKCS11 or the SW BCCSP.
    
    Change-Id: I908634cbd4e6215ad682c97f5acc7a064bca643f
    Signed-off-by: Gari Singh <gari.r.singh@gmail.com>

---
+++ bccsp/factory/pluginfactory_test.go
@@ -0,0 +1,91 @@
+// +build linux,go1.9,cgo
+// +build !ppc64le
+
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+package factory
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func buildPlugin(lib string, t *testing.T) {
+	t.Helper()
+	// check to see if the example plugin exists
+	if _, err := os.Stat(lib); err != nil {
+		// build the example plugin
+		cmd := exec.Command("go", "build", "-buildmode=plugin", "github.com/hyperledger/fabric/examples/plugins/bccsp")
+		err := cmd.Run()
+		if err != nil {
+			t.Fatalf("Could not build plugin: [%s]", err)
+		}
+	}
+}
+
+func TestPluginFactoryName(t *testing.T) {
+	f := &PluginFactory{}
+	assert.Equal(t, f.Name(), PluginFactoryName)
+}
+
+func TestPluginFactoryInvalidConfig(t *testing.T) {
+	f := &PluginFactory{}
+	opts := &FactoryOpts{}
+
+	_, err := f.Get(nil)
+	assert.Error(t, err)
+
+	_, err = f.Get(opts)
+	assert.Error(t, err)
+
+	opts.PluginOpts = &PluginOpts{}
+	_, err = f.Get(opts)
+	assert.Error(t, err)
+}
+
+func TestPluginFactoryValidConfig(t *testing.T) {
+	// build plugin
+	lib := "./bccsp.so"
+	defer os.Remove(lib)
+	buildPlugin(lib, t)
+
+	f := &PluginFactory{}
+	opts := &FactoryOpts{
+		PluginOpts: &PluginOpts{
+			Library: lib,
+		},
+	}
+
+	csp, err := f.Get(opts)
+	assert.NoError(t, err)
+	assert.NotNil(t, csp)
+
+	_, err = csp.GetKey([]byte{123})
+	assert.NoError(t, err)
+}
+
+func TestPluginFactoryFromOpts(t *testing.T) {
+	// build plugin
+	lib := "./bccsp.so"
+	defer os.Remove(lib)
+	buildPlugin(lib, t)
+
+	opts := &FactoryOpts{
+		ProviderName: "PLUGIN",
+		PluginOpts: &PluginOpts{
+			Library: lib,
+		},
+	}
+	csp, err := GetBCCSPFromOpts(opts)
+	assert.NoError(t, err)
+	assert.NotNil(t, csp)
+
+	_, err = csp.GetKey([]byte{123})
+	assert.NoError(t, err)
+}
---
+++ examples/plugins/bccsp/plugin.go
@@ -0,0 +1,83 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+package main
+
+import (
+	"C"
+	"hash"
+
+	"github.com/hyperledger/fabric/bccsp"
+)
+
+type impl struct{}
+
+// New returns a new instance of the BCCSP implementation
+func New(config map[string]interface{}) (bccsp.BCCSP, error) {
+	return &impl{}, nil
+}
+
+// KeyGen generates a key using opts.
+func (csp *impl) KeyGen(opts bccsp.KeyGenOpts) (k bccsp.Key, err error) {
+	return nil, nil
+}
+
+// KeyDeriv derives a key from k using opts.
+// The opts argument should be appropriate for the primitive used.
+func (csp *impl) KeyDeriv(k bccsp.Key, opts bccsp.KeyDerivOpts) (dk bccsp.Key, err error) {
+	return nil, nil
+}
+
+// KeyImport imports a key from its raw representation using opts.
+// The opts argument should be appropriate for the primitive used.
+func (csp *impl) KeyImport(raw interface{}, opts bccsp.KeyImportOpts) (k bccsp.Key, err error) {
+	return nil, nil
+}
+
+// GetKey returns the key this CSP associates to
+// the Subject Key Identifier ski.
+func (csp *impl) GetKey(ski []byte) (k bccsp.Key, err error) {
+	return nil, nil
+}
+
+// Hash hashes messages msg using options opts.
+// If opts is nil, the default hash function will be used.
+func (csp *impl) Hash(msg []byte, opts bccsp.HashOpts) (hash []byte, err error) {
+	return nil, nil
+}
+
+// GetHash returns and instance of hash.Hash using options opts.
+// If opts is nil, the default hash function will be returned.
+func (csp *impl) GetHash(opts bccsp.HashOpts) (h hash.Hash, err error) {
+	return nil, nil
+}
+
+// Sign signs digest using key k.
+// The opts argument should be appropriate for the algorithm used.
+//
+// Note that when a signature of a hash of a larger message is needed,
+// the caller is responsible for hashing the larger message and passing
+// the hash (as digest).
+func (csp *impl) Sign(k bccsp.Key, digest []byte, opts bccsp.SignerOpts) (signature []byte, err error) {
+	return nil, nil
+}
+
+// Verify verifies signature against key k and digest
+// The opts argument should be appropriate for the algorithm used.
+func (csp *impl) Verify(k bccsp.Key, signature, digest []byte, opts bccsp.SignerOpts) (valid bool, err error) {
+	return true, nil
+}
+
+// Encrypt encrypts plaintext using key k.
+// The opts argument should be appropriate for the algorithm used.
+func (csp *impl) Encrypt(k bccsp.Key, plaintext []byte, opts bccsp.EncrypterOpts) (ciphertext []byte, err error) {
+	return nil, nil
+}
+
+// Decrypt decrypts ciphertext using key k.
+// The opts argument should be appropriate for the algorithm used.
+func (csp *impl) Decrypt(k bccsp.Key, ciphertext []byte, opts bccsp.DecrypterOpts) (plaintext []byte, err error) {
+	return nil, nil
+}
