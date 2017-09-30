959a295cbc2192079d88fb1d82025a12f397e1a5
Jingxiao Gu <gujingxiao@gmail.com>
2017-09-30 16:35:28 +0800

    [FAB-4072] Enable experimental Java chain code support
    Undo the commit `29e0c40` that disabled the Java chaincode
    Use conditional go build tag `experimental` to enable Java chaincode support
    
    Change-Id: I16bb902e76c59b68ea1d47c382749c146ef33dd1
    Signed-off-by: Jingxiao Gu <gujingxiao@gmail.com>

---
+++ core/endorser/nojava.go
@@ -0,0 +1,13 @@
+//+build !experimental
+
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package endorser
+
+func javaEnabled() bool {
+	return false
+}
--- peer/chaincode/common.go
+++ peer/chaincode/common.go
@@ -1,17 +1,7 @@
 /*
 Copyright IBM Corp. 2016 All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+SPDX-License-Identifier: Apache-2.0
 */
 
 package chaincode
@@ -85,8 +75,13 @@ func getChaincodeSpec(cmd *cobra.Command) (*pb.ChaincodeSpec, error) {
 	}
 
 	chaincodeLang = strings.ToUpper(chaincodeLang)
-	if pb.ChaincodeSpec_Type_value[chaincodeLang] == int32(pb.ChaincodeSpec_JAVA) {
-		return nil, fmt.Errorf("Java chaincode is work-in-progress and disabled")
+	if javaEnabled() {
+		logger.Debug("java chaincode enabled")
+	} else {
+		logger.Debug("java chaincode disabled")
+		if pb.ChaincodeSpec_Type_value[chaincodeLang] == int32(pb.ChaincodeSpec_JAVA) {
+			return nil, fmt.Errorf("Java chaincode is work-in-progress and disabled")
+		}
 	}
 	spec = &pb.ChaincodeSpec{
 		Type:        pb.ChaincodeSpec_Type(pb.ChaincodeSpec_Type_value[chaincodeLang]),
