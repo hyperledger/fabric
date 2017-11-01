6ca4744428e39e83bbff664a21ba8749f5a9d338
yacovm <yacovm@il.ibm.com>
2017-11-01 19:27:16 +0200

    [FAB-6733] service discovery gRPC service
    This change set implements the discovery service gRPC server-side.
    It depends heavily on a support interface that will be implemented
    in upcoming change sets.
    
    Tests that cover 100% of the code were introduced.
    
    Change-Id: I6b85f996915103b11e716bbd138e21afb991ac57
    Signed-off-by: yacovm <yacovm@il.ibm.com>

---
+++ discovery/service_test.go
@@ -0,0 +1,426 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package discovery
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric/gossip/api"
+	common2 "github.com/hyperledger/fabric/gossip/common"
+	discovery2 "github.com/hyperledger/fabric/gossip/discovery"
+	"github.com/hyperledger/fabric/protos/common"
+	"github.com/hyperledger/fabric/protos/discovery"
+	"github.com/hyperledger/fabric/protos/gossip"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"golang.org/x/net/context"
+)
+
+func TestService(t *testing.T) {
+	ctx := context.Background()
+	req := &discovery.Request{
+		Authentication: &discovery.AuthInfo{
+			ClientIdentity: []byte{1, 2, 3},
+		},
+		Queries: []*discovery.Query{
+			{
+				Channel: "noneExistentChannel",
+			},
+		},
+	}
+	mockSup := &mockSupport{}
+	mockSup.On("ChannelExists", "noneExistentChannel").Return(false)
+	mockSup.On("ChannelExists", "channelWithAccessDenied").Return(true)
+	mockSup.On("ChannelExists", "channelWithAccessGranted").Return(true)
+	mockSup.On("EligibleForService", "channelWithAccessDenied", mock.Anything).Return(errors.New("foo"))
+	mockSup.On("EligibleForService", "channelWithAccessGranted", mock.Anything).Return(nil)
+	ed1 := &discovery.EndorsementDescriptor{
+		Chaincode: "cc1",
+	}
+	ed2 := &discovery.EndorsementDescriptor{
+		Chaincode: "cc2",
+	}
+	ed3 := &discovery.EndorsementDescriptor{
+		Chaincode: "cc3",
+	}
+	mockSup.On("PeersForEndorsement", "unknownCC").Return(nil, errors.New("unknown chaincode"))
+	mockSup.On("PeersForEndorsement", "cc1").Return(ed1, nil)
+	mockSup.On("PeersForEndorsement", "cc2").Return(ed2, nil)
+	mockSup.On("PeersForEndorsement", "cc3").Return(ed3, nil)
+
+	service := newService(false, mockSup)
+
+	// Scenario I: Channel does not exist
+	resp, err := service.Discover(ctx, toSignedRequest(req))
+	assert.NoError(t, err)
+	assert.Equal(t, wrapResult(&discovery.Error{Content: "access denied"}), resp)
+
+	// Scenario II: Channel does not exist
+	req.Queries[0].Channel = "channelWithAccessDenied"
+	resp, err = service.Discover(ctx, toSignedRequest(req))
+	assert.NoError(t, err)
+	assert.Equal(t, wrapResult(&discovery.Error{Content: "access denied"}), resp)
+
+	// Scenario III: Request with nil query
+	req.Queries[0].Channel = "channelWithAccessGranted"
+	req.Queries[0].Query = nil
+	resp, err = service.Discover(ctx, toSignedRequest(req))
+	assert.NoError(t, err)
+	assert.Contains(t, resp.Results[0].GetError().Content, "unknown or missing request type")
+
+	// Scenario IV: Request with a hollow sub-query
+	for _, subQuery := range []interface{}{
+		&discovery.Query_PeerQuery{},
+		&discovery.Query_CcQuery{},
+		&discovery.Query_ConfigQuery{},
+	} {
+		// The Query field is un-exported, so lets use reflection to set it manually.
+		field := reflect.ValueOf(req.Queries[0]).Elem().FieldByName("Query")
+		field.Set(reflect.ValueOf(subQuery))
+		resp, err = service.Discover(ctx, toSignedRequest(req))
+		assert.Nil(t, resp)
+		assert.Contains(t, err.Error(), "failed parsing request")
+	}
+
+	// Scenario V: Request with a CC query where one chaincode is unavailable
+	req.Queries[0].Query = &discovery.Query_CcQuery{
+		CcQuery: &discovery.ChaincodeQuery{
+			Chaincodes: []string{"unknownCC", "cc1"},
+		},
+	}
+
+	resp, err = service.Discover(ctx, toSignedRequest(req))
+	assert.NoError(t, err)
+	assert.Contains(t, resp.Results[0].GetError().Content, "failed constructing descriptor for chaincode unknownCC")
+
+	// Scenario VI: Request with a CC query where all are available
+	req.Queries[0].Query = &discovery.Query_CcQuery{
+		CcQuery: &discovery.ChaincodeQuery{
+			Chaincodes: []string{"cc1", "cc2", "cc3"},
+		},
+	}
+	resp, err = service.Discover(ctx, toSignedRequest(req))
+	assert.NoError(t, err)
+	expected := wrapResult(&discovery.ChaincodeQueryResult{
+		Content: []*discovery.EndorsementDescriptor{ed1, ed2, ed3},
+	})
+	assert.Equal(t, expected, resp)
+
+	// Scenario VII: Request with a config query
+	mockSup.On("Config", mock.Anything).Return(nil, errors.New("failed fetching config")).Once()
+	req.Queries[0].Query = &discovery.Query_ConfigQuery{
+		ConfigQuery: &discovery.ConfigQuery{},
+	}
+	resp, err = service.Discover(ctx, toSignedRequest(req))
+	assert.NoError(t, err)
+	assert.Contains(t, resp.Results[0].GetError().Content, "failed fetching config for channel channelWithAccessGranted")
+
+	// Scenario VII: Request with a config query
+	mockSup.On("Config", mock.Anything).Return(&discovery.ConfigResult{}, nil).Once()
+	req.Queries[0].Query = &discovery.Query_ConfigQuery{
+		ConfigQuery: &discovery.ConfigQuery{},
+	}
+	resp, err = service.Discover(ctx, toSignedRequest(req))
+	assert.NoError(t, err)
+	assert.NotNil(t, resp.Results[0].GetConfigResult())
+
+	// Scenario VIII: Request with a membership query
+	// Peers in membership view: { p0, p1, p2, p3}
+	// Peers in channel view: {p1, p2, p4}
+	// So that means that the returned peers should be the intersection
+	// which is: {p1, p2}
+	peersInMembershipView := discovery2.Members{
+		aliveMsg(0), aliveMsg(1), aliveMsg(2), aliveMsg(3),
+	}
+	peersInChannelView := discovery2.Members{
+		stateInfoMsg(1), stateInfoMsg(2), stateInfoMsg(4),
+	}
+	mockSup.On("PeersOfChannel", common2.ChainID("channelWithAccessGranted")).Return(peersInChannelView).Once()
+	mockSup.On("Peers").Return(peersInMembershipView).Once()
+	mockSup.On("IdentityInfo").Return(api.PeerIdentitySet{
+		idInfo(0, "O2"), idInfo(1, "O2"), idInfo(2, "O3"),
+		idInfo(3, "O3"), idInfo(4, "O3"),
+	}).Once()
+
+	req.Queries[0].Query = &discovery.Query_PeerQuery{
+		PeerQuery: &discovery.PeerMembershipQuery{},
+	}
+	resp, err = service.Discover(ctx, toSignedRequest(req))
+	expected = wrapResult(&discovery.PeerMembershipResult{
+		PeersByOrg: map[string]*discovery.Peers{
+			"O2": {
+				Peers: []*discovery.Peer{
+					{
+						Identity:       idInfo(1, "O2").Identity,
+						StateInfo:      stateInfoMsg(1).Envelope,
+						MembershipInfo: aliveMsg(1).Envelope,
+					},
+				},
+			},
+			"O3": {
+				Peers: []*discovery.Peer{
+					{
+						Identity:       idInfo(2, "O3").Identity,
+						StateInfo:      stateInfoMsg(2).Envelope,
+						MembershipInfo: aliveMsg(2).Envelope,
+					},
+				},
+			},
+		},
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, resp)
+}
+
+func TestValidateStructure(t *testing.T) {
+	extractHash := func(ctx context.Context) []byte {
+		return nil
+	}
+	// Scenarios I-V without TLS, scenarios VI onwards TLS
+
+	// Scenario I: Nil request
+	res, err := validateStructure(context.Background(), nil, "", false, extractHash)
+	assert.Nil(t, res)
+	assert.Equal(t, "nil request", err.Error())
+
+	// Scenario II: Malformed envelope
+	res, err = validateStructure(context.Background(), &discovery.SignedRequest{
+		Payload: []byte{1, 2, 3},
+	}, "", false, extractHash)
+	assert.Nil(t, res)
+	assert.Contains(t, err.Error(), "failed parsing request")
+
+	// Scenario III: Empty request
+	res, err = validateStructure(context.Background(), &discovery.SignedRequest{}, "", false, extractHash)
+	assert.Nil(t, res)
+	assert.Equal(t, "access denied, no authentication info in request", err.Error())
+
+	// Scenario IV: request without a client identity
+	req := &discovery.Request{
+		Authentication: &discovery.AuthInfo{},
+	}
+	b, _ := proto.Marshal(req)
+	res, err = validateStructure(context.Background(), &discovery.SignedRequest{
+		Payload: b,
+	}, "", false, extractHash)
+	assert.Nil(t, res)
+	assert.Equal(t, "access denied, client identity wasn't supplied", err.Error())
+
+	// Scenario V: request with a client identity, should succeed because no TLS is used
+	req = &discovery.Request{
+		Authentication: &discovery.AuthInfo{
+			ClientIdentity: []byte{1, 2, 3},
+		},
+	}
+	b, _ = proto.Marshal(req)
+	res, err = validateStructure(context.Background(), &discovery.SignedRequest{
+		Payload: b,
+	}, "", false, extractHash)
+	assert.NoError(t, err)
+	// Ensure returned request is as before serialization to bytes
+	assert.Equal(t, req, res)
+
+	// Scenario VI: request with a client identity but with TLS enabled but client doesn't send a TLS cert
+	req = &discovery.Request{
+		Authentication: &discovery.AuthInfo{
+			ClientIdentity: []byte{1, 2, 3},
+		},
+	}
+	b, _ = proto.Marshal(req)
+	res, err = validateStructure(context.Background(), &discovery.SignedRequest{
+		Payload: b,
+	}, "", true, extractHash)
+	assert.Nil(t, res)
+	assert.Equal(t, "client didn't send a TLS certificate", err.Error())
+
+	// Scenario VII: request with a client identity and with TLS enabled but the TLS cert hash doesn't match
+	// the computed one
+	extractHash = func(ctx context.Context) []byte {
+		return []byte{1, 2}
+	}
+	req = &discovery.Request{
+		Authentication: &discovery.AuthInfo{
+			ClientIdentity:    []byte{1, 2, 3},
+			ClientTlsCertHash: []byte{1, 2, 3},
+		},
+	}
+	b, _ = proto.Marshal(req)
+	res, err = validateStructure(context.Background(), &discovery.SignedRequest{
+		Payload: b,
+	}, "", true, extractHash)
+	assert.Nil(t, res)
+	assert.Equal(t, "client claimed TLS hash doesn't match computed TLS hash from gRPC stream", err.Error())
+
+	// Scenario VIII: request with a client identity and with TLS enabled and the TLS cert hash doesn't match
+	// the computed one
+	extractHash = func(ctx context.Context) []byte {
+		return []byte{1, 2, 3}
+	}
+	req = &discovery.Request{
+		Authentication: &discovery.AuthInfo{
+			ClientIdentity:    []byte{1, 2, 3},
+			ClientTlsCertHash: []byte{1, 2, 3},
+		},
+	}
+	b, _ = proto.Marshal(req)
+	res, err = validateStructure(context.Background(), &discovery.SignedRequest{
+		Payload: b,
+	}, "", true, extractHash)
+}
+
+func wrapResult(res interface{}) *discovery.Response {
+	if err, isErr := res.(*discovery.Error); isErr {
+		return &discovery.Response{
+			Results: []*discovery.QueryResult{
+				{
+					Result: &discovery.QueryResult_Error{
+						Error: err,
+					},
+				},
+			},
+		}
+	}
+	if ccRes, isCCQuery := res.(*discovery.ChaincodeQueryResult); isCCQuery {
+		return &discovery.Response{
+			Results: []*discovery.QueryResult{
+				{
+					Result: &discovery.QueryResult_CcQueryRes{
+						CcQueryRes: ccRes,
+					},
+				},
+			},
+		}
+	}
+	if membRes, isMembershipQuery := res.(*discovery.PeerMembershipResult); isMembershipQuery {
+		return &discovery.Response{
+			Results: []*discovery.QueryResult{
+				{
+					Result: &discovery.QueryResult_Members{
+						Members: membRes,
+					},
+				},
+			},
+		}
+	}
+	if confRes, isConfQuery := res.(*discovery.ConfigResult); isConfQuery {
+		return &discovery.Response{
+			Results: []*discovery.QueryResult{
+				{
+					Result: &discovery.QueryResult_ConfigResult{
+						ConfigResult: confRes,
+					},
+				},
+			},
+		}
+	}
+	panic(fmt.Sprint("invalid type:", reflect.TypeOf(res)))
+}
+
+func toSignedRequest(req *discovery.Request) *discovery.SignedRequest {
+	b, _ := proto.Marshal(req)
+	return &discovery.SignedRequest{
+		Payload: b,
+	}
+}
+
+type mockSupport struct {
+	mock.Mock
+}
+
+func (ms *mockSupport) IdentityInfo() api.PeerIdentitySet {
+	return ms.Called().Get(0).(api.PeerIdentitySet)
+}
+
+func (ms *mockSupport) ChannelExists(channel string) bool {
+	return ms.Called(channel).Get(0).(bool)
+}
+
+func (ms *mockSupport) PeersOfChannel(channel common2.ChainID) discovery2.Members {
+	return ms.Called(channel).Get(0).(discovery2.Members)
+}
+
+func (ms *mockSupport) Peers() discovery2.Members {
+	return ms.Called().Get(0).(discovery2.Members)
+}
+
+func (ms *mockSupport) PeersForEndorsement(cc string, channel common2.ChainID) (*discovery.EndorsementDescriptor, error) {
+	args := ms.Called(cc)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*discovery.EndorsementDescriptor), args.Error(1)
+}
+
+func (*mockSupport) Chaincodes(id common2.ChainID) []*gossip.Chaincode {
+	panic("implement me")
+}
+
+func (ms *mockSupport) EligibleForService(channel string, data common.SignedData) error {
+	return ms.Called(channel, data).Error(0)
+}
+
+func (ms *mockSupport) Config(channel string) (*discovery.ConfigResult, error) {
+	args := ms.Called(channel)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*discovery.ConfigResult), args.Error(1)
+}
+
+func idInfo(id int, org string) api.PeerIdentityInfo {
+	endpoint := fmt.Sprintf("p%d", id)
+	return api.PeerIdentityInfo{
+		PKIId:        common2.PKIidType(endpoint),
+		Organization: api.OrgIdentityType(org),
+		Identity:     api.PeerIdentityType(endpoint),
+	}
+}
+
+func stateInfoMsg(id int) discovery2.NetworkMember {
+	endpoint := fmt.Sprintf("p%d", id)
+	pkiID := common2.PKIidType(endpoint)
+	si := &gossip.StateInfo{
+		PkiId: pkiID,
+	}
+	gm := &gossip.GossipMessage{
+		Content: &gossip.GossipMessage_StateInfo{
+			StateInfo: si,
+		},
+	}
+	sm, _ := gm.NoopSign()
+	return discovery2.NetworkMember{
+		PKIid:    pkiID,
+		Envelope: sm.Envelope,
+	}
+}
+
+func aliveMsg(id int) discovery2.NetworkMember {
+	endpoint := fmt.Sprintf("p%d", id)
+	pkiID := common2.PKIidType(endpoint)
+	am := &gossip.AliveMessage{
+		Membership: &gossip.Member{
+			PkiId:    pkiID,
+			Endpoint: endpoint,
+		},
+	}
+	gm := &gossip.GossipMessage{
+		Content: &gossip.GossipMessage_AliveMsg{
+			AliveMsg: am,
+		},
+	}
+	sm, _ := gm.NoopSign()
+	return discovery2.NetworkMember{
+		PKIid:    pkiID,
+		Endpoint: endpoint,
+		Envelope: sm.Envelope,
+	}
+}
