071f81fb2dd1a7323be9065131f9ab7d9c739fcb
Christopher Ferris <chrisfer@us.ibm.com>
2017-11-22 17:17:04 -0500

    FAB-7090 improve d/l tracking process
    provide a generic bootstrap.sh script that defaults to the
    latest release but can be provided a specific version to
    download/install.
    
    update the docs to reflect the modified short URL and
    current version.
    
    add more comprehensice handling for older versions of curl.
    
    Change-Id: I79e7326ae42ac25b1aabc754159d0d9aa67b62be
    Signed-off-by: Christopher Ferris <chrisfer@us.ibm.com>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,14 +5,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-export VERSION=1.0.0-alpha2
+# current version of fabric released
+export VERSION=${1:-1.0.4}
+# current version of fabric-ca released
+export CA_VERSION=${2:-1.0.4}
 export ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
 #Set MARCH variable i.e ppc64le,s390x,x86_64,i386
 MARCH=`uname -m`
 
 dockerFabricPull() {
   local FABRIC_TAG=$1
-  for IMAGES in peer orderer couchdb ccenv javaenv kafka zookeeper; do
+  for IMAGES in peer orderer couchdb ccenv javaenv kafka zookeeper tools; do
       echo "==> FABRIC IMAGE: $IMAGES"
       echo
       docker pull hyperledger/fabric-$IMAGES:$FABRIC_TAG
@@ -29,10 +32,10 @@ dockerCaPull() {
 }
 
 : ${CA_TAG:="$MARCH-$VERSION"}
-: ${FABRIC_TAG:="$MARCH-$VERSION"}
+: ${FABRIC_TAG:="$MARCH-$CA_VERSION"}
 
 echo "===> Downloading platform binaries"
-curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/fabric-binary/${ARCH}-${VERSION}/fabric-binary-${ARCH}-${VERSION}.tar.gz | tar xz
+curl https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${ARCH}-${VERSION}/hyperledger-fabric-${ARCH}-${VERSION}.tar.gz | tar xz
 
 echo "===> Pulling fabric Images"
 dockerFabricPull ${FABRIC_TAG}
