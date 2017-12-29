052ec35f533b8b55c46df8e328ab3075a6ccd9f2
amandahla <amanda.andrade@serpro.gov.br>
2017-12-29 17:02:35 -0200

    [FAB-7406] Deploy chaincode in Kubernetes
    Signed-off-by: amandahla <amanda.andrade@serpro.gov.br>

---
+++ core/container/kubernetescontroller/kubernetescontroller.go
@@ -0,0 +1,46 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kubernetescontroller
+
+// NewKubernetesVM returns a new KubernetesVM instance
+/*func NewKubernetesVM() *KubernetesVM {
+	vm := KubernetesVM{}
+	vm.getClientFnc = getKubernetesClient
+	return &vm
+}
+
+func getKubernetesClient() (kubernetesClient, error) {
+	return cutil.NewKubernetesClient()
+}
+
+func (vm *KubernetesVM) Deploy(ctxt context.Context, ccid ccintf.CCID,
+}
+
+func (vm *KubernetesVM) Start(ctxt context.Context, ccid ccintf.CCID,
+	args []string, env []string, filesToUpload map[string][]byte, builder container.BuildSpecFactory, prelaunchFunc container.PrelaunchFunc) error {
+}
+
+func (vm *KubernetesVM) Stop(ctxt context.Context, ccid ccintf.CCID, timeout uint, dontkill bool, dontremove bool) error {
+}
+
+func (vm *KubernetesVM) Destroy(ctxt context.Context, ccid ccintf.CCID, force bool, noprune bool) error {
+}
+
+func (vm *KubernetesVM) GetVMName(ccid ccintf.CCID, format func(string) (string, error)) (string, error) {
+}
+*/
+
