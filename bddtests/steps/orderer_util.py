b68b09b377231fba98f32ed7f4a0aca4892ae6c2
Luis Sanchez <sanchezl@us.ibm.com>
2018-01-25 15:44:15 -0500

    [FAB-6058] context.ordererUsers not initialized
    Change-Id: I2d3751a67989a4fcdabae80b0ba6ac9e7abb4c35
    Signed-off-by: Luis Sanchez <sanchezl@us.ibm.com>

--- bddtests/steps/orderer_util.py
+++ bddtests/steps/orderer_util.py
@@ -252,7 +252,7 @@ def getUserRegistration(context, enrollId):
     if 'ordererUsers' in context:
         pass
     else:
-        ordererContext.ordererUsers = {}
+        context.ordererUsers = {}
     if enrollId in context.ordererUsers:
         userRegistration = context.ordererUsers[enrollId]
     else:
