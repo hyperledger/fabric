a8d0253ed4bf6c40007caa7e4c34fdeb709d3241
Matthew Sykes <sykesmat@us.ibm.com>
2018-03-20 08:23:20 -0400

    [FAB-8976] Move fixtures closer to tests
    Test fixtures should typically be placed in `testdata` directories to
    prevent the go tooling from operating on them by default. There are a
    number of these fixtures that live in the incorrectly named top level
    `test` directory.
    
    Instead of keeping fixtures in one location, this commit moves the
    assets closer to the tests that use them and makes the necessary path
    modifications to reference them.
    
    Tests that were relying on fixtures to live on the go path, were
    modified to explicitly set the gopath to the testdata directory.
    
    Change-Id: I5dbc0cb5af41c63289aea925b08fea1584ca598f
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

---
+++ core/container/util/testdata/sourcefiles/src/Hello.java
@@ -0,0 +1,12 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+ */
+
+public class Hello {
+    public static void main(String []args) {
+        System.out.println("Hello");
+        System.exit(0);
+    }
+}
