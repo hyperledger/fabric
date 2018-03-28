9595d613cbe8c7fc90e4fde38ee3c291f39db96a
Robert P. J. Day <rpjday@crashcourse.ca>
2018-03-28 05:58:19 -0400

    [FAB-9206] Remove "html_add_permalinks" config setting
    The Sphinx configuration line:
    
      html_add_permalinks = True
    
    generates the documentation build warning:
    
      WARNING: The config value `html_add_permalinks' has
      type `bool', defaults to `str'.
    
    "True" is not a valid value for that setting and, in any event,
    permalinks are on by default so that line can be removed to
    get rid of the build warning.
    
    Change-Id: I3dd0185de8231264436a114d506461c75613acc0
    Signed-off-by: Robert P. J. Day <rpjday@crashcourse.ca>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -105,8 +105,6 @@ html_theme = 'sphinx_rtd_theme'
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_add_permalinks = True
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
