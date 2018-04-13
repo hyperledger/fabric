81e4983a2e8d0eeda76d23f134f7b5a32270d542
Angelo De Caro <adc@zurich.ibm.com>
2018-04-13 08:56:12 +0200

    [FAB-9671] Idemix Issuer Key Generator
    This change-set does the following:
    - implement the idemix issuer key generator
    - tests
    
    This change-set is the first of a set of change-sets
    that will introduce all the idemix crypto algorithm wrappers.
    The last change-set will provide the idemix-based BCCSP implementation.
    
    Change-Id: Id54ddd9af5fe0210cc9844405dc477492026788f
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>

---
+++ bccsp/idemix/idemix.go
@@ -0,0 +1,29 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+package idemix
+
+// IssuerPublicKey is the issuer public key
+type IssuerPublicKey interface {
+
+	// Bytes returns the byte representation of this key
+	Bytes() ([]byte, error)
+}
+
+// IssuerPublicKey is the issuer secret key
+type IssuerSecretKey interface {
+
+	// Bytes returns the byte representation of this key
+	Bytes() ([]byte, error)
+
+	// Public returns the corresponding public key
+	Public() IssuerPublicKey
+}
+
+// Issuer is a local interface to decouple from the idemix implementation
+type Issuer interface {
+	// NewKey generates a new idemix issuer key w.r.t the passed attribute names.
+	NewKey(AttributeNames []string) (IssuerSecretKey, error)
+}
