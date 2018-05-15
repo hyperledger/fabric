2141d8983d4975ecdaf2012f6cbc9c1de8ce9a50
Alessandro Sorniotti <ale.linux@sopit.net>
2018-05-15 17:28:34 +0200

    [FAB-10115] Function to generate idemix creds in e2e
    This change set introduces a function in the generateArtifacts.sh script to
    generate credentials for the identity mixer MSP.
    
    Change-Id: If8cfd66a7fca9cabc3c620441690658096292ebc
    Signed-off-by: Alessandro Sorniotti <ale.linux@sopit.net>

--- examples/e2e_cli/generateArtifacts.sh
+++ examples/e2e_cli/generateArtifacts.sh
@@ -59,6 +59,35 @@ function generateCerts (){
 	echo
 }
 
+function generateIdemixMaterial (){
+	IDEMIXGEN=$FABRIC_ROOT/release/$OS_ARCH/bin/idemixgen
+	CURDIR=`pwd`
+	IDEMIXMATDIR=$CURDIR/crypto-config/idemix
+
+	if [ -f "$IDEMIXGEN" ]; then
+            echo "Using idemixgen -> $IDEMIXGEN"
+	else
+	    echo "Building idemixgen"
+	    make -C $FABRIC_ROOT release
+	fi
+
+	echo
+	echo "####################################################################"
+	echo "##### Generate idemix crypto material using idemixgen tool #########"
+	echo "####################################################################"
+
+	mkdir -p $IDEMIXMATDIR
+	cd $IDEMIXMATDIR
+
+	# Generate the idemix issuer keys
+	$IDEMIXGEN ca-keygen
+
+	# Generate the idemix signer keys
+	$IDEMIXGEN signerconfig -u OU1 -e OU1 -r 1
+
+	cd $CURDIR
+}
+
 ## Generate orderer genesis block , channel configuration transaction and anchor peer update transactions
 function generateChannelArtifacts() {
 
