63c34a6f54b5e225bf44d5dedd07c10d9b6c67fe
Matthias Neugschwandtner <eug@zurich.ibm.com>
2018-05-18 16:00:12 +0200

    [FAB-9111] CC convenience layer for state-based EP
    This implementation of the CC convenience layer interface facilitates
    the usage of state-based endorsement. It allows easy modification of
    a state-based endorsement policy providing Add/Delete functions that
    are based on MSP-IDs and automatically creates the underlying
    endorsement policy.
    
    Change-Id: Ic71f17ba6d3a6004e6d5b265bb8cb562eac2eddb
    Signed-off-by: Matthias Neugschwandtner <eug@zurich.ibm.com>
    Signed-off-by: Alessandro Sorniotti <ale.linux@sopit.net>

---
+++ core/chaincode/shim/ext/statebased/statebasedimpl_test.go
@@ -0,0 +1,70 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package statebased_test
+
+import (
+	"testing"
+
+	"github.com/hyperledger/fabric/common/cauthdsl"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric/core/chaincode/shim/ext/statebased"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddOrg(t *testing.T) {
+	// add an org
+	ep, err := statebased.NewStateEP(nil)
+	assert.NoError(t, err)
+	err = ep.AddOrgs(statebased.RoleTypePeer, "Org1")
+	assert.NoError(t, err)
+	err = ep.AddOrgs("unknown", "Org1")
+	assert.Error(t, err)
+	rtdnee, ok := err.(*statebased.RoleTypeDoesNotExistError)
+	assert.True(t, ok)
+	assert.Equal(t, statebased.RoleType("unknown"), rtdnee.RoleType)
+	epBytes, err := ep.Policy()
+	assert.NoError(t, err)
+	expectedEP := cauthdsl.SignedByMspPeer("Org1")
+	expectedEPBytes, err := proto.Marshal(expectedEP)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedEPBytes, epBytes)
+}
+
+func TestListOrgs(t *testing.T) {
+	expectedEP := cauthdsl.SignedByMspPeer("Org1")
+	expectedEPBytes, err := proto.Marshal(expectedEP)
+	assert.NoError(t, err)
+
+	// retrieve the orgs
+	ep, err := statebased.NewStateEP(expectedEPBytes)
+	orgs := ep.ListOrgs()
+	assert.Equal(t, []string{"Org1"}, orgs)
+}
+
+func TestDelAddOrg(t *testing.T) {
+	expectedEP := cauthdsl.SignedByMspPeer("Org1")
+	expectedEPBytes, err := proto.Marshal(expectedEP)
+	assert.NoError(t, err)
+	ep, err := statebased.NewStateEP(expectedEPBytes)
+
+	// retrieve the orgs
+	orgs := ep.ListOrgs()
+	assert.Equal(t, []string{"Org1"}, orgs)
+
+	// mod the endorsement policy
+	ep.AddOrgs(statebased.RoleTypePeer, "Org2")
+	ep.DelOrgs("Org1")
+
+	// check whether what is stored is correct
+	epBytes, err := ep.Policy()
+	assert.NoError(t, err)
+	expectedEP = cauthdsl.SignedByMspPeer("Org2")
+	expectedEPBytes, err = proto.Marshal(expectedEP)
+	assert.NoError(t, err)
+	assert.Equal(t, expectedEPBytes, epBytes)
+}
