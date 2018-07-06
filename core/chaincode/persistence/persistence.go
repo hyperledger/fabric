563374a371d96bdfa5ee17eff540cdfbf2371314
Will Lahti <wtlahti@us.ibm.com>
2018-07-06 16:40:10 -0400

    FAB-10723 Retrieve persisted cc install pkg by hash
    This CR adds a way to retrieve a persisted ChaincodeInstallPackage
    from the filesystem using the hash of the package.
    
    Change-Id: Icbced3c99424fe346f98d80cc23abb54cf2972dc
    Signed-off-by: Will Lahti <wtlahti@us.ibm.com>

--- core/chaincode/persistence/persistence.go
+++ core/chaincode/persistence/persistence.go
@@ -20,38 +20,44 @@ import (
 
 var logger = flogging.MustGetLogger("chaincode/persistence")
 
-// IOWriter defines the interface needed for adding/removing/checking for existence
-// of a specified file
-type IOWriter interface {
-	WriteFile(string, []byte, os.FileMode) error
-	Stat(string) (os.FileInfo, error)
+// IOReadWriter defines the interface needed for reading, writing, removing, and
+// checking for existence of a specified file
+type IOReadWriter interface {
+	ReadFile(string) ([]byte, error)
 	Remove(name string) error
+	Stat(string) (os.FileInfo, error)
+	WriteFile(string, []byte, os.FileMode) error
 }
 
-// FilesystemWriter is the production implementation of the IOWriter interface
-type FilesystemWriter struct {
+// FilesystemIO is the production implementation of the IOWriter interface
+type FilesystemIO struct {
 }
 
 // WriteFile writes a file to the filesystem
-func (f *FilesystemWriter) WriteFile(filename string, data []byte, perm os.FileMode) error {
+func (f *FilesystemIO) WriteFile(filename string, data []byte, perm os.FileMode) error {
 	return ioutil.WriteFile(filename, data, perm)
 }
 
 // Stat checks for existence of the file on the filesystem
-func (f *FilesystemWriter) Stat(name string) (os.FileInfo, error) {
+func (f *FilesystemIO) Stat(name string) (os.FileInfo, error) {
 	return os.Stat(name)
 }
 
 // Remove removes a file from the filesystem - used for rolling back an in-flight
 // Save operation upon a failure
-func (f *FilesystemWriter) Remove(name string) error {
+func (f *FilesystemIO) Remove(name string) error {
 	return os.Remove(name)
+}
+
+// ReadFile reads a file from the filesystem
+func (f *FilesystemIO) ReadFile(filename string) ([]byte, error) {
+	return ioutil.ReadFile(filename)
 }
 
 // Store holds the information needed for persisting a chaincode install package
 type Store struct {
-	Path   string
-	Writer IOWriter
+	Path       string
+	ReadWriter IOReadWriter
 }
 
 // Save persists chaincode install package bytes with the given name
@@ -64,31 +70,61 @@ func (s *Store) Save(name, version string, ccInstallPkg []byte) error {
 
 	hashString := hex.EncodeToString(util.ComputeSHA256(ccInstallPkg))
 	metadataPath := filepath.Join(s.Path, hashString+".json")
-	if _, err := s.Writer.Stat(metadataPath); err == nil {
+	if _, err := s.ReadWriter.Stat(metadataPath); err == nil {
 		return errors.Errorf("chaincode metadata already exists at %s", metadataPath)
 	}
 
 	ccInstallPkgPath := filepath.Join(s.Path, hashString+".bin")
-	if _, err := s.Writer.Stat(ccInstallPkgPath); err == nil {
+	if _, err := s.ReadWriter.Stat(ccInstallPkgPath); err == nil {
 		return errors.Errorf("ChaincodeInstallPackage already exists at %s", ccInstallPkgPath)
 	}
 
-	if err := s.Writer.WriteFile(metadataPath, metadataJSON, 0600); err != nil {
+	if err := s.ReadWriter.WriteFile(metadataPath, metadataJSON, 0600); err != nil {
 		return errors.Wrapf(err, "error writing metadata file to %s", metadataPath)
 	}
 
-	if err := s.Writer.WriteFile(ccInstallPkgPath, ccInstallPkg, 0600); err != nil {
+	if err := s.ReadWriter.WriteFile(ccInstallPkgPath, ccInstallPkg, 0600); err != nil {
 		err = errors.Wrapf(err, "error writing chaincode install package to %s", ccInstallPkgPath)
 		logger.Error(err.Error())
 
 		// need to roll back metadata write above on error
-		if err2 := s.Writer.Remove(metadataPath); err2 != nil {
+		if err2 := s.ReadWriter.Remove(metadataPath); err2 != nil {
 			logger.Errorf("error removing metadata file at %s: %s", metadataPath, err2)
 		}
 		return err
 	}
 
 	return nil
+}
+
+// Load loads a persisted chaincode install package bytes with the given hash
+// and also returns the name and version
+func (s *Store) Load(hash []byte) (ccInstallPkg []byte, name, version string, err error) {
+	hashString := hex.EncodeToString(hash)
+	ccInstallPkgPath := filepath.Join(s.Path, hashString+".bin")
+	ccInstallPkg, err = s.ReadWriter.ReadFile(ccInstallPkgPath)
+	if err != nil {
+		err = errors.Wrapf(err, "error reading chaincode install package at %s", ccInstallPkgPath)
+		return nil, "", "", err
+	}
+
+	metadataPath := filepath.Join(s.Path, hashString+".json")
+	metadataBytes, err := s.ReadWriter.ReadFile(metadataPath)
+	if err != nil {
+		err = errors.Wrapf(err, "error reading metadata at %s", metadataPath)
+		return nil, "", "", err
+	}
+	ccMetadata := &ChaincodeMetadata{}
+	err = json.Unmarshal(metadataBytes, ccMetadata)
+	if err != nil {
+		err = errors.Wrapf(err, "error unmarshaling metadata at %s", metadataPath)
+		return nil, "", "", err
+	}
+
+	name = ccMetadata.Name
+	version = ccMetadata.Version
+
+	return ccInstallPkg, name, version, nil
 }
 
 // ChaincodeMetadata holds the name and version of a chaincode
@@ -105,7 +141,7 @@ func toJSON(name, version string) ([]byte, error) {
 
 	metadataBytes, err := json.Marshal(metadata)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to marshal name and version into JSON")
+		return nil, errors.Wrap(err, "error marshaling name and version into JSON")
 	}
 
 	return metadataBytes, nil
