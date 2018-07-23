90ca61cd857ebe1d5f086e3c83262b33c1b1ccf3
Jason Clark <jtclark@linux.vnet.ibm.com>
2018-07-23 13:06:27 -0400

    [FAB-11180] Adding docs-link to tox.ini
    This is required in order for tox to check
    for broken links.
    
    Change-Id: I88b3acb501f94513240ef66a02f279a8458fb74c
    Signed-off-by: Jason Clark <jtclark@linux.vnet.ibm.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -197,7 +197,8 @@ epub_copyright = copyright
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
 
-
-
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+# Skip the links with anchor tags during the linkcheck
+linkcheck_anchors = False
