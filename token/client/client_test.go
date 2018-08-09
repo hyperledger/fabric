891291bd081eeda4577f3a2bd483fc9ce5b71bc2
Kaoutar Elkhiyaoui <kao@zurich.ibm.com>
2018-08-09 18:24:47 +0200

    [FAB-11527] Transfer functions
    - implement transfer function using
    a mock prover peer and a mock tx submitter
    - add mocks for prover peer and transaction submitter
    - add test files
    
    Change-Id: Ide7449bcc9a78c4223c911ba9a3e4bdad589a4b6
    Signed-off-by: Kaoutar Elkhiyaoui <kao@zurich.ibm.com>

--- token/client/client_test.go
+++ token/client/client_test.go
@@ -124,5 +124,116 @@ func TestClient_Issue(t *testing.T) {
 
 		})
 	}
+}
 
+func TestClient_Transfer(t *testing.T) {
+
+	t.Parallel()
+
+	prover := &mocks.Prover{}
+	txSubmitter := &mocks.FabricTxSubmitter{}
+	signingIdentity := &mocks.SigningIdentity{}
+
+	resetMocks := func() {
+		prover.Mock = mock.Mock{}
+		txSubmitter.Mock = mock.Mock{}
+		signingIdentity.Mock = mock.Mock{}
+	}
+
+	tokenIDs, shares := prepareInputsForTransfer(t)
+
+	for _, testCase := range []struct {
+		name                   string
+		requestTransferReturns []interface{}
+		signReturns            []interface{}
+		submitReturns          interface{}
+		expectedErr            string
+	}{
+		{
+			name:                   "RequestTransfer() fails",
+			requestTransferReturns: []interface{}{nil, errors.New("wild banana")},
+			expectedErr:            "wild banana",
+		},
+		{
+			name:                   "Sign() fails",
+			requestTransferReturns: []interface{}{[]byte("pineapple"), nil},
+			signReturns:            []interface{}{nil, errors.New("wild carrots")},
+			expectedErr:            "wild carrots",
+		},
+		{
+			name:                   "Submit() fails",
+			requestTransferReturns: []interface{}{[]byte("pineapple"), nil},
+			signReturns:            []interface{}{[]byte("carrots"), nil},
+			submitReturns:          errors.New("wild banana"),
+			expectedErr:            "wild banana",
+		},
+		{
+			name:                   "Transfer() Succeeds",
+			requestTransferReturns: []interface{}{[]byte("pineapple"), nil},
+			signReturns:            []interface{}{[]byte("carrots"), nil},
+			submitReturns:          nil,
+		},
+	} {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+
+			defer resetMocks()
+
+			payload := &common.Payload{Data: []byte("pineapple")}
+			payloadBytes, err := proto.Marshal(payload)
+			assert.NoError(t, err)
+			envelope := &common.Envelope{Payload: payloadBytes, Signature: []byte("carrots")}
+			tx, err := proto.Marshal(envelope)
+			assert.NoError(t, err)
+
+			prover.On("RequestTransfer", tokenIDs, shares, signingIdentity).Return(
+				testCase.requestTransferReturns[0], testCase.requestTransferReturns[1])
+			if testCase.signReturns != nil {
+				signingIdentity.On("Sign", payloadBytes).Return(
+					testCase.signReturns[0], testCase.signReturns[1])
+			}
+			txSubmitter.On("Submit", tx).Return(testCase.submitReturns)
+
+			c := Client{Prover: prover, TxSubmitter: txSubmitter, SigningIdentity: signingIdentity}
+			_, err = c.Transfer(tokenIDs, shares)
+
+			if testCase.expectedErr == "" {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+				assert.EqualError(t, err, testCase.expectedErr)
+			}
+
+			prover.AssertCalled(t, "RequestTransfer", tokenIDs, shares, signingIdentity)
+			if testCase.requestTransferReturns[1] != nil {
+				signingIdentity.AssertNumberOfCalls(t, "Sign", 0)
+				txSubmitter.AssertNumberOfCalls(t, "Submit", 0)
+			} else {
+				payload = &common.Payload{Data: []byte("pineapple")}
+				payloadBytes, err = proto.Marshal(payload)
+				assert.NoError(t, err)
+
+				if testCase.signReturns != nil && testCase.signReturns[1] != nil {
+					signingIdentity.AssertCalled(t, "Sign", payloadBytes)
+					txSubmitter.AssertNumberOfCalls(t, "Submit", 0)
+				}
+
+				if testCase.submitReturns != nil {
+					tx, err := proto.Marshal(envelope)
+					assert.NoError(t, err)
+					signingIdentity.AssertCalled(t, "Sign", payloadBytes)
+					txSubmitter.AssertCalled(t, "Submit", tx)
+				}
+			}
+		})
+	}
+}
+
+func prepareInputsForTransfer(t *testing.T) ([][]byte, []*token.RecipientTransferShare) {
+	tokenIDs := [][]byte{[]byte("id1"), []byte("id2")}
+	shares := []*token.RecipientTransferShare{
+		{Recipient: []byte("alice"), Quantity: 100},
+		{Recipient: []byte("Bob"), Quantity: 50},
+	}
+	return tokenIDs, shares
 }
