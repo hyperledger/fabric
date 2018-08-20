4601bec66ef00f21eaa0f3e308d9477abc097616
Jason Yellick <jyellick@us.ibm.com>
2018-08-20 12:10:33 -0400

    FAB-13504 Add shims for uniform ledger access
    This first pair of shims is for accessing the public or private state in
    a consistent way.  In the future, additional shims for accessing state
    directly from the ledger and not from the chaincode APIs will be added.
    
    Change-Id: If7d4a08116e4ded521327795276a1c185d36b56b
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

---
+++ core/chaincode/lifecycle/ledger_shim.go
@@ -0,0 +1,33 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package lifecycle
+
+import (
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+// ChaincodePrivateLedgerShim wraps the chaincode shim to make access to keys in a collection
+// have the same semantics as normal public keys.
+type ChaincodePrivateLedgerShim struct {
+	Stub       shim.ChaincodeStubInterface
+	Collection string
+}
+
+// GetState returns the value for the key in the configured collection.
+func (cls *ChaincodePrivateLedgerShim) GetState(key string) ([]byte, error) {
+	return cls.Stub.GetPrivateData(cls.Collection, key)
+}
+
+// PutState sets the value for the key in the configured collection.
+func (cls *ChaincodePrivateLedgerShim) PutState(key string, value []byte) error {
+	return cls.Stub.PutPrivateData(cls.Collection, key, value)
+}
+
+// DelState deletes the key in the configured collection.
+func (cls *ChaincodePrivateLedgerShim) DelState(key string) error {
+	return cls.Stub.DelPrivateData(cls.Collection, key)
+}
---
+++ core/chaincode/lifecycle/ledger_shim_test.go
@@ -0,0 +1,87 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package lifecycle_test
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/hyperledger/fabric/core/chaincode/lifecycle"
+	"github.com/hyperledger/fabric/core/chaincode/lifecycle/mock"
+)
+
+var _ = Describe("LedgerShims", func() {
+	var (
+		fakeStub *mock.ChaincodeStub
+	)
+
+	BeforeEach(func() {
+		fakeStub = &mock.ChaincodeStub{}
+	})
+
+	Describe("ChaincodePrivateLedgerShim", func() {
+		var (
+			cls *lifecycle.ChaincodePrivateLedgerShim
+		)
+
+		BeforeEach(func() {
+			cls = &lifecycle.ChaincodePrivateLedgerShim{
+				Stub:       fakeStub,
+				Collection: "fake-collection",
+			}
+		})
+
+		Describe("GetState", func() {
+			BeforeEach(func() {
+				fakeStub.GetPrivateDataReturns([]byte("fake-value"), fmt.Errorf("fake-getstate-error"))
+			})
+
+			It("passes through to the stub private data implementation", func() {
+				res, err := cls.GetState("fake-key")
+				Expect(res).To(Equal([]byte("fake-value")))
+				Expect(err).To(MatchError(fmt.Errorf("fake-getstate-error")))
+				Expect(fakeStub.GetPrivateDataCallCount()).To(Equal(1))
+				collection, key := fakeStub.GetPrivateDataArgsForCall(0)
+				Expect(collection).To(Equal("fake-collection"))
+				Expect(key).To(Equal("fake-key"))
+			})
+		})
+
+		Describe("PutState", func() {
+			BeforeEach(func() {
+				fakeStub.PutPrivateDataReturns(fmt.Errorf("fake-putstate-error"))
+			})
+
+			It("passes through to the stub private data implementation", func() {
+				err := cls.PutState("fake-key", []byte("fake-value"))
+				Expect(err).To(MatchError(fmt.Errorf("fake-putstate-error")))
+				Expect(fakeStub.PutPrivateDataCallCount()).To(Equal(1))
+				collection, key, value := fakeStub.PutPrivateDataArgsForCall(0)
+				Expect(collection).To(Equal("fake-collection"))
+				Expect(key).To(Equal("fake-key"))
+				Expect(value).To(Equal([]byte("fake-value")))
+			})
+		})
+
+		Describe("DelState", func() {
+			BeforeEach(func() {
+				fakeStub.DelPrivateDataReturns(fmt.Errorf("fake-delstate-error"))
+			})
+
+			It("passes through to the stub private data implementation", func() {
+				err := cls.DelState("fake-key")
+				Expect(err).To(MatchError(fmt.Errorf("fake-delstate-error")))
+				Expect(fakeStub.DelPrivateDataCallCount()).To(Equal(1))
+				collection, key := fakeStub.DelPrivateDataArgsForCall(0)
+				Expect(collection).To(Equal("fake-collection"))
+				Expect(key).To(Equal("fake-key"))
+			})
+		})
+	})
+})
