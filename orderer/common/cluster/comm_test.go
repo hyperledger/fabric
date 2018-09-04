edd4a57e7843a11d02be396289ce3be654ca5bb8
yacovm <yacovm@il.ibm.com>
2018-09-04 22:57:28 +0300

    [FAB-11862] Retry operation in cluster UT
    The test TestMembershipReconfiguration sometimes fails in PPC builds
    because not enough time is given.
    
    I changed the test so it will use Eventually() and thus retry.
    
    Change-Id: I20bcff4d6224e5e329260712403c37819194b6df
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- orderer/common/cluster/comm_test.go
+++ orderer/common/cluster/comm_test.go
@@ -608,9 +608,14 @@ func TestMembershipReconfiguration(t *testing.T) {
 	_, err := node1.c.Remote(testChannel, node2.nodeInfo.ID)
 	assert.EqualError(t, err, fmt.Sprintf("node %d doesn't exist in channel test's membership", node2.nodeInfo.ID))
 	// Node 2 can connect to node 1, but it can't send it messages because node 1 doesn't know node 2 yet.
-	stub, err := node2.c.Remote(testChannel, node1.nodeInfo.ID)
-	assert.NoError(t, err)
 
+	gt := gomega.NewGomegaWithT(t)
+	gt.Eventually(func() (bool, error) {
+		_, err := node2.c.Remote(testChannel, node1.nodeInfo.ID)
+		return true, err
+	})
+
+	stub, err := node2.c.Remote(testChannel, node1.nodeInfo.ID)
 	_, err = stub.Step(testStepReq)
 	assert.EqualError(t, err, "rpc error: code = Unknown desc = certificate extracted from TLS connection isn't authorized")
 
