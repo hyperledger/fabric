c26c595e46b71acf8a9626fe189e6c8cc805744c
Nao Nishijima <nao.nishijima@hal.hitachi.com>
2018-09-24 23:14:46 +0000

    [FAB-12154] Configtxgen:Fix mismatched hyphens
    The usage of configtxgen shows "configPath" with a single hyphen
    prefix. However, error messages use a double hyphens prefix.
    This CR changes the error messages to a single hyphen prefix.
    
    FAB-12154 #done
    
    Change-Id: I81bdba89d445a82b76976d8777d1a044cc549598
    Signed-off-by: Nao Nishijima <nao.nishijima@hal.hitachi.com>

--- common/tools/configtxgen/main.go
+++ common/tools/configtxgen/main.go
@@ -237,13 +237,13 @@ func main() {
 		if err := recover(); err != nil {
 			if strings.Contains(fmt.Sprint(err), "Error reading configuration: Unsupported Config Type") {
 				logger.Error("Could not find configtx.yaml. " +
-					"Please make sure that FABRIC_CFG_PATH or --configPath is set to a path " +
+					"Please make sure that FABRIC_CFG_PATH or -configPath is set to a path " +
 					"which contains configtx.yaml")
 				os.Exit(1)
 			}
 			if strings.Contains(fmt.Sprint(err), "Could not find profile") {
 				logger.Error(fmt.Sprint(err) + ". " +
-					"Please make sure that FABRIC_CFG_PATH or --configPath is set to a path " +
+					"Please make sure that FABRIC_CFG_PATH or -configPath is set to a path " +
 					"which contains configtx.yaml with the specified profile")
 				os.Exit(1)
 			}
