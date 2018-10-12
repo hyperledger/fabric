c8401e4c34cb54b5e91010021ce588e00d774f56
Chris Elder <chris.elder@us.ibm.com>
2018-10-12 19:12:33 -0400

    [FAB-12443] Tolerate CouchDB 2.2 Changes
    CouchDB 2.2 does not throw an exception anymore
    if a specified index does not exist.
    This is a change and unit tests will need to not check
    for exceptions if a named index in the query does not exist.
    
    Example:
    {"selector":\{"owner":"tom"}
    , "use_index":["indexOwnerDoc","indexOwner"]}
    
    If the document indexOwnerDoc or the index indexOwner does not exist,
    an exception will not be thrown.
    
    CouchDB unit tests are changed to reflect the changes.
    
    Change-Id: I627a500599e696d580d06fe92fe8362f6923440e
    Signed-off-by: Chris Elder <chris.elder@us.ibm.com>

--- core/ledger/util/couchdb/couchdb_test.go
+++ core/ledger/util/couchdb/couchdb_test.go
@@ -1282,12 +1282,6 @@ func TestRichQuery(t *testing.T) {
 	//There should be 2 results for owner="tom" with a limit of 2
 	assert.Equal(t, 2, len(queryResult))
 
-	//Test query with invalid index  -------------------------------------------------------------------
-	queryString = `{"selector":{"owner":"tom"}, "use_index":["indexOwnerDoc","indexOwner"]}`
-
-	_, _, err = db.QueryDocuments(queryString)
-	assert.Error(t, err, "Error should have been thrown for an invalid index")
-
 	//Create an index definition
 	indexDefSize := `{"index":{"fields":[{"size":"desc"}]},"ddoc":"indexSizeSortDoc", "name":"indexSizeSortName","type":"json"}`
 
@@ -1303,22 +1297,6 @@ func TestRichQuery(t *testing.T) {
 
 	_, _, err = db.QueryDocuments(queryString)
 	assert.NoError(t, err, "Error when attempting to execute a query with a valid index")
-
-	//Test query with wrong fields for a valid index  -------------------------------------------------------------------
-	queryString = `{"selector":{"owner":{"$eq":"tom"}}, "use_index":"indexSizeSortName"}`
-
-	// no design doc specified, this should return a 400 error, indicating index not found
-	_, _, err = db.QueryDocuments(queryString)
-	assert.Error(t, err, "400 error should have been thrown for a missing index")
-	assert.Equal(t, true, strings.Contains(err.Error(), "Status Code:400"))
-
-	//Test query with wrong fields for a valid index  -------------------------------------------------------------------
-	queryString = `{"selector":{"owner":{"$eq":"tom"}}, "use_index":["indexSizeSortDoc","indexSizeSortName"]}`
-
-	// design doc specified, this should return a 500 error, indicating a bad match
-	_, _, err = db.QueryDocuments(queryString)
-	assert.Error(t, err, "500 error should have been thrown for a missing index with design doc specified")
-	assert.Equal(t, true, strings.Contains(err.Error(), "Status Code:500"))
 
 }
 
