d942308df6302d3510e835bad62f861ad854b4b3
Matthew Sykes <sykesmat@us.ibm.com>
2018-10-16 14:56:39 -0400

    [FAB-12440] disable goimports in linting
    Change-Id: I2d0daf47f881ae398e83ac322a71fdeb4c7bf090
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- scripts/golinter.sh
+++ scripts/golinter.sh
@@ -23,15 +23,6 @@ if [ -n "$OUTPUT" ]; then
     exit 1
 fi
 
-echo "Checking with goimports"
-OUTPUT="$(goimports -l ${source_dirs} | grep -Ev '(^|/)testdata/' || true)"
-if [ -n "$OUTPUT" ]; then
-    echo "The following files contain goimports errors"
-    echo $OUTPUT
-    echo "The goimports command 'goimports -l -w' must be run for these files"
-    exit 1
-fi
-
 # Now that context is part of the standard library, we should use it
 # consistently. The only place where the legacy golang.org version should be
 # referenced is in the generated protos.
