acc9b5a36fd055a938a48ce9f2d9d08e0234c9bc
Matthew Sykes <sykesmat@us.ibm.com>
2018-10-16 16:49:48 -0400

    [FAB-12440] disable goimports in linting
    Change-Id: Id83d2829f240b47dcd609c7e9ee95f2ba937ba29
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- scripts/golinter.sh
+++ scripts/golinter.sh
@@ -35,15 +35,6 @@ do
         exit 1
     fi
 
-    echo "Checking with goimports"
-    OUTPUT="$(goimports -srcdir $GOPATH/src/github.com/hyperledger/fabric -l $i)"
-    if [[ $OUTPUT ]]; then
-        echo "The following files contain goimports errors"
-        echo $OUTPUT
-        echo "The goimports command 'goimports -l -w' must be run for these files"
-        exit 1
-    fi
-
     echo "Checking with go vet"
     OUTPUT="$(go vet $i/...)"
     if [[ $OUTPUT ]]; then
