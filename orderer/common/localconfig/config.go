3b8445d94476aa8570816f59168c50b993e22ea7
Jay Guo <guojiannan1101@gmail.com>
2018-10-19 15:44:37 +0800

    [FAB-11995] Add Consensus section to orderer.yaml
    This CR adds an opaque section 'Consensus' in orderer.yaml to
    contain arbitrary config options needed by consensus plugin.
    
    Consensus plugin should use viperutil.Decode to decode the
    content of this section to a predefined struct.
    
    In this way, we don't need to expand TopLevel config for each
    consensus plugin, therefore maximizing flexibility.
    
    Change-Id: Ie657f1ca69cc79a8aaaf629d2df1bd0164cf5164
    Signed-off-by: Jay Guo <guojiannan1101@gmail.com>

--- orderer/common/localconfig/config.go
+++ orderer/common/localconfig/config.go
@@ -33,6 +33,7 @@ type TopLevel struct {
 	RAMLedger  RAMLedger
 	Kafka      Kafka
 	Debug      Debug
+	Consensus  interface{}
 }
 
 // General contains config which should be common among all orderer types.
