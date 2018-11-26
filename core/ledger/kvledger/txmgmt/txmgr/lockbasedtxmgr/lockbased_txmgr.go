6da148b339b31b704194292c0cf6bea88bc2ea81
Senthil Nathan N <cendhu@gmail.com>
2018-11-26 21:14:03 +0530

    recon: store the last committed BlockNum in txmgr
    FAB-12951 #done
    
    Change-Id: Iaa486bf436eae96d6331fbbcbfec942732a9f066
    Signed-off-by: senthil <cendhu@gmail.com>
    Signed-off-by: manish <manish.sethi@gmail.com>

--- core/ledger/kvledger/txmgmt/txmgr/lockbasedtxmgr/lockbased_txmgr.go
+++ core/ledger/kvledger/txmgmt/txmgr/lockbasedtxmgr/lockbased_txmgr.go
@@ -31,15 +31,16 @@ var logger = flogging.MustGetLogger("lockbasedtxmgr")
 // LockBasedTxMgr a simple implementation of interface `txmgmt.TxMgr`.
 // This implementation uses a read-write lock to prevent conflicts between transaction simulation and committing
 type LockBasedTxMgr struct {
-	ledgerid        string
-	db              privacyenabledstate.DB
-	pvtdataPurgeMgr *pvtdataPurgeMgr
-	validator       validator.Validator
-	stateListeners  []ledger.StateListener
-	ccInfoProvider  ledger.DeployedChaincodeInfoProvider
-	commitRWLock    sync.RWMutex
-	oldBlockCommit  sync.Mutex
-	current         *current
+	ledgerid              string
+	db                    privacyenabledstate.DB
+	pvtdataPurgeMgr       *pvtdataPurgeMgr
+	validator             validator.Validator
+	stateListeners        []ledger.StateListener
+	ccInfoProvider        ledger.DeployedChaincodeInfoProvider
+	commitRWLock          sync.RWMutex
+	oldBlockCommit        sync.Mutex
+	current               *current
+	lastCommittedBlockNum uint64
 }
 
 type current struct {
@@ -60,7 +61,12 @@ func (c *current) maxTxNumber() uint64 {
 func NewLockBasedTxMgr(ledgerid string, db privacyenabledstate.DB, stateListeners []ledger.StateListener,
 	btlPolicy pvtdatapolicy.BTLPolicy, bookkeepingProvider bookkeeping.Provider, ccInfoProvider ledger.DeployedChaincodeInfoProvider) (*LockBasedTxMgr, error) {
 	db.Open()
-	txmgr := &LockBasedTxMgr{ledgerid: ledgerid, db: db, stateListeners: stateListeners, ccInfoProvider: ccInfoProvider}
+	txmgr := &LockBasedTxMgr{
+		ledgerid:       ledgerid,
+		db:             db,
+		stateListeners: stateListeners,
+		ccInfoProvider: ccInfoProvider,
+	}
 	pvtstatePurgeMgr, err := pvtstatepurgemgmt.InstantiatePurgeMgr(ledgerid, db, btlPolicy, bookkeepingProvider)
 	if err != nil {
 		return nil, err
@@ -164,12 +170,13 @@ func (txmgr *LockBasedTxMgr) RemoveStaleAndCommitPvtDataOfOldBlocks(blocksPvtDat
 	// PrepareExpiringKeys from Commit but we rerun this here because,
 	// RemoveStaleAndCommitPvtDataOfOldBlocks may have added new data which might be
 	// eligible for expiry during the next regular block commit.
-	txmgr.pvtdataPurgeMgr.UpdateBookkeepingForPvtDataOfOldBlocks(batch.PvtUpdates)
-	nextBlockNumToBeCommitted, err := txmgr.getNextBlockNumberToBeCommitted()
-	if err != nil {
+	if err := txmgr.pvtdataPurgeMgr.UpdateBookkeepingForPvtDataOfOldBlocks(batch.PvtUpdates); err != nil {
 		return err
 	}
-	txmgr.pvtdataPurgeMgr.PrepareForExpiringKeys(nextBlockNumToBeCommitted)
+
+	if txmgr.lastCommittedBlockNum > 0 {
+		txmgr.pvtdataPurgeMgr.PrepareForExpiringKeys(txmgr.lastCommittedBlockNum + 1)
+	}
 
 	// (6) commit the pvt data to the stateDB
 	if err := txmgr.db.ApplyPrivacyAwareUpdates(batch, nil); err != nil {
@@ -351,16 +358,6 @@ func checkIfPvtWriteIsStale(hashedKey *privacyenabledstate.HashedCompositeKey,
 	return true, nil
 }
 
-// getNextBlockNumberToBeCommittedreturn the last committed block number + 1
-func (txmgr *LockBasedTxMgr) getNextBlockNumberToBeCommitted() (uint64, error) {
-	lastCommittedBlk, err := txmgr.db.GetLatestSavePoint()
-	if err != nil {
-		return 0, err
-	}
-
-	return lastCommittedBlk.BlockNum + 1, nil
-}
-
 func (uniquePvtData uniquePvtDataMap) transformToUpdateBatch() *privacyenabledstate.UpdateBatch {
 	batch := privacyenabledstate.NewUpdateBatch()
 	for hashedCompositeKey, pvtWrite := range uniquePvtData {
@@ -472,6 +469,7 @@ func (txmgr *LockBasedTxMgr) Commit() error {
 	// In the case of error state listeners will not recieve this call - instead a peer panic is caused by the ledger upon receiveing
 	// an error from this function
 	txmgr.updateStateListeners()
+	txmgr.lastCommittedBlockNum = txmgr.current.blockNum()
 	return nil
 }
 
