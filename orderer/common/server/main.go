3713cc30cdfb023f094f6baee97752816290afd8
yacovm <yacovm@il.ibm.com>
2018-12-04 17:52:06 +0200

    [FAB-13150] Re-enable etcdraft for v2.0 development
    This change set re-enables etcdraft to resume v2.0 development
    after v1.4 branch is cut.
    
    Change-Id: I384247a9de2763a207bbde4fa8e519d703241ad5
    Signed-off-by: yacovm <yacovm@il.ibm.com>
    Signed-off-by: Artem Barger <bartem@il.ibm.com>

--- orderer/common/server/main.go
+++ orderer/common/server/main.go
@@ -19,6 +19,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric-lib-go/healthz"
 	"github.com/hyperledger/fabric/common/channelconfig"
 	"github.com/hyperledger/fabric/common/crypto"
@@ -51,7 +52,7 @@ import (
 	"github.com/hyperledger/fabric/protos/utils"
 	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc"
-	kingpin "gopkg.in/alecthomas/kingpin.v2"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var logger = flogging.MustGetLogger("orderer.common.server")
@@ -357,8 +358,24 @@ func initializeBootstrapChannel(genesisBlock *cb.Block, lf blockledger.Factory) 
 	}
 }
 
-func isClusterType(_ *cb.Block) bool {
-	return false
+func isClusterType(genesisBlock *cb.Block) bool {
+	if genesisBlock.Data == nil || len(genesisBlock.Data.Data) == 0 {
+		logger.Fatalf("Empty genesis block")
+	}
+	env := &cb.Envelope{}
+	if err := proto.Unmarshal(genesisBlock.Data.Data[0], env); err != nil {
+		logger.Fatalf("Failed to unmarshal the genesis block's envelope: %v", err)
+	}
+	bundle, err := channelconfig.NewBundleFromEnvelope(env)
+	if err != nil {
+		logger.Fatalf("Failed creating bundle from the genesis block: %v", err)
+	}
+	ordConf, exists := bundle.OrdererConfig()
+	if !exists {
+		logger.Fatalf("Orderer config doesn't exist in bundle derived from genesis block")
+	}
+	_, exists = clusterTypes[ordConf.ConsensusType()]
+	return exists
 }
 
 func initializeGrpcServer(conf *localconfig.TopLevel, serverConfig comm.ServerConfig) *comm.GRPCServer {
