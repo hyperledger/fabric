7c384c521f5dc620869e6af957433e7e1235d8f7
Yoav Tock <tock@il.ibm.com>
2018-12-13 14:29:14 +0200

    FAB-13265 migration status in channelconfig
    Change the channelconfig of an Orderer to reflect the extension of ConsensusType:
    - MigrationState
    - MigrationContext
    
    Add a method to the bundle to validate the migartion steps of
    a new versus old config.
    
    Add test-cases to bundle_test.go to unit-test said method.
    
    Improved comments language.
    
    Needed to regenerate and update mocks in 'common' and 'blockcutter' packages
    for unit tests to build correctly.
    
    Change-Id: If060c05bcb9a0e0ca81b1f754a2b0e69a7f6c896
    Signed-off-by: Yoav Tock <tock@il.ibm.com>

--- common/channelconfig/bundle_test.go
+++ common/channelconfig/bundle_test.go
@@ -9,6 +9,7 @@ package channelconfig
 import (
 	"testing"
 
+	cc "github.com/hyperledger/fabric/common/capabilities"
 	cb "github.com/hyperledger/fabric/protos/common"
 	ab "github.com/hyperledger/fabric/protos/orderer"
 	"github.com/stretchr/testify/assert"
@@ -64,43 +65,46 @@ func TestValidateNew(t *testing.T) {
 	})
 
 	t.Run("ConsensusTypeChange", func(t *testing.T) {
-		cb := &Bundle{
+		currb := &Bundle{
 			channelConfig: &ChannelConfig{
 				ordererConfig: &OrdererConfig{
 					protos: &OrdererProtos{
 						ConsensusType: &ab.ConsensusType{
 							Type: "type1",
 						},
+						Capabilities: &cb.Capabilities{},
 					},
 				},
 			},
 		}
 
-		nb := &Bundle{
+		newb := &Bundle{
 			channelConfig: &ChannelConfig{
 				ordererConfig: &OrdererConfig{
 					protos: &OrdererProtos{
 						ConsensusType: &ab.ConsensusType{
 							Type: "type2",
 						},
+						Capabilities: &cb.Capabilities{},
 					},
 				},
 			},
 		}
 
-		err := cb.ValidateNew(nb)
+		err := currb.ValidateNew(newb)
 		assert.Error(t, err)
 		assert.Regexp(t, "Attempted to change consensus type from", err.Error())
 	})
 
 	t.Run("OrdererOrgMSPIDChange", func(t *testing.T) {
-		cb := &Bundle{
+		currb := &Bundle{
 			channelConfig: &ChannelConfig{
 				ordererConfig: &OrdererConfig{
 					protos: &OrdererProtos{
 						ConsensusType: &ab.ConsensusType{
 							Type: "type1",
 						},
+						Capabilities: &cb.Capabilities{},
 					},
 					orgs: map[string]Org{
 						"org1": &OrganizationConfig{mspID: "org1msp"},
@@ -111,13 +115,14 @@ func TestValidateNew(t *testing.T) {
 			},
 		}
 
-		nb := &Bundle{
+		newb := &Bundle{
 			channelConfig: &ChannelConfig{
 				ordererConfig: &OrdererConfig{
 					protos: &OrdererProtos{
 						ConsensusType: &ab.ConsensusType{
 							Type: "type1",
 						},
+						Capabilities: &cb.Capabilities{},
 					},
 					orgs: map[string]Org{
 						"org1": &OrganizationConfig{mspID: "org1msp"},
@@ -127,7 +132,7 @@ func TestValidateNew(t *testing.T) {
 			},
 		}
 
-		err := cb.ValidateNew(nb)
+		err := currb.ValidateNew(newb)
 		assert.Error(t, err)
 		assert.Regexp(t, "Orderer org org3 attempted to change MSP ID from", err.Error())
 	})
@@ -199,6 +204,241 @@ func TestValidateNew(t *testing.T) {
 		assert.Error(t, err)
 		assert.Regexp(t, "Consortium consortium1 org org3 attempted to change MSP ID from", err.Error())
 	})
+}
+
+func TestValidateNewWithConsensusMigration(t *testing.T) {
+	t.Run("ConsensusTypeMigration Green Path on System Channel", func(t *testing.T) {
+		b0 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		b1 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err := b0.ValidateNew(b1)
+		assert.NoError(t, err)
+
+		b2 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_START, 0)
+		err = b1.ValidateNew(b2)
+		assert.NoError(t, err)
+
+		b3 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_COMMIT, 4)
+		err = b2.ValidateNew(b3)
+		assert.NoError(t, err)
+
+		b4 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err = b3.ValidateNew(b4)
+		assert.NoError(t, err)
+
+		b5 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err = b4.ValidateNew(b5)
+		assert.NoError(t, err)
+	})
+
+	t.Run("ConsensusTypeMigration Green Path on Standard Channel", func(t *testing.T) {
+		b1 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		b2 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_CONTEXT, 7)
+		err := b1.ValidateNew(b2)
+		assert.NoError(t, err)
+
+		b3 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err = b2.ValidateNew(b3)
+		assert.NoError(t, err)
+	})
+
+	t.Run("ConsensusTypeMigration Abort Path on System Channel", func(t *testing.T) {
+		b1 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		b2 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_START, 0)
+		err := b1.ValidateNew(b2)
+		assert.NoError(t, err)
+
+		b3 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_ABORT, 0)
+		err = b2.ValidateNew(b3)
+		assert.NoError(t, err)
+
+		b4none := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err = b3.ValidateNew(b4none)
+		assert.NoError(t, err)
+
+		b4retry := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_START, 0)
+		err = b3.ValidateNew(b4retry)
+		assert.NoError(t, err)
+	})
+
+	t.Run("ConsensusTypeMigration Abort Path on Standard Channel", func(t *testing.T) {
+		b1 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		b2 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_CONTEXT, 7)
+		err := b1.ValidateNew(b2)
+		assert.NoError(t, err)
+
+		b3 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err = b2.ValidateNew(b3)
+		assert.NoError(t, err)
+	})
+
+	t.Run("ConsensusTypeMigration Bad Transitions on System Channel, from NONE", func(t *testing.T) {
+		b1 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		b2 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_COMMIT, 4)
+		err := b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from kafka to etcdraft, unexpected migration state transition: MIG_STATE_NONE to MIG_STATE_COMMIT")
+
+		updateConsensusType(b2, "kafka", ab.ConsensusType_MIG_STATE_COMMIT, 2)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Consensus type kafka, unexpected migration state transition: MIG_STATE_NONE to MIG_STATE_COMMIT")
+
+		updateConsensusType(b2, "etcdraft", ab.ConsensusType_MIG_STATE_ABORT, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from kafka to etcdraft, unexpected migration state transition: MIG_STATE_NONE to MIG_STATE_ABORT")
+	})
+
+	t.Run("ConsensusTypeMigration Bad Transitions on System Channel, from START", func(t *testing.T) {
+		b1 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_START, 0)
+		b2 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_COMMIT, 4)
+		err := b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Consensus type kafka, unexpected migration state transition: MIG_STATE_START to MIG_STATE_COMMIT")
+
+		updateConsensusType(b2, "kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Consensus type kafka, unexpected migration state transition: MIG_STATE_START to MIG_STATE_NONE")
+
+		updateConsensusType(b2, "etcdraft", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from kafka to etcdraft, unexpected migration state transition: MIG_STATE_START to MIG_STATE_NONE")
+
+		updateConsensusType(b2, "etcdraft", ab.ConsensusType_MIG_STATE_ABORT, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from kafka to etcdraft, unexpected migration state transition: MIG_STATE_START to MIG_STATE_ABORT")
+
+		updateConsensusType(b2, "etcdraft", ab.ConsensusType_MIG_STATE_COMMIT, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Consensus migration state MIG_STATE_COMMIT, unexpected migration context: 0 (expected >0)")
+	})
+
+	t.Run("ConsensusTypeMigration Bad Transitions on System Channel, from COMMIT", func(t *testing.T) {
+		b1 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_COMMIT, 4)
+		b2 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_ABORT, 0)
+		err := b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from etcdraft to kafka, unexpected migration state transition: MIG_STATE_COMMIT to MIG_STATE_ABORT")
+
+		updateConsensusType(b2, "kafka", ab.ConsensusType_MIG_STATE_START, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from etcdraft to kafka, unexpected migration state transition: MIG_STATE_COMMIT to MIG_STATE_START")
+
+		updateConsensusType(b2, "etcdraft", ab.ConsensusType_MIG_STATE_START, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Consensus type etcdraft, unexpected migration state transition: MIG_STATE_COMMIT to MIG_STATE_START")
+
+		updateConsensusType(b2, "etcdraft", ab.ConsensusType_MIG_STATE_ABORT, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Consensus type etcdraft, unexpected migration state transition: MIG_STATE_COMMIT to MIG_STATE_ABORT")
+
+		updateConsensusType(b2, "kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from etcdraft to kafka, unexpected migration state transition: MIG_STATE_COMMIT to MIG_STATE_NONE")
+	})
+
+	t.Run("ConsensusTypeMigration Bad Transitions on Standard Channel, from NONE-1", func(t *testing.T) {
+		b1 := generateMigrationBundle("kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		b2 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_CONTEXT, 0)
+		err := b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Consensus migration state MIG_STATE_CONTEXT, unexpected migration context: 0 (expected >0)")
+
+		updateConsensusType(b2, "kafka", ab.ConsensusType_MIG_STATE_CONTEXT, 7)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Consensus type kafka, unexpected migration state transition: MIG_STATE_NONE to MIG_STATE_CONTEXT")
+
+		updateConsensusType(b2, "etcdraft", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from kafka to etcdraft, unexpected migration state transition: MIG_STATE_NONE to MIG_STATE_NONE")
+	})
+
+	t.Run("ConsensusTypeMigration Bad Transitions on Standard Channel, from NONE-2", func(t *testing.T) {
+		b1 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_NONE, 0)
+		b2 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_CONTEXT, 0)
+		err := b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Consensus type etcdraft, unexpected migration state transition: MIG_STATE_NONE to MIG_STATE_CONTEXT")
+
+		updateConsensusType(b2, "kafka", ab.ConsensusType_MIG_STATE_CONTEXT, 7)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from etcdraft to kafka, unexpected migration state transition: MIG_STATE_NONE to MIG_STATE_CONTEXT")
+
+		updateConsensusType(b2, "kafka", ab.ConsensusType_MIG_STATE_NONE, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from etcdraft to kafka, unexpected migration state transition: MIG_STATE_NONE to MIG_STATE_NONE")
+	})
+
+	t.Run("ConsensusTypeMigration Bad Transitions on Standard Channel, from CONTEXT", func(t *testing.T) {
+		b1 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_CONTEXT, 7)
+		b2 := generateMigrationBundle("etcdraft", ab.ConsensusType_MIG_STATE_CONTEXT, 8)
+		err := b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Consensus type etcdraft, unexpected migration state transition: MIG_STATE_CONTEXT to MIG_STATE_CONTEXT")
+
+		updateConsensusType(b2, "kafka", ab.ConsensusType_MIG_STATE_CONTEXT, 8)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from etcdraft to kafka, unexpected migration state transition: MIG_STATE_CONTEXT to MIG_STATE_CONTEXT")
+
+		updateConsensusType(b2, "kafka", ab.ConsensusType_MIG_STATE_START, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err,
+			"Attempted to change consensus type from etcdraft to kafka, unexpected migration state transition: MIG_STATE_CONTEXT to MIG_STATE_START")
+
+		updateConsensusType(b2, "etcdraft", ab.ConsensusType_MIG_STATE_START, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err, "Consensus type etcdraft, unexpected migration state transition: MIG_STATE_CONTEXT to MIG_STATE_START")
+
+		updateConsensusType(b2, "kafka", ab.ConsensusType_MIG_STATE_ABORT, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err, "Attempted to change consensus type from etcdraft to kafka, unexpected migration state transition: MIG_STATE_CONTEXT to MIG_STATE_ABORT")
+
+		updateConsensusType(b2, "etcdraft", ab.ConsensusType_MIG_STATE_ABORT, 0)
+		err = b1.ValidateNew(b2)
+		assert.EqualError(t, err, "Consensus type etcdraft, unexpected migration state transition: MIG_STATE_CONTEXT to MIG_STATE_ABORT")
+	})
+}
+
+func updateConsensusType(b2 *Bundle, cType string, cState ab.ConsensusType_MigrationState, cContext uint64) {
+	b2.channelConfig.ordererConfig.protos.ConsensusType.Type = cType
+	b2.channelConfig.ordererConfig.protos.ConsensusType.MigrationState = cState
+	b2.channelConfig.ordererConfig.protos.ConsensusType.MigrationContext = cContext
+}
+
+func generateMigrationBundle(cType string, cState ab.ConsensusType_MigrationState, cContext uint64) *Bundle {
+	b := &Bundle{
+		channelConfig: &ChannelConfig{
+			ordererConfig: &OrdererConfig{
+				protos: &OrdererProtos{
+					ConsensusType: &ab.ConsensusType{
+						Type:             cType,
+						MigrationState:   cState,
+						MigrationContext: cContext,
+					},
+					Capabilities: &cb.Capabilities{
+						Capabilities: map[string]*cb.Capability{
+							cc.OrdererV2_0: {},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return b
 }
 
 func TestPrevalidation(t *testing.T) {
--- orderer/common/blockcutter/mock/metrics_provider.go
+++ orderer/common/blockcutter/mock/metrics_provider.go
@@ -2,9 +2,9 @@
 package mock
 
 import (
-	"sync"
+	sync "sync"
 
-	"github.com/hyperledger/fabric/common/metrics"
+	metrics "github.com/hyperledger/fabric/common/metrics"
 )
 
 type MetricsProvider struct {
@@ -59,7 +59,8 @@ func (fake *MetricsProvider) NewCounter(arg1 metrics.CounterOpts) metrics.Counte
 	if specificReturn {
 		return ret.result1
 	}
-	return fake.newCounterReturns.result1
+	fakeReturns := fake.newCounterReturns
+	return fakeReturns.result1
 }
 
 func (fake *MetricsProvider) NewCounterCallCount() int {
@@ -68,13 +69,22 @@ func (fake *MetricsProvider) NewCounterCallCount() int {
 	return len(fake.newCounterArgsForCall)
 }
 
+func (fake *MetricsProvider) NewCounterCalls(stub func(metrics.CounterOpts) metrics.Counter) {
+	fake.newCounterMutex.Lock()
+	defer fake.newCounterMutex.Unlock()
+	fake.NewCounterStub = stub
+}
+
 func (fake *MetricsProvider) NewCounterArgsForCall(i int) metrics.CounterOpts {
 	fake.newCounterMutex.RLock()
 	defer fake.newCounterMutex.RUnlock()
-	return fake.newCounterArgsForCall[i].arg1
+	argsForCall := fake.newCounterArgsForCall[i]
+	return argsForCall.arg1
 }
 
 func (fake *MetricsProvider) NewCounterReturns(result1 metrics.Counter) {
+	fake.newCounterMutex.Lock()
+	defer fake.newCounterMutex.Unlock()
 	fake.NewCounterStub = nil
 	fake.newCounterReturns = struct {
 		result1 metrics.Counter
@@ -82,6 +92,8 @@ func (fake *MetricsProvider) NewCounterReturns(result1 metrics.Counter) {
 }
 
 func (fake *MetricsProvider) NewCounterReturnsOnCall(i int, result1 metrics.Counter) {
+	fake.newCounterMutex.Lock()
+	defer fake.newCounterMutex.Unlock()
 	fake.NewCounterStub = nil
 	if fake.newCounterReturnsOnCall == nil {
 		fake.newCounterReturnsOnCall = make(map[int]struct {
@@ -107,7 +119,8 @@ func (fake *MetricsProvider) NewGauge(arg1 metrics.GaugeOpts) metrics.Gauge {
 	if specificReturn {
 		return ret.result1
 	}
-	return fake.newGaugeReturns.result1
+	fakeReturns := fake.newGaugeReturns
+	return fakeReturns.result1
 }
 
 func (fake *MetricsProvider) NewGaugeCallCount() int {
@@ -116,13 +129,22 @@ func (fake *MetricsProvider) NewGaugeCallCount() int {
 	return len(fake.newGaugeArgsForCall)
 }
 
+func (fake *MetricsProvider) NewGaugeCalls(stub func(metrics.GaugeOpts) metrics.Gauge) {
+	fake.newGaugeMutex.Lock()
+	defer fake.newGaugeMutex.Unlock()
+	fake.NewGaugeStub = stub
+}
+
 func (fake *MetricsProvider) NewGaugeArgsForCall(i int) metrics.GaugeOpts {
 	fake.newGaugeMutex.RLock()
 	defer fake.newGaugeMutex.RUnlock()
-	return fake.newGaugeArgsForCall[i].arg1
+	argsForCall := fake.newGaugeArgsForCall[i]
+	return argsForCall.arg1
 }
 
 func (fake *MetricsProvider) NewGaugeReturns(result1 metrics.Gauge) {
+	fake.newGaugeMutex.Lock()
+	defer fake.newGaugeMutex.Unlock()
 	fake.NewGaugeStub = nil
 	fake.newGaugeReturns = struct {
 		result1 metrics.Gauge
@@ -130,6 +152,8 @@ func (fake *MetricsProvider) NewGaugeReturns(result1 metrics.Gauge) {
 }
 
 func (fake *MetricsProvider) NewGaugeReturnsOnCall(i int, result1 metrics.Gauge) {
+	fake.newGaugeMutex.Lock()
+	defer fake.newGaugeMutex.Unlock()
 	fake.NewGaugeStub = nil
 	if fake.newGaugeReturnsOnCall == nil {
 		fake.newGaugeReturnsOnCall = make(map[int]struct {
@@ -155,7 +179,8 @@ func (fake *MetricsProvider) NewHistogram(arg1 metrics.HistogramOpts) metrics.Hi
 	if specificReturn {
 		return ret.result1
 	}
-	return fake.newHistogramReturns.result1
+	fakeReturns := fake.newHistogramReturns
+	return fakeReturns.result1
 }
 
 func (fake *MetricsProvider) NewHistogramCallCount() int {
@@ -164,13 +189,22 @@ func (fake *MetricsProvider) NewHistogramCallCount() int {
 	return len(fake.newHistogramArgsForCall)
 }
 
+func (fake *MetricsProvider) NewHistogramCalls(stub func(metrics.HistogramOpts) metrics.Histogram) {
+	fake.newHistogramMutex.Lock()
+	defer fake.newHistogramMutex.Unlock()
+	fake.NewHistogramStub = stub
+}
+
 func (fake *MetricsProvider) NewHistogramArgsForCall(i int) metrics.HistogramOpts {
 	fake.newHistogramMutex.RLock()
 	defer fake.newHistogramMutex.RUnlock()
-	return fake.newHistogramArgsForCall[i].arg1
+	argsForCall := fake.newHistogramArgsForCall[i]
+	return argsForCall.arg1
 }
 
 func (fake *MetricsProvider) NewHistogramReturns(result1 metrics.Histogram) {
+	fake.newHistogramMutex.Lock()
+	defer fake.newHistogramMutex.Unlock()
 	fake.NewHistogramStub = nil
 	fake.newHistogramReturns = struct {
 		result1 metrics.Histogram
@@ -178,6 +212,8 @@ func (fake *MetricsProvider) NewHistogramReturns(result1 metrics.Histogram) {
 }
 
 func (fake *MetricsProvider) NewHistogramReturnsOnCall(i int, result1 metrics.Histogram) {
+	fake.newHistogramMutex.Lock()
+	defer fake.newHistogramMutex.Unlock()
 	fake.NewHistogramStub = nil
 	if fake.newHistogramReturnsOnCall == nil {
 		fake.newHistogramReturnsOnCall = make(map[int]struct {
