f065e3774aa2db173099ff3e72849b452db3872a
yacovm <yacovm@il.ibm.com>
2018-12-16 01:06:56 +0200

    [FAB-13297] Unused code chaincode/transaction_contexts
    This change set removes un-used code
    
    Change-Id: Ia0fec9d65000ba68992e07e0d0f961e24dbb0a5a
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- core/chaincode/transaction_contexts.go
+++ core/chaincode/transaction_contexts.go
@@ -7,26 +7,12 @@ SPDX-License-Identifier: Apache-2.0
 package chaincode
 
 import (
-	"context"
 	"sync"
 
 	commonledger "github.com/hyperledger/fabric/common/ledger"
 	"github.com/hyperledger/fabric/core/common/ccprovider"
-	"github.com/hyperledger/fabric/core/ledger"
 	pb "github.com/hyperledger/fabric/protos/peer"
 	"github.com/pkg/errors"
-)
-
-type key string
-
-const (
-	// TXSimulatorKey is the context key used to provide a ledger.TxSimulator
-	// from the endorser to the chaincode.
-	TXSimulatorKey key = "txsimulatorkey"
-
-	// HistoryQueryExecutorKey is the context key used to provide a
-	// ledger.HistoryQueryExecutor from the endorser to the chaincode.
-	HistoryQueryExecutorKey key = "historyqueryexecutorkey"
 )
 
 // TransactionContexts maintains active transaction contexts for a Handler.
@@ -77,20 +63,6 @@ func (c *TransactionContexts) Create(txParams *ccprovider.TransactionParams) (*T
 	c.contexts[ctxID] = txctx
 
 	return txctx, nil
-}
-
-func getTxSimulator(ctx context.Context) ledger.TxSimulator {
-	if txsim, ok := ctx.Value(TXSimulatorKey).(ledger.TxSimulator); ok {
-		return txsim
-	}
-	return nil
-}
-
-func getHistoryQueryExecutor(ctx context.Context) ledger.HistoryQueryExecutor {
-	if historyQueryExecutor, ok := ctx.Value(HistoryQueryExecutorKey).(ledger.HistoryQueryExecutor); ok {
-		return historyQueryExecutor
-	}
-	return nil
 }
 
 // Get retrieves the transaction context associated with the chain and
