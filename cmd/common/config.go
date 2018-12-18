08dcf250b2fd4649b7c90c8ccc7f1aed1aa7e9fd
Matthew Sykes <sykesmat@us.ibm.com>
2018-12-18 13:36:35 -0500

    [FAB-13344] remove dead production code from cmd
    Change-Id: Id9c323ecc5d356168e51dee26a7d0fb0ecc16e5d
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- cmd/common/config.go
+++ cmd/common/config.go
@@ -12,11 +12,7 @@ import (
 	"github.com/hyperledger/fabric/cmd/common/comm"
 	"github.com/hyperledger/fabric/cmd/common/signer"
 	"github.com/pkg/errors"
-	"gopkg.in/yaml.v2"
-)
-
-const (
-	v12 = iota
+	yaml "gopkg.in/yaml.v2"
 )
 
 // Config aggregates configuration of TLS and signing
