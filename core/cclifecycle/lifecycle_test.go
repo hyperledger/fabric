908739e0f5526cd408759d629db58b77763dac9f
yacovm <yacovm@il.ibm.com>
2019-01-21 02:39:11 +0200

    [FAB-13471] lifecycle to handle multiple updates
    The ledger calls several HandleChaincodeDeploy for each update
    but only a single ChaincodeDeployDone after all invocations
    to HandleChaincodeDeploy were made.
    
    The current implementation only supported a single update,
    and as a result - a second ChaincodeDeployDone will get stuck
    writing to a channel.
    
    This change set makes the Lifecycle in core/cclifecycle
    to be able to handle any number of updates in a single block.
    
    Change-Id: I1d85018af398bd5cb968e42031986a999f6be444
    Signed-off-by: yacovm <yacovm@il.ibm.com>

--- core/cclifecycle/lifecycle_test.go
+++ core/cclifecycle/lifecycle_test.go
@@ -11,11 +11,12 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/hyperledger/fabric/common/chaincode"
 	"github.com/hyperledger/fabric/common/flogging/floggingtest"
-	"github.com/hyperledger/fabric/core/cclifecycle"
+	cc "github.com/hyperledger/fabric/core/cclifecycle"
 	"github.com/hyperledger/fabric/core/cclifecycle/mocks"
 	"github.com/hyperledger/fabric/core/common/ccprovider"
 	"github.com/hyperledger/fabric/core/common/privdata"
@@ -262,7 +263,85 @@ func TestHandleChaincodeDeployFailures(t *testing.T) {
 	sub.HandleChaincodeDeploy(&cceventmgmt.ChaincodeDefinition{Name: "cc1", Version: "1.1", Hash: []byte{42}}, nil)
 	sub.ChaincodeDeployDone(false)
 	lsnr.AssertNumberOfCalls(t, "LifeCycleChangeListener", 3)
-	assertLogged(t, recorder, "Chaincode deploy for cc1 failed")
+	assertLogged(t, recorder, "Chaincode deploy for updates [Name=cc1, Version=1.1, Hash=[]byte{0x2a}] failed")
+}
+
+func TestMultipleUpdates(t *testing.T) {
+	recorder, restoreLogger := newLogRecorder(t)
+
+	cc1Bytes := utils.MarshalOrPanic(&ccprovider.ChaincodeData{
+		Name:    "cc1",
+		Version: "1.1",
+		Id:      []byte{42},
+		Policy:  []byte{1, 2, 3, 4, 5},
+	})
+	cc2Bytes := utils.MarshalOrPanic(&ccprovider.ChaincodeData{
+		Name:    "cc2",
+		Version: "1.0",
+		Id:      []byte{50},
+		Policy:  []byte{1, 2, 3, 4, 5},
+	})
+
+	query := &mocks.Query{}
+	query.On("GetState", "lscc", "cc1").Return(cc1Bytes, nil)
+	query.On("GetState", "lscc", "cc2").Return(cc2Bytes, nil)
+	query.On("Done")
+	queryCreator := &mocks.QueryCreator{}
+	queryCreator.On("NewQuery").Return(query, nil)
+
+	enum := &mocks.Enumerator{}
+	enum.On("Enumerate").Return([]chaincode.InstalledChaincode{
+		{
+			Name:    "cc1",
+			Version: "1.1",
+			Id:      []byte{42},
+		},
+		{
+			Name:    "cc2",
+			Version: "1.0",
+			Id:      []byte{50},
+		},
+	}, nil)
+
+	lc, err := cc.NewLifeCycle(enum)
+	assert.NoError(t, err)
+
+	var lsnrCalled sync.WaitGroup
+	lsnrCalled.Add(3)
+	lsnr := &mocks.LifeCycleChangeListener{}
+	lsnr.On("LifeCycleChangeListener", mock.Anything, mock.Anything).Run(func(arguments mock.Arguments) {
+		lsnrCalled.Done()
+	})
+	lc.AddListener(lsnr)
+
+	sub, err := lc.NewChannelSubscription("mychannel", queryCreator)
+	assert.NoError(t, err)
+
+	sub.HandleChaincodeDeploy(&cceventmgmt.ChaincodeDefinition{Name: "cc1", Version: "1.1", Hash: []byte{42}}, nil)
+	sub.HandleChaincodeDeploy(&cceventmgmt.ChaincodeDefinition{Name: "cc2", Version: "1.0", Hash: []byte{50}}, nil)
+	sub.ChaincodeDeployDone(true)
+
+	assertLogged(t, recorder, "Listeners for channel mychannel invoked")
+	cc1MD := chaincode.Metadata{
+		Name:    "cc1",
+		Version: "1.1",
+		Id:      []byte{42},
+		Policy:  []byte{1, 2, 3, 4, 5},
+	}
+	cc2MD := chaincode.Metadata{
+		Name:    "cc2",
+		Version: "1.0",
+		Id:      []byte{50},
+		Policy:  []byte{1, 2, 3, 4, 5},
+	}
+	metadataSetWithBothChaincodes := chaincode.MetadataSet{cc1MD, cc2MD}
+
+	lsnrCalled.Wait()
+	// We need to sort the metadata passed to the call because map iteration is involved in building the
+	// metadata set.
+	expectedMetadata := sortedMetadataSet(lsnr.Calls[2].Arguments.Get(1).(chaincode.MetadataSet)).sort()
+	assert.Equal(t, metadataSetWithBothChaincodes, expectedMetadata)
+	restoreLogger()
 }
 
 func TestMetadata(t *testing.T) {
