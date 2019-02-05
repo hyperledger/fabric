519c315b7fcee38f755a61626d58b669d74fb41b
Brett Logan <Brett.T.Logan@ibm.com>
2019-02-05 16:45:00 -0500

    [FAB-13752] Cleanup Docker related infrastructure
    Removed code made obsolete by recent changes to the
    Docker build process
    
    Change-Id: If971ea69ba5a3291afb93d408254be8c861da1cb
    Signed-off-by: Brett Logan <Brett.T.Logan@ibm.com>

--- docker-env.mk
+++ docker-env.mk
@@ -49,16 +49,8 @@ BASE_DOCKER_TAG=$(ARCH)-$(BASEIMAGE_RELEASE)
 
 DOCKER_NS ?= hyperledger
 DOCKER_TAG=$(ARCH)-$(PROJECT_VERSION)
-PREV_TAG=$(ARCH)-$(PREV_VERSION)
 
 BASE_DOCKER_LABEL=org.hyperledger.fabric
-
-DOCKER_DYNAMIC_LINK ?= false
-DOCKER_GO_LDFLAGS += $(GO_LDFLAGS)
-
-ifeq ($(DOCKER_DYNAMIC_LINK),false)
-DOCKER_GO_LDFLAGS += -linkmode external -extldflags '-static -lpthread'
-endif
 
 #
 # What is a .dummy file?
