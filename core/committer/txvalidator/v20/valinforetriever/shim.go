b2bb8ed778297e0d7f331ae8c6e256f352d2901c
Jason Yellick <jyellick@us.ibm.com>
2019-02-07 21:25:17 -0500

    FAB-14112 Add channel id to validation info intf
    In order to support using channel config for endorsement policy for the
    lifecycle system chaincode, we need to be able to check if the
    endorsement policy exists or fall back to a signature based one.  This
    CR simply alters the interface for ValidationInfo to pass this info.
    
    Change-Id: I67921a14d15f2afdddb274f4892346648b64a4fe
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- core/committer/txvalidator/v20/valinforetriever/shim.go
+++ core/committer/txvalidator/v20/valinforetriever/shim.go
@@ -23,11 +23,11 @@ type ValidationInfoRetrieveShim struct {
 	New    plugindispatcher.LifecycleResources
 }
 
-func (v *ValidationInfoRetrieveShim) ValidationInfo(chaincodeName string, qe ledger.QueryExecutor) (plugin string, args []byte, unexpectedErr error, validationErr error) {
-	plugin, args, unexpectedErr, validationErr = v.New.ValidationInfo(chaincodeName, qe)
+func (v *ValidationInfoRetrieveShim) ValidationInfo(channelID, chaincodeName string, qe ledger.QueryExecutor) (plugin string, args []byte, unexpectedErr error, validationErr error) {
+	plugin, args, unexpectedErr, validationErr = v.New.ValidationInfo(channelID, chaincodeName, qe)
 	if unexpectedErr != nil || validationErr != nil || plugin != "" || args != nil {
 		return
 	}
 
-	return v.Legacy.ValidationInfo(chaincodeName, qe)
+	return v.Legacy.ValidationInfo(channelID, chaincodeName, qe)
 }
