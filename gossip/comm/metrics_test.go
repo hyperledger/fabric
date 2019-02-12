76096f2f2e0cdba0c92c6cb0a7c98d33db689802
Hagar Meir <hagar.meir@ibm.com>
2019-02-12 11:05:20 +0200

    [FAB-14143] use ephemeral ports in gossip comm tests
    Change tests so that only ephemeral ports are used
    and the test constructor NewCommInstanceWithServer is not used
    
    Change-Id: I0cf71eb52b6db8863d532fbcbba65e2883c18ea4
    Signed-off-by: Hagar Meir <hagar.meir@ibm.com>

--- gossip/comm/metrics_test.go
+++ gossip/comm/metrics_test.go
@@ -7,23 +7,19 @@ SPDX-License-Identifier: Apache-2.0
 package comm
 
 import (
-	"fmt"
 	"sync/atomic"
 	"testing"
 	"time"
 
-	"github.com/hyperledger/fabric/gossip/identity"
 	"github.com/hyperledger/fabric/gossip/metrics"
 	"github.com/hyperledger/fabric/gossip/metrics/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
-func newCommInstanceMetrics(port int, sec *naiveSecProvider, commMetrics *metrics.CommMetrics) (Comm, error) {
-	endpoint := fmt.Sprintf("localhost:%d", port)
-	id := []byte(endpoint)
-	inst, err := NewCommInstanceWithServer(port, identity.NewIdentityMapper(sec, id, noopPurgeIdentity, sec),
-		id, nil, sec, commMetrics)
-	return inst, err
+func newCommInstanceWithMetrics(t *testing.T, sec *naiveSecProvider, metrics *metrics.CommMetrics) (c Comm, port int) {
+	port, gRPCServer, certs, secureDialOpts, dialOpts := prepareForNewComm(t)
+	comm := newCommInstanceOnlyWithMetrics(t, metrics, sec, gRPCServer, certs, secureDialOpts, dialOpts...)
+	return comm, port
 }
 
 func TestMetrics(t *testing.T) {
@@ -38,14 +34,14 @@ func TestMetrics(t *testing.T) {
 
 	fakeCommMetrics := metrics.NewGossipMetrics(testMetricProvider.FakeProvider).CommMetrics
 
-	comm1, _ := newCommInstanceMetrics(6870, naiveSec, fakeCommMetrics)
-	comm2, _ := newCommInstanceMetrics(6871, naiveSec, fakeCommMetrics)
+	comm1, _ := newCommInstanceWithMetrics(t, naiveSec, fakeCommMetrics)
+	comm2, port2 := newCommInstanceWithMetrics(t, naiveSec, fakeCommMetrics)
 	defer comm1.Stop()
 	defer comm2.Stop()
 
 	// Establish preliminary connection
 	fromComm1 := comm2.Accept(acceptAll)
-	comm1.Send(createGossipMsg(), remotePeer(6871))
+	comm1.Send(createGossipMsg(), remotePeer(port2))
 	<-fromComm1
 
 	// Drain messages slowly by comm2
@@ -63,7 +59,7 @@ func TestMetrics(t *testing.T) {
 
 	// Send messages until the buffer overflow event emission is detected
 	for {
-		comm1.Send(createGossipMsg(), remotePeer(6871))
+		comm1.Send(createGossipMsg(), remotePeer(port2))
 		if atomic.LoadUint32(&overflown) == uint32(1) {
 			t.Log("Buffer overflow detected")
 			break
