3f14a2bb2d9f2132edf71f172e5561410979e30b
Matthew Sykes <sykesmat@us.ibm.com>
2019-02-20 17:19:41 -0500

    [FAB-14275] fix compile_protos.sh
    When our build environment was based on ubuntu, we were using GNU
    coreutils; now that we've moved to alpine, we're using busybox.
    
    The protobuf compilation script was taking advantage of a GNU extension
    to `dirname` by passing multiple path arguments and this does not work
    in busybox.
    
    Restructured the script to use xargs to invoke dirname with one path at
    a time. Also enabled the pipefail shell option to help catch issues like
    this earlier.
    
    Change-Id: Iaf07526be9853a2055925da740aa7bd72113d58f
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- scripts/compile_protos.sh
+++ scripts/compile_protos.sh
@@ -5,31 +5,26 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-
-set -eux
+set -eux -o pipefail
 
 # To set a proto root for a set of protos, create a .protoroot file in one of the parent directories
 # which you wish to use as the proto root.  If no .protoroot file exists within fabric/.../<your_proto>
 # then the proto root for that proto is inferred to be its containing directory.
 
 # Find explicit proto roots
-PROTO_ROOT_FILES="$(find . -name ".protoroot" -exec readlink -f {} \;)"
-PROTO_ROOT_DIRS="$(dirname $PROTO_ROOT_FILES)"
+PROTO_ROOT_DIRS="$(find . -name ".protoroot" -exec readlink -f {} \; | xargs -n 1 dirname | sort -u)"
 
-
-# Find all proto files to be compiled, excluding any which are in a proto root or in the vendor folder
-ROOTLESS_PROTO_FILES="$(find $PWD \
-                             $(for dir in $PROTO_ROOT_DIRS ; do echo "-path $dir -prune -o " ; done) \
-                             -path $PWD/vendor -prune -o \
-                             -path $PWD/.build -prune -o \
-                             -path $PWD/core/chaincode/shim/java -prune -o \
-                             -name "*.proto" -exec readlink -f {} \;)"
-ROOTLESS_PROTO_DIRS="$(dirname $ROOTLESS_PROTO_FILES | sort | uniq)"
+# Find all proto dirs to be processed, excluding any which are in a proto root or in the vendor folder
+ROOTLESS_PROTO_DIRS=$(find $PWD \
+    $(for dir in $PROTO_ROOT_DIRS ; do echo "-path $dir -prune -o " ; done) \
+    -path $PWD/vendor -prune -o \
+    -path $PWD/.build -prune -o \
+    -name "*.proto" -exec readlink -f {} \; | xargs -n 1 dirname | sort -u)
 
 for dir in $ROOTLESS_PROTO_DIRS $PROTO_ROOT_DIRS; do
-echo Working on dir $dir
-        # As this is a proto root, and there may be subdirectories with protos, compile the protos for each sub-directory which contains them
-	for protos in $(find "$dir" -name '*.proto' -exec dirname {} \; | sort | uniq) ; do
-	       protoc --proto_path="$dir" --go_out=plugins=grpc:$GOPATH/src "$protos"/*.proto
-	done
+    echo Working on dir $dir
+    # As this is a proto root, and there may be subdirectories with protos, compile the protos for each sub-directory which contains them
+    for protos in $(find "$dir" -name '*.proto' -exec dirname {} \; | sort | uniq) ; do
+        protoc --proto_path="$dir" --go_out=plugins=grpc:$GOPATH/src "$protos"/*.proto
+    done
 done
