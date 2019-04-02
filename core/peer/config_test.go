14283c9b2715bdee7fc30b2c7bd86d08a7caf480
Matthew Sykes <sykesmat@us.ibm.com>
2019-04-02 16:57:54 -0400

    [FAB-14845] reformat core/peer/config_test.go
    No functional changes - just updates for readability.
    
    Change-Id: I683f4a61ad7bcc2e064cd535bb9ed516a5732aa1
    Signed-off-by: Dereck Luo <Chongxin.Luo@ibm.com>
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- core/peer/config_test.go
+++ core/peer/config_test.go
@@ -19,7 +19,6 @@ import (
 )
 
 func TestCacheConfigurationNegative(t *testing.T) {
-
 	// set a bad peer.address
 	viper.Set("peer.addressAutoDetect", true)
 	viper.Set("peer.address", "testing.com")
@@ -29,19 +28,19 @@ func TestCacheConfigurationNegative(t *testing.T) {
 }
 
 func TestConfiguration(t *testing.T) {
+	// get the interface addresses
+	addresses, err := net.InterfaceAddrs()
+	if err != nil {
+		t.Fatal("Failed to get interface addresses")
+	}
 
 	var ips []string
-	// get the interface addresses
-	if addresses, err := net.InterfaceAddrs(); err == nil {
-		for _, address := range addresses {
-			// eliminate loopback interfaces
-			if ip, ok := address.(*net.IPNet); ok && !ip.IP.IsLoopback() {
-				ips = append(ips, ip.IP.String()+":7051")
-				t.Logf("found interface address [%s]", ip.IP.String())
-			}
+	for _, address := range addresses {
+		// eliminate loopback interfaces
+		if ip, ok := address.(*net.IPNet); ok && !ip.IP.IsLoopback() {
+			ips = append(ips, ip.IP.String()+":7051")
+			t.Logf("found interface address [%s]", ip.IP.String())
 		}
-	} else {
-		t.Fatal("Failed to get interface addresses")
 	}
 
 	// There is a flake where sometimes this returns no IP address.
@@ -97,19 +96,15 @@ func TestConfiguration(t *testing.T) {
 			// GetLocalAddress
 			address, err := GetLocalAddress()
 			assert.NoError(t, err, "GetLocalAddress returned unexpected error")
-			assert.Contains(t, test.validAddresses, address,
-				"GetLocalAddress returned unexpected address")
-			assert.NotContains(t, test.invalidAddresses, address,
-				"GetLocalAddress returned invalid address")
+			assert.Contains(t, test.validAddresses, address, "GetLocalAddress returned unexpected address")
+			assert.NotContains(t, test.invalidAddresses, address, "GetLocalAddress returned invalid address")
 			// reset the cache
 			configurationCached = false
 			// GetPeerEndpoint
 			pe, err := GetPeerEndpoint()
 			assert.NoError(t, err, "GetPeerEndpoint returned unexpected error")
-			assert.Equal(t, test.settings["peer.id"], pe.Id.Name,
-				"GetPeerEndpoint returned the wrong peer ID")
-			assert.Equal(t, address, pe.Address,
-				"GetPeerEndpoint returned the wrong peer address")
+			assert.Equal(t, test.settings["peer.id"], pe.Id.Name, "GetPeerEndpoint returned the wrong peer ID")
+			assert.Equal(t, address, pe.Address, "GetPeerEndpoint returned the wrong peer address")
 
 			// now check with cached configuration
 			err = CacheConfiguration()
@@ -118,44 +113,34 @@ func TestConfiguration(t *testing.T) {
 			// GetLocalAddress
 			address, err = GetLocalAddress()
 			assert.NoError(t, err, "GetLocalAddress should not have returned error")
-			assert.Contains(t, test.validAddresses, address,
-				"GetLocalAddress returned unexpected address")
-			assert.NotContains(t, test.invalidAddresses, address,
-				"GetLocalAddress returned invalid address")
+			assert.Contains(t, test.validAddresses, address, "GetLocalAddress returned unexpected address")
+			assert.NotContains(t, test.invalidAddresses, address, "GetLocalAddress returned invalid address")
 			// GetPeerEndpoint
 			pe, err = GetPeerEndpoint()
 			assert.NoError(t, err, "GetPeerEndpoint returned unexpected error")
-			assert.Equal(t, test.settings["peer.id"], pe.Id.Name,
-				"GetPeerEndpoint returned the wrong peer ID")
-			assert.Equal(t, address, pe.Address,
-				"GetPeerEndpoint returned the wrong peer address")
+			assert.Equal(t, test.settings["peer.id"], pe.Id.Name, "GetPeerEndpoint returned the wrong peer ID")
+			assert.Equal(t, address, pe.Address, "GetPeerEndpoint returned the wrong peer address")
 		})
 	}
 }
 
 func TestGetServerConfig(t *testing.T) {
-
 	// good config without TLS
 	viper.Set("peer.tls.enabled", false)
 	sc, _ := GetServerConfig()
-	assert.Equal(t, false, sc.SecOpts.UseTLS,
-		"ServerConfig.SecOpts.UseTLS should be false")
+	assert.Equal(t, false, sc.SecOpts.UseTLS, "ServerConfig.SecOpts.UseTLS should be false")
 
 	// keepalive options
-	assert.Equal(t, comm.DefaultKeepaliveOptions, sc.KaOpts,
-		"ServerConfig.KaOpts should be set to default values")
+	assert.Equal(t, comm.DefaultKeepaliveOptions, sc.KaOpts, "ServerConfig.KaOpts should be set to default values")
 	viper.Set("peer.keepalive.interval", "60m")
 	sc, _ = GetServerConfig()
-	assert.Equal(t, time.Duration(60)*time.Minute, sc.KaOpts.ServerInterval,
-		"ServerConfig.KaOpts.ServerInterval should be set to 60 min")
+	assert.Equal(t, time.Duration(60)*time.Minute, sc.KaOpts.ServerInterval, "ServerConfig.KaOpts.ServerInterval should be set to 60 min")
 	viper.Set("peer.keepalive.timeout", "30s")
 	sc, _ = GetServerConfig()
-	assert.Equal(t, time.Duration(30)*time.Second, sc.KaOpts.ServerTimeout,
-		"ServerConfig.KaOpts.ServerTimeout should be set to 30 sec")
+	assert.Equal(t, time.Duration(30)*time.Second, sc.KaOpts.ServerTimeout, "ServerConfig.KaOpts.ServerTimeout should be set to 30 sec")
 	viper.Set("peer.keepalive.minInterval", "2m")
 	sc, _ = GetServerConfig()
-	assert.Equal(t, time.Duration(2)*time.Minute, sc.KaOpts.ServerMinInterval,
-		"ServerConfig.KaOpts.ServerMinInterval should be set to 2 min")
+	assert.Equal(t, time.Duration(2)*time.Minute, sc.KaOpts.ServerMinInterval, "ServerConfig.KaOpts.ServerMinInterval should be set to 2 min")
 
 	// good config with TLS
 	viper.Set("peer.tls.enabled", true)
@@ -164,17 +149,15 @@ func TestGetServerConfig(t *testing.T) {
 	viper.Set("peer.tls.rootcert.file", filepath.Join("testdata", "Org1-cert.pem"))
 	sc, _ = GetServerConfig()
 	assert.Equal(t, true, sc.SecOpts.UseTLS, "ServerConfig.SecOpts.UseTLS should be true")
-	assert.Equal(t, false, sc.SecOpts.RequireClientCert,
-		"ServerConfig.SecOpts.RequireClientCert should be false")
+	assert.Equal(t, false, sc.SecOpts.RequireClientCert, "ServerConfig.SecOpts.RequireClientCert should be false")
 	viper.Set("peer.tls.clientAuthRequired", true)
-	viper.Set("peer.tls.clientRootCAs.files",
-		[]string{filepath.Join("testdata", "Org1-cert.pem"),
-			filepath.Join("testdata", "Org2-cert.pem")})
+	viper.Set("peer.tls.clientRootCAs.files", []string{
+		filepath.Join("testdata", "Org1-cert.pem"),
+		filepath.Join("testdata", "Org2-cert.pem"),
+	})
 	sc, _ = GetServerConfig()
-	assert.Equal(t, true, sc.SecOpts.RequireClientCert,
-		"ServerConfig.SecOpts.RequireClientCert should be true")
-	assert.Equal(t, 2, len(sc.SecOpts.ClientRootCAs),
-		"ServerConfig.SecOpts.ClientRootCAs should contain 2 entries")
+	assert.Equal(t, true, sc.SecOpts.RequireClientCert, "ServerConfig.SecOpts.RequireClientCert should be true")
+	assert.Equal(t, 2, len(sc.SecOpts.ClientRootCAs), "ServerConfig.SecOpts.ClientRootCAs should contain 2 entries")
 
 	// bad config with TLS
 	viper.Set("peer.tls.rootcert.file", filepath.Join("testdata", "Org11-cert.pem"))
@@ -187,7 +170,6 @@ func TestGetServerConfig(t *testing.T) {
 	// disable TLS for remaining tests
 	viper.Set("peer.tls.enabled", false)
 	viper.Set("peer.tls.clientAuthRequired", false)
-
 }
 
 func TestGetClientCertificate(t *testing.T) {
@@ -201,14 +183,12 @@ func TestGetClientCertificate(t *testing.T) {
 	assert.Error(t, err)
 
 	viper.Set("peer.tls.key.file", "")
-	viper.Set("peer.tls.cert.file",
-		filepath.Join("testdata", "Org1-server1-cert.pem"))
+	viper.Set("peer.tls.cert.file", filepath.Join("testdata", "Org1-server1-cert.pem"))
 	// missing server key file - expect error
 	_, err = GetClientCertificate()
 	assert.Error(t, err)
 
-	viper.Set("peer.tls.key.file",
-		filepath.Join("testdata", "Org1-server1-key.pem"))
+	viper.Set("peer.tls.key.file", filepath.Join("testdata", "Org1-server1-key.pem"))
 	viper.Set("peer.tls.cert.file", "")
 	// missing server cert file - expect error
 	_, err = GetClientCertificate()
@@ -216,33 +196,29 @@ func TestGetClientCertificate(t *testing.T) {
 
 	// set server TLS settings to ensure we get the client TLS settings
 	// when they are set properly
-	viper.Set("peer.tls.key.file",
-		filepath.Join("testdata", "Org1-server1-key.pem"))
-	viper.Set("peer.tls.cert.file",
-		filepath.Join("testdata", "Org1-server1-cert.pem"))
+	viper.Set("peer.tls.key.file", filepath.Join("testdata", "Org1-server1-key.pem"))
+	viper.Set("peer.tls.cert.file", filepath.Join("testdata", "Org1-server1-cert.pem"))
 
 	// peer.tls.clientCert.file not set - expect error
-	viper.Set("peer.tls.clientKey.file",
-		filepath.Join("testdata", "Org2-server1-key.pem"))
+	viper.Set("peer.tls.clientKey.file", filepath.Join("testdata", "Org2-server1-key.pem"))
 	_, err = GetClientCertificate()
 	assert.Error(t, err)
 
 	// peer.tls.clientKey.file not set - expect error
 	viper.Set("peer.tls.clientKey.file", "")
-	viper.Set("peer.tls.clientCert.file",
-		filepath.Join("testdata", "Org2-server1-cert.pem"))
+	viper.Set("peer.tls.clientCert.file", filepath.Join("testdata", "Org2-server1-cert.pem"))
 	_, err = GetClientCertificate()
 	assert.Error(t, err)
 
 	// client auth required and clientKey/clientCert set
 	expected, err := tls.LoadX509KeyPair(
 		filepath.Join("testdata", "Org2-server1-cert.pem"),
-		filepath.Join("testdata", "Org2-server1-key.pem"))
+		filepath.Join("testdata", "Org2-server1-key.pem"),
+	)
 	if err != nil {
 		t.Fatalf("Failed to load test certificate (%s)", err)
 	}
-	viper.Set("peer.tls.clientKey.file",
-		filepath.Join("testdata", "Org2-server1-key.pem"))
+	viper.Set("peer.tls.clientKey.file", filepath.Join("testdata", "Org2-server1-key.pem"))
 	cert, err := GetClientCertificate()
 	assert.NoError(t, err)
 	assert.Equal(t, expected, cert)
@@ -253,7 +229,8 @@ func TestGetClientCertificate(t *testing.T) {
 	viper.Set("peer.tls.clientCert.file", "")
 	expected, err = tls.LoadX509KeyPair(
 		filepath.Join("testdata", "Org1-server1-cert.pem"),
-		filepath.Join("testdata", "Org1-server1-key.pem"))
+		filepath.Join("testdata", "Org1-server1-key.pem"),
+	)
 	if err != nil {
 		t.Fatalf("Failed to load test certificate (%s)", err)
 	}
