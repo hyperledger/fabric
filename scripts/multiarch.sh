d12c613169a2126429d8e233fdcde55c2ecf4790
Sambhav Nidamarty <sambhavdutt@gmail.com>
2019-04-10 12:51:06 -0400

    FAB-15134 Update mutiarch publish script
    Update the multiarch script to allow publish
    of multiarch images to Nexus and Dockerhub
    
    Change-Id: Ia9d9cf88507a7213da792370df9292b93066ea99
    Signed-off-by: Sambhav Nidamarty <sambhavdutt@gmail.com>

--- scripts/multiarch.sh
+++ scripts/multiarch.sh
@@ -4,12 +4,18 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# This script publishes the fabric docker images to hyperledger dockerhub and nexus3 repositories.
+# when publishing the images to dockerhub the values for NS_PULL & NS_PUSH variables set to default values.
+# when publishing the images to nexus3 repository the values for NS_PULL & NS_PUSH variables set like below
+# NS_PULL=nexus3.hyperledger.org:10001/hyperledger & NS_PUSH=nexus3.hyperledger.org:10002/hyperledger
+# since nexus has separate port numbers to pull and push the images to nexus3.
 
 usage() {
   echo "Usage: $0 <username> <password>"
   echo "<username> and <password> credentials for the repository"
   echo "ENV:"
-  echo "  NS=$NS"
+  echo "  NS_PULL=$NS_PULL"
+  echo "  NS_PUSH=$NS_PUSH"
   echo "  VERSION=$VERSION"
   echo "  TWO_DIGIT_VERSION=$TWO_DIGIT_VERSION"
   exit 1
@@ -18,7 +24,8 @@ usage() {
 missing() {
   echo "Error: some image(s) missing from registry"
   echo "ENV:"
-  echo "  NS=$NS"
+  echo "  NS_PULL=$NS_PULL"
+  echo "  NS_PUSH=$NS_PUSH"
   echo "  VERSION=$VERSION"
   echo "  TWO_DIGIT_VERSION=$TWO_DIGIT_VERSION"
   exit 1
@@ -27,7 +34,8 @@ missing() {
 failed() {
   echo "Error: multiarch manifest push failed"
   echo "ENV:"
-  echo "  NS=$NS"
+  echo "  NS_PULL=$NS_PULL"
+  echo "  NS_PUSH=$NS_PUSH"
   echo "  VERSION=$VERSION"
   echo "  TWO_DIGIT_VERSION=$TWO_DIGIT_VERSION"
   exit 1
@@ -35,9 +43,10 @@ failed() {
 
 USER=${1:-nobody}
 PASSWORD=${2:-nohow}
-NS=${NS:-hyperledger}
-VERSION=${BASE_VERSION:-1.3.0}
-TWO_DIGIT_VERSION=${TWO_DIGIT_VERSION:-1.3}
+NS_PULL=${NS_PULL:-hyperledger}
+NS_PUSH=${NS_PUSH:-hyperledger}
+VERSION=${BASE_VERSION:-1.4.1}
+TWO_DIGIT_VERSION=${TWO_DIGIT_VERSION:-1.4}
 
 if [ "$#" -ne 2 ]; then
   usage
@@ -54,28 +63,28 @@ IMAGES="fabric-peer fabric-orderer fabric-ccenv fabric-tools"
 
 # check that all images have been published
 for image in ${IMAGES}; do
-  docker pull ${NS}/${image}:amd64-${VERSION} || missing
-  docker pull ${NS}/${image}:s390x-${VERSION} || missing
+  docker pull ${NS_PULL}/${image}:amd64-${VERSION} || missing
+  docker pull ${NS_PULL}/${image}:s390x-${VERSION} || missing
 done
 
-# push the multiarch manifest and tag with just $VERSION and 'latest'
+# push the multiarch manifest and tag with $VERSION,$TWO_DIGIT_VERSION and latest tag
 for image in ${IMAGES}; do
   manifest-tool --username ${USER} --password ${PASSWORD} push from-args\
-   --platforms linux/amd64,linux/s390x --template "${NS}/${image}:ARCH-${VERSION}"\
-   --target "${NS}/${image}:${VERSION}"
+   --platforms linux/amd64,linux/s390x --template "${NS_PULL}/${image}:ARCH-${VERSION}"\
+   --target "${NS_PUSH}/${image}:${VERSION}"
   manifest-tool --username ${USER} --password ${PASSWORD} push from-args\
-   --platforms linux/amd64,linux/s390x --template "${NS}/${image}:ARCH-${VERSION}"\
-   --target "${NS}/${image}:latest"
+   --platforms linux/amd64,linux/s390x --template "${NS_PULL}/${image}:ARCH-${VERSION}"\
+   --target "${NS_PUSH}/${image}:latest"
   manifest-tool --username ${USER} --password ${PASSWORD} push from-args\
-   --platforms linux/amd64,linux/s390x --template "${NS}/${image}:ARCH-${VERSION}"\
-   --target "${NS}/${image}:${TWO_DIGIT_VERSION}"
+   --platforms linux/amd64,linux/s390x --template "${NS_PULL}/${image}:ARCH-${VERSION}"\
+   --target "${NS_PUSH}/${image}:${TWO_DIGIT_VERSION}"
 done
 
 # test that manifest is working as expected
 for image in ${IMAGES}; do
-  docker pull ${NS}/${image}:${VERSION} || failed
-  docker pull ${NS}/${image}:${TWO_DIGIT_VERSION} || failed
-  docker pull ${NS}/${image}:latest || failed
+  docker pull ${NS_PULL}/${image}:${VERSION} || failed
+  docker pull ${NS_PULL}/${image}:${TWO_DIGIT_VERSION} || failed
+  docker pull ${NS_PULL}/${image}:latest || failed
 done
 
 echo "Successfully pushed multiarch manifest"
