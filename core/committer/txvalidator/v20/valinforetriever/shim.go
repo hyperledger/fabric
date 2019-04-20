6ddd8a3736a3dc977f72082e1bea4f53d93a800f
Alessandro Sorniotti <ale.linux@sopit.net>
2019-04-20 21:41:02 +0200

    [FAB-15240] translate LSCC policies for 2.0 validator
    The 2.0 validator expects ApplicationPolicy instances but endorsement
    policies defined through LSCC are SignaturePolicyEnvelope instances and
    so they won't be interpreted correctly by VSCC. This change set
    translates policies from SignaturePolicyEnvelope to ApplicationPolicy
    before returning them to VSCC.
    
    Change-Id: Ic140f44e9564eca16bf84cfa785600178e0f7ed0
    Signed-off-by: Alessandro Sorniotti <ale.linux@sopit.net>

--- core/committer/txvalidator/v20/valinforetriever/shim.go
+++ core/committer/txvalidator/v20/valinforetriever/shim.go
@@ -7,8 +7,11 @@ SPDX-License-Identifier: Apache-2.0
 package valinforetriever
 
 import (
+	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric/core/committer/txvalidator/v20/plugindispatcher"
 	"github.com/hyperledger/fabric/core/ledger"
+	"github.com/hyperledger/fabric/protos/common"
+	"github.com/hyperledger/fabric/protos/peer"
 )
 
 //go:generate mockery -dir ../plugindispatcher/ -name LifecycleResources -case underscore -output mocks/
@@ -23,11 +26,53 @@ type ValidationInfoRetrieveShim struct {
 	New    plugindispatcher.LifecycleResources
 }
 
+// ValidationInfo implements the function of the LifecycleResources interface.
+// It returns the name and arguments of the validation plugin for the supplied
+// chaincode. The function merely acts as a pass-through - returning validation
+// info from the new or the legacy lifecycle, unless the legacy lifecycle successfully
+// returns validation info requiring the built-in validation plugin and arguments
+// that can be successfully parsed as a SignaturePolicyEnvelope and that can be
+// successfully converted into ApplicationPolicy. In that case, and in that case
+// only does this function modify the return values from the underlying lifecycle.
 func (v *ValidationInfoRetrieveShim) ValidationInfo(channelID, chaincodeName string, qe ledger.SimpleQueryExecutor) (plugin string, args []byte, unexpectedErr error, validationErr error) {
 	plugin, args, unexpectedErr, validationErr = v.New.ValidationInfo(channelID, chaincodeName, qe)
-	if unexpectedErr != nil || validationErr != nil || plugin != "" || args != nil {
+	if unexpectedErr != nil || validationErr != nil || plugin != "" || len(args) != 0 {
+		// in case of any errors or any actual information
+		// returned by the new lifecycle, we don't perform
+		// any translation
 		return
 	}
 
-	return v.Legacy.ValidationInfo(channelID, chaincodeName, qe)
+	plugin, args, unexpectedErr, validationErr = v.Legacy.ValidationInfo(channelID, chaincodeName, qe)
+	if unexpectedErr != nil || validationErr != nil || plugin != "vscc" || len(args) == 0 {
+		// in case of any errors or in case the plugin is
+		// not the default one ("vscc") or we have an empty
+		// policy, we don't perform any translation
+		return
+	}
+
+	spe := &common.SignaturePolicyEnvelope{}
+	err := proto.Unmarshal(args, spe)
+	if err != nil {
+		// if we can't interpret the policy, we don't attempt
+		// any translation
+		return
+	}
+
+	newArgs, err := proto.Marshal(
+		&peer.ApplicationPolicy{
+			Type: &peer.ApplicationPolicy_SignaturePolicy{
+				SignaturePolicy: spe,
+			},
+		},
+	)
+	if err != nil {
+		// if we can't marshal the translated policy, we don't attempt
+		// any translation
+		return
+	}
+
+	// if marshalling is successful, we return the translated policy
+	args = newArgs
+	return
 }
