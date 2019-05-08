b58b76e126c84353c8b0a361b45cb8be919b1231
linson <linson365@126.com>
2019-05-08 11:46:05 +0800

    [FAB-15365] Unified script indentation style
    Some scripts in the scripts directory
    use a mixture of tabs and spaces for indenting.
    These scripts should uniformly use spaces
    with appropriate, consistent indenting.
    
    Change-Id: Ib4624c393cbe3d3c4bb8a4cdffd984ed3fee71fb
    Signed-off-by: linson <linson365@126.com>

--- scripts/check_spelling.sh
+++ scripts/check_spelling.sh
@@ -6,10 +6,10 @@
 #
 
 CHECK=$(git diff --name-only HEAD * | grep -v .png$ | grep -v .git | grep -v ^CHANGELOG \
-  | grep -v ^vendor/ | grep -v ^build/ | sort -u)
+| grep -v ^vendor/ | grep -v ^build/ | sort -u)
 
 if [[ -z "$CHECK" ]]; then
-  CHECK=$(git diff-tree --no-commit-id --name-only -r $(git log -2 \
+    CHECK=$(git diff-tree --no-commit-id --name-only -r $(git log -2 \
     --pretty=format:"%h") | grep -v .png$ | grep -v .git | grep -v ^CHANGELOG \
     | grep -v ^vendor/ | grep -v ^build/ | sort -u)
 fi
@@ -17,8 +17,8 @@ fi
 echo "Checking changed go files for spelling errors ..."
 errs=`echo $CHECK | xargs misspell -source=text`
 if [ -z "$errs" ]; then
-   echo "spell checker passed"
-   exit 0
+    echo "spell checker passed"
+    exit 0
 fi
 echo "The following files are have spelling errors:"
 echo "$errs"
--- scripts/multiarch.sh
+++ scripts/multiarch.sh
@@ -11,34 +11,34 @@
 # since nexus has separate port numbers to pull and push the images to nexus3.
 
 usage() {
-  echo "Usage: $0 <username> <password>"
-  echo "<username> and <password> credentials for the repository"
-  echo "ENV:"
-  echo "  NS_PULL=$NS_PULL"
-  echo "  NS_PUSH=$NS_PUSH"
-  echo "  VERSION=$VERSION"
-  echo "  TWO_DIGIT_VERSION=$TWO_DIGIT_VERSION"
-  exit 1
+    echo "Usage: $0 <username> <password>"
+    echo "<username> and <password> credentials for the repository"
+    echo "ENV:"
+    echo "  NS_PULL=$NS_PULL"
+    echo "  NS_PUSH=$NS_PUSH"
+    echo "  VERSION=$VERSION"
+    echo "  TWO_DIGIT_VERSION=$TWO_DIGIT_VERSION"
+    exit 1
 }
 
 missing() {
-  echo "Error: some image(s) missing from registry"
-  echo "ENV:"
-  echo "  NS_PULL=$NS_PULL"
-  echo "  NS_PUSH=$NS_PUSH"
-  echo "  VERSION=$VERSION"
-  echo "  TWO_DIGIT_VERSION=$TWO_DIGIT_VERSION"
-  exit 1
+    echo "Error: some image(s) missing from registry"
+    echo "ENV:"
+    echo "  NS_PULL=$NS_PULL"
+    echo "  NS_PUSH=$NS_PUSH"
+    echo "  VERSION=$VERSION"
+    echo "  TWO_DIGIT_VERSION=$TWO_DIGIT_VERSION"
+    exit 1
 }
 
 failed() {
-  echo "Error: multiarch manifest push failed"
-  echo "ENV:"
-  echo "  NS_PULL=$NS_PULL"
-  echo "  NS_PUSH=$NS_PUSH"
-  echo "  VERSION=$VERSION"
-  echo "  TWO_DIGIT_VERSION=$TWO_DIGIT_VERSION"
-  exit 1
+    echo "Error: multiarch manifest push failed"
+    echo "ENV:"
+    echo "  NS_PULL=$NS_PULL"
+    echo "  NS_PUSH=$NS_PUSH"
+    echo "  VERSION=$VERSION"
+    echo "  TWO_DIGIT_VERSION=$TWO_DIGIT_VERSION"
+    exit 1
 }
 
 USER=${1:-nobody}
@@ -49,42 +49,38 @@ VERSION=${BASE_VERSION:-1.3.0}
 TWO_DIGIT_VERSION=${TWO_DIGIT_VERSION:-1.3}
 
 if [ "$#" -ne 2 ]; then
-  usage
+    usage
 fi
 
 # verify that manifest-tool is installed and found on PATH
 which manifest-tool
 if [ "$?" -ne 0 ]; then
-  echo "manifest-tool not installed or not found on PATH"
-  exit 1
+    echo "manifest-tool not installed or not found on PATH"
+    exit 1
 fi
 
 IMAGES="fabric-baseos fabric-peer fabric-orderer fabric-ccenv fabric-tools"
 
 # check that all images have been published
 for image in ${IMAGES}; do
-  docker pull ${NS_PULL}/${image}:amd64-${VERSION} || missing
-  docker pull ${NS_PULL}/${image}:s390x-${VERSION} || missing
+    docker pull ${NS_PULL}/${image}:amd64-${VERSION} || missing
+    docker pull ${NS_PULL}/${image}:s390x-${VERSION} || missing
 done
 
 # push the multiarch manifest and tag with just $VERSION
 for image in ${IMAGES}; do
-  manifest-tool --username ${USER} --password ${PASSWORD} push from-args\
-   --platforms linux/amd64,linux/s390x --template "${NS_PULL}/${image}:ARCH-${VERSION}"\
-   --target "${NS_PUSH}/${image}:${VERSION}"
- # manifest-tool --username ${USER} --password ${PASSWORD} push from-args\
- #  --platforms linux/amd64,linux/s390x --template "${NS_PULL}/${image}:ARCH-${VERSION}"\
- #  --target "${NS_PUSH}/${image}:latest"
-  manifest-tool --username ${USER} --password ${PASSWORD} push from-args\
-   --platforms linux/amd64,linux/s390x --template "${NS_PULL}/${image}:ARCH-${VERSION}"\
-   --target "${NS_PUSH}/${image}:${TWO_DIGIT_VERSION}"
+    manifest-tool --username ${USER} --password ${PASSWORD} push from-args\
+        --platforms linux/amd64,linux/s390x --template "${NS_PULL}/${image}:ARCH-${VERSION}"\
+        --target "${NS_PUSH}/${image}:${VERSION}"
+    manifest-tool --username ${USER} --password ${PASSWORD} push from-args\
+        --platforms linux/amd64,linux/s390x --template "${NS_PULL}/${image}:ARCH-${VERSION}"\
+        --target "${NS_PUSH}/${image}:${TWO_DIGIT_VERSION}"
 done
 
 # test that manifest is working as expected
 for image in ${IMAGES}; do
-  docker pull ${NS_PULL}/${image}:${VERSION} || failed
-  docker pull ${NS_PULL}/${image}:${TWO_DIGIT_VERSION} || failed
-#  docker pull ${NS_PULL}/${image}:latest || failed
+    docker pull ${NS_PULL}/${image}:${VERSION} || failed
+    docker pull ${NS_PULL}/${image}:${TWO_DIGIT_VERSION} || failed
 done
 
 echo "Successfully pushed multiarch manifest"
