d73c34427503406c53f99b75684271ecae1960d6
Matthew Sykes <sykesmat@us.ibm.com>
2019-05-13 14:42:42 +0000

    [FAB-15316] Revert Shellcheck fixes
    The changes break bootstrap.sh
    
    This reverts commit c322e835ec66ae35c0ee38731d897dc5293b5975.
    
    Change-Id: I13f4c6c8034889ba5260acf8c6cbe0617ad6b9be
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,13 +6,13 @@
 #
 
 # if version not passed in, default to latest released version
-VERSION=1.4.1
+export VERSION=1.4.1
 # if ca version not passed in, default to latest released version
-CA_VERSION=1.4.1
+export CA_VERSION=1.4.1
 # current version of thirdparty images (couchdb, kafka and zookeeper) released
-THIRDPARTY_IMAGE_VERSION=0.4.15
-ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')")
-MARCH=$(uname -m)
+export THIRDPARTY_IMAGE_VERSION=0.4.15
+export ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')")
+export MARCH=$(uname -m)
 
 printHelp() {
   echo "Usage: bootstrap.sh [version [ca_version [thirdparty_version]]] [options]"
@@ -35,8 +35,8 @@ dockerFabricPull() {
   for IMAGES in peer orderer ccenv tools baseos nodeenv javaenv; do
       echo "==> FABRIC IMAGE: $IMAGES"
       echo
-      docker pull "hyperledger/fabric-$IMAGES:$FABRIC_TAG"
-      docker tag "hyperledger/fabric-$IMAGES:$FABRIC_TAG" "hyperledger/fabric-$IMAGES"
+      docker pull hyperledger/fabric-$IMAGES:$FABRIC_TAG
+      docker tag hyperledger/fabric-$IMAGES:$FABRIC_TAG hyperledger/fabric-$IMAGES
   done
 }
 
@@ -45,8 +45,8 @@ dockerThirdPartyImagesPull() {
   for IMAGES in couchdb kafka zookeeper; do
       echo "==> THIRDPARTY DOCKER IMAGE: $IMAGES"
       echo
-      docker pull "hyperledger/fabric-$IMAGES:$THIRDPARTY_TAG"
-      docker tag "hyperledger/fabric-$IMAGES:$THIRDPARTY_TAG" "hyperledger/fabric-$IMAGES"
+      docker pull hyperledger/fabric-$IMAGES:$THIRDPARTY_TAG
+      docker tag hyperledger/fabric-$IMAGES:$THIRDPARTY_TAG hyperledger/fabric-$IMAGES
   done
 }
 
@@ -54,8 +54,8 @@ dockerCaPull() {
       local CA_TAG=$1
       echo "==> FABRIC CA IMAGE"
       echo
-      docker pull "hyperledger/fabric-ca:$CA_TAG"
-      docker tag "hyperledger/fabric-ca:$CA_TAG" hyperledger/fabric-ca
+      docker pull hyperledger/fabric-ca:$CA_TAG
+      docker tag hyperledger/fabric-ca:$CA_TAG hyperledger/fabric-ca
 }
 
 samplesInstall() {
@@ -82,7 +82,7 @@ samplesInstall() {
 binaryIncrementalDownload() {
       local BINARY_FILE=$1
       local URL=$2
-      curl -f -s -C - "${URL}" -o "${BINARY_FILE}" || rc=$?
+      curl -f -s -C - ${URL} -o ${BINARY_FILE} || rc=$?
       # Due to limitations in the current Nexus repo:
       # curl returns 33 when there's a resume attempt with no more bytes to download
       # curl returns 2 after finishing a resumed download
@@ -94,16 +94,16 @@ binaryIncrementalDownload() {
       if [ -z "$rc" ] || [ $rc -eq 33 ] || [ $rc -eq 2 ]; then
           # The checksum validates that RC 33 or 2 are not real failures
           echo "==> File downloaded. Verifying the md5sum..."
-          localMd5sum=$(md5sum "${BINARY_FILE}" | awk '{print $1}')
-          remoteMd5sum=$(curl -s "${URL}".md5)
+          localMd5sum=$(md5sum ${BINARY_FILE} | awk '{print $1}')
+          remoteMd5sum=$(curl -s ${URL}.md5)
           if [ "$localMd5sum" == "$remoteMd5sum" ]; then
               echo "==> Extracting ${BINARY_FILE}..."
-              tar xzf ./"${BINARY_FILE}" --overwrite
+              tar xzf ./${BINARY_FILE} --overwrite
 	      echo "==> Done."
-              rm -f "${BINARY_FILE}" "${BINARY_FILE}".md5
+              rm -f ${BINARY_FILE} ${BINARY_FILE}.md5
           else
               echo "Download failed: the local md5sum is different from the remote md5sum. Please try again."
-              rm -f "${BINARY_FILE}" "${BINARY_FILE}".md5
+              rm -f ${BINARY_FILE} ${BINARY_FILE}.md5
               exit 1
           fi
       else
@@ -118,17 +118,17 @@ binaryIncrementalDownload() {
 binaryDownload() {
       local BINARY_FILE=$1
       local URL=$2
-      echo "===> Downloading: " "${URL}"
+      echo "===> Downloading: " ${URL}
       # Check if a previous failure occurred and the file was partially downloaded
-      if [ -e "${BINARY_FILE}" ]; then
+      if [ -e ${BINARY_FILE} ]; then
           echo "==> Partial binary file found. Resuming download..."
-          binaryIncrementalDownload "${BINARY_FILE}" "${URL}"
+          binaryIncrementalDownload ${BINARY_FILE} ${URL}
       else
-          curl "${URL}" | tar xz || rc=$?
-          if [ -n "$rc" ]; then
+          curl ${URL} | tar xz || rc=$?
+          if [ ! -z "$rc" ]; then
               echo "==> There was an error downloading the binary file. Switching to incremental download."
               echo "==> Downloading file..."
-              binaryIncrementalDownload "${BINARY_FILE}" "${URL}"
+              binaryIncrementalDownload ${BINARY_FILE} ${URL}
 	  else
 	      echo "==> Done."
           fi
@@ -137,7 +137,7 @@ binaryDownload() {
 
 binariesInstall() {
   echo "===> Downloading version ${FABRIC_TAG} platform specific fabric binaries"
-  binaryDownload "${BINARY_FILE}" "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${ARCH}-${VERSION}/${BINARY_FILE}"
+  binaryDownload ${BINARY_FILE} https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric/hyperledger-fabric/${ARCH}-${VERSION}/${BINARY_FILE}
   if [ $? -eq 22 ]; then
      echo
      echo "------> ${FABRIC_TAG} platform specific fabric binary is not available to download <----"
@@ -145,7 +145,7 @@ binariesInstall() {
    fi
 
   echo "===> Downloading version ${CA_TAG} platform specific fabric-ca-client binary"
-  binaryDownload "${CA_BINARY_FILE}" "https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric-ca/hyperledger-fabric-ca/${ARCH}-${CA_VERSION}/${CA_BINARY_FILE}"
+  binaryDownload ${CA_BINARY_FILE} https://nexus.hyperledger.org/content/repositories/releases/org/hyperledger/fabric-ca/hyperledger-fabric-ca/${ARCH}-${CA_VERSION}/${CA_BINARY_FILE}
   if [ $? -eq 22 ]; then
      echo
      echo "------> ${CA_TAG} fabric-ca-client binary is not available to download  (Available from 1.1.0-rc1) <----"
@@ -154,18 +154,18 @@ binariesInstall() {
 }
 
 dockerInstall() {
-  command -v docker >& /dev/null
+  which docker >& /dev/null
   NODOCKER=$?
   if [ "${NODOCKER}" == 0 ]; then
 	  echo "===> Pulling fabric Images"
-	  dockerFabricPull "${FABRIC_TAG}"
+	  dockerFabricPull ${FABRIC_TAG}
 	  echo "===> Pulling fabric ca Image"
-	  dockerCaPull "${CA_TAG}"
+	  dockerCaPull ${CA_TAG}
 	  echo "===> Pulling thirdparty docker images"
-	  dockerThirdPartyImagesPull "${THIRDPARTY_TAG}"
+	  dockerThirdPartyImagesPull ${THIRDPARTY_TAG}
 	  echo
 	  echo "===> List out hyperledger docker images"
-	  docker images | grep hyperledger
+	  docker images | grep hyperledger*
   else
     echo "========================================================="
     echo "Docker not installed, bypassing download of Fabric images"
@@ -179,11 +179,11 @@ BINARIES=true
 
 # Parse commandline args pull out
 # version and/or ca-version strings first
-if [ -n "$1" ] -a [ "${1:0:1}" != "-" ]; then
+if [ ! -z "$1" -a "${1:0:1}" != "-" ]; then
   VERSION=$1;shift
-  if [ -n "$1" ] -a [ "${1:0:1}" != "-" ]; then
+  if [ ! -z "$1"  -a "${1:0:1}" != "-" ]; then
     CA_VERSION=$1;shift
-    if [ -n "$1" ] -a [ "${1:0:1}" != "-" ]; then
+    if [ ! -z "$1"  -a "${1:0:1}" != "-" ]; then
       THIRDPARTY_IMAGE_VERSION=$1;shift
     fi
   fi
@@ -196,9 +196,9 @@ if [[ $VERSION =~ ^1\.[0-1]\.* ]]; then
   export THIRDPARTY_TAG=${MARCH}-${THIRDPARTY_IMAGE_VERSION}
 else
   # starting with 1.2.0, multi-arch images will be default
-  : "${CA_TAG:=$CA_VERSION}"
-  : "${FABRIC_TAG:=$VERSION}"
-  : "${THIRDPARTY_TAG:=$THIRDPARTY_IMAGE_VERSION}"
+  : ${CA_TAG:="$CA_VERSION"}
+  : ${FABRIC_TAG:="$VERSION"}
+  : ${THIRDPARTY_TAG:="$THIRDPARTY_IMAGE_VERSION"}
 fi
 
 BINARY_FILE=hyperledger-fabric-${ARCH}-${VERSION}.tar.gz
