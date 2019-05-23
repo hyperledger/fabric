bbd0324b247e9e9d24ff445c7234225aaf552f4d
Wenjian Qiao <wenjianq@gmail.com>
2019-05-23 14:50:25 -0400

    [FAB-15450] validate historydb key if it contains nils
    There was a logic to skip keys with nil bytes from the historydb results.
    However, because some valid blockNumTranNumBytes may also contain
    nil bytes, these history keys are filtered out.
    
    This CR updates the logic to handle decoding error and process all keys
    returned by range query no matter if blockNumTranNumBytes contains
    a nil byte. If there is a decoding error, block:tran cannot be found,
    or key is not present in write set, it means that the key is a false key
    and will be skipped.
    
    Change-Id: I296479af55b7c750ef8f797faa807ee0f5489b0a
    Signed-off-by: Wenjian Qiao <wenjianq@gmail.com>

--- core/ledger/kvledger/history/historydb/historyleveldb/historyleveldb_test.go
+++ core/ledger/kvledger/history/historydb/historyleveldb/historyleveldb_test.go
@@ -7,6 +7,8 @@ SPDX-License-Identifier: Apache-2.0
 package historyleveldb
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 	"strconv"
 	"testing"
@@ -16,6 +18,8 @@ import (
 	"github.com/hyperledger/fabric/common/ledger/testutil"
 	util2 "github.com/hyperledger/fabric/common/util"
 	"github.com/hyperledger/fabric/core/ledger"
+	"github.com/hyperledger/fabric/core/ledger/kvledger/history/historydb"
+	"github.com/hyperledger/fabric/core/ledger/kvledger/history/historydb/historyleveldb/fakes"
 	"github.com/hyperledger/fabric/core/ledger/util"
 	"github.com/hyperledger/fabric/protos/common"
 	"github.com/hyperledger/fabric/protos/ledger/queryresult"
@@ -301,9 +305,33 @@ func TestHistoryWithKeyContainingNilBytes(t *testing.T) {
 	//block2 tran2
 	txid2 := util2.GenerateUUID()
 	simulator2, _ := env.txmgr.NewTxSimulator(txid2)
-	// add another key <key\x00\x01\x01\x15> that contains a nil byte - such that when a range query is formed using old format, this key falls in the range
-	simulator2.SetState("ns1", "key\x00\x01\x01\x15", []byte("dummyVal1"))
-	simulator2.SetState("ns1", "\x00key\x00\x01\x01\x15", []byte("dummyVal2"))
+
+	// key1 should not fall in the range
+	key1 := "\x00key\x00\x01\x01\x15"
+	simulator2.SetState("ns1", key1, []byte("dummyVal1"))
+
+	// add other keys that contain nil byte(s) - such that when a range query is formed, these keys fall in the range
+	// key2 is skipped due to tran num decoding error (decode size 21 > 8)
+	// blockNumTranNumBytes are 0x1, 0x1, 0x15, 0x0 (separator), 0x1, 0x2, 0x1, 0x1
+	key2 := "key\x00\x01\x01\x15" // \x15 is 21
+	simulator2.SetState("ns1", key2, []byte("dummyVal2"))
+
+	// key3 is skipped due to block num decoding error (decoded size 12 > 8)
+	// blockNumTranNumBytes are 0xc, 0x0 (separtor), 0x1, 0x2, 0x1, 0x1
+	key3 := "key\x00\x0c" // \x0c is 12
+	simulator2.SetState("ns1", key3, []byte("dummyVal3"))
+
+	// key4 is skipped because blockBytesConsumed (2) + tranBytesConsumed (2) != len(blockNumTranNum) (6)
+	// blockNumTranNumBytes are 0x1, 0x0 (separator), 0x1, 0x2, 0x1, 0x1
+	key4 := "key\x00\x01"
+	simulator2.SetState("ns1", key4, []byte("dummyVal4"))
+
+	// key5 is skipped due to ErrNotFoundInIndex, where history key is <ns, key\x00\x04\x01, 2, 1>, same as <ns, key, 16777474, 1>.
+	// blockNumTranNumBytes are 0x4, 0x1, 0x0 (separator), 0x1, 0x2, 0x1, 0x1
+	key5 := "key\x00\x04\x01"
+	simulator2.SetState("ns1", key5, []byte("dummyVal5"))
+
+	// commit block2
 	simulator2.Done()
 	simRes2, _ := simulator2.GetTxSimulationResults()
 	pubSimResBytes2, _ := simRes2.GetPubSimulationBytes()
@@ -317,8 +345,178 @@ func TestHistoryWithKeyContainingNilBytes(t *testing.T) {
 	qhistory, err := env.testHistoryDB.NewHistoryQueryExecutor(store1)
 	assert.NoError(t, err, "Error upon NewHistoryQueryExecutor")
 	testutilVerifyResults(t, qhistory, "ns1", "key", []string{"value1", "value2"})
-	testutilVerifyResults(t, qhistory, "ns1", "key\x00\x01\x01\x15", []string{"dummyVal1"})
-	testutilVerifyResults(t, qhistory, "ns1", "\x00key\x00\x01\x01\x15", []string{"dummyVal2"})
+	testutilVerifyResults(t, qhistory, "ns1", key1, []string{"dummyVal1"})
+	testutilVerifyResults(t, qhistory, "ns1", key2, []string{"dummyVal2"})
+	testutilVerifyResults(t, qhistory, "ns1", key3, []string{"dummyVal3"})
+	testutilVerifyResults(t, qhistory, "ns1", key4, []string{"dummyVal4"})
+	testutilVerifyResults(t, qhistory, "ns1", key5, []string{"dummyVal5"})
+
+	// verify key2-key5 fall in range but key1 is not in range
+	testutilCheckKeyInRange(t, qhistory, "ns1", "key", key1, 0)
+	testutilCheckKeyInRange(t, qhistory, "ns1", "key", key2, 1)
+	testutilCheckKeyInRange(t, qhistory, "ns1", "key", key3, 1)
+	testutilCheckKeyInRange(t, qhistory, "ns1", "key", key4, 1)
+	testutilCheckKeyInRange(t, qhistory, "ns1", "key", key5, 1)
+
+	// temporarily replace logger with a fake logger so that we can verify Warnf message call count and call args
+	origLogger := logger
+	fakeLogger := &fakes.HistoryleveldbLogger{HistorydbLogger: &fakes.HistorydbLogger{}}
+	logger = fakeLogger
+	defer func() {
+		logger = origLogger
+	}()
+	exhaustQueryResultsForKey(t, qhistory, "ns1", "key")
+	assert.Equalf(t, 4, fakeLogger.WarnfCallCount(), "Get expected number of warning messages")
+	testutilVerifyWarningMsg(t, fakeLogger, "ns1", key2,
+		"Some other key [%#v] found in the range while scanning history for key [%#v]. Skipping (decoding error: %s)",
+		"decoded size from DecodeVarint is invalid, expected <=8, but got 21")
+	testutilVerifyWarningMsg(t, fakeLogger, "ns1", key3,
+		"Some other key [%#v] found in the range while scanning history for key [%#v]. Skipping (decoding error: %s)",
+		"decoded size from DecodeVarint is invalid, expected <=8, but got 12")
+	testutilVerifyWarningMsg(t, fakeLogger, "ns1", key4,
+		"Some other key [%#v] found in the range while scanning history for key [%#v]. Skipping (decoding error: %s)",
+		"number of decoded bytes (4) is not equal to the length of blockNumTranNumBytes (6)")
+	testutilVerifyWarningMsg(t, fakeLogger, "ns1", key5,
+		"Some other clashing key [%#v] found in the range while scanning history for key [%#v]. Skipping (cannot find block:tx)",
+		"")
+}
+
+// TestNoKeyFoundForFalseKey creates a false key that maps to block 257: tran 0.
+// The test creates block 257, but the false key is skipped because the key is not found in block 257.
+func TestNoKeyFoundForFalseKey(t *testing.T) {
+	env := newTestHistoryEnv(t)
+	defer env.cleanup()
+	provider := env.testBlockStorageEnv.provider
+	ledger1id := "ledger1"
+	store1, err := provider.OpenBlockStore(ledger1id)
+	assert.NoError(t, err, "Error upon provider.OpenBlockStore()")
+	defer store1.Shutdown()
+
+	bg, gb := testutil.NewBlockGenerator(t, ledger1id, false)
+	assert.NoError(t, store1.AddBlock(gb))
+	assert.NoError(t, env.testHistoryDB.Commit(gb))
+
+	// block1 is used to test a false key that is decoded to valid blockNum(257):tranNum(0),
+	// but it is skipped because "key" is not found in block(257):tran(0).
+	// In this case, otherKey's history key is <ns, key\x00\x04\x00, 1, 0> that would be decoded
+	// to the same blockNum:tranNum as history key <ns, key, 257, 0>.
+	// blockNumTranNumBytes are 0x4, 0x0, 0x0 (separator), 0x1, 0x1, 0x0 when getting history for "key".
+	otherKey := "key\x00\x04\x00"
+	txid := util2.GenerateUUID()
+	simulator, _ := env.txmgr.NewTxSimulator(txid)
+	simulator.SetState("ns1", otherKey, []byte("otherValue"))
+	simulator.Done()
+	simRes, _ := simulator.GetTxSimulationResults()
+	pubSimResBytes, _ := simRes.GetPubSimulationBytes()
+	block1 := bg.NextBlock([][]byte{pubSimResBytes})
+	err = store1.AddBlock(block1)
+	assert.NoError(t, err)
+	err = env.testHistoryDB.Commit(block1)
+	assert.NoError(t, err)
+
+	// add blocks 2-256, each block has 1 transaction setting state for "ns1" and "key", value is "value<blockNum>"
+	for i := 2; i <= 256; i++ {
+		txid := util2.GenerateUUID()
+		simulator, _ := env.txmgr.NewTxSimulator(txid)
+		value := fmt.Sprintf("value%d", i)
+		simulator.SetState("ns1", "key", []byte(value))
+		simulator.Done()
+		simRes, _ := simulator.GetTxSimulationResults()
+		pubSimResBytes, _ := simRes.GetPubSimulationBytes()
+		block := bg.NextBlock([][]byte{pubSimResBytes})
+		err = store1.AddBlock(block)
+		assert.NoError(t, err)
+		err = env.testHistoryDB.Commit(block)
+		assert.NoError(t, err)
+	}
+
+	// add block 257 with a different key so that otherKey cannot find "key" in this block(257):tran(0)
+	txid = util2.GenerateUUID()
+	simulator, _ = env.txmgr.NewTxSimulator(txid)
+	simulator.SetState("ns1", "key2", []byte("key2Value"))
+	simulator.Done()
+	simRes, _ = simulator.GetTxSimulationResults()
+	pubSimResBytes, _ = simRes.GetPubSimulationBytes()
+	block257 := bg.NextBlock([][]byte{pubSimResBytes})
+	err = store1.AddBlock(block257)
+	assert.NoError(t, err)
+	err = env.testHistoryDB.Commit(block257)
+	assert.NoError(t, err)
+
+	// query history db for "ns1", "key"
+	qhistory, err := env.testHistoryDB.NewHistoryQueryExecutor(store1)
+	assert.NoError(t, err, "Error upon NewHistoryQueryExecutor")
+
+	// temporarily replace logger with a fake logger so that we can verify Warnf call count and call args
+	origLogger := logger
+	fakeLogger := &fakes.HistoryleveldbLogger{HistorydbLogger: &fakes.HistorydbLogger{}}
+	logger = fakeLogger
+	defer func() {
+		logger = origLogger
+	}()
+	exhaustQueryResultsForKey(t, qhistory, "ns1", "key")
+	assert.Equalf(t, 1, fakeLogger.WarnfCallCount(), "Get expected number of warning messages")
+	msg, _ := fakeLogger.WarnfArgsForCall(0)
+	// key "ns, key\x00\x04\x00, 1, 0" is returned as a false key in range query for "key".
+	// Its "x04\x00, 1, 0" portion happens to be decoded to a valid blockNum:tranNum (257:0)
+	// but the desired ns/key is not present in the blockNum:tranNum.
+	assert.Equal(t, "Some other key [%#v] found in the range while scanning history for key [%#v]. Skipping (namespace or key not found)", msg)
+}
+
+// TestHistoryWithBlockNumber256 creates 256 blocks and then
+// search historydb to verify that block number 256 is correctly returned
+// even if its blockNumTranNumBytes contains a nil byte (FAB-15450).
+func TestHistoryWithBlockNumber256(t *testing.T) {
+	env := newTestHistoryEnv(t)
+	defer env.cleanup()
+	provider := env.testBlockStorageEnv.provider
+	ledger1id := "ledger1"
+	store1, err := provider.OpenBlockStore(ledger1id)
+	assert.NoError(t, err, "Error upon provider.OpenBlockStore()")
+	defer store1.Shutdown()
+
+	bg, gb := testutil.NewBlockGenerator(t, ledger1id, false)
+	assert.NoError(t, store1.AddBlock(gb))
+	assert.NoError(t, env.testHistoryDB.Commit(gb))
+
+	// add 256 blocks, each block has 1 transaction setting state for "ns1" and "key", value is "value<blockNum>"
+	for i := 1; i <= 256; i++ {
+		txid := util2.GenerateUUID()
+		simulator, _ := env.txmgr.NewTxSimulator(txid)
+		value := fmt.Sprintf("value%d", i)
+		simulator.SetState("ns1", "key", []byte(value))
+		simulator.Done()
+		simRes, _ := simulator.GetTxSimulationResults()
+		pubSimResBytes, _ := simRes.GetPubSimulationBytes()
+		block := bg.NextBlock([][]byte{pubSimResBytes})
+		err = store1.AddBlock(block)
+		assert.NoError(t, err)
+		err = env.testHistoryDB.Commit(block)
+		assert.NoError(t, err)
+	}
+
+	// query history db for "ns1", "key"
+	qhistory, err := env.testHistoryDB.NewHistoryQueryExecutor(store1)
+	assert.NoError(t, err, "Error upon NewHistoryQueryExecutor")
+	itr, err := qhistory.GetHistoryForKey("ns1", "key")
+	assert.NoError(t, err, "Error upon GetHistoryForKey()")
+
+	// iterate query result - there should be 256 entries
+	numEntries := 0
+	valueInBlock256 := "unknown"
+	for {
+		kmod, _ := itr.Next()
+		if kmod == nil {
+			break
+		}
+		numEntries++
+		retrievedValue := string(kmod.(*queryresult.KeyModification).Value)
+		if numEntries == 256 {
+			valueInBlock256 = retrievedValue
+		}
+	}
+	assert.Equal(t, 256, numEntries)
+	assert.Equal(t, "value256", valueInBlock256)
 }
 
 func testutilVerifyResults(t *testing.T, hqe ledger.HistoryQueryExecutor, ns, key string, expectedVals []string) {
@@ -336,4 +534,63 @@ func testutilVerifyResults(t *testing.T, hqe ledger.HistoryQueryExecutor, ns, ke
 		t.Logf("Retrieved history record at TxId=%s with value %s", txid, retrievedValue)
 	}
 	assert.Equal(t, expectedVals, retrievedVals)
+}
+
+// testutilCheckKeyInRange check if falseKey falls in range query when searching for desiredKey
+func testutilCheckKeyInRange(t *testing.T, hqe ledger.HistoryQueryExecutor, ns, desiredKey, falseKey string, expectedMatchCount int) {
+	itr, err := hqe.GetHistoryForKey(ns, desiredKey)
+	assert.NoError(t, err, "Error upon GetHistoryForKey()")
+	scanner := itr.(*historyScanner)
+	compositePartialKey := historydb.ConstructPartialCompositeHistoryKey(ns, falseKey, false)
+	count := 0
+	for {
+		if !scanner.dbItr.Next() {
+			break
+		}
+		historyKey := scanner.dbItr.Key()
+		if bytes.Contains(historyKey, compositePartialKey) {
+			count++
+		}
+	}
+	assert.Equal(t, expectedMatchCount, count)
+}
+
+// exhaustQueryResultsForKey is a helper method to exhaust all the results returned by history query.
+// A fake logger can be set before calling this method to verify if a false key is skipped.
+func exhaustQueryResultsForKey(t *testing.T, hqe ledger.HistoryQueryExecutor, ns, key string) {
+	itr, err := hqe.GetHistoryForKey(ns, key)
+	assert.NoError(t, err, "Error upon GetHistoryForKey()")
+	for {
+		kmod, _ := itr.Next()
+		if kmod == nil {
+			break
+		}
+	}
+}
+
+// testutilVerifyWarningMsg verifies fakeLogger had a Warnf call for the falseKey.
+// It iterates fakeLogger.WarnfCallCount() to find a history key that matches compositePartialKey
+// and then verifies warnMsg and decodeErrMsg (if not empty).
+func testutilVerifyWarningMsg(t *testing.T, fakeLogger *fakes.HistoryleveldbLogger, ns, falseKey, warnMsg, decodeErrMsg string) {
+	// because there is no particular order, iterate each call to find the matched one
+	compositePartialKey := historydb.ConstructPartialCompositeHistoryKey(ns, falseKey, false)
+	for i := 0; i < fakeLogger.WarnfCallCount(); i++ {
+		msg, args := fakeLogger.WarnfArgsForCall(i)
+
+		if !bytes.Contains(args[0].([]byte), compositePartialKey) {
+			// not matched
+			continue
+		}
+
+		assert.Equal(t, warnMsg, msg)
+
+		if decodeErrMsg == "" {
+			assert.Equal(t, 2, len(args))
+		} else {
+			assert.Equal(t, 3, len(args))
+			assert.Equal(t, decodeErrMsg, fmt.Sprintf("%s", args[2]))
+		}
+		return
+	}
+	assert.Fail(t, fmt.Sprintf("did not find false key %s in range", falseKey))
 }
