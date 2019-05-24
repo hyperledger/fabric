cdad5040ce8085c8c10766c3fdf9ee7950316e49
Jay Guo <guojiannan1101@gmail.com>
2019-05-24 16:53:55 +0800

    FAB-15534 switch orderer IT to used in-proc client 2/2
    This CR removes complete Fabric tx flow from config_test.go
    in favor of in-proc clients.
    
    Change-Id: Ibd5f9b3ac895307335f83c9e3f0797618d1fc612
    Signed-off-by: Jay Guo <guojiannan1101@gmail.com>

--- integration/raft/raft_suite_test.go
+++ integration/raft/raft_suite_test.go
@@ -8,20 +8,12 @@ package raft
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 
-	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric/integration"
 	"github.com/hyperledger/fabric/integration/nwo"
-	"github.com/hyperledger/fabric/integration/nwo/commands"
-	"github.com/hyperledger/fabric/protos/common"
-	pb "github.com/hyperledger/fabric/protos/peer"
-	"github.com/hyperledger/fabric/protoutil"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gbytes"
-	"github.com/onsi/gomega/gexec"
 )
 
 func TestRaft(t *testing.T) {
@@ -51,74 +43,4 @@ var _ = SynchronizedAfterSuite(func() {
 
 func StartPort() int {
 	return suiteBase + (GinkgoParallelNode()-1)*100
-}
-
-func RunInvoke(n *nwo.Network, orderer *nwo.Orderer, peer *nwo.Peer, channel string) {
-	By("Querying chaincode")
-	sess, err := n.PeerUserSession(peer, "User1", commands.ChaincodeInvoke{
-		ChannelID: channel,
-		Orderer:   n.OrdererAddress(orderer, nwo.ListenPort),
-		Name:      "mycc",
-		Ctor:      `{"Args":["invoke","a","b","10"]}`,
-		PeerAddresses: []string{
-			n.PeerAddress(n.Peer("Org1", "peer0"), nwo.ListenPort),
-			n.PeerAddress(n.Peer("Org2", "peer1"), nwo.ListenPort),
-		},
-		WaitForEvent: true,
-	})
-	Expect(err).NotTo(HaveOccurred())
-	Eventually(sess, n.EventuallyTimeout).Should(gexec.Exit(0))
-	Expect(sess.Err).To(gbytes.Say("Chaincode invoke successful. result: status:200"))
-}
-
-func RunQuery(n *nwo.Network, orderer *nwo.Orderer, peer *nwo.Peer, channel string) int {
-	By("Invoking chaincode")
-	sess, err := n.PeerUserSession(peer, "User1", commands.ChaincodeQuery{
-		ChannelID: channel,
-		Name:      "mycc",
-		Ctor:      `{"Args":["query","a"]}`,
-	})
-	Expect(err).NotTo(HaveOccurred())
-	Eventually(sess, n.EventuallyTimeout).Should(gexec.Exit(0))
-
-	var result int
-	i, err := fmt.Sscanf(string(sess.Out.Contents()), "%d", &result)
-	Expect(err).NotTo(HaveOccurred())
-	Expect(i).To(Equal(1))
-	return int(result)
-}
-
-func RunQueryInvokeQuery(n *nwo.Network, orderer *nwo.Orderer, peer *nwo.Peer, channel string) {
-	res := RunQuery(n, orderer, peer, channel)
-	Expect(res).To(Equal(100))
-
-	RunInvoke(n, orderer, peer, channel)
-
-	res = RunQuery(n, orderer, peer, channel)
-	Expect(res).To(Equal(90))
-}
-
-// SetACLPolicy sets the ACL policy for a running network. It resets all
-// previously defined ACL policies, generates the config update, signs the
-// configuration with Org2's signer, and then submits the config update using
-// Org1.
-func SetACLPolicy(network *nwo.Network, channel, policyName, policy string, ordererName string) {
-	orderer := network.Orderer(ordererName)
-	submitter := network.Peer("Org1", "peer0")
-	signer := network.Peer("Org2", "peer0")
-
-	config := nwo.GetConfig(network, submitter, orderer, channel)
-	updatedConfig := proto.Clone(config).(*common.Config)
-
-	// set the policy
-	updatedConfig.ChannelGroup.Groups["Application"].Values["ACLs"] = &common.ConfigValue{
-		ModPolicy: "Admins",
-		Value: protoutil.MarshalOrPanic(&pb.ACLs{
-			Acls: map[string]*pb.APIResource{
-				policyName: {PolicyRef: policy},
-			},
-		}),
-	}
-
-	nwo.UpdateConfig(network, orderer, channel, config, updatedConfig, true, submitter, signer)
 }
