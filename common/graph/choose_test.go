8d353ba5bf389c92d2c95cab9d8677a06c6d447c
yacovm <yacovm@il.ibm.com>
2019-05-24 23:10:04 +0300

    [FAB-15549] Restrict discovery max combinations
    This change set restricts discovery maximum combinations via
    a heiuristic that detects if the number of combinations of
    descendants of a policy vertex is too large, and randomly
    prunes the descendants according to the policy threshold.
    
    Change-Id: I5bfe1e4b5586cbb647d20b961cb37c39f09779da
    Signed-off-by: yacovm <yacovm@il.ibm.com>
    (cherry picked from commit f9caa00af6bb36bd1e4c4d154c802c6bde1c4149)

--- common/graph/choose_test.go
+++ common/graph/choose_test.go
@@ -12,15 +12,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestChoose(t *testing.T) {
-	assert.Equal(t, 24, factorial(4))
-	assert.Equal(t, 1, factorial(0))
-	assert.Equal(t, 1, factorial(1))
-	assert.Equal(t, 15504, nChooseK(20, 5))
-	for n := 1; n < 20; n++ {
-		for k := 1; k < n; k++ {
-			g := chooseKoutOfN(n, k)
-			assert.Equal(t, nChooseK(n, k), len(g), "n=%d, k=%d", n, k)
-		}
-	}
+func TestCombinationsExceed(t *testing.T) {
+	// 20 choose 5 is 15504.
+	assert.False(t, CombinationsExceed(20, 5, 15504))
+	assert.False(t, CombinationsExceed(20, 5, 15505))
+	assert.True(t, CombinationsExceed(20, 5, 15503))
+
+	// A huge number of combinations doesn't overflow.
+	assert.True(t, CombinationsExceed(10000, 500, 9000))
+
+	// N < K returns false
+	assert.False(t, CombinationsExceed(20, 30, 0))
 }
