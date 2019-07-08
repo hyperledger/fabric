59feca2e213129377e2c9eb6f7c3dd19c9198dfa
senthil <cendhu@gmail.com>
2019-07-08 17:49:46 +0530

    remove rollback capability from mock
    In the mock, we have both Rollback and StorePvtDataOfInvalidTx
    capabilities. As the Rollback capability was renamed to
    StorePvtDataOfInvalidTx in production code, we need to remove
    the Rollback capability from the mock.
    
    FAB-15887 #done
    
    Change-Id: If5c1d43310b1011bc0ae70fb6febe4d704d04977
    Signed-off-by: senthil <cendhu@gmail.com>

--- core/committer/txvalidator/mocks/capabilities.go
+++ core/committer/txvalidator/mocks/capabilities.go
@@ -107,20 +107,6 @@ func (_m *Capabilities) PrivateChannelData() bool {
 	return r0
 }
 
-// Rollback provides a mock function with given fields:
-func (_m *Capabilities) Rollback() bool {
-	ret := _m.Called()
-
-	var r0 bool
-	if rf, ok := ret.Get(0).(func() bool); ok {
-		r0 = rf()
-	} else {
-		r0 = ret.Get(0).(bool)
-	}
-
-	return r0
-}
-
 // StorePvtDataOfInvalidTx provides a mock function with given fields:
 func (_m *Capabilities) StorePvtDataOfInvalidTx() bool {
 	ret := _m.Called()
