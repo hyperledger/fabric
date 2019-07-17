469501a6d3deceb4f3f63f07e5b4a0950f0ce17a
David Enyeart <enyeart@us.ibm.com>
2019-07-17 18:11:06 -0400

    [FAB-15998] Prepare for next fabric rel (1.4.3)
    Change-Id: Ida6b225a71936e14b386facf09b8e6df4baeda7f
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- common/tools/configtxlator/metadata/metadata.go
+++ common/tools/configtxlator/metadata/metadata.go
@@ -14,7 +14,7 @@ import (
 // package-scoped variables
 
 // Package version
-const Version = "1.4.2"
+const Version = "1.4.3"
 
 var CommitSHA string
 
--- common/tools/idemixgen/metadata/metadata.go
+++ common/tools/idemixgen/metadata/metadata.go
@@ -12,7 +12,7 @@ import (
 )
 
 // Package version
-const Version = "1.4.2"
+const Version = "1.4.3"
 
 var CommitSHA string
 
