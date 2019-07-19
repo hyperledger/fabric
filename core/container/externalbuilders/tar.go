005b2ec7abff07c0c5f61dab3781134bacfa12a6
Jason Yellick <jyellick@us.ibm.com>
2019-07-19 12:35:31 -0400

    FAB-14491 Add basic sanitizing untar utility
    For the external builders to work, we need to be able to expand code
    packages into a tmp directory to run 'detect' against.  This CR adds a
    basic util function for safely expanding tar files, ensuring that only
    normal files and directories are created and as children of the
    temporary source directory.
    
    Change-Id: I51201288d515d506d49863d2f5ebce6b862cb549
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

--- core/container/externalbuilders/tar.go
+++ core/container/externalbuilders/tar.go
@@ -7,9 +7,71 @@ SPDX-License-Identifier: Apache-2.0
 package externalbuilders
 
 import (
+	"archive/tar"
+	"compress/gzip"
+	"io"
+	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/pkg/errors"
 )
+
+// Untar takes a gzip-ed tar archive, and extracts it to dst.
+// It returns an error if the tar contains any files which would escape to a
+// parent of dst, or if the archive contains any files whose type is not
+// a regular file or directory.
+func Untar(buffer io.Reader, dst string) error {
+	gzr, err := gzip.NewReader(buffer)
+	if err != nil {
+		return err
+	}
+	defer gzr.Close()
+
+	tr := tar.NewReader(gzr)
+
+	for {
+		header, err := tr.Next()
+
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return errors.WithMessage(err, "could not get next tar element")
+		}
+
+		if !ValidPath(header.Name) {
+			return errors.Errorf("tar contains the absolute or escaping path '%s'", header.Name)
+		}
+
+		target := filepath.Join(dst, header.Name)
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if err := os.MkdirAll(target, 0700); err != nil {
+				return errors.WithMessagef(err, "could not create directory '%s'", header.Name)
+			}
+		case tar.TypeReg:
+			if err := os.MkdirAll(filepath.Dir(target), 0700); err != nil {
+				return errors.WithMessagef(err, "could not create directory '%s'", filepath.Dir(header.Name))
+			}
+
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return errors.WithMessagef(err, "could not create file '%s'", header.Name)
+			}
+
+			// copy over contents
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+
+			f.Close()
+		default:
+			return errors.Errorf("invalid file type '%v' contained in archive for file '%s'", header.Typeflag, header.Name)
+		}
+	}
+}
 
 // ValidPath checks to see if the path is absolute, or if it is a
 // relative path higher in the tree.  In these cases it returns false.
