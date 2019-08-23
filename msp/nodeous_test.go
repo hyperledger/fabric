05479d9571121348b666dd27d93f4201a921c04c
Angelo De Caro <adc@zurich.ibm.com>
2019-08-23 08:11:27 +0200

    [FAB-16376] MSP_1.4.3: support for admincerts
    This change-sets introduces a new test that check that
    when Node OU classificationi is enabled, certificates
    in the admincerts folder are considered administrator
    even if they carry the Client OU. Notice that
    certificates in the admincerts folder, when node ou
    classification is enabled, are required to contain
    either the client or admin OU.
    
    Signed-off-by: Angelo De Caro <adc@zurich.ibm.com>
    Change-Id: I7a418bfae4ac1490c9e640bb864a6f7b94089db9

--- msp/nodeous_test.go
+++ msp/nodeous_test.go
@@ -7,6 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 package msp
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/golang/protobuf/proto"
@@ -257,6 +258,37 @@ func TestLoad143MSPWithInvalidAdminConfiguration(t *testing.T) {
 	// testdata/nodeouadmin3:
 	// the configuration enables NodeOUs (with adminOU) but no valid identifier for the AdminOU
 	getLocalMSPWithVersionErr(t, "testdata/nodeouadmin3", MSPv1_4_3, "administrators must be declared when no admin ou classification is set")
+}
+
+func TestAdminInAdmincertsWith143MSP(t *testing.T) {
+	// testdata/nodeouadminclient enables NodeOU classification and contains in the admincerts folder
+	// a certificate classified as client. This test checks that that identity is considered an admin anyway.
+	// testdata/nodeouadminclient2 enables NodeOU classification and contains in the admincerts folder
+	// a certificate classified as client. This test checks that that identity is considered an admin anyway.
+	// Notice that the configuration used is one that is usually expected for MSP version < 1.4.3 which
+	// only define peer and client OU.
+	testFolders := []string{"testdata/nodeouadminclient", "testdata/nodeouadminclient2"}
+
+	for _, testFolder := range testFolders {
+		localMSP := getLocalMSPWithVersion(t, testFolder, MSPv1_4_3)
+
+		cert, err := readFile(filepath.Join(testFolder, "admincerts", "admin.pem"))
+		assert.NoError(t, err)
+
+		id, _, err := localMSP.(*bccspmsp).getIdentityFromConf(cert)
+		assert.NoError(t, err)
+		for _, ou := range id.GetOrganizationalUnits() {
+			assert.NotEqual(t, "admin", ou.OrganizationalUnitIdentifier)
+		}
+
+		principalBytes, err := proto.Marshal(&msp.MSPRole{Role: msp.MSPRole_ADMIN, MspIdentifier: "SampleOrg"})
+		assert.NoError(t, err)
+		principal := &msp.MSPPrincipal{
+			PrincipalClassification: msp.MSPPrincipal_ROLE,
+			Principal:               principalBytes}
+		err = id.SatisfiesPrincipal(principal)
+		assert.NoError(t, err)
+	}
 }
 
 func TestSatisfiesPrincipalOrderer(t *testing.T) {
