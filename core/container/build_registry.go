90e9b548a1ba0abd0bc344e100136ddc5de9ee82
Jason Yellick <jyellick@us.ibm.com>
2019-09-10 14:17:38 -0400

    FAB-13528 Add basic build status registry
    Because both the chaincode launch-on-invoke path and the
    build-on-install path will need to be able to build a chaincode, some
    form of synchronization is needed between them.  This CR creates a small
    registry, following the 'context' model of a done channel and an error
    set on the structure, to allow coordination between the two.  It does
    not attempt to wire it yet.
    
    Change-Id: If89135b1f2e40cf15c38ea481be198da992f61d0
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>

---
+++ core/container/build_registry.go
@@ -0,0 +1,65 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package container
+
+import (
+	"sync"
+)
+
+type BuildRegistry struct {
+	mutex  sync.Mutex
+	builds map[string]*BuildStatus
+}
+
+// BuildStatus returns a BuildStatus for the ccid, and whether the caller
+// is waiting in line (true), or this build status is new and their responsibility.
+// If the build status is new, then the caller must call Notify with the error
+// (or nil) upon completion.
+func (br *BuildRegistry) BuildStatus(ccid string) (*BuildStatus, bool) {
+	br.mutex.Lock()
+	defer br.mutex.Unlock()
+	if br.builds == nil {
+		br.builds = map[string]*BuildStatus{}
+	}
+
+	bs, ok := br.builds[ccid]
+	if !ok {
+		bs = NewBuildStatus()
+		br.builds[ccid] = bs
+	}
+
+	return bs, ok
+}
+
+type BuildStatus struct {
+	mutex sync.Mutex
+	doneC chan struct{}
+	err   error
+}
+
+func NewBuildStatus() *BuildStatus {
+	return &BuildStatus{
+		doneC: make(chan struct{}),
+	}
+}
+
+func (bs *BuildStatus) Err() error {
+	bs.mutex.Lock()
+	defer bs.mutex.Unlock()
+	return bs.err
+}
+
+func (bs *BuildStatus) Notify(err error) {
+	bs.mutex.Lock()
+	defer bs.mutex.Unlock()
+	bs.err = err
+	close(bs.doneC)
+}
+
+func (bs *BuildStatus) Done() <-chan struct{} {
+	return bs.doneC
+}
