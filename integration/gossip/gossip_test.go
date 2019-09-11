d2aaa98dbdaaef2804dab3c83541654179c817a7
Dongming <dongming@ibm.com>
2019-09-11 14:34:19 -0400

    [FAB-15980] IT: stop the leader peer
    This CR is to execute IT: stop the leader peer,
    send TXs, and restart it to confirm that it
    steps dwon and catches up as a nonleader
    
    Signed-off-by: Dongming <dongming@ibm.com>
    Change-Id: I975b1bf9623191b0bc434067f6ae5fd9fc3f08c5

--- integration/gossip/gossip_test.go
+++ integration/gossip/gossip_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 	"github.com/tedsuo/ifrit"
+	"github.com/tedsuo/ifrit/ginkgomon"
 )
 
 var _ = Describe("Gossip Test", func() {
@@ -67,6 +68,7 @@ var _ = Describe("Gossip Test", func() {
 		var (
 			ordererProcess ifrit.Process
 			peerProcesses  = map[string]ifrit.Process{}
+			peerRunners    = map[string]*ginkgomon.Runner{}
 		)
 
 		BeforeEach(func() {
@@ -99,6 +101,7 @@ var _ = Describe("Gossip Test", func() {
 			for _, peer := range []*nwo.Peer{peer0Org1, peer1Org1, peer0Org2, peer1Org2} {
 				runner := network.PeerRunner(peer)
 				peerProcesses[peer.ID()] = ifrit.Invoke(runner)
+				peerRunners[peer.ID()] = runner
 			}
 
 			channelName := "testchannel"
@@ -114,7 +117,7 @@ var _ = Describe("Gossip Test", func() {
 				}).Should(BeNumerically(">=", 2))
 			}
 
-			By("stop peers to make sure they cannot get blocks from orderer")
+			By("stop peers except peer0Org1 to make sure they cannot get blocks from orderer")
 			for id, proc := range peerProcesses {
 				if id == peer0Org1.ID() {
 					continue
@@ -125,21 +128,7 @@ var _ = Describe("Gossip Test", func() {
 			}
 
 			By("create transactions")
-			for i := 0; i < 10; i++ {
-				sess, err := network.PeerUserSession(peer0Org1, "User1", commands.ChaincodeInvoke{
-					ChannelID: channelName,
-					Orderer:   network.OrdererAddress(orderer, nwo.ListenPort),
-					Name:      "mycc",
-					Ctor:      `{"Args":["invoke","a","b","10"]}`,
-					PeerAddresses: []string{
-						network.PeerAddress(peer0Org1, nwo.ListenPort),
-					},
-					WaitForEvent: true,
-				})
-				Expect(err).NotTo(HaveOccurred())
-				Eventually(sess, network.EventuallyTimeout).Should(gexec.Exit(0))
-				Expect(sess.Err).To(gbytes.Say("Chaincode invoke successful. result: status:200"))
-			}
+			runTransactions(network, orderer, peer0Org1, "mycc", channelName)
 
 			peer0LedgerHeight := nwo.GetLedgerHeight(network, peer0Org1, channelName)
 
@@ -148,20 +137,84 @@ var _ = Describe("Gossip Test", func() {
 			Eventually(ordererProcess.Wait(), network.EventuallyTimeout).Should(Receive())
 			ordererProcess = nil
 
-			By("wait for rest of the peers to join and startup")
-			for _, peer := range []*nwo.Peer{peer1Org1, peer0Org2, peer1Org2} {
-				runner := network.PeerRunner(peer, fmt.Sprint("CORE_PEER_GOSSIP_STATE_CHECKINTERVAL=200ms"),
-					fmt.Sprint("FABRIC_LOGGING_SPEC=info:gossip.state=debug"),
-				)
-				peerProcesses[peer.ID()] = ifrit.Invoke(runner)
-			}
+			By("restart the three peers that were stopped")
+			peerList := []*nwo.Peer{peer1Org1, peer0Org2, peer1Org2}
+			peersRestart(network, orderer, peerList, peerProcesses, peerRunners)
 
 			By("Make sure peers are synced up")
-			for _, peer := range []*nwo.Peer{peer1Org1, peer0Org2, peer1Org2} {
-				Eventually(func() int {
-					return nwo.GetLedgerHeight(network, peer, channelName)
-				}, time.Second*10).Should(Equal(peer0LedgerHeight))
-			}
+			assertPeersLedgerHeight(network, orderer, peerList, peer0LedgerHeight, channelName)
+
+			By("start the orderer")
+			orderer = network.Orderer("orderer")
+			ordererRunner = network.OrdererRunner(orderer)
+			ordererProcess = ifrit.Invoke(ordererRunner)
+
+			By("install chaincode")
+			nwo.InstallChaincodeLegacy(network, chaincode, peer1Org1)
+
+			By("stop leader, peer0Org1, to make sure it cannot get blocks from orderer")
+			id := peer0Org1.ID()
+			proc := peerProcesses[id]
+			proc.Signal(syscall.SIGTERM)
+			Eventually(proc.Wait(), network.EventuallyTimeout).Should(Receive())
+
+			expectedMsg := "Stopped being a leader"
+			Eventually(peerRunners[id].Err(), time.Minute).Should(gbytes.Say(expectedMsg))
+
+			delete(peerProcesses, id)
+
+			By("create transactions")
+			runTransactions(network, orderer, peer1Org1, "mycc", channelName)
+
+			peer1LedgerHeight := nwo.GetLedgerHeight(network, peer1Org1, channelName)
+
+			By("turning down ordering service")
+			ordererProcess.Signal(syscall.SIGTERM)
+			Eventually(ordererProcess.Wait(), network.EventuallyTimeout).Should(Receive())
+			ordererProcess = nil
+
+			By("restart peer0Org1")
+			peerList = []*nwo.Peer{peer0Org1}
+			peersRestart(network, orderer, peerList, peerProcesses, peerRunners)
+
+			By("Make sure peer0Org1 is synced up")
+			assertPeersLedgerHeight(network, orderer, peerList, peer1LedgerHeight, channelName)
 		})
 	})
 })
+
+func runTransactions(n *nwo.Network, orderer *nwo.Orderer, peer *nwo.Peer, chaincodeName string, channelID string) {
+	for i := 0; i < 10; i++ {
+		sess, err := n.PeerUserSession(peer, "User1", commands.ChaincodeInvoke{
+			ChannelID: channelID,
+			Orderer:   n.OrdererAddress(orderer, nwo.ListenPort),
+			Name:      chaincodeName,
+			Ctor:      `{"Args":["invoke","a","b","10"]}`,
+			PeerAddresses: []string{
+				n.PeerAddress(peer, nwo.ListenPort),
+			},
+			WaitForEvent: true,
+		})
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(sess, n.EventuallyTimeout).Should(gexec.Exit(0))
+		Expect(sess.Err).To(gbytes.Say("Chaincode invoke successful. result: status:200"))
+	}
+}
+
+func peersRestart(n *nwo.Network, orderer *nwo.Orderer, peerList []*nwo.Peer, peerProc map[string]ifrit.Process, peerRun map[string]*ginkgomon.Runner) {
+	for _, peer := range peerList {
+		runner := n.PeerRunner(peer, fmt.Sprint("CORE_PEER_GOSSIP_STATE_CHECKINTERVAL=200ms"),
+			fmt.Sprint("FABRIC_LOGGING_SPEC=info:gossip.state=debug"),
+		)
+		peerProc[peer.ID()] = ifrit.Invoke(runner)
+		peerRun[peer.ID()] = runner
+	}
+}
+
+func assertPeersLedgerHeight(n *nwo.Network, orderer *nwo.Orderer, peerList []*nwo.Peer, expectedVal int, channelID string) {
+	for _, peer := range peerList {
+		Eventually(func() int {
+			return nwo.GetLedgerHeight(n, peer, channelID)
+		}, time.Second*10).Should(Equal(expectedVal))
+	}
+}
