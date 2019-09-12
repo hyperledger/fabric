f55f46bbc2741c0ae7b9e824cb2a8b7c61dc5ce1
Mingliang Yin <ymlbright.h.cn@gmail.com>
2019-09-12 10:39:11 +0800

    [FAB-16571] Fix peer panic when package java chaincode
    Check error first in walkFn, if there is an error when walk through the path, the info variable will be nil, and the err shows the reason.
    
    Change-Id: Ic1307e0993d21454d13412dcd0669202c2bffdb3
    Signed-off-by: MingLiang Yin <ymlbright.h.cn@gmail.com>

--- core/chaincode/platforms/util/writer_test.go
+++ core/chaincode/platforms/util/writer_test.go
@@ -201,6 +201,26 @@ func TestWriteFolderToTarPackageFailure3(t *testing.T) {
 	gw.Close()
 }
 
+// Failure case 4: with lstat failed
+func Test_WriteFolderToTarPackageFailure4(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "WriteFolderToTarPackageFailure4BadFileMode")
+	require.NoError(t, err)
+	defer os.RemoveAll(tempDir)
+	testFile := filepath.Join(tempDir, "test.java")
+	err = ioutil.WriteFile(testFile, []byte("Content"), 0644)
+	require.NoError(t, err, "Error creating file", testFile)
+	err = os.Chmod(tempDir, 0644)
+	require.NoError(t, err)
+
+	buf := bytes.NewBuffer(nil)
+	tw := tar.NewWriter(buf)
+	defer tw.Close()
+
+	err = WriteFolderToTarPackage(tw, tempDir, []string{}, nil, nil)
+	assert.Error(t, err, "Should have received error writing folder to package")
+	assert.Contains(t, err.Error(), "permission denied")
+}
+
 func createTestTar(t *testing.T, srcPath string, excludeDir []string, includeFileTypeMap map[string]bool, excludeFileTypeMap map[string]bool) []byte {
 	buf := bytes.NewBuffer(nil)
 	gw := gzip.NewWriter(buf)
