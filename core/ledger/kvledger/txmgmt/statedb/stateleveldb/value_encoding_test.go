db920bba43c474ee1bfe04b921640802f1655a89
manish <manish.sethi@gmail.com>
2019-09-16 23:07:07 -0400

    FAB-15491 Remove code dealing with old formatting
    This CR cleans up the state leveldb code removing the support
    for version 1.1 encoding of values. This requires rebuild of statedb.
    
    Change-Id: Ifa425dffe6f3984a02c4955e8e53942ce9d4c8f7
    Signed-off-by: manish <manish.sethi@gmail.com>

--- core/ledger/kvledger/txmgmt/statedb/stateleveldb/value_encoding_test.go
+++ core/ledger/kvledger/txmgmt/statedb/stateleveldb/value_encoding_test.go
@@ -15,27 +15,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestEncodeString tests encoding and decoding a string value using old format
-func TestEncodeDecodeStringOldFormat(t *testing.T) {
-	bytesString1 := []byte("value1")
-	version1 := version.NewHeight(1, 1)
-	encodedValue := encodeValueOldFormat(bytesString1, version1)
-	decodedValue, err := decodeValue(encodedValue)
-	assert.NoError(t, err)
-	assert.Equal(t, &statedb.VersionedValue{Version: version1, Value: bytesString1}, decodedValue)
-}
-
-// TestEncodeDecodeJSONOldFormat tests encoding and decoding a JSON value using old format
-func TestEncodeDecodeJSONOldFormat(t *testing.T) {
-	bytesJSON2 := []byte(`{"asset_name":"marble1","color":"blue","size":"35","owner":"jerry"}`)
-	version2 := version.NewHeight(1, 1)
-	encodedValue := encodeValueOldFormat(bytesJSON2, version2)
-	decodedValue, err := decodeValue(encodedValue)
-	assert.NoError(t, err)
-	assert.Equal(t, &statedb.VersionedValue{Version: version2, Value: bytesJSON2}, decodedValue)
-}
-
-func TestEncodeDecodeOldAndNewFormat(t *testing.T) {
+func TestEncodeDecodeVersionedValues(t *testing.T) {
 	testdata := []*statedb.VersionedValue{
 		{
 			Value:   []byte("value0"),
@@ -59,34 +39,15 @@ func TestEncodeDecodeOldAndNewFormat(t *testing.T) {
 
 	for i, testdatum := range testdata {
 		t.Run(fmt.Sprintf("testcase-newfmt-%d", i),
-			func(t *testing.T) { testEncodeDecodeNewFormat(t, testdatum) },
+			func(t *testing.T) { testEncodeDecodeVersionedValues(t, testdatum) },
 		)
 	}
-
-	for i, testdatum := range testdata {
-		t.Run(fmt.Sprintf("testcase-oldfmt-%d", i),
-			func(t *testing.T) {
-				testdatum.Metadata = nil
-				testEncodeDecodeOldFormat(t, testdatum)
-			},
-		)
-	}
-
 }
 
-func testEncodeDecodeNewFormat(t *testing.T, v *statedb.VersionedValue) {
-	encodedNewFmt, err := encodeValue(v)
+func testEncodeDecodeVersionedValues(t *testing.T, v *statedb.VersionedValue) {
+	encodedVal, err := encodeValue(v)
 	assert.NoError(t, err)
-	// encoding-decoding using new format should return the same versioned_value
-	decodedFromNewFmt, err := decodeValue(encodedNewFmt)
+	decodedVal, err := decodeValue(encodedVal)
 	assert.NoError(t, err)
-	assert.Equal(t, v, decodedFromNewFmt)
-}
-
-func testEncodeDecodeOldFormat(t *testing.T, v *statedb.VersionedValue) {
-	encodedOldFmt := encodeValueOldFormat(v.Value, v.Version)
-	// decodeValue should be able to handle the old format
-	decodedFromOldFmt, err := decodeValue(encodedOldFmt)
-	assert.NoError(t, err)
-	assert.Equal(t, v, decodedFromOldFmt)
+	assert.Equal(t, v, decodedVal)
 }
