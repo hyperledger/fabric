dfe42d3cdde9980743dfc8f6e4e291ce9f9bceba
Will Lahti <wtlahti@us.ibm.com>
2019-09-23 15:34:16 -0400

    Refactor integration/nwo/package.go
    Addressing review comments from
    https://gerrit.hyperledger.org/r/c/fabric/+/33542/10/integration/nwo/package.go
    
    FAB-14494
    
    Change-Id: I02dbc0f35a9a14255c693499d535721b0ba8cb9f
    Signed-off-by: Will Lahti <wtlahti@us.ibm.com>
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- integration/nwo/package.go
+++ integration/nwo/package.go
@@ -8,13 +8,13 @@ package nwo
 
 import (
 	"archive/tar"
-	"bytes"
 	"compress/gzip"
 	"encoding/json"
+	"io"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 
-	"github.com/hyperledger/fabric/core/chaincode/platforms/util"
 	. "github.com/onsi/gomega"
 )
 
@@ -22,72 +22,87 @@ import (
 // an already built chaincode and write it to the location
 // specified by Chaincode.PackageFile.
 func PackageChaincodeBinary(c Chaincode) {
-	tarGzBytes := getTarGzBytes(c)
-	err := ioutil.WriteFile(c.PackageFile, tarGzBytes, 0100644)
+	file, err := os.Create(c.PackageFile)
 	Expect(err).NotTo(HaveOccurred())
+	defer file.Close()
+	writeTarGz(c, file)
 }
 
-func getTarGzBytes(c Chaincode) []byte {
-	payload := bytes.NewBuffer(nil)
-	gw := gzip.NewWriter(payload)
+func writeTarGz(c Chaincode, w io.Writer) {
+	gw := gzip.NewWriter(w)
 	tw := tar.NewWriter(gw)
+
 	_, normalizedPath := filepath.Split(c.Path)
-	metadataBytes := toJSON(normalizedPath, "binary", c.Label)
-	writeBytesToPackage(tw, "metadata.json", metadataBytes)
-	codeBytes := getCodeBytes(c.Path)
-	writeBytesToPackage(tw, "code.tar.gz", codeBytes)
-	err := tw.Close()
-	Expect(err).NotTo(HaveOccurred())
-	err = gw.Close()
-	Expect(err).NotTo(HaveOccurred())
-	return payload.Bytes()
+	writeMetadataJSON(tw, normalizedPath, "binary", c.Label)
+	writeCodeTarGz(tw, c.Path)
+
+	// close down the tar streams
+	closeAll(tw, gw)
 }
 
-func getCodeBytes(path string) []byte {
-	// read the compiled chaincode located at the specified path
-	// and tar it with the name "chaincode"
-	payload := bytes.NewBuffer(nil)
-	gw := gzip.NewWriter(payload)
-	tw := tar.NewWriter(gw)
-	err := tw.WriteHeader(&tar.Header{
-		Typeflag: tar.TypeReg,
-		Name:     "chaincode",
-		Mode:     0100700,
-	})
-	Expect(err).NotTo(HaveOccurred())
-	err = util.WriteFileToPackage(path, "chaincode", tw)
-	Expect(err).NotTo(HaveOccurred())
-	err = tw.Close()
-	Expect(err).NotTo(HaveOccurred())
-	err = gw.Close()
-	return payload.Bytes()
-}
-
-func writeBytesToPackage(tw *tar.Writer, name string, payload []byte) {
-	err := tw.WriteHeader(&tar.Header{
-		Name: name,
-		Size: int64(len(payload)),
-		Mode: 0100644,
-	})
-	Expect(err).NotTo(HaveOccurred())
-	_, err = tw.Write(payload)
-	Expect(err).NotTo(HaveOccurred())
-}
-
-// packageMetadata holds the path and type for a chaincode package
+// packageMetadata holds the path, type, and label for a chaincode package
 type packageMetadata struct {
 	Path  string `json:"path"`
 	Type  string `json:"type"`
 	Label string `json:"label"`
 }
 
-func toJSON(path, ccType, label string) []byte {
-	metadata := &packageMetadata{
+func writeMetadataJSON(tw *tar.Writer, path, ccType, label string) {
+	metadata, err := json.Marshal(&packageMetadata{
 		Path:  path,
 		Type:  ccType,
 		Label: label,
-	}
-	metadataBytes, err := json.Marshal(metadata)
+	})
 	Expect(err).NotTo(HaveOccurred())
-	return metadataBytes
+
+	// write it to the package as metadata.json
+	err = tw.WriteHeader(&tar.Header{
+		Name: "metadata.json",
+		Size: int64(len(metadata)),
+		Mode: 0100644,
+	})
+	_, err = tw.Write(metadata)
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func writeCodeTarGz(tw *tar.Writer, path string) {
+	// create temp file to hold code.tar.gz
+	tempfile, err := ioutil.TempFile("", "code.tar.gz")
+	Expect(err).NotTo(HaveOccurred())
+	defer os.Remove(tempfile.Name())
+
+	gzipWriter := gzip.NewWriter(tempfile)
+	tarWriter := tar.NewWriter(gzipWriter)
+
+	code, err := os.Open(path)
+	Expect(err).NotTo(HaveOccurred())
+	writeFileToTar(tarWriter, code, "chaincode")
+
+	// close down the inner tar
+	closeAll(code, tarWriter, gzipWriter)
+
+	writeFileToTar(tw, tempfile, "code.tar.gz")
+}
+
+func writeFileToTar(tw *tar.Writer, file *os.File, name string) {
+	_, err := file.Seek(0, 0)
+	Expect(err).NotTo(HaveOccurred())
+
+	fi, err := file.Stat()
+	Expect(err).NotTo(HaveOccurred())
+	header, err := tar.FileInfoHeader(fi, "")
+	Expect(err).NotTo(HaveOccurred())
+
+	header.Name = name
+	err = tw.WriteHeader(header)
+	Expect(err).NotTo(HaveOccurred())
+
+	_, err = io.Copy(tw, file)
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func closeAll(closers ...io.Closer) {
+	for _, c := range closers {
+		Expect(c.Close()).To(Succeed())
+	}
 }
