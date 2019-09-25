2def46bd172373b9a52e767c186ed8cd61c32662
Will Lahti <wtlahti@us.ibm.com>
2019-09-25 15:57:13 -0400

    Update idemix integration to use binary launcher
    FAB-14494
    
    Change-Id: I0d65c0bab0269573890a5059b395bee2eb4e0c9a
    Signed-off-by: Will Lahti <wtlahti@us.ibm.com>

--- integration/idemix/idemix_test.go
+++ integration/idemix/idemix_test.go
@@ -7,12 +7,12 @@ SPDX-License-Identifier: Apache-2.0
 package idemix
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"syscall"
 
-	"github.com/fsouza/go-dockerclient"
+	docker "github.com/fsouza/go-dockerclient"
 	"github.com/hyperledger/fabric/integration/nwo"
 	"github.com/hyperledger/fabric/integration/nwo/commands"
 	. "github.com/onsi/ginkgo"
@@ -35,17 +35,21 @@ var _ = Describe("EndToEnd", func() {
 		var err error
 		testDir, err = ioutil.TempDir("", "idemix_e2e")
 		Expect(err).NotTo(HaveOccurred())
-		fmt.Println(testDir)
 
 		client, err = docker.NewClientFromEnv()
 		Expect(err).NotTo(HaveOccurred())
 
 		chaincode = nwo.Chaincode{
-			Name:    "mycc",
-			Version: "0.0",
-			Path:    "github.com/hyperledger/fabric/integration/chaincode/simple/cmd",
-			Ctor:    `{"Args":["init","a","100","b","200"]}`,
-			Policy:  `AND ('Org1MSP.member','Org2MSP.member')`,
+			Name:            "mycc",
+			Version:         "0.0",
+			Path:            components.Build("github.com/hyperledger/fabric/integration/chaincode/module"),
+			Lang:            "binary",
+			PackageFile:     filepath.Join(testDir, "modulecc.tar.gz"),
+			Ctor:            `{"Args":["init","a","100","b","200"]}`,
+			SignaturePolicy: `AND ('Org1MSP.member','Org2MSP.member')`,
+			Sequence:        "1",
+			InitRequired:    true,
+			Label:           "my_prebuilt_chaincode",
 		}
 	})
 
@@ -61,7 +65,6 @@ var _ = Describe("EndToEnd", func() {
 	})
 
 	Describe("basic solo network with 2 orgs", func() {
-
 		BeforeEach(func() {
 			network = nwo.New(nwo.BasicSoloWithIdemix(), testDir, client, StartPort(), components)
 			network.GenerateConfigTree()
@@ -78,9 +81,10 @@ var _ = Describe("EndToEnd", func() {
 
 			By("setting up the channel")
 			network.CreateAndJoinChannel(orderer, "testchannel")
+			nwo.EnableCapabilities(network, "testchannel", "Application", "V2_0", orderer, network.Peer("Org1", "peer0"), network.Peer("Org2", "peer0"))
 
 			By("deploying the chaincode")
-			nwo.DeployChaincodeLegacy(network, "testchannel", orderer, chaincode)
+			nwo.DeployChaincode(network, "testchannel", orderer, chaincode)
 
 			By("getting the client peer by name")
 			peer := network.Peer("Org1", "peer1")
