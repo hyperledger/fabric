8196d66cb609a29c87ccba62e5c051c6d3dc8319
SamYuan <yy19902439@126.com>
2019-10-26 19:15:18 +0800

    [FAB-16948] Fabric go cid interface enhancement
    Signed-off-by: SamYuan <yy19902439@126.com>
    Change-Id: I6e6321eb778d47865c2e7ee5e537b2da1ae81298

--- core/chaincode/shim/ext/cid/cid.go
+++ core/chaincode/shim/ext/cid/cid.go
@@ -99,6 +99,12 @@ func New(stub ChaincodeStubInterface) (ClientIdentity, error) {
 
 // GetID returns a unique ID associated with the invoking identity.
 func (c *clientIdentityImpl) GetID() (string, error) {
+	// When IdeMix, c.cert is nil for x509 type
+	// Here will return "", as there is no x509 type cert for generate id value with logic below.
+	cert, _ := c.GetX509Certificate()
+	if cert == nil {
+		return "", nil
+	}
 	// The leading "x509::" distinguishes this as an X509 certificate, and
 	// the subject and issuer DNs uniquely identify the X509 certificate.
 	// The resulting ID will remain the same if the certificate is renewed.
