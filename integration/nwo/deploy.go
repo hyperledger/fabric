292ddab899c34bb1aad7ae77b8fad94f51e985e5
Chongxin Luo <Chongxin.Luo@ibm.com>
2019-10-29 15:32:22 -0400

    [FAB-16939] Fix new lifecycle chaincode inconsistency
    Fixing inconsistency between new lifecycle and old chaincode
    in integration tests.
    
    FAB-16939 #done.
    
    Change-Id: I8533e5211c0abe9de908d83915789cd6f26e65a8
    Signed-off-by: Chongxin Luo <Chongxin.Luo@ibm.com>

--- integration/nwo/deploy.go
+++ integration/nwo/deploy.go
@@ -32,11 +32,11 @@ type Chaincode struct {
 	Version             string
 	Path                string
 	Ctor                string
-	Policy              string
+	Policy              string // only used for legacy lifecycle. For new lifecycle use SignaturePolicy
 	Lang                string
 	CollectionsConfig   string // optional
 	PackageFile         string
-	PackageID           string            // if unspecified, chaincode won't be executable
+	PackageID           string            // if unspecified, chaincode won't be executable. Can use SetPackageIDFromPackageFile() to set.
 	CodeFiles           map[string]string // map from paths on the filesystem to code.tar.gz paths
 	Sequence            string
 	EndorsementPlugin   string
