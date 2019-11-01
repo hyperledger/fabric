a618203128005a5b180407ae2e694a207f4b321c
Jason Yellick <jyellick@us.ibm.com>
2019-11-01 14:12:18 -0400

    FAB-16544 Add orderer overrides to config
    This CR simply adds a new section parseable to the peer config for
    overriding orderer endpoints found in the channel config.  It only adds
    the config section and parsing, but does not attempt to utilize the new
    config.
    
    Signed-off-by: Jason Yellick <jyellick@us.ibm.com>
    Change-Id: Ibbd47540ea1325d3a7eb2ab25d5e7b88e394d8f3

--- core/deliverservice/config.go
+++ core/deliverservice/config.go
@@ -7,11 +7,13 @@ SPDX-License-Identifier: Apache-2.0
 package deliverservice
 
 import (
+	"crypto/x509"
 	"io/ioutil"
 	"time"
 
 	"github.com/hyperledger/fabric/core/comm"
 	"github.com/hyperledger/fabric/core/config"
+	"github.com/hyperledger/fabric/internal/pkg/peer/orderers"
 
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
@@ -38,6 +40,16 @@ type DeliverServiceConfig struct {
 	KeepaliveOptions comm.KeepaliveOptions
 	// SecOpts provides the TLS info for connections
 	SecOpts comm.SecureOptions
+
+	// OrdererEndpointOverrides is a map of orderer addresses which should be
+	// re-mapped to a different orderer endpoint.
+	OrdererEndpointOverrides map[string]*orderers.Endpoint
+}
+
+type AddressOverride struct {
+	From        string `mapstructure:"from"`
+	To          string `mapstructure:"to"`
+	CACertsFile string `mapstructure:"caCertsFile"`
 }
 
 // GlobalConfig obtains a set of configuration from viper, build and returns the config struct.
@@ -45,6 +57,41 @@ func GlobalConfig() *DeliverServiceConfig {
 	c := &DeliverServiceConfig{}
 	c.loadDeliverServiceConfig()
 	return c
+}
+
+func LoadOverridesMap() (map[string]*orderers.Endpoint, error) {
+	var overrides []AddressOverride
+	err := viper.UnmarshalKey("peer.deliveryclient.addressOverrides", &overrides)
+	if err != nil {
+		return nil, errors.WithMessage(err, "could not unmarshal peer.deliveryclient.addressOverrides")
+	}
+
+	if len(overrides) == 0 {
+		return nil, nil
+	}
+
+	overrideMap := map[string]*orderers.Endpoint{}
+	for _, override := range overrides {
+		certPool := x509.NewCertPool()
+		if override.CACertsFile != "" {
+			pem, err := ioutil.ReadFile(override.CACertsFile)
+			if err != nil {
+				logger.Warningf("could not read file '%s' specified for caCertsFile of orderer endpoint override from '%s' to '%s': %s", override.CACertsFile, override.From, override.To, err)
+				continue
+			}
+			success := certPool.AppendCertsFromPEM(pem)
+			if !success {
+				logger.Warningf("Attempted to create a cert pool for override of orderer address '%s' to '%s' but did not find any valid certs in '%s'", override.From, override.To, override.CACertsFile)
+				continue
+			}
+		}
+		overrideMap[override.From] = &orderers.Endpoint{
+			Address:  override.To,
+			CertPool: certPool,
+		}
+	}
+
+	return overrideMap, nil
 }
 
 func (c *DeliverServiceConfig) loadDeliverServiceConfig() {
@@ -100,4 +147,11 @@ func (c *DeliverServiceConfig) loadDeliverServiceConfig() {
 		}
 		c.SecOpts.Certificate = certPEM
 	}
+
+	overridesMap, err := LoadOverridesMap()
+	if err != nil {
+		panic(err)
+	}
+
+	c.OrdererEndpointOverrides = overridesMap
 }
