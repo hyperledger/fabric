96fefd0e6b906ad513f8e37afc721a911c61486c
Sajan Jaiswal <sjnjaiswal@gmail.com>
2019-12-09 12:19:50 +0530

    FAB-17222 updating go version to go1.13.4 in vagrant dev_env
    Signed-off-by: Sajan Jaiswal <sjnjaiswal@gmail.com>

--- vagrant/golang.sh
+++ vagrant/golang.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 GOROOT='/opt/go'
-GO_VERSION=1.13.3
+GO_VERSION=1.13.4
 
 # ----------------------------------------------------------------
 # Install Golang
