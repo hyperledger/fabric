43d4b186ce8b6d39c90af57b4b9efd5c2a23617a
Senthil Nathan N <cendhu@gmail.com>
2019-12-17 16:20:22 +0530

    define couchDB connection pool size FAB-17277 fix too many TCP connections between peer and CouchDB
    Increase the connection pool size per host from 2 to 2000.
    Also, increase the total connection pool size to 2000.
    
    Signed-off-by: Senthil Nathan N <cendhu@gmail.com>
    (cherry picked from commit 28c6efdaaffd1d7e17859b0ae98e0bb9594b3eaa)

--- core/ledger/util/couchdb/couchdbutil.go
+++ core/ledger/util/couchdb/couchdbutil.go
@@ -8,6 +8,7 @@ package couchdb
 import (
 	"bytes"
 	"encoding/hex"
+	"net"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -48,8 +49,21 @@ func CreateCouchInstance(couchDBConnectURL, id, pw string, maxRetries,
 	// and for efficiency should only be created once and re-used.
 	client := &http.Client{Timeout: couchConf.RequestTimeout}
 
-	transport := &http.Transport{Proxy: http.ProxyFromEnvironment}
-	transport.DisableCompression = false
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   5 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          2000,
+		MaxIdleConnsPerHost:   2000,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
+
 	client.Transport = transport
 
 	//Create the CouchDB instance
