5eaae3a9940d576e99b2202b243e56fc75eda070
Matthew Sykes <sykesmat@us.ibm.com>
2020-02-10 10:56:12 -0500

    Explicitly enumerate orderer and peer metrics
    Prior to this change, the documentation did not identify metrics that
    were specific to the orderer, specific to the peer, or common across the
    two. This change improves the situation by providing separate tables for
    the orderer and peer.
    
    FAB-17422
    
    Change-Id: Ia5e6ceb3ebfa4c3a26670367962fe2396b632eaa
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- scripts/metrics_doc.sh
+++ scripts/metrics_doc.sh
@@ -5,16 +5,84 @@
 # SPDX-License-Identifier: Apache-2.0
 
 fabric_dir="$(cd "$(dirname "$0")/.." && pwd)"
-metrics_template="${fabric_dir}/docs/source/metrics_reference.rst.tmpl"
 metrics_doc="${fabric_dir}/docs/source/metrics_reference.rst"
 
-gendoc_command="go run github.com/hyperledger/fabric/common/metrics/cmd/gendoc -template ${metrics_template}"
+generate_doc() {
+    local gendoc_command="go run github.com/hyperledger/fabric/common/metrics/cmd/gendoc"
+    local orderer_prom
+    local orderer_statsd
+    local peer_prom
+    local peer_statsd
+
+    local orderer_deps=()
+    while IFS= read -r pkg; do orderer_deps+=("$pkg"); done < <(go list -deps github.com/hyperledger/fabric/orderer | sort -u | grep hyperledger)
+    orderer_prom="$($gendoc_command -template <(echo '{{PrometheusTable}}') "${orderer_deps[@]}")"
+    orderer_statsd="$($gendoc_command -template <(echo '{{StatsdTable}}') "${orderer_deps[@]}")"
+
+    local peer_deps=()
+    while IFS= read -r pkg; do peer_deps+=("$pkg"); done < <(go list -deps github.com/hyperledger/fabric/peer | sort -u | grep hyperledger)
+    peer_prom="$($gendoc_command -template <(echo '{{PrometheusTable}}') "${peer_deps[@]}")"
+    peer_statsd="$($gendoc_command -template <(echo '{{StatsdTable}}') "${peer_deps[@]}")"
+
+cat <<eof
+Metrics Reference
+=================
+
+Orderer Metrics
+---------------
+
+Prometheus
+~~~~~~~~~~
+
+The following orderer metrics are exported for consumption by Prometheus.
+
+${orderer_prom}
+
+StatsD
+~~~~~~
+
+The following orderer metrics are emitted for consumption by StatsD. The
+\`\`%{variable_name}\`\` nomenclature represents segments that vary based on
+context.
+
+For example, \`\`%{channel}\`\` will be replaced with the name of the channel
+associated with the metric.
+
+${orderer_statsd}
+
+Peer Metrics
+------------
+
+Prometheus
+~~~~~~~~~~
+
+The following peer metrics are exported for consumption by Prometheus.
+
+${peer_prom}
+
+StatsD
+~~~~~~
+
+The following peer metrics are emitted for consumption by StatsD. The
+\`\`%{variable_name}\`\` nomenclature represents segments that vary based on
+context.
+
+For example, \`\`%{channel}\`\` will be replaced with the name of the channel
+associated with the metric.
+
+${peer_statsd}
+
+.. Licensed under Creative Commons Attribution 4.0 International License
+   https://creativecommons.org/licenses/by/4.0/
+eof
+}
+
 
 case "$1" in
     # check if the metrics documentation is up to date with the metrics
     # options in the tree
     "check")
-        if [ -n "$(diff -u <(cd ${fabric_dir} && ${gendoc_command}) ${metrics_doc})" ]; then
+        if [ -n "$(diff -u <(generate_doc) "${metrics_doc}")" ]; then
             echo "The Fabric metrics reference documentation is out of date."
             echo "Please run '$0 generate' to update the documentation."
             exit 1
@@ -23,7 +91,7 @@ case "$1" in
 
     # generate the metrics documentation
     "generate")
-         (cd "${fabric_dir}" && ${gendoc_command} > ${metrics_doc})
+         generate_doc > "${metrics_doc}"
         ;;
 
     *)
