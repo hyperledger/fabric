4a2a6717417f7b25ec8b7726637329333bd38b8e
Artem Barger <bartem@il.ibm.com>
2020-02-24 00:29:06 +0200

    FAB-17524 rm custom CollectionAccessPolicy mock
    This commit removes custom CollectionAccessPolicy mock from
    gossip/privdata package and replace with generate ones.
    
    Signed-off-by: Artem Barger <bartem@il.ibm.com>

--- gossip/privdata/distributor_test.go
+++ gossip/privdata/distributor_test.go
@@ -22,56 +22,14 @@ import (
 	gossip2 "github.com/hyperledger/fabric/gossip/gossip"
 	"github.com/hyperledger/fabric/gossip/metrics"
 	"github.com/hyperledger/fabric/gossip/metrics/mocks"
+	mocks2 "github.com/hyperledger/fabric/gossip/privdata/mocks"
 	"github.com/hyperledger/fabric/gossip/protoext"
 	"github.com/hyperledger/fabric/protoutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-type collectionAccessFactoryMock struct {
-	mock.Mock
-}
-
-func (mock *collectionAccessFactoryMock) AccessPolicy(config *peer.CollectionConfig, chainID string) (privdata.CollectionAccessPolicy, error) {
-	res := mock.Called(config, chainID)
-	return res.Get(0).(privdata.CollectionAccessPolicy), res.Error(1)
-}
-
-type collectionAccessPolicyMock struct {
-	mock.Mock
-}
-
-func (mock *collectionAccessPolicyMock) AccessFilter() privdata.Filter {
-	args := mock.Called()
-	return args.Get(0).(privdata.Filter)
-}
-
-func (mock *collectionAccessPolicyMock) RequiredPeerCount() int {
-	args := mock.Called()
-	return args.Int(0)
-}
-
-func (mock *collectionAccessPolicyMock) MaximumPeerCount() int {
-	args := mock.Called()
-	return args.Int(0)
-}
-
-func (mock *collectionAccessPolicyMock) MemberOrgs() map[string]struct{} {
-	args := mock.Called()
-	return args.Get(0).(map[string]struct{})
-}
-
-func (mock *collectionAccessPolicyMock) IsMemberOnlyRead() bool {
-	args := mock.Called()
-	return args.Get(0).(bool)
-}
-
-func (mock *collectionAccessPolicyMock) IsMemberOnlyWrite() bool {
-	args := mock.Called()
-	return args.Get(0).(bool)
-}
-
-func (mock *collectionAccessPolicyMock) Setup(requiredPeerCount int, maxPeerCount int,
+func Setup(mock *mocks2.CollectionAccessPolicy, requiredPeerCount int, maxPeerCount int,
 	accessFilter privdata.Filter, orgs map[string]struct{}, memberOnlyRead bool) {
 	mock.On("AccessFilter").Return(accessFilter)
 	mock.On("RequiredPeerCount").Return(requiredPeerCount)
@@ -154,7 +112,7 @@ func TestDistributor(t *testing.T) {
 			SendCriteria:   sendCriteria,
 		}
 	}).Return(nil)
-	accessFactoryMock := &collectionAccessFactoryMock{}
+	accessFactoryMock := &mocks2.CollectionAccessFactory{}
 	c1ColConfig := &peer.CollectionConfig{
 		Payload: &peer.CollectionConfig_StaticCollectionConfig{
 			StaticCollectionConfig: &peer.StaticCollectionConfig{
@@ -175,8 +133,8 @@ func TestDistributor(t *testing.T) {
 		},
 	}
 
-	policyMock := &collectionAccessPolicyMock{}
-	policyMock.Setup(1, 2, func(_ protoutil.SignedData) bool {
+	policyMock := &mocks2.CollectionAccessPolicy{}
+	Setup(policyMock, 1, 2, func(_ protoutil.SignedData) bool {
 		return true
 	}, map[string]struct{}{
 		"org1": {},
