23ff89ae48bf719bc7c0f6baba3ad2f4784ae6cc
Brett Logan <brett.t.logan@ibm.com>
2020-02-24 15:41:40 -0500

    [FAB-17530] Update Go Version to 1.13.8
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- vagrant/golang.sh
+++ vagrant/golang.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 GOROOT='/opt/go'
-GO_VERSION=1.13.4
+GO_VERSION=1.13.8
 
 # ----------------------------------------------------------------
 # Install Golang
