48910e94f985731d1733fb6a0568e417724e0c12
David Enyeart <enyeart@us.ibm.com>
2020-02-25 04:06:14 -0500

    [FAB-17431] Decouple javaenv from Fabric version
    By default Fabric was configured to use the following javaenv image for java chaincode:
    runtime: $(DOCKER_NS)/fabric-javaenv:$(ARCH)-$(PROJECT_VERSION)
    
    Since javaenv versioning may deviate from Fabric versioning,
    it is important to decouple the versions. This commit updates
    the javaenv reference to use the latest two digit version:
    runtime: $(DOCKER_NS)/fabric-javaenv:$(TWO_DIGIT_VERSION)
    
    For example, Fabric v1.4.5 would utilize fabric-javaenv:1.4.
    
    This is a backport of the two digit change in master, applied to javaenv.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- core/container/util/dockerutil.go
+++ core/container/util/dockerutil.go
@@ -35,11 +35,21 @@ func ParseDockerfileTemplate(template string) string {
 	r := strings.NewReplacer(
 		"$(ARCH)", runtime.GOARCH,
 		"$(PROJECT_VERSION)", metadata.Version,
+		"$(TWO_DIGIT_VERSION)", twoDigitVersion(metadata.Version),
 		"$(BASE_VERSION)", metadata.BaseVersion,
 		"$(DOCKER_NS)", metadata.DockerNamespace,
 		"$(BASE_DOCKER_NS)", metadata.BaseDockerNamespace)
 
 	return r.Replace(template)
+}
+
+// twoDigitVersion truncates a 3 digit version (e.g. 2.0.0) to a 2 digit version (e.g. 2.0),
+// If version does not include dots (e.g. latest), just return the passed version
+func twoDigitVersion(version string) string {
+	if strings.LastIndex(version, ".") < 0 {
+		return version
+	}
+	return version[0:strings.LastIndex(version, ".")]
 }
 
 func GetDockerfileFromConfig(path string) string {
