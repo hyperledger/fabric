a78e2da7c0873c35a7dd6f44980e1bbde6551de6
Danny Cao <dcao@us.ibm.com>
2020-02-27 16:12:07 -0500

    [FAB-17543] Support adding anchor peers to existing config
    Signed-off-by: Danny Cao <dcao@us.ibm.com>
    Signed-off-by: Tiffany Harris <tiffany.harris@ibm.com>

--- pkg/config/application_test.go
+++ pkg/config/application_test.go
@@ -129,6 +129,349 @@ func TestNewApplicationGroupSkipAsForeign(t *testing.T) {
 	}))
 }
 
+func TestAddAnchorPeer(t *testing.T) {
+	t.Parallel()
+
+	gt := NewGomegaWithT(t)
+
+	baseApplicationConf := baseApplication()
+
+	applicationGroup, err := newApplicationGroup(baseApplicationConf)
+	gt.Expect(err).NotTo(HaveOccurred())
+
+	config := &cb.Config{
+		ChannelGroup: &cb.ConfigGroup{
+			Groups: map[string]*cb.ConfigGroup{
+				ApplicationGroupKey: applicationGroup,
+			},
+		},
+	}
+
+	newOrg1AnchorPeer := &AnchorPeer{
+		Host: "host3",
+		Port: 123,
+	}
+
+	newOrg2AnchorPeer := &AnchorPeer{
+		Host: "host4",
+		Port: 123,
+	}
+
+	expectedUpdatedConfigJSON := `
+{
+	"channel_group": {
+		"groups": {
+			"Application": {
+				"groups": {
+					"Org1": {
+						"groups": {},
+						"mod_policy": "Admins",
+						"policies": {
+							"Admins": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Admins"
+									}
+								},
+								"version": "0"
+							},
+							"Endorsement": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Endorsement"
+									}
+								},
+								"version": "0"
+							},
+							"LifecycleEndorsement": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Endorsement"
+									}
+								},
+								"version": "0"
+							},
+							"Readers": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "ANY",
+									"sub_policy": "Readers"
+									}
+								},
+								"version": "0"
+							},
+							"Writers": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "ANY",
+									"sub_policy": "Writers"
+									}
+								},
+								"version": "0"
+							}
+						},
+						"values": {
+							"AnchorPeers": {
+								"mod_policy": "Admins",
+								"value": {
+									"anchor_peers": [
+									{
+									"host": "host1",
+									"port": 123
+									},
+									{
+									"host": "host3",
+									"port": 123
+									}
+									]
+								},
+								"version": "0"
+							},
+							"MSP": {
+								"mod_policy": "Admins",
+								"value": {
+									"config": null,
+									"type": 0
+								},
+								"version": "0"
+							}
+						},
+						"version": "0"
+					},
+					"Org2": {
+						"groups": {},
+						"mod_policy": "Admins",
+						"policies": {
+							"Admins": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Admins"
+									}
+								},
+								"version": "0"
+							},
+							"Endorsement": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Endorsement"
+									}
+								},
+								"version": "0"
+							},
+							"LifecycleEndorsement": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Endorsement"
+									}
+								},
+								"version": "0"
+							},
+							"Readers": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "ANY",
+									"sub_policy": "Readers"
+									}
+								},
+								"version": "0"
+							},
+							"Writers": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "ANY",
+									"sub_policy": "Writers"
+									}
+								},
+								"version": "0"
+							}
+						},
+						"values": {
+							"AnchorPeers": {
+								"mod_policy": "Admins",
+								"value": {
+									"anchor_peers": [
+									{
+									"host": "host2",
+									"port": 123
+									},
+									{
+									"host": "host4",
+									"port": 123
+									}
+									]
+								},
+								"version": "0"
+							},
+							"MSP": {
+								"mod_policy": "Admins",
+								"value": {
+									"config": null,
+									"type": 0
+								},
+								"version": "0"
+							}
+						},
+						"version": "0"
+					}
+				},
+				"mod_policy": "Admins",
+				"policies": {
+					"Admins": {
+						"mod_policy": "Admins",
+						"policy": {
+							"type": 3,
+							"value": {
+								"rule": "MAJORITY",
+								"sub_policy": "Admins"
+							}
+						},
+						"version": "0"
+					},
+					"Readers": {
+						"mod_policy": "Admins",
+						"policy": {
+							"type": 3,
+							"value": {
+								"rule": "ANY",
+								"sub_policy": "Readers"
+							}
+						},
+						"version": "0"
+					},
+					"Writers": {
+						"mod_policy": "Admins",
+						"policy": {
+							"type": 3,
+							"value": {
+								"rule": "ANY",
+								"sub_policy": "Writers"
+							}
+						},
+						"version": "0"
+					}
+				},
+				"values": {
+					"ACLs": {
+						"mod_policy": "Admins",
+						"value": {
+							"acls": {
+								"acl1": {
+									"policy_ref": "hi"
+								}
+							}
+						},
+						"version": "0"
+					},
+					"Capabilities": {
+						"mod_policy": "Admins",
+						"value": {
+							"capabilities": {
+								"V1_3": {}
+							}
+						},
+						"version": "0"
+					}
+				},
+				"version": "0"
+			}
+		},
+		"mod_policy": "",
+		"policies": {},
+		"values": {},
+		"version": "0"
+	},
+	"sequence": "0"
+}
+	`
+
+	expectedUpdatedConfig := &cb.Config{}
+
+	err = protolator.DeepUnmarshalJSON(bytes.NewBufferString(expectedUpdatedConfigJSON), expectedUpdatedConfig)
+	gt.Expect(err).ToNot(HaveOccurred())
+
+	err = AddAnchorPeer(config, "Org1", newOrg1AnchorPeer)
+	gt.Expect(err).NotTo(HaveOccurred())
+
+	err = AddAnchorPeer(config, "Org2", newOrg2AnchorPeer)
+	gt.Expect(err).NotTo(HaveOccurred())
+
+	gt.Expect(proto.Equal(config, expectedUpdatedConfig)).To(BeTrue())
+}
+
+func TestAddAnchorPeerFailure(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		testName      string
+		orgName       string
+		newAnchorPeer *AnchorPeer
+		expectedErr   string
+	}{
+		{
+			testName:      "When the org for the application does not exist",
+			orgName:       "BadOrg",
+			newAnchorPeer: &AnchorPeer{Host: "host3", Port: 123},
+			expectedErr:   "application org BadOrg does not exist in channel config",
+		},
+		{
+			testName:      "When the anchor peer being added already exists in the org",
+			orgName:       "Org1",
+			newAnchorPeer: &AnchorPeer{Host: "host1", Port: 123},
+			expectedErr:   "application org Org1 already contains anchor peer endpoint host1:123",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.testName, func(t *testing.T) {
+			t.Parallel()
+
+			gt := NewGomegaWithT(t)
+
+			baseApplicationConf := baseApplication()
+
+			applicationGroup, err := newApplicationGroup(baseApplicationConf)
+			gt.Expect(err).NotTo(HaveOccurred())
+
+			config := &cb.Config{
+				ChannelGroup: &cb.ConfigGroup{
+					Groups: map[string]*cb.ConfigGroup{
+						ApplicationGroupKey: applicationGroup,
+					},
+				},
+			}
+			err = AddAnchorPeer(config, tt.orgName, tt.newAnchorPeer)
+			gt.Expect(err).To(MatchError(tt.expectedErr))
+		})
+	}
+}
+
 func TestRemoveAnchorPeer(t *testing.T) {
 	t.Parallel()
 
@@ -415,7 +758,7 @@ func TestRemoveAnchorPeerFailure(t *testing.T) {
 			testName:           "When the anchor peer being removed doesn't exist in the org",
 			orgName:            "Org1",
 			anchorPeerToRemove: &AnchorPeer{Host: "host2", Port: 123},
-			expectedErr:        "could not find anchor peer with host: host2, port: 123 in existing anchor peers",
+			expectedErr:        "could not find anchor peer host2:123 in Org1's anchor peer endpoints",
 		},
 	}
 
