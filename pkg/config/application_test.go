50f50ea9ab7337a0faa1096cfa5869abbfc54ae8
Danny Cao <dcao@us.ibm.com>
2020-02-28 13:34:25 -0500

    [FAB-17544] Support removing anchor peers from an existing config
    Signed-off-by: Tiffany Harris <tiffany.harris@ibm.com>
    Signed-off-by: Danny Cao <dcao@us.ibm.com>

--- pkg/config/application_test.go
+++ pkg/config/application_test.go
@@ -7,11 +7,14 @@ SPDX-License-Identifier: Apache-2.0
 package config
 
 import (
+	"bytes"
 	"errors"
 	"testing"
 
+	"github.com/golang/protobuf/proto"
 	cb "github.com/hyperledger/fabric-protos-go/common"
 	mb "github.com/hyperledger/fabric-protos-go/msp"
+	"github.com/hyperledger/fabric/common/tools/protolator"
 	. "github.com/onsi/gomega"
 )
 
@@ -132,6 +135,322 @@ func TestNewApplicationGroupSkipAsForeign(t *testing.T) {
 	}))
 }
 
+func TestRemoveAnchorPeer(t *testing.T) {
+	t.Parallel()
+
+	gt := NewGomegaWithT(t)
+
+	baseApplicationConf := baseApplication()
+
+	applicationGroup, err := NewApplicationGroup(baseApplicationConf, &mb.MSPConfig{})
+	gt.Expect(err).NotTo(HaveOccurred())
+
+	config := &cb.Config{
+		ChannelGroup: &cb.ConfigGroup{
+			Groups: map[string]*cb.ConfigGroup{
+				"Application": applicationGroup,
+			},
+		},
+	}
+
+	expectedUpdatedConfigJSON := `
+{
+	"channel_group": {
+		"groups": {
+			"Application": {
+				"groups": {
+					"Org1": {
+						"groups": {},
+						"mod_policy": "Admins",
+						"policies": {
+							"Admins": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Admins"
+									}
+								},
+								"version": "0"
+							},
+							"Endorsement": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Endorsement"
+									}
+								},
+								"version": "0"
+							},
+							"LifecycleEndorsement": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Endorsement"
+									}
+								},
+								"version": "0"
+							},
+							"Readers": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "ANY",
+									"sub_policy": "Readers"
+									}
+								},
+								"version": "0"
+							},
+							"Writers": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "ANY",
+									"sub_policy": "Writers"
+									}
+								},
+								"version": "0"
+							}
+						},
+						"values": {
+							"AnchorPeers": {
+								"mod_policy": "Admins",
+								"value": {
+									"anchor_peers": []
+								},
+								"version": "0"
+							},
+							"MSP": {
+								"mod_policy": "Admins",
+								"value": {
+									"config": null,
+									"type": 0
+								},
+								"version": "0"
+							}
+						},
+						"version": "0"
+					},
+					"Org2": {
+						"groups": {},
+						"mod_policy": "Admins",
+						"policies": {
+							"Admins": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Admins"
+									}
+								},
+								"version": "0"
+							},
+							"Endorsement": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Endorsement"
+									}
+								},
+								"version": "0"
+							},
+							"LifecycleEndorsement": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "MAJORITY",
+									"sub_policy": "Endorsement"
+									}
+								},
+								"version": "0"
+							},
+							"Readers": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "ANY",
+									"sub_policy": "Readers"
+									}
+								},
+								"version": "0"
+							},
+							"Writers": {
+								"mod_policy": "Admins",
+								"policy": {
+									"type": 3,
+									"value": {
+									"rule": "ANY",
+									"sub_policy": "Writers"
+									}
+								},
+								"version": "0"
+							}
+						},
+						"values": {
+							"AnchorPeers": {
+								"mod_policy": "Admins",
+								"value": {
+									"anchor_peers": []
+								},
+								"version": "0"
+							},
+							"MSP": {
+								"mod_policy": "Admins",
+								"value": {
+									"config": null,
+									"type": 0
+								},
+								"version": "0"
+							}
+						},
+						"version": "0"
+					}
+				},
+				"mod_policy": "Admins",
+				"policies": {
+					"Admins": {
+						"mod_policy": "Admins",
+						"policy": {
+							"type": 3,
+							"value": {
+								"rule": "MAJORITY",
+								"sub_policy": "Admins"
+							}
+						},
+						"version": "0"
+					},
+					"Readers": {
+						"mod_policy": "Admins",
+						"policy": {
+							"type": 3,
+							"value": {
+								"rule": "ANY",
+								"sub_policy": "Readers"
+							}
+						},
+						"version": "0"
+					},
+					"Writers": {
+						"mod_policy": "Admins",
+						"policy": {
+							"type": 3,
+							"value": {
+								"rule": "ANY",
+								"sub_policy": "Writers"
+							}
+						},
+						"version": "0"
+					}
+				},
+				"values": {
+					"ACLs": {
+						"mod_policy": "Admins",
+						"value": {
+							"acls": {
+								"acl1": {
+									"policy_ref": "hi"
+								}
+							}
+						},
+						"version": "0"
+					},
+					"Capabilities": {
+						"mod_policy": "Admins",
+						"value": {
+							"capabilities": {
+								"V1_3": {}
+							}
+						},
+						"version": "0"
+					}
+				},
+				"version": "0"
+			}
+		},
+		"mod_policy": "",
+		"policies": {},
+		"values": {},
+		"version": "0"
+	},
+	"sequence": "0"
+}
+	`
+
+	expectedUpdatedConfig := &cb.Config{}
+
+	err = protolator.DeepUnmarshalJSON(bytes.NewBufferString(expectedUpdatedConfigJSON), expectedUpdatedConfig)
+	gt.Expect(err).ToNot(HaveOccurred())
+
+	err = RemoveAnchorPeer(config, "Org1", baseApplicationConf.Organizations[0].AnchorPeers[0])
+	gt.Expect(err).NotTo(HaveOccurred())
+
+	err = RemoveAnchorPeer(config, "Org2", baseApplicationConf.Organizations[1].AnchorPeers[0])
+	gt.Expect(err).NotTo(HaveOccurred())
+
+	gt.Expect(proto.Equal(config, expectedUpdatedConfig)).To(BeTrue())
+}
+
+func TestRemoveAnchorPeerFailure(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		testName           string
+		orgName            string
+		anchorPeerToRemove *AnchorPeer
+		expectedErr        string
+	}{
+		{
+			testName:           "When the org for the application does not exist",
+			orgName:            "BadOrg",
+			anchorPeerToRemove: &AnchorPeer{Host: "host1", Port: 123},
+			expectedErr:        "application org BadOrg does not exist in channel config",
+		},
+		{
+			testName:           "When the anchor peer being removed doesn't exist in the org",
+			orgName:            "Org1",
+			anchorPeerToRemove: &AnchorPeer{Host: "host2", Port: 123},
+			expectedErr:        "could not find anchor peer with host: host2, port: 123 in existing anchor peers",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.testName, func(t *testing.T) {
+			t.Parallel()
+
+			gt := NewGomegaWithT(t)
+
+			baseApplicationConf := baseApplication()
+
+			applicationGroup, err := NewApplicationGroup(baseApplicationConf, &mb.MSPConfig{})
+			gt.Expect(err).NotTo(HaveOccurred())
+
+			config := &cb.Config{
+				ChannelGroup: &cb.ConfigGroup{
+					Groups: map[string]*cb.ConfigGroup{
+						"Application": applicationGroup,
+					},
+				},
+			}
+
+			err = RemoveAnchorPeer(config, tt.orgName, tt.anchorPeerToRemove)
+			gt.Expect(err).To(MatchError(tt.expectedErr))
+		})
+	}
+}
+
 func baseApplication() *Application {
 	return &Application{
 		Policies: standardPolicies(),
@@ -149,7 +468,7 @@ func baseApplication() *Application {
 				ID:       "Org2MSP",
 				Policies: applicationOrgStandardPolicies(),
 				AnchorPeers: []*AnchorPeer{
-					{Host: "host1", Port: 123},
+					{Host: "host2", Port: 123},
 				},
 			},
 		},
