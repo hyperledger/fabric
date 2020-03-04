7a5dbb1b78ae4435db4b9a592ce93f596a44b97f
Will Lahti <wtlahti@us.ibm.com>
2020-03-04 11:09:32 -0500

    Revoke certificate from application org MSP
    And simplify base application channel config for MSP tests.
    
    FAB-17545
    
    Signed-off-by: Will Lahti <wtlahti@us.ibm.com>
    Signed-off-by: Chongxin Luo <Chongxin.Luo@ibm.com>

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -22,7 +22,7 @@ func TestSignConfigUpdate(t *testing.T) {
 
 	gt := NewGomegaWithT(t)
 
-	cert, privateKey := generateCertAndPrivateKey()
+	cert, privateKey := generateCACertAndPrivateKey("org1.example.com")
 	signingIdentity := SigningIdentity{
 		Certificate: cert,
 		PrivateKey:  privateKey,
@@ -405,7 +405,7 @@ func TestCreateSignedConfigUpdateEnvelope(t *testing.T) {
 	gt := NewGomegaWithT(t)
 
 	// create signingIdentity
-	cert, privateKey := generateCertAndPrivateKey()
+	cert, privateKey := generateCACertAndPrivateKey("org1.example.com")
 	signingIdentity := SigningIdentity{
 		Certificate: cert,
 		PrivateKey:  privateKey,
@@ -446,7 +446,7 @@ func TestCreateSignedConfigUpdateEnvelopeFailures(t *testing.T) {
 	gt := NewGomegaWithT(t)
 
 	// create signingIdentity
-	cert, privateKey := generateCertAndPrivateKey()
+	cert, privateKey := generateCACertAndPrivateKey("org1.example.com")
 	signingIdentity := SigningIdentity{
 		Certificate: cert,
 		PrivateKey:  privateKey,
@@ -650,7 +650,9 @@ func ordererStandardPolicies() map[string]Policy {
 	return policies
 }
 
-func baseChannelGroup() (*cb.ConfigGroup, error) {
+// baseApplicationChannelGroup creates a channel config group
+// that only contains an Application group.
+func baseApplicationChannelGroup() (*cb.ConfigGroup, error) {
 	channelGroup := newConfigGroup()
 
 	application := baseApplication()
@@ -667,14 +669,7 @@ func baseChannelGroup() (*cb.ConfigGroup, error) {
 		applicationGroup.Groups[org.Name] = orgGroup
 	}
 
-	orderer := baseOrderer()
-	ordererGroup, err := newOrdererGroup(orderer)
-	if err != nil {
-		return nil, err
-	}
-
 	channelGroup.Groups[ApplicationGroupKey] = applicationGroup
-	channelGroup.Groups[OrdererGroupKey] = ordererGroup
 
 	return channelGroup, nil
 }
