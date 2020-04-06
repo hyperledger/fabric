a9f4ddc98ff3384ad0aaad5a93775307a9116062
Morgan Bauer <mbauer@us.ibm.com>
2020-04-06 11:25:52 -0700

    check-deps replacement
    Change-Id: Iad450b21a63156a73b4299fbc22745cd2fe96e1f
    Signed-off-by: Morgan Bauer <mbauer@us.ibm.com>

--- scripts/check_deps.sh
+++ scripts/check_deps.sh
@@ -4,4 +4,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-echo "Dependency checking implementation is TBD..."
+go mod verify
+# check vendor by restoring it from module definition and checking if
+# there are any differences from the code which is committed
+go mod vendor
+if [ -n "$(git diff HEAD --stat -- vendor/)" ] ;
+then
+    echo "vendor modified"
+    git diff -- vendor/
+    exit 1
+fi
+
