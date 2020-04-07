cdbb275ac77c88d2af8bbdd07c55caed85743e61
Senthil Nathan N <cendhu@users.noreply.github.com>
2020-04-07 20:18:21 +0530

    rm proto msg folder present in stateleveldb (#996)
    In goleveldb, we store a KV pair of format {key, [value, version, metadata]}
    for every state managed by the blockchain. As the [value, version, metadata] must
    be passed as single []byte (as per goleveldb requirements), we use a proto to
    serialize the combined values.
    
    Currently, we have a separate package to have this proto. This is unjustifiable.
    Hence, we move this proto to stateleveldb package itself and change the proto
    message to DBValue from VersionedValueProto for better readability.
    
    Signed-off-by: senthil <cendhu@gmail.com>

--- core/ledger/kvledger/txmgmt/statedb/stateleveldb/value_encoding.go
+++ core/ledger/kvledger/txmgmt/statedb/stateleveldb/value_encoding.go
@@ -10,33 +10,32 @@ import (
 	proto "github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric/core/ledger/internal/version"
 	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/statedb"
-	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/statedb/stateleveldb/msgs"
 )
 
 // encodeValue encodes the value, version, and metadata
 func encodeValue(v *statedb.VersionedValue) ([]byte, error) {
 	return proto.Marshal(
-		&msgs.VersionedValueProto{
-			VersionBytes: v.Version.ToBytes(),
-			Value:        v.Value,
-			Metadata:     v.Metadata,
+		&DBValue{
+			Version:  v.Version.ToBytes(),
+			Value:    v.Value,
+			Metadata: v.Metadata,
 		},
 	)
 }
 
 // decodeValue decodes the statedb value bytes
 func decodeValue(encodedValue []byte) (*statedb.VersionedValue, error) {
-	msg := &msgs.VersionedValueProto{}
-	err := proto.Unmarshal(encodedValue, msg)
+	dbValue := &DBValue{}
+	err := proto.Unmarshal(encodedValue, dbValue)
 	if err != nil {
 		return nil, err
 	}
-	ver, _, err := version.NewHeightFromBytes(msg.VersionBytes)
+	ver, _, err := version.NewHeightFromBytes(dbValue.Version)
 	if err != nil {
 		return nil, err
 	}
-	val := msg.Value
-	metadata := msg.Metadata
+	val := dbValue.Value
+	metadata := dbValue.Metadata
 	// protobuf always makes an empty byte array as nil
 	if val == nil {
 		val = []byte{}
