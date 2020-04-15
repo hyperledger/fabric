1bdf975374ee93001e5818b3b33089f0be310d93
David Enyeart <enyeart@us.ibm.com>
2020-04-15 16:16:27 -0400

    Fabric v2.1.0 release commit
    Final doc updates for v2.1.0.
    Change log.
    Remove releases from readme, point to GitHub releases page instead.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,7 +6,7 @@
 #
 
 # if version not passed in, default to latest released version
-VERSION=2.0.0
+VERSION=2.1.0
 # if ca version not passed in, default to latest released version
 CA_VERSION=1.4.6
 # current version of thirdparty images (couchdb, kafka and zookeeper) released
@@ -23,8 +23,8 @@ printHelp() {
     echo "-s : bypass fabric-samples repo clone"
     echo "-b : bypass download of platform-specific binaries"
     echo
-    echo "e.g. bootstrap.sh 2.0.0 1.4.6 0.4.18 -s"
-    echo "would download docker images and binaries for Fabric v2.0.0 and Fabric CA v1.4.6"
+    echo "e.g. bootstrap.sh 2.1.0 1.4.6 0.4.18 -s"
+    echo "would download docker images and binaries for Fabric v2.1.0 and Fabric CA v1.4.6"
 }
 
 # dockerPull() pulls docker images from fabric and chaincode repositories
