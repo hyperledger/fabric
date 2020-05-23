dfd9e97d49765effce60b663bf61ce4414ae0dad
Matthew Sykes <sykesmat@us.ibm.com>
2020-05-23 09:42:28 -0400

    Remove unused helpers from raft suite
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- integration/raft/raft_suite_test.go
+++ integration/raft/raft_suite_test.go
@@ -8,17 +8,13 @@ package raft
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 
 	"github.com/hyperledger/fabric/common/flogging"
 	"github.com/hyperledger/fabric/integration"
 	"github.com/hyperledger/fabric/integration/nwo"
-	"github.com/hyperledger/fabric/integration/nwo/commands"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gbytes"
-	"github.com/onsi/gomega/gexec"
 )
 
 func TestRaft(t *testing.T) {
@@ -54,39 +50,4 @@ var _ = SynchronizedAfterSuite(func() {
 
 func StartPort() int {
 	return integration.RaftBasePort.StartPortForNode()
-}
-
-func RunInvoke(n *nwo.Network, orderer *nwo.Orderer, peer *nwo.Peer, channel string) {
-	By("Querying chaincode")
-	sess, err := n.PeerUserSession(peer, "User1", commands.ChaincodeInvoke{
-		ChannelID: channel,
-		Orderer:   n.OrdererAddress(orderer, nwo.ListenPort),
-		Name:      "mycc",
-		Ctor:      `{"Args":["invoke","a","b","10"]}`,
-		PeerAddresses: []string{
-			n.PeerAddress(n.Peer("Org1", "peer0"), nwo.ListenPort),
-			n.PeerAddress(n.Peer("Org2", "peer0"), nwo.ListenPort),
-		},
-		WaitForEvent: true,
-	})
-	Expect(err).NotTo(HaveOccurred())
-	Eventually(sess, n.EventuallyTimeout).Should(gexec.Exit(0))
-	Expect(sess.Err).To(gbytes.Say("Chaincode invoke successful. result: status:200"))
-}
-
-func RunQuery(n *nwo.Network, orderer *nwo.Orderer, peer *nwo.Peer, channel string) int {
-	By("Invoking chaincode")
-	sess, err := n.PeerUserSession(peer, "User1", commands.ChaincodeQuery{
-		ChannelID: channel,
-		Name:      "mycc",
-		Ctor:      `{"Args":["query","a"]}`,
-	})
-	Expect(err).NotTo(HaveOccurred())
-	Eventually(sess, n.EventuallyTimeout).Should(gexec.Exit(0))
-
-	var result int
-	i, err := fmt.Sscanf(string(sess.Out.Contents()), "%d", &result)
-	Expect(err).NotTo(HaveOccurred())
-	Expect(i).To(Equal(1))
-	return int(result)
 }
