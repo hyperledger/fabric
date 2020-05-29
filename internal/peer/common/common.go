adf74361453a65a35e4d09777563406c7ef2f73e
Tiffany Harris <tiffany.harris@ibm.com>
2020-05-29 15:24:14 -0400

    [FAB-17900] Fixes numeric env variable override bug
    Signed-off-by: Tiffany Harris <tiffany.harris@ibm.com>

--- internal/peer/common/common.go
+++ internal/peer/common/common.go
@@ -135,7 +135,7 @@ func InitCrypto(mspMgrConfigDir, localMSPID, localMSPType string) error {
 	SetBCCSPKeystorePath()
 	bccspConfig := factory.GetDefaultOpts()
 	if config := viper.Get("peer.BCCSP"); config != nil {
-		err = mapstructure.Decode(config, bccspConfig)
+		err = mapstructure.WeakDecode(config, bccspConfig)
 		if err != nil {
 			return errors.WithMessage(err, "could not decode peer BCCSP configuration")
 		}
