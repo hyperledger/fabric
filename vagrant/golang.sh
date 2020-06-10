1d4ec99b46570533ce1530e71d14ab52eef35254
Brett Logan <brett.t.logan@ibm.com>
2020-06-10 15:56:53 -0400

    Bump Go Version in Vagrant
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- vagrant/golang.sh
+++ vagrant/golang.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 GOROOT='/opt/go'
-GO_VERSION=1.14.2
+GO_VERSION=1.14.4
 
 # ----------------------------------------------------------------
 # Install Golang
