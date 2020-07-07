75891af07e8639ae661dec0f171ccde8706f987d
Yoav Tock <tock@il.ibm.com>
2020-07-07 16:39:21 +0300

    FAB-18035 Ch.Part.API: consenter JoinChain creates a follower.Chain (#1481)
    Implement the JoinChain() method in the consenter, and create a
    skeleton implementation of a follower.Chain.
    
    Create a cluster.BlockPuller that is initiated from the join-block.
    
    Create a SelfMembershipPredicate for the follower.Chain.
    
    The JoinChain method tries to be strict and checj the inputs before
    chain creation, in order to minimize the risk errors after the
    chain is started.
    
    Signed-off-by: Yoav Tock <tock@il.ibm.com>
    Change-Id: I94043a66c73488e32ac2a49606886e1f94be5cf0

---
+++ orderer/consensus/follower/block_puller_test.go
@@ -0,0 +1,141 @@
+/*
+Copyright IBM Corp. 2017 All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package follower_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+
+	cb "github.com/hyperledger/fabric-protos-go/common"
+	"github.com/hyperledger/fabric/bccsp/sw"
+	"github.com/hyperledger/fabric/common/crypto/tlsgen"
+	"github.com/hyperledger/fabric/core/config/configtest"
+	"github.com/hyperledger/fabric/internal/configtxgen/encoder"
+	"github.com/hyperledger/fabric/internal/configtxgen/genesisconfig"
+	"github.com/hyperledger/fabric/internal/pkg/comm"
+	"github.com/hyperledger/fabric/orderer/common/cluster"
+	"github.com/hyperledger/fabric/orderer/common/localconfig"
+	"github.com/hyperledger/fabric/orderer/consensus/follower"
+	"github.com/hyperledger/fabric/orderer/consensus/follower/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestBlockPullerFromJoinBlock(t *testing.T) {
+	tlsCA, _ := tlsgen.NewCA()
+	channelID := "my-raft-channel"
+	joinBlockAppRaft := generateJoinBlock(t, tlsCA, channelID, 10)
+	require.NotNil(t, joinBlockAppRaft)
+
+	mockSupport := &mocks.BlockPullerSupport{}
+
+	dialer := &cluster.PredicateDialer{
+		Config: comm.ClientConfig{
+			SecOpts: comm.SecureOptions{
+				Certificate: tlsCA.CertBytes(),
+			},
+		},
+	}
+
+	cryptoProvider, err := sw.NewDefaultSecurityLevelWithKeystore(sw.NewDummyKeyStore())
+	assert.NoError(t, err)
+
+	t.Run("good", func(t *testing.T) {
+		mockSupport.ChannelIDReturns(channelID)
+		bp, err := follower.BlockPullerFromJoinBlock(
+			joinBlockAppRaft,
+			mockSupport,
+			dialer,
+			localconfig.Cluster{},
+			cryptoProvider,
+		)
+		assert.NoError(t, err)
+		assert.NotNil(t, bp)
+	})
+
+	t.Run("dialer is nil", func(t *testing.T) {
+		mockSupport.ChannelIDReturns(channelID)
+		bp, err := follower.BlockPullerFromJoinBlock(
+			joinBlockAppRaft,
+			mockSupport,
+			nil,
+			localconfig.Cluster{},
+			cryptoProvider,
+		)
+		assert.EqualError(t, err, "base dialer is nil")
+		assert.Nil(t, bp)
+	})
+
+	t.Run("dialer has bad cert", func(t *testing.T) {
+		mockSupport.ChannelIDReturns(channelID)
+		badDialer := &cluster.PredicateDialer{
+			Config: dialer.Config.Clone(),
+		}
+		badDialer.Config.SecOpts.Certificate = []byte("not-a-certificate")
+		bp, err := follower.BlockPullerFromJoinBlock(
+			joinBlockAppRaft,
+			mockSupport,
+			badDialer,
+			localconfig.Cluster{},
+			cryptoProvider,
+		)
+		assert.EqualError(t, err, "client certificate isn't in PEM format: not-a-certificate")
+		assert.Nil(t, bp)
+	})
+
+	t.Run("bad join block", func(t *testing.T) {
+		mockSupport.ChannelIDReturns(channelID)
+		bp, err := follower.BlockPullerFromJoinBlock(
+			&cb.Block{Header: &cb.BlockHeader{}},
+			mockSupport,
+			dialer,
+			localconfig.Cluster{},
+			cryptoProvider,
+		)
+		assert.EqualError(t, err, "error extracting endpoints from config block: block data is nil")
+		assert.Nil(t, bp)
+	})
+}
+
+func generateJoinBlock(t *testing.T, tlsCA tlsgen.CA, channelID string, number uint64) *cb.Block {
+	tmpdir, err := ioutil.TempDir("", "block-puller-test-")
+	require.NoError(t, err)
+	defer os.RemoveAll(tmpdir)
+
+	confAppRaft := genesisconfig.Load(genesisconfig.SampleDevModeEtcdRaftProfile, configtest.GetDevConfigDir())
+	confAppRaft.Consortiums = nil
+	confAppRaft.Consortium = ""
+	generateCertificates(t, confAppRaft, tlsCA, tmpdir)
+	bootstrapper, err := encoder.NewBootstrapper(confAppRaft)
+	require.NoError(t, err, "cannot create bootstrapper")
+
+	joinBlockAppRaft := bootstrapper.GenesisBlockForChannel(channelID)
+	joinBlockAppRaft.Header.Number = number
+	return joinBlockAppRaft
+}
+
+func generateCertificates(t *testing.T, confAppRaft *genesisconfig.Profile, tlsCA tlsgen.CA, certDir string) {
+	for i, c := range confAppRaft.Orderer.EtcdRaft.Consenters {
+		srvC, err := tlsCA.NewServerCertKeyPair(c.Host)
+		require.NoError(t, err)
+		srvP := path.Join(certDir, fmt.Sprintf("server%d.crt", i))
+		err = ioutil.WriteFile(srvP, srvC.Cert, 0644)
+		require.NoError(t, err)
+
+		clnC, err := tlsCA.NewClientCertKeyPair()
+		require.NoError(t, err)
+		clnP := path.Join(certDir, fmt.Sprintf("client%d.crt", i))
+		err = ioutil.WriteFile(clnP, clnC.Cert, 0644)
+		require.NoError(t, err)
+
+		c.ServerTlsCert = []byte(srvP)
+		c.ClientTlsCert = []byte(clnP)
+	}
+}
--- orderer/consensus/follower/follower_chain_test.go
+++ orderer/consensus/follower/follower_chain_test.go
@@ -7,29 +7,88 @@ SPDX-License-Identifier: Apache-2.0
 package follower_test
 
 import (
-	"github.com/hyperledger/fabric/orderer/common/types"
 	"testing"
 
+	"github.com/hyperledger/fabric-protos-go/common"
+	"github.com/hyperledger/fabric/common/crypto/tlsgen"
+	"github.com/hyperledger/fabric/common/flogging"
+	"github.com/hyperledger/fabric/orderer/common/types"
 	"github.com/hyperledger/fabric/orderer/consensus/follower"
+	"github.com/hyperledger/fabric/orderer/consensus/follower/mocks"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 //TODO skeleton
 
-func TestFollowerChain(t *testing.T) {
-	err := errors.New("bar")
-	chain := &follower.Chain{Err: err}
+var testLogger = flogging.MustGetLogger("follower.test")
 
-	assert.Equal(t, err, chain.Order(nil, 0))
-	assert.Equal(t, err, chain.Configure(nil, 0))
-	assert.Equal(t, err, chain.WaitReady())
-	assert.NotPanics(t, chain.Start)
-	assert.NotPanics(t, chain.Halt)
-	_, open := <-chain.Errored()
-	assert.False(t, open)
+var iAmNotInChannel = func(configBlock *common.Block) error {
+	return errors.New("not in channel")
+}
 
-	cRel, status := chain.StatusReport()
-	assert.Equal(t, types.ClusterRelationFollower, cRel)
-	assert.True(t, status == types.StatusActive || status == types.StatusOnBoarding)
+var iAmInChannel = func(configBlock *common.Block) error {
+	return nil
+}
+
+func TestFollowerNewChain(t *testing.T) {
+	tlsCA, _ := tlsgen.NewCA()
+	channelID := "my-raft-channel"
+	joinBlockAppRaft := generateJoinBlock(t, tlsCA, channelID, 10)
+	require.NotNil(t, joinBlockAppRaft)
+	mockSupport := &mocks.Support{}
+	mockSupport.On("ChannelID").Return("my-channel")
+	mockSupport.On("Height").Return(uint64(5))
+	options := follower.Options{Logger: testLogger, Cert: []byte{1, 2, 3, 4}}
+
+	t.Run("with join block, not in channel", func(t *testing.T) {
+		chain, err := follower.NewChain(mockSupport, joinBlockAppRaft, options, nil, nil, nil, iAmNotInChannel)
+		assert.NoError(t, err)
+		err = chain.Order(nil, 0)
+		assert.EqualError(t, err, "orderer is a follower of channel my-channel")
+		err = chain.Configure(nil, 0)
+		assert.EqualError(t, err, "orderer is a follower of channel my-channel")
+		err = chain.WaitReady()
+		assert.EqualError(t, err, "orderer is a follower of channel my-channel")
+		_, open := <-chain.Errored()
+		assert.False(t, open)
+
+		cRel, status := chain.StatusReport()
+		assert.Equal(t, types.ClusterRelationFollower, cRel)
+		assert.Equal(t, types.StatusOnBoarding, status)
+	})
+
+	t.Run("with join block, in channel", func(t *testing.T) {
+		chain, err := follower.NewChain(mockSupport, joinBlockAppRaft, options, nil, nil, nil, iAmInChannel)
+		assert.NoError(t, err)
+
+		cRel, status := chain.StatusReport()
+		assert.Equal(t, types.ClusterRelationMember, cRel)
+		assert.Equal(t, types.StatusOnBoarding, status)
+
+		assert.NotPanics(t, chain.Start)
+		assert.NotPanics(t, chain.Start)
+		assert.NotPanics(t, chain.Halt)
+		assert.NotPanics(t, chain.Halt)
+		assert.NotPanics(t, chain.Start)
+	})
+
+	t.Run("bad join block", func(t *testing.T) {
+		chain, err := follower.NewChain(mockSupport, &common.Block{}, options, nil, nil, nil, nil)
+		assert.EqualError(t, err, "block header is nil")
+		assert.Nil(t, chain)
+		chain, err = follower.NewChain(mockSupport, &common.Block{Header: &common.BlockHeader{}}, options, nil, nil, nil, nil)
+		assert.EqualError(t, err, "block data is nil")
+		assert.Nil(t, chain)
+	})
+
+	t.Run("without join block", func(t *testing.T) {
+		chain, err := follower.NewChain(mockSupport, nil, options, nil, nil, nil, nil)
+		assert.NoError(t, err)
+
+		cRel, status := chain.StatusReport()
+		assert.Equal(t, types.ClusterRelationFollower, cRel)
+		assert.True(t, status == types.StatusActive)
+	})
 }
