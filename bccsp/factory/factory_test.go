693b84be3e79264990d373d976b979acd8317df6
Matthew Sykes <sykesmat@us.ibm.com>
2020-07-08 14:30:38 -0400

    [FAB-18323] CherryPick: remove ephemeral from BCCSP SW options (#1553)
    Signed-off-by: Matthew Sykes <matthew.sykes@gmail.com>
    Signed-off-by: Wenjian Qiao <wenjianq@gmail.com>

--- bccsp/factory/factory_test.go
+++ bccsp/factory/factory_test.go
@@ -62,7 +62,7 @@ BCCSP:
 	cfgVariations := []*FactoryOpts{
 		{},
 		{ProviderName: "SW"},
-		{ProviderName: "SW", SwOpts: &SwOpts{HashFamily: "SHA2", SecLevel: 256, Ephemeral: true}},
+		{ProviderName: "SW", SwOpts: &SwOpts{HashFamily: "SHA2", SecLevel: 256}},
 		yamlBCCSP,
 	}
 
