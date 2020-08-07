9fa390b4c113126ee0e6be64b61ace99ff820223
Chen Kai <281165273grape@gmail.com>
2020-08-08 00:57:33 +0800

    Remove dbname in redolog struct (#1686)
    Signed-off-by: grapebaba <281165273@qq.com>

--- core/ledger/kvledger/txmgmt/statedb/statecouchdb/redolog.go
+++ core/ledger/kvledger/txmgmt/statedb/statecouchdb/redolog.go
@@ -15,13 +15,12 @@ import (
 	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/statedb"
 )
 
-var redologKeyPrefix = []byte{byte(0)}
+var redoLogKey = []byte{byte(0)}
 
 type redoLoggerProvider struct {
 	leveldbProvider *leveldbhelper.Provider
 }
 type redoLogger struct {
-	dbName   string
 	dbHandle *leveldbhelper.DBHandle
 }
 
@@ -49,25 +48,19 @@ func (p *redoLoggerProvider) close() {
 }
 
 func (l *redoLogger) persist(r *redoRecord) error {
-	k := encodeRedologKey(l.dbName)
 	v, err := encodeRedologVal(r)
 	if err != nil {
 		return err
 	}
-	return l.dbHandle.Put(k, v, true)
+	return l.dbHandle.Put(redoLogKey, v, true)
 }
 
 func (l *redoLogger) load() (*redoRecord, error) {
-	k := encodeRedologKey(l.dbName)
-	v, err := l.dbHandle.Get(k)
+	v, err := l.dbHandle.Get(redoLogKey)
 	if err != nil || v == nil {
 		return nil, err
 	}
 	return decodeRedologVal(v)
-}
-
-func encodeRedologKey(dbName string) []byte {
-	return append(redologKeyPrefix, []byte(dbName)...)
 }
 
 func encodeRedologVal(r *redoRecord) ([]byte, error) {
