fa3a974531195fadb678cc5edd4f8b6fe5f86dbb
Matthew Sykes <sykesmat@us.ibm.com>
2020-08-17 16:22:41 -0400

    Use regexp to handle possible quotes in error
    time.ParseDuration now quotes the original value provided to the
    function. This change modifies the assertion to handle the quotes when
    present.
    
    See https://github.com/golang/go/issues/38295
    
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- core/container/externalbuilder/instance_test.go
+++ core/container/externalbuilder/instance_test.go
@@ -249,7 +249,7 @@ var _ = Describe("Instance", func() {
 			Entry("Number", `100`, externalbuilder.Duration(100), BeNil()),
 			Entry("Duration", `"1s"`, externalbuilder.Duration(time.Second), BeNil()),
 			Entry("List", `[1, 2, 3]`, externalbuilder.Duration(time.Second), MatchError("invalid duration")),
-			Entry("Nonsense", `"nonsense"`, externalbuilder.Duration(time.Second), MatchError("time: invalid duration nonsense")),
+			Entry("Nonsense", `"nonsense"`, externalbuilder.Duration(time.Second), MatchError(MatchRegexp(`time: invalid duration "?nonsense"?`))),
 		)
 
 		DescribeTable("Round Trip",
