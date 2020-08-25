a63bbe9acd9ed8c45bc0779ca9f75745421fa386
David Enyeart <enyeart@us.ibm.com>
2020-08-25 18:21:04 -0400

    [FAB-18188] Log orderer and peer cert expiration date upon startup
    FAB-17000 added a warning if expiration is within a week.
    
    This change additionally info logs the future enrollment, tls server, and tls client
    cert expiration dates upon startup, regardless of whether they will expire within a week or not.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- internal/peer/node/start.go
+++ internal/peer/node/start.go
@@ -314,6 +314,7 @@ func serve(args []string) error {
 		serverConfig.SecOpts.Certificate,
 		cs.GetClientCertificate().Certificate,
 		signingIdentityBytes,
+		expirationLogger.Infof,
 		expirationLogger.Warnf, // This can be used to piggyback a metric event in the future
 		time.Now(),
 		time.AfterFunc,
