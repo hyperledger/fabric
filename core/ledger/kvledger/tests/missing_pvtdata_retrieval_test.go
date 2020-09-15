ce715d814692746da13ea3b7028343171cc0521b
Senthil Nathan N <cendhu@gmail.com>
2020-09-15 16:10:07 +0530

    minor refactoring of missing pvtdata retrieval test
    Signed-off-by: Senthil Nathan N <cendhu@gmail.com>
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- core/ledger/kvledger/tests/missing_pvtdata_retrieval_test.go
+++ core/ledger/kvledger/tests/missing_pvtdata_retrieval_test.go
@@ -11,84 +11,110 @@ import (
 
 	"github.com/hyperledger/fabric/core/ledger"
 	"github.com/hyperledger/fabric/core/ledger/kvledger"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestGetMissingPvtDataAfterRollback(t *testing.T) {
-	env := newEnv(t)
-	defer env.cleanup()
-	env.initLedgerMgmt()
-	h := env.newTestHelperCreateLgr("ledger1", t)
+func TestGetMissingPvtData(t *testing.T) {
+	setup := func(h *testhelper) (*ledger.BlockAndPvtData, ledger.MissingPvtDataInfo) {
+		collConf := []*collConf{{
+			name: "coll1",
+			btl:  5,
+		}}
 
-	collConf := []*collConf{{name: "coll1", btl: 5}}
+		// deploy cc1 with 'collConf'
+		h.simulateDeployTx("cc1", collConf)
+		h.cutBlockAndCommitLegacy()
 
-	// deploy cc1 with 'collConf'
-	h.simulateDeployTx("cc1", collConf)
-	h.cutBlockAndCommitLegacy()
+		// pvtdata simulation
+		h.simulateDataTx("", func(s *simulator) {
+			s.setPvtdata("cc1", "coll1", "key1", "value1")
+		})
+		// another pvtdata simulation
+		h.simulateDataTx("", func(s *simulator) {
+			s.setPvtdata("cc1", "coll1", "key2", "value2")
+		})
+		// another pvtdata simulation
+		h.simulateDataTx("", func(s *simulator) {
+			s.setPvtdata("cc1", "coll1", "key3", "value3")
+		})
 
-	// pvtdata simulation
-	h.simulateDataTx("", func(s *simulator) {
-		s.setPvtdata("cc1", "coll1", "key1", "value1")
+		// two transactions are missing some pvtdata
+		h.causeMissingPvtData(0)
+		h.causeMissingPvtData(2)
+		blk2 := h.cutBlockAndCommitLegacy()
+
+		h.verifyPvtState("cc1", "coll1", "key2", "value2") // key2 should have been committed
+		h.simulateDataTx("", func(s *simulator) {
+			h.assertError(s.GetPrivateData("cc1", "coll1", "key1")) // key1 would be stale with respect to hashed version
+		})
+		h.simulateDataTx("", func(s *simulator) {
+			h.assertError(s.GetPrivateData("cc1", "coll1", "key3")) // key3 would be stale with respect to hashed version
+		})
+
+		// verify missing pvtdata info
+		h.verifyBlockAndPvtDataSameAs(2, blk2)
+		expectedMissingPvtDataInfo := make(ledger.MissingPvtDataInfo)
+		expectedMissingPvtDataInfo.Add(2, 0, "cc1", "coll1")
+		expectedMissingPvtDataInfo.Add(2, 2, "cc1", "coll1")
+		h.verifyMissingPvtDataSameAs(2, expectedMissingPvtDataInfo)
+
+		return blk2, expectedMissingPvtDataInfo
+	}
+
+	t.Run("get missing data after rollback", func(t *testing.T) {
+		env := newEnv(t)
+		defer env.cleanup()
+		env.initLedgerMgmt()
+		h := env.newTestHelperCreateLgr("ledger1", t)
+
+		blk, expectedMissingPvtDataInfo := setup(h)
+
+		// verify missing pvtdata info
+		require.Equal(t, uint64(2), blk.Block.Header.Number)
+		h.verifyBlockAndPvtDataSameAs(2, blk)
+		h.verifyMissingPvtDataSameAs(int(2), expectedMissingPvtDataInfo)
+
+		// commit block 3
+		h.simulateDataTx("", func(s *simulator) {
+			s.setPvtdata("cc1", "coll1", "key4", "value4")
+		})
+		blk3 := h.cutBlockAndCommitLegacy()
+
+		// commit block 4
+		h.simulateDataTx("", func(s *simulator) {
+			s.setPvtdata("cc1", "coll1", "key5", "value5")
+		})
+		blk4 := h.cutBlockAndCommitLegacy()
+
+		// verify missing pvtdata info
+		h.verifyMissingPvtDataSameAs(5, expectedMissingPvtDataInfo)
+
+		// rollback ledger to block 2
+		h.verifyLedgerHeight(5)
+		env.closeLedgerMgmt()
+		err := kvledger.RollbackKVLedger(env.initializer.Config.RootFSPath, "ledger1", 2)
+		require.NoError(t, err)
+		env.initLedgerMgmt()
+
+		h = env.newTestHelperOpenLgr("ledger1", t)
+		h.verifyLedgerHeight(3)
+
+		// verify block & pvtdata
+		h.verifyBlockAndPvtDataSameAs(2, blk)
+		// when the pvtdata store is ahead of blockstore,
+		// missing pvtdata info for block 2 would not be returned.
+		h.verifyMissingPvtDataSameAs(5, nil)
+
+		// recommit block 3
+		require.NoError(t, h.lgr.CommitLegacy(blk3, &ledger.CommitOptions{}))
+		// when the pvtdata store is ahead of blockstore,
+		// missing pvtdata info for block 2 would not be returned.
+		h.verifyMissingPvtDataSameAs(5, nil)
+
+		// recommit block 4
+		require.NoError(t, h.lgr.CommitLegacy(blk4, &ledger.CommitOptions{}))
+		// once the pvtdata store and blockstore becomes equal,
+		// missing pvtdata info for block 2 would be returned.
+		h.verifyMissingPvtDataSameAs(5, expectedMissingPvtDataInfo)
 	})
-	// another pvtdata simulation
-	h.simulateDataTx("", func(s *simulator) {
-		s.setPvtdata("cc1", "coll1", "key2", "value2")
-	})
-
-	h.causeMissingPvtData(0)
-	blk2 := h.cutBlockAndCommitLegacy()
-
-	h.verifyPvtState("cc1", "coll1", "key2", "value2") // key2 should have been committed
-	h.simulateDataTx("", func(s *simulator) {
-		h.assertError(s.GetPrivateData("cc1", "coll1", "key1")) // key1 would be stale with respect to hashed version
-	})
-
-	// verify missing pvtdata info
-	h.verifyBlockAndPvtDataSameAs(2, blk2)
-	expectedMissingPvtDataInfo := make(ledger.MissingPvtDataInfo)
-	expectedMissingPvtDataInfo.Add(2, 0, "cc1", "coll1")
-	h.verifyMissingPvtDataSameAs(2, expectedMissingPvtDataInfo)
-
-	// commit block 3
-	h.simulateDataTx("", func(s *simulator) {
-		s.setPvtdata("cc1", "coll1", "key3", "value2")
-	})
-	blk3 := h.cutBlockAndCommitLegacy()
-
-	// commit block 4
-	h.simulateDataTx("", func(s *simulator) {
-		s.setPvtdata("cc1", "coll1", "key3", "value2")
-	})
-	blk4 := h.cutBlockAndCommitLegacy()
-
-	// verify missing pvtdata info
-	h.verifyMissingPvtDataSameAs(5, expectedMissingPvtDataInfo)
-
-	// rollback ledger to block 2
-	h.verifyLedgerHeight(5)
-	env.closeLedgerMgmt()
-	err := kvledger.RollbackKVLedger(env.initializer.Config.RootFSPath, "ledger1", 2)
-	assert.NoError(t, err)
-	env.initLedgerMgmt()
-
-	h = env.newTestHelperOpenLgr("ledger1", t)
-	h.verifyLedgerHeight(3)
-
-	// verify block & pvtdata
-	h.verifyBlockAndPvtDataSameAs(2, blk2)
-	// when the pvtdata store is ahead of blockstore,
-	// missing pvtdata info for block 2 would not be returned.
-	h.verifyMissingPvtDataSameAs(5, nil)
-
-	// recommit block 3
-	assert.NoError(t, h.lgr.CommitLegacy(blk3, &ledger.CommitOptions{}))
-	// when the pvtdata store is ahead of blockstore,
-	// missing pvtdata info for block 2 would not be returned.
-	h.verifyMissingPvtDataSameAs(5, nil)
-
-	// recommit block 4
-	assert.NoError(t, h.lgr.CommitLegacy(blk4, &ledger.CommitOptions{}))
-	// once the pvtdata store and blockstore becomes equal,
-	// missing pvtdata info for block 2 would be returned.
-	h.verifyMissingPvtDataSameAs(5, expectedMissingPvtDataInfo)
 }
