efbb0c9de8f19fca6fe1c3d246538d0fe8076904
Brett Logan <brett.t.logan@ibm.com>
2020-09-30 15:54:49 -0400

    Add two and three digit publishing
    The two and three digit images were accidentally
    removed from publishing in a prior commit
    
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- ci/scripts/publish_docker.sh
+++ ci/scripts/publish_docker.sh
@@ -10,6 +10,8 @@ docker login --username "${DOCKER_USERNAME}" --password "${DOCKER_PASSWORD}"
 for image in baseos peer orderer ccenv tools; do
   for release in ${RELEASE} ${TWO_DIGIT_RELEASE}; do
     docker tag "hyperledger/fabric-${image}" "hyperledger/fabric-${image}:amd64-${release}"
+    docker tag "hyperledger/fabric-${image}" "hyperledger/fabric-${image}:${release}"
     docker push "hyperledger/fabric-${image}:amd64-${release}"
+    docker push "hyperledger/fabric-${image}:${release}"
   done
 done
