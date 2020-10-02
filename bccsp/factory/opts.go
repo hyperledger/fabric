0a9f7663a3a548385f3e4fa55e147b6dbce055d4
Will Lahti <wtlahti@us.ibm.com>
2020-10-02 11:03:05 -0400

    Allow BCCSP config to be set using env var (#1900)
    FAB-17969
    
    Signed-off-by: Tiffany Harris <tiffany.harris@ibm.com>
    Signed-off-by: Will Lahti <wtlahti@us.ibm.com>

--- bccsp/factory/opts.go
+++ bccsp/factory/opts.go
@@ -1,20 +1,19 @@
 /*
-Copyright IBM Corp. 2016 All Rights Reserved.
+Copyright IBM Corp. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-		 http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+SPDX-License-Identifier: Apache-2.0
 */
 
 package factory
+
+import "github.com/hyperledger/fabric/bccsp/pkcs11"
+
+// FactoryOpts holds configuration information used to initialize factory implementations
+type FactoryOpts struct {
+	Default string             `json:"default" yaml:"Default"`
+	SW      *SwOpts            `json:"SW,omitempty" yaml:"SW,omitempty"`
+	PKCS11  *pkcs11.PKCS11Opts `json:"PKCS11,omitempty" yaml:"PKCS11"`
+}
 
 // GetDefaultOpts offers a default implementation for Opts
 // returns a new instance every time
@@ -22,6 +21,10 @@ func GetDefaultOpts() *FactoryOpts {
 	return &FactoryOpts{
 		Default: "SW",
 		SW: &SwOpts{
+			Hash:     "SHA2",
+			Security: 256,
+		},
+		PKCS11: &pkcs11.PKCS11Opts{
 			Hash:     "SHA2",
 			Security: 256,
 		},
