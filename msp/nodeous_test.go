f4bd90a50c6cac89e8c52215db022a3dc93cd3a2
David Enyeart <enyeart@us.ibm.com>
2020-10-07 00:22:43 -0400

    Fix Node OU error message
    If signing identity didn't match any expected Node OU roles,
    the error message indicated that it matched multiple roles. This confused users.
    Now there will be a separate error message for no OU roles versus multiple OU roles.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- msp/nodeous_test.go
+++ msp/nodeous_test.go
@@ -41,28 +41,46 @@ func TestInvalidAdminNodeOU(t *testing.T) {
 }
 
 func TestInvalidSigningIdentityNodeOU(t *testing.T) {
-	// testdata/nodeous2:
-	// the configuration enables NodeOUs but the signing identity does not carry
-	// any valid NodeOUS. Therefore signing identity validation should fail
-	thisMSP := getLocalMSPWithVersion(t, "testdata/nodeous2", MSPv1_1)
-	assert.True(t, thisMSP.(*bccspmsp).ouEnforcement)
+	t.Run("signing_identity_validation_fails_with_MSPv1_4_3", func(t *testing.T) {
+		// testdata/nodeous2:
+		// the configuration enables NodeOUs but the signing identity does not carry
+		// any valid NodeOUS. Therefore signing identity validation should fail
+		thisMSP := getLocalMSPWithVersion(t, "testdata/nodeous2", MSPv1_4_3)
+		assert.True(t, thisMSP.(*bccspmsp).ouEnforcement)
 
-	id, err := thisMSP.GetDefaultSigningIdentity()
-	assert.NoError(t, err)
+		id, err := thisMSP.GetDefaultSigningIdentity()
+		assert.NoError(t, err)
 
-	err = id.Validate()
-	assert.Error(t, err)
+		err = id.Validate()
+		assert.EqualError(t, err, "could not validate identity's OUs: the identity does not have an OU that resolves to client, peer, orderer, or admin role. OUs: [], MSP: [SampleOrg]")
+	})
 
-	// MSPv1_0 should not fail
-	thisMSP, err = getLocalMSPWithVersionAndError(t, "testdata/nodeous1", MSPv1_0)
-	assert.False(t, thisMSP.(*bccspmsp).ouEnforcement)
-	assert.NoError(t, err)
+	t.Run("signing_identity_validation_fails_with_MSPv1_1", func(t *testing.T) {
+		// testdata/nodeous2:
+		// the configuration enables NodeOUs but the signing identity does not carry
+		// any valid NodeOUS. Therefore signing identity validation should fail
+		thisMSP := getLocalMSPWithVersion(t, "testdata/nodeous2", MSPv1_1)
+		assert.True(t, thisMSP.(*bccspmsp).ouEnforcement)
 
-	id, err = thisMSP.GetDefaultSigningIdentity()
-	assert.NoError(t, err)
+		id, err := thisMSP.GetDefaultSigningIdentity()
+		assert.NoError(t, err)
 
-	err = id.Validate()
-	assert.NoError(t, err)
+		err = id.Validate()
+		assert.EqualError(t, err, "could not validate identity's OUs: the identity does not have an OU that resolves to client or peer. OUs: [], MSP: [SampleOrg]")
+	})
+
+	t.Run("signing_identity_validation_succeeds_with_MSPv1_0", func(t *testing.T) {
+		// MSPv1_0 should not fail, node OUs not yet implemented in 1_0
+		thisMSP, err := getLocalMSPWithVersionAndError(t, "testdata/nodeous1", MSPv1_0)
+		assert.False(t, thisMSP.(*bccspmsp).ouEnforcement)
+		assert.NoError(t, err)
+
+		id, err := thisMSP.GetDefaultSigningIdentity()
+		assert.NoError(t, err)
+
+		err = id.Validate()
+		assert.NoError(t, err)
+	})
 }
 
 func TestValidMSPWithNodeOU(t *testing.T) {
