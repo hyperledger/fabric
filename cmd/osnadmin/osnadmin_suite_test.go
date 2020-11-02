ea81d212cb51109a517bdf9ee2c00e2f5aaa30eb
stephyee <tiffany.harris@ibm.com>
2020-11-02 16:34:12 -0500

    osnadmin CLI (#1907)
    Adds CLI commands for the list, join, and remove using the new channel
    participation API.
    
    FAB-18196 #done
    
    Signed-off-by: Will Lahti <wtlahti@us.ibm.com>
    Signed-off-by: Tiffany Harris <tiffany.harris@ibm.com>
    
    Co-authored-by: Will Lahti <wtlahti@us.ibm.com>

---
+++ cmd/osnadmin/osnadmin_suite_test.go
@@ -0,0 +1,30 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"testing"
+
+	cb "github.com/hyperledger/fabric-protos-go/common"
+	"github.com/hyperledger/fabric/orderer/common/types"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+//go:generate counterfeiter -o mocks/channel_management.go -fake-name ChannelManagement . channelManagement
+
+type channelManagement interface {
+	ChannelList() types.ChannelList
+	ChannelInfo(channelID string) (types.ChannelInfo, error)
+	JoinChannel(channelID string, configBlock *cb.Block, isAppChannel bool) (types.ChannelInfo, error)
+	RemoveChannel(channelID string) error
+}
+
+func TestOsnadmin(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "osnadmin Suite")
+}
