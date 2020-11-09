5b3daf2b275789c962bf655892a1c65aef330ac4
Brett Logan <brett.t.logan@ibm.com>
2020-11-09 12:48:15 -0500

    mac_pkcs11
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- ci/scripts/setup_hsm.sh
+++ ci/scripts/setup_hsm.sh
@@ -4,9 +4,5 @@
 # SPDX-License-Identifier: Apache-2.0
 set -euo pipefail
 
-sudo apt-get install -y softhsm2
-sudo mkdir -p /var/lib/softhsm/tokens
-sudo softhsm2-util --init-token --slot 0 --label "ForFabric" --so-pin 1234 --pin 98765432
-sudo chmod -R 777 /var/lib/softhsm
-mkdir -p ~/.config/softhsm2
-cp /usr/share/softhsm/softhsm2.conf ~/.config/softhsm2
+brew install softhsm
+softhsm2-util --init-token --slot 0 --label "ForFabric" --so-pin 1234 --pin 98765432
