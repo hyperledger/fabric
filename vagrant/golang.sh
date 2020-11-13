eccce85f99699793df5d60eb3e08f4c72741b7dc
Brett Logan <brett.t.logan@ibm.com>
2020-11-12 22:14:46 -0500

    Update Go to 1.14.12
    Signed-off-by: Brett Logan <brett.t.logan@ibm.com>

--- vagrant/golang.sh
+++ vagrant/golang.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 GOROOT='/opt/go'
-GO_VERSION=1.14.4
+GO_VERSION=1.14.12
 
 # ----------------------------------------------------------------
 # Install Golang
