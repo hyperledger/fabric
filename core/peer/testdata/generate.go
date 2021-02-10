862687afc71701843b336316e59fb9982e46076a
Matthew Sykes <sykesmat@us.ibm.com>
2021-02-10 09:06:33 -0500

    Format and errcheck test cert generation helper
    - Run gofumpt
    - Fix error checks
    - Update path to tool in go:generate comments
    - Regenerate certs in internal/pkg/comm
    
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- core/peer/testdata/generate.go
+++ core/peer/testdata/generate.go
@@ -6,7 +6,7 @@ SPDX-License-Identifier: Apache-2.0
 
 // +build ignore
 
-//go:generate -command gencerts go run github.com/hyperledger/fabric/core/comm/testdata/certs
+//go:generate -command gencerts go run github.com/hyperledger/fabric/internal/pkg/comm/testdata/certs
 //go:generate gencerts -orgs 3 -child-orgs 1 -servers 1 -clients 0
 
 package testdata
