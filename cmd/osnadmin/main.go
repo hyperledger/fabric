e708729651233edcb3175640349546557d5518f6
Matthew Sykes <sykesmat@us.ibm.com>
2021-02-23 15:51:28 -0500

    osnadmin: add --no-status flag to remove Status
    The `osnadmin` command produces JSON output but the output cannot be
    passed directly to a JSON parser because the tool writes a line of text
    before printing the JSON.
    
    This behavior can now be controlled by using the `--no-status` flag. The
    inverted behavior is used to maintain backward compatibility.
    
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- cmd/osnadmin/main.go
+++ cmd/osnadmin/main.go
@@ -45,6 +45,7 @@ func executeForArgs(args []string) (output string, exit int, err error) {
 	caFile := app.Flag("ca-file", "Path to file containing PEM-encoded TLS CA certificate(s) for the OSN").String()
 	clientCert := app.Flag("client-cert", "Path to file containing PEM-encoded X509 public key to use for mutual TLS communication with the OSN").String()
 	clientKey := app.Flag("client-key", "Path to file containing PEM-encoded private key to use for mutual TLS communication with the OSN").String()
+	noStatus := app.Flag("no-status", "Remove the HTTP status message from the command output").Default("false").Bool()
 
 	channel := app.Command("channel", "Channel actions")
 
@@ -132,19 +133,25 @@ func executeForArgs(args []string) (output string, exit int, err error) {
 		return errorOutput(err), 1, nil
 	}
 
-	return responseOutput(resp.StatusCode, bodyBytes), 0, nil
+	output, err = responseOutput(!*noStatus, resp.StatusCode, bodyBytes)
+	if err != nil {
+		return errorOutput(err), 1, nil
+	}
+
+	return output, 0, nil
 }
 
-func responseOutput(statusCode int, responseBody []byte) string {
-	status := fmt.Sprintf("Status: %d", statusCode)
-
+func responseOutput(showStatus bool, statusCode int, responseBody []byte) (string, error) {
 	var buffer bytes.Buffer
-	json.Indent(&buffer, responseBody, "", "\t")
-	response := fmt.Sprintf("%s", buffer.Bytes())
-
-	output := fmt.Sprintf("%s\n%s", status, response)
-
-	return output
+	if showStatus {
+		fmt.Fprintf(&buffer, "Status: %d\n", statusCode)
+	}
+	if len(responseBody) != 0 {
+		if err := json.Indent(&buffer, responseBody, "", "\t"); err != nil {
+			return "", err
+		}
+	}
+	return buffer.String(), nil
 }
 
 func readBodyBytes(body io.ReadCloser) ([]byte, error) {
