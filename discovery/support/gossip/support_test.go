b2be0032343fde936529fa12853f97301357723d
Matthew Sykes <sykesmat@us.ibm.com>
2021-02-24 14:34:09 -0500

    hygiene: remove camelCase import name
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- discovery/support/gossip/support_test.go
+++ discovery/support/gossip/support_test.go
@@ -9,8 +9,8 @@ package gossip_test
 import (
 	"testing"
 
-	"github.com/hyperledger/fabric-protos-go/gossip"
-	gossipSupport "github.com/hyperledger/fabric/discovery/support/gossip"
+	gp "github.com/hyperledger/fabric-protos-go/gossip"
+	"github.com/hyperledger/fabric/discovery/support/gossip"
 	"github.com/hyperledger/fabric/discovery/support/gossip/mocks"
 	"github.com/hyperledger/fabric/gossip/common"
 	"github.com/hyperledger/fabric/gossip/discovery"
@@ -20,16 +20,16 @@ import (
 
 func TestChannelExists(t *testing.T) {
 	g := &mocks.Gossip{}
-	sup := gossipSupport.NewDiscoverySupport(g)
+	sup := gossip.NewDiscoverySupport(g)
 	require.False(t, sup.ChannelExists(""))
 }
 
 func TestPeers(t *testing.T) {
 	g := &mocks.Gossip{}
-	sup := gossipSupport.NewDiscoverySupport(g)
-	p1Envelope := &gossip.Envelope{
+	sup := gossip.NewDiscoverySupport(g)
+	p1Envelope := &gp.Envelope{
 		Payload: []byte{4, 5, 6},
-		SecretEnvelope: &gossip.SecretEnvelope{
+		SecretEnvelope: &gp.SecretEnvelope{
 			Payload: []byte{1, 2, 3},
 		},
 	}
@@ -39,7 +39,7 @@ func TestPeers(t *testing.T) {
 	}
 	g.PeersReturnsOnCall(0, peers)
 	g.SelfMembershipInfoReturnsOnCall(0, discovery.NetworkMember{PKIid: common.PKIidType("p0"), Endpoint: "p0"})
-	p1ExpectedEnvelope := &gossip.Envelope{
+	p1ExpectedEnvelope := &gp.Envelope{
 		Payload: []byte{4, 5, 6},
 	}
 	expected := discovery.Members{{PKIid: common.PKIidType("p1"), Endpoint: "p1", Envelope: p1ExpectedEnvelope}, {PKIid: common.PKIidType("p0"), Endpoint: "p0"}}
@@ -48,9 +48,9 @@ func TestPeers(t *testing.T) {
 }
 
 func TestPeersOfChannel(t *testing.T) {
-	stateInfo := &gossip.GossipMessage{
-		Content: &gossip.GossipMessage_StateInfo{
-			StateInfo: &gossip.StateInfo{
+	stateInfo := &gp.GossipMessage{
+		Content: &gp.GossipMessage_StateInfo{
+			StateInfo: &gp.StateInfo{
 				PkiId: common.PKIidType("px"),
 			},
 		},
@@ -60,7 +60,7 @@ func TestPeersOfChannel(t *testing.T) {
 	g.SelfChannelInfoReturnsOnCall(0, nil)
 	g.SelfChannelInfoReturnsOnCall(1, sMsg)
 	g.PeersOfChannelReturnsOnCall(0, []discovery.NetworkMember{{PKIid: common.PKIidType("p1")}, {PKIid: common.PKIidType("p2")}})
-	sup := gossipSupport.NewDiscoverySupport(g)
+	sup := gossip.NewDiscoverySupport(g)
 	require.Empty(t, sup.PeersOfChannel(common.ChannelID("")))
 	expected := discovery.Members{{PKIid: common.PKIidType("p1")}, {PKIid: common.PKIidType("p2")}, {PKIid: common.PKIidType("px"), Envelope: sMsg.Envelope}}
 	require.Equal(t, expected, sup.PeersOfChannel(common.ChannelID("")))
