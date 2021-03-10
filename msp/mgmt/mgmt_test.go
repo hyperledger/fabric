8846408baac8e43e9565ffe76f537bfa00002d42
Matthew Sykes <sykesmat@us.ibm.com>
2021-03-10 11:12:30 -0500

    msp/mgmt: move TestLocalMSP into mgmt_test
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- msp/mgmt/mgmt_test.go
+++ msp/mgmt/mgmt_test.go
@@ -146,6 +146,21 @@ func LoadMSPSetupForTesting() (bccsp.BCCSP, error) {
 	return cryptoProvider, nil
 }
 
+func TestLocalMSP(t *testing.T) {
+	mspDir := configtest.GetDevMspDir()
+	err := LoadLocalMsp(mspDir, nil, "SampleOrg")
+	if err != nil {
+		t.Fatalf("LoadLocalMsp failed, err %s", err)
+	}
+
+	cryptoProvider, err := sw.NewDefaultSecurityLevelWithKeystore(sw.NewDummyKeyStore())
+	require.NoError(t, err)
+	_, err = GetLocalMSP(cryptoProvider).GetDefaultSigningIdentity()
+	if err != nil {
+		t.Fatalf("GetDefaultSigningIdentity failed, err %s", err)
+	}
+}
+
 func TestMain(m *testing.M) {
 	mspDir := configtest.GetDevMspDir()
 
