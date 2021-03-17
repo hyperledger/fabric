fad002c11ee3b6f106c4a2c47652cbad1dac1e59
andrew-coleman <andrew_coleman@uk.ibm.com>
2021-03-17 10:50:40 +0000

    Gateway error logic refactor
    Implements the suggested improvement as per previous review comment:
    https://github.com/hyperledger/fabric/pull/2475#discussion_r593330491
    
    Replaces the if/else block with a switch statement and helper function.
    
    Signed-off-by: andrew-coleman <andrew_coleman@uk.ibm.com>

--- internal/pkg/gateway/api.go
+++ internal/pkg/gateway/api.go
@@ -101,17 +101,15 @@ func (gs *Server) Endorse(ctx context.Context, request *gp.EndorseRequest) (*gp.
 		go func(e *endorser) {
 			defer wg.Done()
 			response, err := e.client.ProcessProposal(ctx, signedProposal)
-			var detail *gp.EndpointError
-			if err != nil {
-				detail = &gp.EndpointError{Address: e.address, MspId: e.mspid, Message: err.Error()}
-				responseCh <- &endorserResponse{err: detail}
-				return
-			}
-			if response.Response.Status < 200 || response.Response.Status >= 400 {
+			switch {
+			case err != nil:
+				responseCh <- &endorserResponse{err: endpointError(e, err)}
+			case response.Response.Status < 200 || response.Response.Status >= 400:
 				// this is an error case and will be returned in the error details to the client
-				detail = &gp.EndpointError{Address: e.address, MspId: e.mspid, Message: fmt.Sprintf("error %d, %s", response.Response.Status, response.Response.Message)}
+				responseCh <- &endorserResponse{err: endpointError(e, fmt.Errorf("error %d, %s", response.Response.Status, response.Response.Message))}
+			default:
+				responseCh <- &endorserResponse{pr: response}
 			}
-			responseCh <- &endorserResponse{pr: response, err: detail}
 		}(e)
 	}
 	wg.Wait()
@@ -199,4 +197,8 @@ func (gs *Server) CommitStatus(ctx context.Context, request *gp.CommitStatusRequ
 		return nil, status.Error(codes.InvalidArgument, "a commit status request is required")
 	}
 	return nil, status.Error(codes.Unimplemented, "Not implemented")
+}
+
+func endpointError(e *endorser, err error) *gp.EndpointError {
+	return &gp.EndpointError{Address: e.address, MspId: e.mspid, Message: err.Error()}
 }
