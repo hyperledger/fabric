19dcb112ea5c421ac7771d83c0a5ee406c88a801
yacovm <yacovm@users.noreply.github.com>
2021-05-26 01:35:43 +0300

    Optionally disable gossip block forwarding (#2606)
    This commit adds a new configuration option to the peer which makes peers
    not forward blocks that they pull from the ordering service.
    
    If all peers in an organization explicitly set "peer.deliveryclient.blockGossipEnabled" to false,
    no peer in the organization gossips blocks to any other peer in that organization.
    
    Change-Id: I5d9b278ae72f239129827c044fa78179f6ba87ab
    Signed-off-by: Yacov Manevich <yacovm@il.ibm.com>
    (cherry picked from commit 4e201afc69efb6d6546e1be904c5e658ca56b9a0)

--- core/deliverservice/config_test.go
+++ core/deliverservice/config_test.go
@@ -92,6 +92,7 @@ func TestGlobalConfig(t *testing.T) {
 	coreConfig := deliverservice.GlobalConfig()
 
 	expectedConfig := &deliverservice.DeliverServiceConfig{
+		BlockGossipEnabled:          true,
 		PeerTLSEnabled:              true,
 		ReConnectBackoffThreshold:   25 * time.Second,
 		ReconnectTotalTimeThreshold: 20 * time.Second,
@@ -118,6 +119,7 @@ func TestGlobalConfigDefault(t *testing.T) {
 	coreConfig := deliverservice.GlobalConfig()
 
 	expectedConfig := &deliverservice.DeliverServiceConfig{
+		BlockGossipEnabled:          true,
 		PeerTLSEnabled:              false,
 		ReConnectBackoffThreshold:   deliverservice.DefaultReConnectBackoffThreshold,
 		ReconnectTotalTimeThreshold: deliverservice.DefaultReConnectTotalTimeThreshold,
