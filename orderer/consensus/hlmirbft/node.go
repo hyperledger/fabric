dacfbf9aea636808a64bd766e5af285c445484d8
renjithpta <renjithkn@gmail.com>
2021-06-17 10:46:28 +0530

    Update node.go

--- orderer/consensus/hlmirbft/node.go
+++ orderer/consensus/hlmirbft/node.go
@@ -7,11 +7,19 @@ SPDX-License-Identifier: Apache-2.0
 package hlmirbft
 
 import (
+	"fmt"
+	"hash"
+	"io"
+	"os"
+	"path/filepath"
 	"sync"
 	"time"
 
 	"github.com/fly2plan/fabric-protos-go/orderer/hlmirbft"
 	"github.com/hyperledger-labs/mirbft"
+	"github.com/hyperledger-labs/mirbft/pkg/pb/msgs"
+	"github.com/hyperledger-labs/mirbft/pkg/processor"
+	"github.com/pkg/errors"
 
 	"code.cloudfoundry.org/clock"
 	"github.com/hyperledger/fabric/common/flogging"
@@ -33,7 +41,25 @@ type node struct {
 
 	rpc RPC
 
+	//storage *MirBFTStorage //SNAPCHANGE- need to change Mir Storage Structure
+
 	chain *Chain
+
+	//JIRA FLY2 - 66 proposed changes Added  new variables
+	// - Hasher
+	// - ActiveHash
+	// - CheckpointSeqNumber
+	// - LastSeqNo
+	// - CheckpointState
+	// - CheckpointHash
+	// - PendingReconfigurations
+	Hasher                  processor.Hasher
+	ActiveHash              hash.Hash
+	CheckpointSeqNo         uint64
+	CheckpointState         *msgs.NetworkState
+	CheckpointHash          []byte
+	PendingReconfigurations []*msgs.Reconfiguration
+	SnapByteMap             map[string]uint64
 
 	tickInterval time.Duration
 	clock        clock.Clock
@@ -45,8 +71,11 @@ type node struct {
 	mirbft.Node
 }
 
+const snapSuffix = ".snap"
+
 // TODO(harry_knight) Of node struct, storage, config, and metadata, need to be replaced with hlmirbft counterparts.
 func (n *node) start(fresh, join bool) {
+
 	/*	raftPeers := RaftPeers(n.metadata.ConsenterIds)
 		n.logger.Debugf("Starting raft node: #peers: %v", len(raftPeers))
 
@@ -287,14 +316,70 @@ func (n *node) logSendFailure(dest uint64, err error) {
 	n.unreachable[dest] = struct{}{}
 }
 
-func (n *node) takeSnapshot(index uint64, cs raftpb.ConfState, data []byte) {
-	/*if err := n.storage.TakeSnapshot(index, cs, data); err != nil {
-		n.logger.Errorf("Failed to create snapshot at index %d: %s", index, err)
-	}*/
-}
-
 func (n *node) lastIndex() uint64 {
 	/*i, _ := n.storage.ram.LastIndex()
 	return i*/
 	return 0
+}
+
+//JIRA FLY2- 66 Proposed changes:Implemented the PersistSnapshot functionality to persist the snaps to local files
+func (n *node) PersistSnapshot(seqNo uint64, Data []byte) error {
+	if err := os.MkdirAll(n.chain.opts.SnapDir, os.ModePerm); err != nil {
+		return errors.Errorf("failed to mkdir '%s' for snapshot: %s", n.chain.opts.SnapDir, err)
+	}
+	TimeStamp := time.Now().Unix()
+	fname := fmt.Sprintf("%016x-%016x%s", seqNo, TimeStamp, snapSuffix )
+
+	spath := filepath.Join(n.chain.opts.SnapDir, fname)
+
+	f, err := os.OpenFile(spath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+
+	byteNumber, err := f.Write(Data)
+	if err == nil && byteNumber < len(Data) {
+		err = io.ErrShortWrite
+		return err
+	}
+	if err1 := f.Close(); err == nil {
+		err = err1
+	}
+	return err
+}
+
+//JIRA FLY2-58 proposed changes:readSnapFiles loads the snap file based on the sequence number and reads the contents
+func (n *node) readSnapFiles(seqNo uint64, SnapDir string) ([]byte, error) {
+	var snapBytes []byte
+	fileNamePattern := fmt.Sprintf("%016x-*", seqNo)
+
+	snapFileList, err := GetFileByPattern(SnapDir, fileNamePattern)
+	if err != nil {
+		return nil, err
+	}
+	numberOfFiles := len(snapFileList)
+	switch {
+	case numberOfFiles == 0:
+		err = errors.Errorf("file not found Error : No files found for sequence number %016x", seqNo)
+		snapBytes = nil
+	case numberOfFiles == 1:
+		snapBytes, err = ReadFile(SnapDir, snapFileList[0])
+	case numberOfFiles > 1:
+		n.logger.Warning("File Duplication : multiple files detected for sequence number %016x", seqNo)
+		snapBytes, err = ReadFile(SnapDir, snapFileList[numberOfFiles-1])
+	}
+	return snapBytes, err
+}
+
+// JIRA FLY2-58 proposed changes: PurgeSnapFiles takes the list of snap files in the snap directory  and removes them
+func (n *node) purgeSnapFiles(SnapDir string) error {
+	snapFileList, err := GetFileByPattern(SnapDir, snapSuffix )
+	if err != nil {
+		return errors.Errorf("Cannot retrive snap files : %s", err)
+	}
+	err = PurgeFiles(snapFileList[:len(snapFileList)-2], SnapDir, n.logger)
+	if err != nil {
+		return err
+	}
+	return nil
 }
