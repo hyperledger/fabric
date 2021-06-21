ac778f6a60abe3050d0bfb34392b86fda66faa2a
renjithpta <renjithkn@gmail.com>
2021-06-21 11:35:34 +0530

    Update node.go

--- orderer/consensus/hlmirbft/node.go
+++ orderer/consensus/hlmirbft/node.go
@@ -8,13 +8,11 @@ package hlmirbft
 
 import (
 	"crypto"
-	"sync"
-
 	"fmt"
-	"hash"
 	"io"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/fly2plan/fabric-protos-go/orderer/hlmirbft"
@@ -27,9 +25,8 @@ import (
 
 	"code.cloudfoundry.org/clock"
 	"github.com/hyperledger/fabric/common/flogging"
-	"go.etcd.io/etcd/raft/raftpb"
-	"github.com/hyperledger-labs/mirbft/pkg/processor"
 	"github.com/pkg/errors"
+	"go.etcd.io/etcd/raft/raftpb"
 )
 
 type node struct {
@@ -42,16 +39,10 @@ type node struct {
 
 	config      *mirbft.Config
 	WALDir      string
-  
 	ReqStoreDir string
-	Hasher                  processor.Hasher  //JIRA FLY2-66
-	ActiveHash              hash.Hash  //JIRA FLY2-66
-	CheckpointSeqNo         uint64  //JIRA FLY2-66
-	CheckpointState         *msgs.NetworkState  //JIRA FLY2-66
-	CheckpointHash          []byte  //JIRA FLY2-66
-	PendingReconfigurations []*msgs.Reconfiguration  //JIRA FLY2-66
-	SnapByteMap             map[string]uint64 //JIRA FLY2-66
 
+	CheckpointSeqNo uint64 //JIRA FLY2-66
+	PendingReconfigurations []*msgs.Reconfiguration  //JIRA FLY2-66
 
 	rpc RPC
 
@@ -63,8 +54,8 @@ type node struct {
 
 	mirbft.Node
 }
-const snapSuffix = ".snap"
 
+const snapSuffix = ".snap"
 
 func (n *node) start(fresh, join bool) {
 	n.logger.Debugf("Starting mirbft node: #peers: %v", len(n.metadata.ConsenterIds))
@@ -116,112 +107,7 @@ func (n *node) start(fresh, join bool) {
 // 	It serves to start, manage, and respond to the internal clock of the FSM.
 // 	Auxiliary calls should be adapted to occur during block genesis/orderer service startup.
 func (n *node) run(campaign bool) {
-	/*electionTimeout := n.tickInterval.Seconds() * float64(n.config.ElectionTick)
-	halfElectionTimeout := electionTimeout / 2
 
-	raftTicker := n.clock.NewTicker(n.tickInterval)
-
-	if s := n.storage.Snapshot(); !raft.IsEmptySnap(s) {
-		n.chain.snapC <- &s
-	}
-
-	elected := make(chan struct{})
-	if campaign {
-		n.logger.Infof("This node is picked to start campaign")
-		go func() {
-			// Attempt campaign every two HeartbeatTimeout elapses, until leader is present - either this
-			// node successfully claims leadership, or another leader already existed when this node starts.
-			// We could do this more lazily and exit proactive campaign once transitioned to Candidate state
-			// (not PreCandidate because other nodes might not have started yet, in which case PreVote
-			// messages are dropped at recipients). But there is no obvious reason (for now) to be lazy.
-			//
-			// 2*HeartbeatTick is used to avoid excessive campaign when network latency is significant and
-			// Raft term keeps advancing in this extreme case.
-			campaignTicker := n.clock.NewTicker(n.tickInterval * time.Duration(n.config.HeartbeatTick) * 2)
-			defer campaignTicker.Stop()
-
-			for {
-				select {
-				case <-campaignTicker.C():
-					n.Campaign(context.TODO())
-				case <-elected:
-					return
-				case <-n.chain.doneC:
-					return
-				}
-			}
-		}()
-	}
-
-	var notifyLeaderChangeC chan uint64
-
-	for {
-		select {
-		case <-raftTicker.C():
-			// grab raft Status before ticking it, so `RecentActive` attributes
-			// are not reset yet.
-			status := n.Status()
-
-			n.Tick()
-			n.tracker.Check(&status)
-
-		case rd := <-n.Ready():
-			startStoring := n.clock.Now()
-			if err := n.storage.Store(rd.Entries, rd.HardState, rd.Snapshot); err != nil {
-				n.logger.Panicf("Failed to persist etcd/raft data: %s", err)
-			}
-			duration := n.clock.Since(startStoring).Seconds()
-			n.metrics.DataPersistDuration.Observe(float64(duration))
-			if duration > halfElectionTimeout {
-				n.logger.Warningf("WAL sync took %v seconds and the network is configured to start elections after %v seconds. Your disk is too slow and may cause loss of quorum and trigger leadership election.", duration, electionTimeout)
-			}
-
-			if !raft.IsEmptySnap(rd.Snapshot) {
-				n.chain.snapC <- &rd.Snapshot
-			}
-
-			if notifyLeaderChangeC != nil && rd.SoftState != nil {
-				if l := atomic.LoadUint64(&rd.SoftState.Lead); l != raft.None {
-					select {
-					case notifyLeaderChangeC <- l:
-					default:
-					}
-
-					notifyLeaderChangeC = nil
-				}
-			}
-
-			// skip empty apply
-			if len(rd.CommittedEntries) != 0 || rd.SoftState != nil {
-				n.chain.applyC <- apply{rd.CommittedEntries, rd.SoftState}
-			}
-
-			if campaign && rd.SoftState != nil {
-				leader := atomic.LoadUint64(&rd.SoftState.Lead) // etcdraft requires atomic access to this var
-				if leader != raft.None {
-					n.logger.Infof("Leader %d is present, quit campaign", leader)
-					campaign = false
-					close(elected)
-				}
-			}
-
-			n.Advance()
-
-			// TODO(jay_guo) leader can write to disk in parallel with replicating
-			// to the followers and them writing to their disks. Check 10.2.1 in thesis
-			n.send(rd.Messages)
-
-		case notifyLeaderChangeC = <-n.subscriberC:
-
-		case <-n.chain.haltC:
-			raftTicker.Stop()
-			n.Stop()
-			n.storage.Close()
-			n.logger.Infof("Raft node stopped")
-			close(n.chain.doneC) // close after all the artifacts are closed
-			return
-		}
-	}*/
 }
 
 func (n *node) Send(dest uint64, msg *msgs.Msg) {
@@ -243,56 +129,7 @@ func (n *node) Send(dest uint64, msg *msgs.Msg) {
 // If this is called on follower, it simply waits for a
 // leader change till timeout (ElectionTimeout).
 func (n *node) abdicateLeader(currentLead uint64) {
-	/*status := n.Status()
 
-	if status.Lead != raft.None && status.Lead != currentLead {
-		n.logger.Warn("Leader has changed since asked to transfer leadership")
-		return
-	}
-
-	// register a leader subscriberC
-	notifyc := make(chan uint64, 1)
-	select {
-	case n.subscriberC <- notifyc:
-	case <-n.chain.doneC:
-		return
-	}
-
-	// Leader initiates leader transfer
-	if status.RaftState == raft.StateLeader {
-		var transferee uint64
-		for id, pr := range status.Progress {
-			if id == status.ID {
-				continue // skip self
-			}
-
-			if pr.RecentActive && !pr.Paused {
-				transferee = id
-				break
-			}
-
-			n.logger.Debugf("Node %d is not qualified as transferee because it's either paused or not active", id)
-		}
-
-		if transferee == raft.None {
-			n.logger.Errorf("No follower is qualified as transferee, abort leader transfer")
-			return
-		}
-
-		n.logger.Infof("Transferring leadership to %d", transferee)
-		n.TransferLeadership(context.TODO(), status.ID, transferee)
-	}
-
-	timer := n.clock.NewTimer(time.Duration(n.config.ElectionTick) * n.tickInterval)
-	defer timer.Stop() // prevent timer leak
-
-	select {
-	case <-timer.C():
-		n.logger.Warn("Leader transfer timeout")
-	case l := <-notifyc:
-		n.logger.Infof("Leader has been transferred from %d to %d", currentLead, l)
-	case <-n.chain.doneC:
-	}*/
 }
 
 func (n *node) logSendFailure(dest uint64, err error) {
@@ -317,12 +154,16 @@ func (n *node) lastIndex() uint64 {
 	return 0
 }
 
+
+
 //JIRA FLY2- 66 Proposed changes:Implemented the PersistSnapshot functionality to persist the snaps to local files
 func (n *node) PersistSnapshot(seqNo uint64, Data []byte) error {
 	if err := os.MkdirAll(n.chain.opts.SnapDir, os.ModePerm); err != nil {
 		return errors.Errorf("failed to mkdir '%s' for snapshot: %s", n.chain.opts.SnapDir, err)
 	}
+
 	TimeStamp := time.Now().Unix()
+
 	fname := fmt.Sprintf("%016x-%016x%s", seqNo, TimeStamp, snapSuffix )
 
 	spath := filepath.Join(n.chain.opts.SnapDir, fname)
@@ -343,9 +184,12 @@ func (n *node) PersistSnapshot(seqNo uint64, Data []byte) error {
 	return err
 }
 
+
 // JIRA FLY2-66 proposed changes: PurgeSnapFiles takes the list of snap files in the snap directory  and removes them
 func (n *node) PurgeSnapFiles(SnapDir string) error {
+
 	snapFileList, err := filepath.Glob(filepath.Join(SnapDir, snapSuffix))
+
 	if err != nil {
 		return errors.Errorf("Cannot retrive snap files : %s", err)
 	}
@@ -355,3 +199,5 @@ func (n *node) PurgeSnapFiles(SnapDir string) error {
 	}
 	return nil
 }
+
+
