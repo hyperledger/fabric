2d2d22f594be2e87b1d8eedef9c386e294370b3b
Yacov Manevich <yacovm@il.ibm.com>
2021-06-21 23:04:02 +0300

    Mandate TLS 1.2 or higher in fabhttp package
    This commit ensures that the HTTP server that is spawned by the fabhttp package
    only accepts TLS handshakes from clients that attempt to use TLS 1.2 or higher.
    
    Change-Id: Ia25482d9c96f68506724a58258451311b3d63208
    Signed-off-by: Yacov Manevich <yacovm@il.ibm.com>
    (cherry picked from commit 16259ed70a4cd57513be40a0678213d9274a9ae8)

--- common/fabhttp/tls_test.go
+++ common/fabhttp/tls_test.go
@@ -60,6 +60,7 @@ var _ = Describe("TLS", func() {
 		tlsConfig, err := httpTLS.Config()
 		Expect(err).NotTo(HaveOccurred())
 		Expect(tlsConfig).To(Equal(&tls.Config{
+			MinVersion:   tls.VersionTLS12,
 			Certificates: []tls.Certificate{cert},
 			CipherSuites: []uint16{
 				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
