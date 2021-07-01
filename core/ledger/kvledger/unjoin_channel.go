648175a1cfc0820eba6abad0a8acf0561d6a7b96
Josh Kneubuhl <jkneubuh@us.ibm.com>
2021-07-01 09:22:52 -0400

    [FAB-11334] Adds a new 'peer node unjoin' feature
    The UnjoinChannel routine will mark a ledger as pending delete, removing all
    channel specific data from the peer.  The routine must be invoked while the
    peer is down.  Peer operators may issue the deletion by running the companion
    'peer node unjoin' command from the console.
    
    Signed-off-by: Josh Kneubuhl <jkneubuh@us.ibm.com>

--- core/ledger/kvledger/unjoin_channel.go
+++ core/ledger/kvledger/unjoin_channel.go
@@ -11,14 +11,55 @@ import (
 	"github.com/hyperledger/fabric-protos-go/ledger/rwset/kvrwset"
 	"github.com/hyperledger/fabric-protos-go/peer"
 	"github.com/hyperledger/fabric/common/ledger/blkstorage"
+	"github.com/hyperledger/fabric/common/ledger/util/leveldbhelper"
 	"github.com/hyperledger/fabric/common/metrics/disabled"
 	"github.com/hyperledger/fabric/core/ledger"
 	"github.com/hyperledger/fabric/core/ledger/confighistory"
 	"github.com/hyperledger/fabric/core/ledger/kvledger/bookkeeping"
 	"github.com/hyperledger/fabric/core/ledger/kvledger/history"
+	"github.com/hyperledger/fabric/core/ledger/kvledger/msgs"
 	"github.com/hyperledger/fabric/core/ledger/kvledger/txmgmt/privacyenabledstate"
 	"github.com/hyperledger/fabric/core/ledger/pvtdatastorage"
+	"github.com/pkg/errors"
 )
+
+// UnjoinChannel removes the data for a ledger and sets the status to UNDER_DELETION.  This function is to be
+// invoked while the peer is shut down.
+func UnjoinChannel(config *ledger.Config, ledgerID string) error {
+	// Ensure the routine is invoked while the peer is down.
+	fileLock := leveldbhelper.NewFileLock(fileLockPath(config.RootFSPath))
+	if err := fileLock.Lock(); err != nil {
+		return errors.WithMessage(err, "as another peer node command is executing,"+
+			" wait for that command to complete its execution or terminate it before retrying")
+	}
+	defer fileLock.Unlock()
+
+	idStore, err := openIDStore(LedgerProviderPath(config.RootFSPath))
+	if err != nil {
+		return errors.WithMessagef(err, "unjoin channel [%s]", ledgerID)
+	}
+	defer idStore.db.Close()
+
+	// Set the ledger to a pending deletion status.  If the contents of the ledger are not
+	// fully removed (e.g. a crash during deletion, i/o error, etc.), the deletion may be
+	// resumed at the next peer start.
+	if err := idStore.updateLedgerStatus(ledgerID, msgs.Status_UNDER_DELETION); err != nil {
+		return errors.WithMessagef(err, "unjoin channel [%s]", ledgerID)
+	}
+
+	// remove the ledger data
+	if err := removeLedgerData(config, ledgerID); err != nil {
+		return errors.WithMessagef(err, "deleting ledger [%s]", ledgerID)
+	}
+
+	// Delete the ledger from the ID storage after the contents have been purged.
+	if err := idStore.deleteLedgerID(ledgerID); err != nil {
+		return errors.WithMessagef(err, "deleting ledger [%s]", ledgerID)
+	}
+
+	logger.Infow("channel has been successfully unjoined", "ledgerID", ledgerID)
+	return nil
+}
 
 // removeLedgerData removes the data for a given ledger. This function should be invoked when the peer is not running and the caller should hold the file lock for the KVLedgerProvider
 func removeLedgerData(config *ledger.Config, ledgerID string) error {
@@ -39,6 +80,7 @@ func removeLedgerData(config *ledger.Config, ledgerID string) error {
 	if err != nil {
 		return err
 	}
+	defer bookkeepingProvider.Close()
 
 	dbProvider, err := privacyenabledstate.NewDBProvider(
 		bookkeepingProvider,
