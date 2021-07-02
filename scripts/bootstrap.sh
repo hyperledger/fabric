c067239b5de7e69ff70f567ca6e80740a177d3ef
Nikhil Gupta <ngupta@symbridge.com>
2021-07-02 16:14:06 -0400

    Update boostrap.sh for test network
    Signed-off-by: Nikhil Gupta <ngupta@symbridge.com>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -49,7 +49,7 @@ dockerPull() {
 cloneSamplesRepo() {
     # clone (if needed) hyperledger/fabric-samples and checkout corresponding
     # version to the binaries and docker images to be downloaded
-    if [ -d first-network ]; then
+    if [ -d test-network ]; then
         # if we are in the fabric-samples repo, checkout corresponding version
         echo "==> Already in fabric-samples repo"
     elif [ -d fabric-samples ]; then
