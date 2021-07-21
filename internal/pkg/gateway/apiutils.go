0a01525d88dc32485f8eb538e8a2614826ab04ec
andrew-coleman <andrew_coleman@uk.ibm.com>
2021-07-21 15:55:36 +0100

    FABGW-25 Endorse using generated ChaincodeInterest
    Change the Endorse() method logic as described in the design document attached to the Jira.
    - Process proposal on local org peer.
    - Using the ChaincodeInterest in the propsal response, invoke discovery to obtain a layout that contains the local org
    - Obtain the remaining endorsements
    - Build the transaction to send back to the client.
    
    Extra unit tests added to test the new logic.
    
    Signed-off-by: andrew-coleman <andrew_coleman@uk.ibm.com>

--- internal/pkg/gateway/apiutils.go
+++ internal/pkg/gateway/apiutils.go
@@ -19,7 +19,7 @@ import (
 func getTransactionResponse(response *peer.ProposalResponse) (*peer.Response, error) {
 	var retVal *peer.Response
 
-	if response != nil && response.Payload != nil {
+	if response.GetPayload() != nil {
 		payload, err := protoutil.UnmarshalProposalResponsePayload(response.Payload)
 		if err != nil {
 			return nil, err
@@ -30,7 +30,7 @@ func getTransactionResponse(response *peer.ProposalResponse) (*peer.Response, er
 			return nil, err
 		}
 
-		if extension != nil && extension.Response != nil {
+		if extension.GetResponse() != nil {
 			if extension.Response.Status < 200 || extension.Response.Status >= 400 {
 				return nil, fmt.Errorf("error %d, %s", extension.Response.Status, extension.Response.Message)
 			}
@@ -41,32 +41,32 @@ func getTransactionResponse(response *peer.ProposalResponse) (*peer.Response, er
 	return retVal, nil
 }
 
-func getChannelAndChaincodeFromSignedProposal(signedProposal *peer.SignedProposal) (string, string, error) {
+func getChannelAndChaincodeFromSignedProposal(signedProposal *peer.SignedProposal) (string, string, bool, error) {
 	if signedProposal == nil {
-		return "", "", fmt.Errorf("a signed proposal is required")
+		return "", "", false, fmt.Errorf("a signed proposal is required")
 	}
 	proposal, err := protoutil.UnmarshalProposal(signedProposal.ProposalBytes)
 	if err != nil {
-		return "", "", err
+		return "", "", false, err
 	}
 	header, err := protoutil.UnmarshalHeader(proposal.Header)
 	if err != nil {
-		return "", "", err
+		return "", "", false, err
 	}
 	channelHeader, err := protoutil.UnmarshalChannelHeader(header.ChannelHeader)
 	if err != nil {
-		return "", "", err
+		return "", "", false, err
 	}
 	payload, err := protoutil.UnmarshalChaincodeProposalPayload(proposal.Payload)
 	if err != nil {
-		return "", "", err
+		return "", "", false, err
 	}
 	spec, err := protoutil.UnmarshalChaincodeInvocationSpec(payload.Input)
 	if err != nil {
-		return "", "", err
+		return "", "", false, err
 	}
 
-	return channelHeader.ChannelId, spec.ChaincodeSpec.ChaincodeId.Name, nil
+	return channelHeader.ChannelId, spec.ChaincodeSpec.ChaincodeId.Name, len(payload.TransientMap) > 0, nil
 }
 
 func rpcError(code codes.Code, message string, details ...proto.Message) error {
