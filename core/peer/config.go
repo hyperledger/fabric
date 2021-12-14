205611130e36256b4de5e9201783d57eda0e2740
andrew-coleman <andrew_coleman@uk.ibm.com>
2021-12-14 16:12:16 +0000

    Enable gateway concurrency limits
    Add support for setting API invocation concurrency limits on the gateway service.  Uses the same mechanism as used by the endorser and deliver services.
    
    The sample config limit has been set to 500 for now.  This can be adjusted in the future based on experience.
    
    Signed-off-by: andrew-coleman <andrew_coleman@uk.ibm.com>

--- core/peer/config.go
+++ core/peer/config.go
@@ -125,6 +125,10 @@ type Config struct {
 	// registered to deliver service for blocks and transaction events.
 	LimitsConcurrencyDeliverService int
 
+	// LimitsConcurrencyGatewayService sets the limits for concurrent requests to
+	// gateway service that handles the submission and evaluation of transactions.
+	LimitsConcurrencyGatewayService int
+
 	// ----- TLS -----
 	// Require server-side TLS.
 	// TODO: create separate sub-struct for PeerTLS config.
@@ -251,6 +255,7 @@ func (c *Config) load() error {
 	c.NetworkID = viper.GetString("peer.networkId")
 	c.LimitsConcurrencyEndorserService = viper.GetInt("peer.limits.concurrency.endorserService")
 	c.LimitsConcurrencyDeliverService = viper.GetInt("peer.limits.concurrency.deliverService")
+	c.LimitsConcurrencyGatewayService = viper.GetInt("peer.limits.concurrency.gatewayService")
 	c.DiscoveryEnabled = viper.GetBool("peer.discovery.enabled")
 	c.ProfileEnabled = viper.GetBool("peer.profile.enabled")
 	c.ProfileListenAddress = viper.GetString("peer.profile.listenAddress")
