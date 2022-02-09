cedbd5102a446a401b676a066df00ddc481583b3
manish <manish.sethi@gmail.com>
2022-02-09 08:50:52 -0500

    Filter out purged data from private data store
    This PR introduces support for the purge marker(s) and filters the data
    marked for purging. In addition, this PR removes the
    
    unused code for V11 format as now we upgraded data format along with
    retroactively creating hashed index at peer start.
    
    Signed-off-by: manish <manish.sethi@gmail.com>

--- core/ledger/pvtdatastorage/v11.go
+++ core/ledger/pvtdatastorage/v11.go
@@ -9,7 +9,6 @@ package pvtdatastorage
 import (
 	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric-protos-go/ledger/rwset"
-	"github.com/hyperledger/fabric/common/ledger/util/leveldbhelper"
 	"github.com/hyperledger/fabric/core/ledger"
 	"github.com/hyperledger/fabric/core/ledger/internal/version"
 )
@@ -37,24 +36,7 @@ func v11DecodePvtRwSet(encodedBytes []byte) (*rwset.TxPvtReadWriteSet, error) {
 	return writeset, proto.Unmarshal(encodedBytes, writeset)
 }
 
-func v11RetrievePvtdata(itr *leveldbhelper.Iterator, filter ledger.PvtNsCollFilter) ([]*ledger.TxPvtData, error) {
-	var blkPvtData []*ledger.TxPvtData
-	txPvtData, err := v11DecodeKV(itr.Key(), itr.Value(), filter)
-	if err != nil {
-		return nil, err
-	}
-	blkPvtData = append(blkPvtData, txPvtData)
-	for itr.Next() {
-		pvtDatum, err := v11DecodeKV(itr.Key(), itr.Value(), filter)
-		if err != nil {
-			return nil, err
-		}
-		blkPvtData = append(blkPvtData, pvtDatum)
-	}
-	return blkPvtData, nil
-}
-
-func v11DecodeKV(k, v []byte, filter ledger.PvtNsCollFilter) (*ledger.TxPvtData, error) {
+func v11DecodeKV(k, v []byte) (*ledger.TxPvtData, error) {
 	bNum, tNum, err := v11DecodePK(k)
 	if err != nil {
 		return nil, err
@@ -64,38 +46,5 @@ func v11DecodeKV(k, v []byte, filter ledger.PvtNsCollFilter) (*ledger.TxPvtData,
 		return nil, err
 	}
 	logger.Debugf("Retrieved V11 private data write set for block [%d] tran [%d]", bNum, tNum)
-	filteredWSet := v11TrimPvtWSet(pvtWSet, filter)
-	return &ledger.TxPvtData{SeqInBlock: tNum, WriteSet: filteredWSet}, nil
-}
-
-func v11TrimPvtWSet(pvtWSet *rwset.TxPvtReadWriteSet, filter ledger.PvtNsCollFilter) *rwset.TxPvtReadWriteSet {
-	if filter == nil {
-		return pvtWSet
-	}
-
-	var filteredNsRwSet []*rwset.NsPvtReadWriteSet
-	for _, ns := range pvtWSet.NsPvtRwset {
-		var filteredCollRwSet []*rwset.CollectionPvtReadWriteSet
-		for _, coll := range ns.CollectionPvtRwset {
-			if filter.Has(ns.Namespace, coll.CollectionName) {
-				filteredCollRwSet = append(filteredCollRwSet, coll)
-			}
-		}
-		if filteredCollRwSet != nil {
-			filteredNsRwSet = append(filteredNsRwSet,
-				&rwset.NsPvtReadWriteSet{
-					Namespace:          ns.Namespace,
-					CollectionPvtRwset: filteredCollRwSet,
-				},
-			)
-		}
-	}
-	var filteredTxPvtRwSet *rwset.TxPvtReadWriteSet
-	if filteredNsRwSet != nil {
-		filteredTxPvtRwSet = &rwset.TxPvtReadWriteSet{
-			DataModel:  pvtWSet.GetDataModel(),
-			NsPvtRwset: filteredNsRwSet,
-		}
-	}
-	return filteredTxPvtRwSet
+	return &ledger.TxPvtData{SeqInBlock: tNum, WriteSet: pvtWSet}, nil
 }
