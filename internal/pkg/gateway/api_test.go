c55b7eed46da67098f9393dbb23df4167a53ebff
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2022-03-15 17:01:58 +0000

    Refactor of ChaincodeEvents service implementation to support resume (#3283)
    Enables the client to (optionally) specify an AfterTransactionId property in addition to a start block number when requesting chaincode events, which causes chaincode events up to that transaction ID (inclusive) to be ignored and not returned to the client. This supports resume of chaincode event listening on client reconnect without duplicating or missing any events.
    
    Signed-off-by: Mark S. Lewis <Mark.S.Lewis@outlook.com>

--- internal/pkg/gateway/api_test.go
+++ internal/pkg/gateway/api_test.go
@@ -152,6 +152,7 @@ type testDef struct {
 	interest           *peer.ChaincodeInterest
 	blocks             []*cp.Block
 	startPosition      *ab.SeekPosition
+	afterTxID          string
 }
 
 type preparedTest struct {
@@ -1389,130 +1390,108 @@ func TestCommitStatus(t *testing.T) {
 
 func TestChaincodeEvents(t *testing.T) {
 	now := time.Now()
-	transactionId := "TRANSACTION_ID"
+	lastTransactionID := "LAST_TX_ID"
 
-	matchChaincodeEvent := &peer.ChaincodeEvent{
-		ChaincodeId: testChaincode,
-		TxId:        transactionId,
-		EventName:   "EVENT_NAME",
-		Payload:     []byte("PAYLOAD"),
+	newChaincodeEvent := func(chaincodeName string, transactionID string) *peer.ChaincodeEvent {
+		return &peer.ChaincodeEvent{
+			ChaincodeId: chaincodeName,
+			TxId:        transactionID,
+			EventName:   "EVENT_NAME",
+			Payload:     []byte("PAYLOAD"),
+		}
 	}
 
-	mismatchChaincodeEvent := &peer.ChaincodeEvent{
-		ChaincodeId: "WRONG_CHAINCODE_ID",
-		TxId:        transactionId,
-		EventName:   "EVENT_NAME",
-		Payload:     []byte("PAYLOAD"),
-	}
-
-	txHeader := &cp.Header{
-		ChannelHeader: protoutil.MarshalOrPanic(&cp.ChannelHeader{
-			Type: int32(cp.HeaderType_ENDORSER_TRANSACTION),
-			Timestamp: &timestamp.Timestamp{
-				Seconds: now.Unix(),
-				Nanos:   int32(now.Nanosecond()),
-			},
-			TxId: transactionId,
-		}),
-	}
-
-	matchTxEnvelope := &cp.Envelope{
-		Payload: protoutil.MarshalOrPanic(&cp.Payload{
-			Header: txHeader,
-			Data: protoutil.MarshalOrPanic(&peer.Transaction{
-				Actions: []*peer.TransactionAction{
-					{
-						Payload: protoutil.MarshalOrPanic(&peer.ChaincodeActionPayload{
-							Action: &peer.ChaincodeEndorsedAction{
-								ProposalResponsePayload: protoutil.MarshalOrPanic(&peer.ProposalResponsePayload{
-									Extension: protoutil.MarshalOrPanic(&peer.ChaincodeAction{
-										Events: protoutil.MarshalOrPanic(matchChaincodeEvent),
-									}),
-								}),
-							},
-						}),
-					},
+	newTransactionHeader := func(transactionID string) *cp.Header {
+		return &cp.Header{
+			ChannelHeader: protoutil.MarshalOrPanic(&cp.ChannelHeader{
+				Type: int32(cp.HeaderType_ENDORSER_TRANSACTION),
+				Timestamp: &timestamp.Timestamp{
+					Seconds: now.Unix(),
+					Nanos:   int32(now.Nanosecond()),
 				},
+				TxId: transactionID,
 			}),
-		}),
+		}
 	}
 
-	mismatchTxEnvelope := &cp.Envelope{
-		Payload: protoutil.MarshalOrPanic(&cp.Payload{
-			Header: txHeader,
-			Data: protoutil.MarshalOrPanic(&peer.Transaction{
-				Actions: []*peer.TransactionAction{
-					{
-						Payload: protoutil.MarshalOrPanic(&peer.ChaincodeActionPayload{
-							Action: &peer.ChaincodeEndorsedAction{
-								ProposalResponsePayload: protoutil.MarshalOrPanic(&peer.ProposalResponsePayload{
-									Extension: protoutil.MarshalOrPanic(&peer.ChaincodeAction{
-										Events: protoutil.MarshalOrPanic(mismatchChaincodeEvent),
+	newTransactionEnvelope := func(event *peer.ChaincodeEvent) *cp.Envelope {
+		return &cp.Envelope{
+			Payload: protoutil.MarshalOrPanic(&cp.Payload{
+				Header: newTransactionHeader(event.GetTxId()),
+				Data: protoutil.MarshalOrPanic(&peer.Transaction{
+					Actions: []*peer.TransactionAction{
+						{
+							Payload: protoutil.MarshalOrPanic(&peer.ChaincodeActionPayload{
+								Action: &peer.ChaincodeEndorsedAction{
+									ProposalResponsePayload: protoutil.MarshalOrPanic(&peer.ProposalResponsePayload{
+										Extension: protoutil.MarshalOrPanic(&peer.ChaincodeAction{
+											Events: protoutil.MarshalOrPanic(event),
+										}),
 									}),
-								}),
-							},
-						}),
-					},
-				},
-			}),
-		}),
-	}
-
-	block100Proto := &cp.Block{
-		Header: &cp.BlockHeader{
-			Number: 100,
-		},
-		Metadata: &cp.BlockMetadata{
-			Metadata: [][]byte{
-				nil,
-				nil,
-				{
-					byte(peer.TxValidationCode_VALID),
-				},
-				nil,
-				nil,
-			},
-		},
-		Data: &cp.BlockData{
-			Data: [][]byte{
-				protoutil.MarshalOrPanic(mismatchTxEnvelope),
-			},
-		},
-	}
-
-	block101Proto := &cp.Block{
-		Header: &cp.BlockHeader{
-			Number: 101,
-		},
-		Metadata: &cp.BlockMetadata{
-			Metadata: [][]byte{
-				nil,
-				nil,
-				{
-					byte(peer.TxValidationCode_VALID),
-					byte(peer.TxValidationCode_VALID),
-					byte(peer.TxValidationCode_VALID),
-				},
-				nil,
-				nil,
-			},
-		},
-		Data: &cp.BlockData{
-			Data: [][]byte{
-				protoutil.MarshalOrPanic(&cp.Envelope{
-					Payload: protoutil.MarshalOrPanic(&cp.Payload{
-						Header: &cp.Header{
-							ChannelHeader: protoutil.MarshalOrPanic(&cp.ChannelHeader{
-								Type: int32(cp.HeaderType_CONFIG_UPDATE),
+								},
 							}),
 						},
-					}),
+					},
 				}),
-				protoutil.MarshalOrPanic(mismatchTxEnvelope),
-				protoutil.MarshalOrPanic(matchTxEnvelope),
-			},
-		},
+			}),
+		}
 	}
+
+	newBlock := func(number uint64) *cp.Block {
+		return &cp.Block{
+			Header: &cp.BlockHeader{
+				Number: number,
+			},
+			Metadata: &cp.BlockMetadata{
+				Metadata: make([][]byte, 5),
+			},
+			Data: &cp.BlockData{
+				Data: [][]byte{},
+			},
+		}
+	}
+
+	addTransaction := func(block *cp.Block, transaction *cp.Envelope, status peer.TxValidationCode) {
+		metadata := block.GetMetadata().GetMetadata()
+		metadata[cp.BlockMetadataIndex_TRANSACTIONS_FILTER] = append(metadata[cp.BlockMetadataIndex_TRANSACTIONS_FILTER], byte(status))
+
+		blockData := block.GetData()
+		blockData.Data = append(blockData.Data, protoutil.MarshalOrPanic(transaction))
+	}
+
+	matchEvent := newChaincodeEvent(testChaincode, "EXPECTED_TX_ID")
+	wrongChaincodeEvent := newChaincodeEvent("WRONG_CHAINCODE", "WRONG__TX_ID")
+	oldTransactionEvent := newChaincodeEvent(testChaincode, "OLD_TX_ID")
+	lastTransactionEvent := newChaincodeEvent(testChaincode, lastTransactionID)
+	lastTransactionWrongChaincodeEvent := newChaincodeEvent("WRONG_CHAINCODE", lastTransactionID)
+
+	configTxEnvelope := &cp.Envelope{
+		Payload: protoutil.MarshalOrPanic(&cp.Payload{
+			Header: &cp.Header{
+				ChannelHeader: protoutil.MarshalOrPanic(&cp.ChannelHeader{
+					Type: int32(cp.HeaderType_CONFIG_UPDATE),
+				}),
+			},
+		}),
+	}
+
+	noMatchingEventsBlock := newBlock(100)
+	addTransaction(noMatchingEventsBlock, newTransactionEnvelope(wrongChaincodeEvent), peer.TxValidationCode_VALID)
+
+	matchingEventBlock := newBlock(101)
+	addTransaction(matchingEventBlock, configTxEnvelope, peer.TxValidationCode_VALID)
+	addTransaction(matchingEventBlock, newTransactionEnvelope(wrongChaincodeEvent), peer.TxValidationCode_VALID)
+	addTransaction(matchingEventBlock, newTransactionEnvelope(matchEvent), peer.TxValidationCode_VALID)
+
+	partReadBlock := newBlock(200)
+	addTransaction(partReadBlock, newTransactionEnvelope(oldTransactionEvent), peer.TxValidationCode_VALID)
+	addTransaction(partReadBlock, newTransactionEnvelope(lastTransactionEvent), peer.TxValidationCode_VALID)
+	addTransaction(partReadBlock, newTransactionEnvelope(matchEvent), peer.TxValidationCode_VALID)
+
+	differentChaincodePartReadBlock := newBlock(300)
+	addTransaction(differentChaincodePartReadBlock, newTransactionEnvelope(oldTransactionEvent), peer.TxValidationCode_VALID)
+	addTransaction(differentChaincodePartReadBlock, newTransactionEnvelope(lastTransactionWrongChaincodeEvent), peer.TxValidationCode_VALID)
+	addTransaction(differentChaincodePartReadBlock, newTransactionEnvelope(matchEvent), peer.TxValidationCode_VALID)
 
 	tests := []testDef{
 		{
@@ -1524,17 +1503,17 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "returns chaincode events",
 			blocks: []*cp.Block{
-				block101Proto,
+				matchingEventBlock,
 			},
 			expectedResponses: []proto.Message{
 				&pb.ChaincodeEventsResponse{
-					BlockNumber: block101Proto.GetHeader().GetNumber(),
+					BlockNumber: matchingEventBlock.GetHeader().GetNumber(),
 					Events: []*peer.ChaincodeEvent{
 						{
 							ChaincodeId: testChaincode,
-							TxId:        matchChaincodeEvent.GetTxId(),
-							EventName:   matchChaincodeEvent.GetEventName(),
-							Payload:     matchChaincodeEvent.GetPayload(),
+							TxId:        matchEvent.GetTxId(),
+							EventName:   matchEvent.GetEventName(),
+							Payload:     matchEvent.GetPayload(),
 						},
 					},
 				},
@@ -1543,18 +1522,79 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "skips blocks containing only non-matching chaincode events",
 			blocks: []*cp.Block{
-				block100Proto,
-				block101Proto,
+				noMatchingEventsBlock,
+				matchingEventBlock,
 			},
 			expectedResponses: []proto.Message{
 				&pb.ChaincodeEventsResponse{
-					BlockNumber: block101Proto.GetHeader().GetNumber(),
+					BlockNumber: matchingEventBlock.GetHeader().GetNumber(),
 					Events: []*peer.ChaincodeEvent{
 						{
 							ChaincodeId: testChaincode,
-							TxId:        matchChaincodeEvent.GetTxId(),
-							EventName:   matchChaincodeEvent.GetEventName(),
-							Payload:     matchChaincodeEvent.GetPayload(),
+							TxId:        matchEvent.GetTxId(),
+							EventName:   matchEvent.GetEventName(),
+							Payload:     matchEvent.GetPayload(),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "skips previously seen transactions",
+			blocks: []*cp.Block{
+				partReadBlock,
+			},
+			afterTxID: lastTransactionID,
+			expectedResponses: []proto.Message{
+				&pb.ChaincodeEventsResponse{
+					BlockNumber: partReadBlock.GetHeader().GetNumber(),
+					Events: []*peer.ChaincodeEvent{
+						{
+							ChaincodeId: testChaincode,
+							TxId:        matchEvent.GetTxId(),
+							EventName:   matchEvent.GetEventName(),
+							Payload:     matchEvent.GetPayload(),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "identifies previous transaction ID if from different chaincode",
+			blocks: []*cp.Block{
+				differentChaincodePartReadBlock,
+			},
+			afterTxID: lastTransactionID,
+			expectedResponses: []proto.Message{
+				&pb.ChaincodeEventsResponse{
+					BlockNumber: differentChaincodePartReadBlock.GetHeader().GetNumber(),
+					Events: []*peer.ChaincodeEvent{
+						{
+							ChaincodeId: testChaincode,
+							TxId:        matchEvent.GetTxId(),
+							EventName:   matchEvent.GetEventName(),
+							Payload:     matchEvent.GetPayload(),
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "identifies previous transaction ID if not in start block",
+			blocks: []*cp.Block{
+				noMatchingEventsBlock,
+				partReadBlock,
+			},
+			afterTxID: lastTransactionID,
+			expectedResponses: []proto.Message{
+				&pb.ChaincodeEventsResponse{
+					BlockNumber: partReadBlock.GetHeader().GetNumber(),
+					Events: []*peer.ChaincodeEvent{
+						{
+							ChaincodeId: testChaincode,
+							TxId:        matchEvent.GetTxId(),
+							EventName:   matchEvent.GetEventName(),
+							Payload:     matchEvent.GetPayload(),
 						},
 					},
 				},
@@ -1570,7 +1610,7 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "returns error obtaining ledger",
 			blocks: []*cp.Block{
-				block101Proto,
+				matchingEventBlock,
 			},
 			errCode:   codes.NotFound,
 			errString: "LEDGER_PROVIDER_ERROR",
@@ -1581,7 +1621,7 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "returns error obtaining ledger height",
 			blocks: []*cp.Block{
-				block101Proto,
+				matchingEventBlock,
 			},
 			errCode:   codes.Aborted,
 			errString: "LEDGER_INFO_ERROR",
@@ -1592,7 +1632,7 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "uses block height as start block if next commit is specified as start position",
 			blocks: []*cp.Block{
-				block101Proto,
+				matchingEventBlock,
 			},
 			postSetup: func(t *testing.T, test *preparedTest) {
 				ledgerInfo := &cp.BlockchainInfo{
@@ -1613,7 +1653,7 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "uses specified start block",
 			blocks: []*cp.Block{
-				block101Proto,
+				matchingEventBlock,
 			},
 			postSetup: func(t *testing.T, test *preparedTest) {
 				ledgerInfo := &cp.BlockchainInfo{
@@ -1636,7 +1676,7 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "defaults to next commit if start position not specified",
 			blocks: []*cp.Block{
-				block101Proto,
+				matchingEventBlock,
 			},
 			postSetup: func(t *testing.T, test *preparedTest) {
 				ledgerInfo := &cp.BlockchainInfo{
@@ -1652,7 +1692,7 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "returns error for unsupported start position type",
 			blocks: []*cp.Block{
-				block101Proto,
+				matchingEventBlock,
 			},
 			startPosition: &ab.SeekPosition{
 				Type: &ab.SeekPosition_Oldest{
@@ -1665,7 +1705,7 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "returns error obtaining ledger iterator",
 			blocks: []*cp.Block{
-				block101Proto,
+				matchingEventBlock,
 			},
 			errCode:   codes.Aborted,
 			errString: "LEDGER_ITERATOR_ERROR",
@@ -1676,7 +1716,7 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "returns canceled status error when client closes stream",
 			blocks: []*cp.Block{
-				block101Proto,
+				matchingEventBlock,
 			},
 			errCode: codes.Canceled,
 			postSetup: func(t *testing.T, test *preparedTest) {
@@ -1686,7 +1726,7 @@ func TestChaincodeEvents(t *testing.T) {
 		{
 			name: "returns status error from send to client",
 			blocks: []*cp.Block{
-				block101Proto,
+				matchingEventBlock,
 			},
 			errCode:   codes.Aborted,
 			errString: "SEND_ERROR",
@@ -1731,6 +1771,9 @@ func TestChaincodeEvents(t *testing.T) {
 			}
 			if tt.startPosition != nil {
 				request.StartPosition = tt.startPosition
+			}
+			if len(tt.afterTxID) > 0 {
+				request.AfterTransactionId = tt.afterTxID
 			}
 			requestBytes, err := proto.Marshal(request)
 			require.NoError(t, err)
