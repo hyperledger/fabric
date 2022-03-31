71b7d0ce6e5c6b190aff9d0937e16a808ec7def9
Matthew B White <whitemat@uk.ibm.com>
2022-03-31 16:57:24 +0100

    CCAAS Builders
    - adjust the makefile to have targets for linux-amd64/darwin-amd64/windows-amd64
    - adjust the packaging as suggested in issue 3286
    
    Signed-off-by: Matthew B White <whitemat@uk.ibm.com>
    (cherry picked from commit 704487f3f048f6d1ca1cae753f4352ffb3d132df)

--- ci/scripts/create_binary_package.sh
+++ ci/scripts/create_binary_package.sh
@@ -5,10 +5,15 @@
 set -euo pipefail
 
 make "release/${TARGET}"
-mkdir "release/${TARGET}/config"
-mv sampleconfig/*yaml "release/${TARGET}/config"
+mkdir -p "release/${TARGET}/config"
+mkdir -p "release/${TARGET}/builders/ccaas"
+
+# cp not move otherwise this breaks your source tree
+cp sampleconfig/*yaml "release/${TARGET}/config"
+cp ccaas_builder/bin/${TARGET}/* "release/${TARGET}/builders/ccaas"
+
 cd "release/${TARGET}"
 if [ "$TARGET" == "windows-amd64" ]; then
     for FILE in bin/*; do mv $FILE $FILE.exe; done
 fi
-tar -czvf "hyperledger-fabric-${TARGET}-${RELEASE}.tar.gz" bin config
+tar -czvf "hyperledger-fabric-${TARGET}-${RELEASE}.tar.gz" bin config builders
