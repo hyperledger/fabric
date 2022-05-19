fcc3248761edf4d4e5ec8ff8acea4d089459b0c2
andrew-coleman <andrew_coleman@uk.ibm.com>
2022-05-19 14:32:20 +0100

    Remove redundant type definition
    The code that used this was rewritten in a previous commit.
    
    Signed-off-by: andrew-coleman <andrew_coleman@uk.ibm.com>

--- internal/pkg/gateway/api.go
+++ internal/pkg/gateway/api.go
@@ -28,12 +28,6 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-type endorserResponse struct {
-	action         *peer.ChaincodeEndorsedAction
-	err            *gp.ErrorDetail
-	timeoutExpired bool
-}
-
 // Evaluate will invoke the transaction function as specified in the SignedProposal
 func (gs *Server) Evaluate(ctx context.Context, request *gp.EvaluateRequest) (*gp.EvaluateResponse, error) {
 	if request == nil {
