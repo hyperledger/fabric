ac8dbf6f82d2151d5d2f9104213afd5ce8798f3d
James Taylor <jamest@uk.ibm.com>
2022-06-10 14:34:10 +0100

    Fix Ginkgo deprecation warning
    Rename GinkgoParallelNode GinkgoParallelProcess as documented in the Ginkgo migration guide:
    
    https://github.com/onsi/ginkgo/blob/ver2/docs/MIGRATING_TO_V2.md#renamed-ginkgoparallelnode
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- integration/ports.go
+++ integration/ports.go
@@ -49,7 +49,7 @@ const (
 func (t TestPortRange) StartPortForNode() int {
 	const startEphemeral, endEphemeral = 32768, 60999
 
-	port := int(t) + portsPerNode*(ginkgo.GinkgoParallelNode()-1)
+	port := int(t) + portsPerNode*(ginkgo.GinkgoParallelProcess()-1)
 	if port >= startEphemeral-portsPerNode && port <= endEphemeral-portsPerNode {
 		fmt.Fprintf(os.Stderr, "WARNING: port %d is part of the default ephemeral port range on linux", port)
 	}
