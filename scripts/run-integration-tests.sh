eaff6f6c0a7c2f13ac54a733c798bd503749623a
Фёдор Партанский <partanskiy.f@n-t.io>
2022-08-04 17:24:28 +0300

    fix timeout ginkgo
    Signed-off-by: Фёдор Партанский <partanskiy.f@n-t.io>

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -30,4 +30,5 @@ for ((i = "$agent_number"; i <= "${#test_dirs[@]}"; )); do
 done
 
 printf "\nRunning the following test suites:\n\n%s\n\nStarting tests...\n\n" "$(echo "${dirs[@]}" | tr -s ' ' '\n')"
+
 ginkgo --keep-going --slow-spec-threshold 60s --timeout 24h "${dirs[@]}"
