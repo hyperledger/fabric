f1a3e2289fb1f483397120668624ead882cc1dea
David Enyeart <enyeart@us.ibm.com>
2022-10-26 09:19:01 -0400

    Updates in main branch for v2.4.7
    Updates in main branch for v2.4.7.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- scripts/install-fabric.sh
+++ scripts/install-fabric.sh
@@ -21,7 +21,7 @@ _arg_comp=('' )
 
 # if version not passed in, default to latest released version
 # if ca version not passed in, default to latest released version
-_arg_fabric_version="2.4.6"
+_arg_fabric_version="2.4.7"
 _arg_ca_version="1.5.5"
 
 ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m |sed 's/x86_64/amd64/g')" |sed 's/darwin-arm64/darwin-amd64/g')
@@ -49,7 +49,7 @@ print_help()
 {
 	printf 'Usage: %s [-f|--fabric-version <arg>] [-c|--ca-version <arg>] <comp-1> [<comp-2>] ... [<comp-n>] ...\n' "$0"
 	printf '\t%s\n' "<comp> Component to install, one or more of  docker | binary | samples | podman  First letter of component also accepted; If none specified docker | binary | samples is assumed"
-	printf '\t%s\n' "-f, --fabric-version: FabricVersion (default: '2.4.6')"
+	printf '\t%s\n' "-f, --fabric-version: FabricVersion (default: '2.4.7')"
 	printf '\t%s\n' "-c, --ca-version: Fabric CA Version (default: '1.5.5')"
 }
 
