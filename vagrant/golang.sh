d0b7a755b0d2bbbec611ff915226194a0364309a
David Enyeart <enyeart@us.ibm.com>
2023-01-26 11:00:33 -0500

    Bump Go to 1.18.10 (main)
    Bump Go to 1.18.10.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- vagrant/golang.sh
+++ vagrant/golang.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 GOROOT='/opt/go'
-GO_VERSION=1.18.7
+GO_VERSION=1.18.10
 
 # ----------------------------------------------------------------
 # Install Golang
