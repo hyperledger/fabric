90acb8ae8c16ee99885a3417896acfa52618dce3
David Enyeart <enyeart@us.ibm.com>
2023-02-20 16:39:23 -0500

    Revert github.com/prometheus/client_golang to v1.11.1 (release-2.5)
    The bump to v1.14.0 causes Fabric compilation issues on ubuntu docker images.
    See issue #4052 for details about the problematic build tag.
    For now, it is acceptable to revert to v1.11.1 where there is no such build tag.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- vendor/github.com/prometheus/client_golang/prometheus/process_collector_other.go
+++ vendor/github.com/prometheus/client_golang/prometheus/process_collector_other.go
@@ -11,8 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !windows && !js
-// +build !windows,!js
+// +build !windows
 
 package prometheus
 
