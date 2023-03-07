7c05c29312019ce37f48a05a9f67ee044a193ad8
Dave Enyeart <enyeart@us.ibm.com>
2023-03-07 18:39:03 -0500

    Updates in main branch for v2.4.9 (#4081)
    Updates in main branch for v2.4.9.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,7 +6,7 @@
 #
 
 # if version not passed in, default to latest released version
-VERSION=2.4.7
+VERSION=2.4.9
 # if ca version not passed in, default to latest released version
 CA_VERSION=1.5.5
 
@@ -30,8 +30,8 @@ printHelp() {
     echo "-s : bypass fabric-samples repo clone"
     echo "-b : bypass download of platform-specific binaries"
     echo
-    echo "e.g. bootstrap.sh 2.4.7 1.5.5 -s"
-    echo "will download docker images and binaries for Fabric v2.4.7 and Fabric CA v1.5.5"
+    echo "e.g. bootstrap.sh 2.4.9 1.5.5 -s"
+    echo "will download docker images and binaries for Fabric v2.4.9 and Fabric CA v1.5.5"
 }
 
 # dockerPull() pulls docker images from fabric and chaincode repositories
