89180b72357b3a0bd1f980466560784396ab6a7d
semil <emildav@gmail.com>
2023-05-04 10:27:54 +0300

    cleanup of dependencies on the onboarding package + removing initializeEtcdraftConsenter (#4191)
    Signed-off-by: Emil Elizarov <emil.elizarov@ibm.com>
    Co-authored-by: Emil Elizarov <emil.elizarov@ibm.com>

--- orderer/common/server/util.go
+++ orderer/common/server/util.go
@@ -7,6 +7,10 @@ SPDX-License-Identifier: Apache-2.0
 package server
 
 import (
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric-protos-go/common"
+	"github.com/hyperledger/fabric/bccsp"
+	"github.com/hyperledger/fabric/common/channelconfig"
 	"github.com/hyperledger/fabric/common/ledger/blockledger"
 	"github.com/hyperledger/fabric/common/ledger/blockledger/fileledger"
 	"github.com/hyperledger/fabric/common/metrics"
@@ -26,4 +30,32 @@ func createLedgerFactory(conf *config.TopLevel, metricsProvider metrics.Provider
 		return nil, errors.WithMessage(err, "Error in opening ledger factory")
 	}
 	return lf, nil
+}
+
+// validateBootstrapBlock returns whether this block can be used as a bootstrap block.
+// A bootstrap block is a block of a system channel, and needs to have a ConsortiumsConfig.
+func validateBootstrapBlock(block *common.Block, bccsp bccsp.BCCSP) error {
+	if block == nil {
+		return errors.New("nil block")
+	}
+
+	if block.Data == nil || len(block.Data.Data) == 0 {
+		return errors.New("empty block data")
+	}
+
+	firstTransaction := &common.Envelope{}
+	if err := proto.Unmarshal(block.Data.Data[0], firstTransaction); err != nil {
+		return errors.Wrap(err, "failed extracting envelope from block")
+	}
+
+	bundle, err := channelconfig.NewBundleFromEnvelope(firstTransaction, bccsp)
+	if err != nil {
+		return err
+	}
+
+	_, exists := bundle.ConsortiumsConfig()
+	if !exists {
+		return errors.New("the block isn't a system channel block because it lacks ConsortiumsConfig")
+	}
+	return nil
 }
--- orderer/common/server/util_test.go
+++ orderer/common/server/util_test.go
@@ -7,10 +7,14 @@ SPDX-License-Identifier: Apache-2.0
 package server
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric-protos-go/common"
+	"github.com/hyperledger/fabric/bccsp/sw"
 	"github.com/hyperledger/fabric/common/metrics/disabled"
 	"github.com/hyperledger/fabric/core/config/configtest"
 	config "github.com/hyperledger/fabric/orderer/common/localconfig"
@@ -53,6 +57,74 @@ func TestCreateLedgerFactory(t *testing.T) {
 				defer os.RemoveAll(tc.ledgerDir)
 				require.Equal(t, []string{}, lf.ChannelIDs())
 			}
+		})
+	}
+}
+
+func TestValidateBootstrapBlock(t *testing.T) {
+	tempDir := t.TempDir()
+	copyYamlFiles("testdata", tempDir)
+	cryptoPath := generateCryptoMaterials(t, cryptogen, tempDir)
+	defer os.RemoveAll(cryptoPath)
+
+	systemChannelBlockPath, _ := produceGenesisFileEtcdRaft(t, "system", tempDir)
+	systemChannelBlockBytes, err := ioutil.ReadFile(systemChannelBlockPath)
+	require.NoError(t, err)
+
+	applicationChannelBlockPath, _ := produceGenesisFileEtcdRaftAppChannel(t, "mychannel", tempDir)
+	applicationChannelBlockBytes, err := ioutil.ReadFile(applicationChannelBlockPath)
+	require.NoError(t, err)
+
+	appBlock := &common.Block{}
+	require.NoError(t, proto.Unmarshal(applicationChannelBlockBytes, appBlock))
+
+	systemBlock := &common.Block{}
+	require.NoError(t, proto.Unmarshal(systemChannelBlockBytes, systemBlock))
+
+	cryptoProvider, err := sw.NewDefaultSecurityLevelWithKeystore(sw.NewDummyKeyStore())
+	require.NoError(t, err)
+
+	for _, testCase := range []struct {
+		description   string
+		block         *common.Block
+		expectedError string
+	}{
+		{
+			description:   "nil block",
+			expectedError: "nil block",
+		},
+		{
+			description:   "empty block",
+			block:         &common.Block{},
+			expectedError: "empty block data",
+		},
+		{
+			description: "bad envelope",
+			block: &common.Block{
+				Data: &common.BlockData{
+					Data: [][]byte{{1, 2, 3}},
+				},
+			},
+			expectedError: "failed extracting envelope from block",
+		},
+		{
+			description:   "application channel block",
+			block:         appBlock,
+			expectedError: "the block isn't a system channel block because it lacks ConsortiumsConfig",
+		},
+		{
+			description: "system channel block",
+			block:       systemBlock,
+		},
+	} {
+		t.Run(testCase.description, func(t *testing.T) {
+			err := validateBootstrapBlock(testCase.block, cryptoProvider)
+			if testCase.expectedError == "" {
+				require.NoError(t, err)
+				return
+			}
+			require.Error(t, err)
+			require.Contains(t, err.Error(), testCase.expectedError)
 		})
 	}
 }
