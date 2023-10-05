d6b9fec091c0ac5339b6b83ebc90c292e777cbcb
Baohua Yang <yangbaohua@gmail.com>
2023-10-05 15:07:33 -0700

    Use the errors.Is to check error type
    errors.Is is recommened since Go 1.13 to check error type. The error may
    be wrapped.
    
    More details: https://github.com/golang/go/wiki/ErrorValueFAQ.
    
    Change-Id: Ieae0eaa2527d05fa67aa31afdda42d150fff0980
    Signed-off-by: Baohua Yang <yangbaohua@gmail.com>
    Signed-off-by: Baohua Yang <baohua.yang@oracle.com>

--- common/ledger/blockledger/fileledger/factory.go
+++ common/ledger/blockledger/fileledger/factory.go
@@ -7,6 +7,7 @@ SPDX-License-Identifier: Apache-2.0
 package fileledger
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"sync"
@@ -59,7 +60,7 @@ func (f *fileLedgerFactory) Remove(channelID string) error {
 	f.mutex.Lock()
 	defer f.mutex.Unlock()
 
-	if err := f.removeFileRepo.Save(channelID, []byte{}); err != nil && err != os.ErrExist {
+	if err := f.removeFileRepo.Save(channelID, []byte{}); err != nil && !errors.Is(err, os.ErrExist) {
 		return err
 	}
 
