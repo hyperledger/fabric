f4a377bd6fba61aa766ebf4aa837a7666d11c522
C0rWin <artem@bargr.net>
2023-11-27 15:28:23 +0200

    Remove deploy legacy from interop integration tests (#4552)
    This commit removes DeployChaincodeLegacy usage from the integration
    interoperability tests, most of the code was removed because with
    removal of legacy LSCC these are not valid cases any longer.
    
    Signed-off-by: Artem Barger <artem@bargr.net>

--- integration/nwo/network_test.go
+++ integration/nwo/network_test.go
@@ -73,23 +73,6 @@ var _ = Describe("Network", func() {
 			network.Cleanup()
 		})
 
-		It("deploys and executes chaincode (simple) using the legacy lifecycle", func() {
-			orderer := network.Orderer("orderer")
-			channelparticipation.JoinOrdererJoinPeersAppChannel(network, "testchannel", orderer, ordererRunner)
-			peer := network.Peer("Org1", "peer0")
-
-			legacyChaincode := nwo.Chaincode{
-				Name:    "mycc",
-				Version: "0.0",
-				Path:    "github.com/hyperledger/fabric/integration/chaincode/simple/cmd",
-				Ctor:    `{"Args":["init","a","100","b","200"]}`,
-				Policy:  `AND ('Org1MSP.member','Org2MSP.member')`,
-			}
-
-			nwo.DeployChaincodeLegacy(network, "testchannel", orderer, legacyChaincode)
-			RunQueryInvokeQuery(network, orderer, peer, 100)
-		})
-
 		It("deploys and executes chaincode (simple) using _lifecycle", func() {
 			orderer := network.Orderer("orderer")
 			channelparticipation.JoinOrdererJoinPeersAppChannel(network, "testchannel", orderer, ordererRunner)
