2696b6687aeeebfca72565086387689c6afa4d8a
Ben Smith <benjsmi@us.ibm.com>
2024-04-09 12:00:55 -0400

    Move to Go v1.21.9.
    As per https://github.com/hyperledger/fabric/issues/4804
    
    Signed-off-by: Ben Smith <benjsmi@us.ibm.com>

--- vagrant/golang.sh
+++ vagrant/golang.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 GOROOT='/opt/go'
-GO_VERSION=1.21.8
+GO_VERSION=1.21.9
 
 # ----------------------------------------------------------------
 # Install Golang
