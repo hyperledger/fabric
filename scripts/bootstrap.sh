5efcea7216c8d316fa3d4a32e008ba22f1757a44
Dave Enyeart <enyeart@us.ibm.com>
2024-04-15 13:25:49 -0400

    Release commit for v2.5.7 (#4817)
    Add docs and release notes for v2.5.7 release.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,9 +6,9 @@
 #
 
 # if version not passed in, default to latest released version
-VERSION=2.5.6
+VERSION=2.5.7
 # if ca version not passed in, default to latest released version
-CA_VERSION=1.5.9
+CA_VERSION=1.5.10
 ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m |sed 's/x86_64/amd64/g')" |sed 's/darwin-arm64/darwin-amd64/g')
 MARCH=$(uname -m)
 
@@ -21,8 +21,8 @@ printHelp() {
     echo "-s : bypass fabric-samples repo clone"
     echo "-b : bypass download of platform-specific binaries"
     echo
-    echo "e.g. bootstrap.sh 2.5.6 1.5.9 -s"
-    echo "will download docker images and binaries for Fabric v2.5.6 and Fabric CA v1.5.9"
+    echo "e.g. bootstrap.sh 2.5.7 1.5.10 -s"
+    echo "will download docker images and binaries for Fabric v2.5.7 and Fabric CA v1.5.10"
 }
 
 # dockerPull() pulls docker images from fabric and chaincode repositories
