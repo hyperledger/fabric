57fe331a22adaf1fcec34250d5dfadf00c9e15d4
Emil Elizarov <emil.elizarov@ibm.com>
2024-06-04 09:55:14 +0300

    block censorship integration tests
    Signed-off-by: Emil Elizarov <emil.elizarov@ibm.com>

--- integration/smartbft/smartbft_block_deliverer_test.go
+++ integration/smartbft/smartbft_block_deliverer_test.go
@@ -32,7 +32,7 @@ import (
 	ginkgomon "github.com/tedsuo/ifrit/ginkgomon_v2"
 )
 
-func ExtractLedger(network *nwo.Network, orderer *nwo.Orderer) []*common.Block {
+func extractLedger(network *nwo.Network, orderer *nwo.Orderer, channelId string) []*common.Block {
 	By("Extracting blocks from ledger")
 	ordererFabricConfig := network.ReadOrdererConfig(orderer)
 	blockStoreProvider, err := blkstorage.NewProvider(
@@ -43,7 +43,7 @@ func ExtractLedger(network *nwo.Network, orderer *nwo.Orderer) []*common.Block {
 		&disabled.Provider{},
 	)
 	Expect(err).NotTo(HaveOccurred())
-	blockStore, err := blockStoreProvider.Open("testchannel1")
+	blockStore, err := blockStoreProvider.Open(channelId)
 	Expect(err).NotTo(HaveOccurred())
 	fileLedger := fileledger.NewFileLedger(blockStore)
 	var ledgerArray []*common.Block
@@ -55,7 +55,6 @@ func ExtractLedger(network *nwo.Network, orderer *nwo.Orderer) []*common.Block {
 	return ledgerArray
 }
 
-
 var _ = Describe("Smart BFT Block Deliverer", func() {
 	var (
 		testDir          string
@@ -66,12 +65,20 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 		peerProcesses    ifrit.Process
 		mocksArray       []*MockOrderer
 		ledgerArray      []*common.Block
-		
+		ordererRunners   []*ginkgomon.Runner
+		allStreams       []ordererProtos.AtomicBroadcast_BroadcastClient
+		channel          string
+		peer             *nwo.Peer
 	)
 
 	BeforeEach(func() {
 		ordererProcesses = nil
 		peerProcesses = nil
+		mocksArray = nil
+		ledgerArray = nil
+		peer = nil
+		ordererRunners = nil
+		allStreams = nil
 		var err error
 		testDir, err = os.MkdirTemp("", "smartbft-block-deliverer-test")
 		Expect(err).NotTo(HaveOccurred())
@@ -84,16 +91,17 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 		/* Create networkConfig for multiple orderers */
 		networkConfig := nwo.MultiNodeSmartBFT()
 		networkConfig.Channels = nil
-
-		channel := "testchannel1"
+		channel = "testchannel1"
 
 		/* Create a network from the networkConfig */
-		startPort := StartPort()
-		network = nwo.New(networkConfig, testDir, client, startPort, components)
+		network = nwo.New(networkConfig, testDir, client, StartPort(), components)
+		network.Profiles[0].SmartBFT = &nwo.SmartBFT{
+			LeaderHeartbeatTimeout: 10,
+			LeaderHeartbeatCount:   10,
+		}
 		network.GenerateConfigTree()
 		network.Bootstrap()
 
-		var ordererRunners []*ginkgomon.Runner
 		for _, orderer := range network.Orderers {
 			runner := network.OrdererRunner(orderer)
 			runner.Command.Env = append(runner.Command.Env, "FABRIC_LOGGING_SPEC=debug")
@@ -110,8 +118,9 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 		Eventually(ordererRunners[2].Err(), network.EventuallyTimeout, time.Second).Should(gbytes.Say("Message from 1"))
 		Eventually(ordererRunners[3].Err(), network.EventuallyTimeout, time.Second).Should(gbytes.Say("Message from 1"))
 
+		peer = network.Peers[0]
+
 		/* Create a stream with client for each orderer*/
-		var allStreams []ordererProtos.AtomicBroadcast_BroadcastClient
 		for _, o := range network.Orderers {
 			conn := network.OrdererClientConn(o)
 			broadcaster, err := ordererProtos.NewAtomicBroadcastClient(conn).Broadcast(context.Background())
@@ -134,10 +143,7 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 				Expect(err).NotTo(HaveOccurred())
 			}
 		}
-		Eventually(ordererRunners[0].Err(), 20*time.Second).Should(gbytes.Say("Delivering proposal, writing block 10"))
-		Eventually(ordererRunners[1].Err(), 20*time.Second).Should(gbytes.Say("Delivering proposal, writing block 10"))
-		Eventually(ordererRunners[2].Err(), 20*time.Second).Should(gbytes.Say("Delivering proposal, writing block 10"))
-		Eventually(ordererRunners[3].Err(), 20*time.Second).Should(gbytes.Say("Delivering proposal, writing block 10"))
+		assertBlockReception(map[string]int{channel: 10}, network.Orderers, peer, network)
 	})
 
 	AfterEach(func() {
@@ -161,7 +167,7 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 		_ = os.RemoveAll(testDir)
 	})
 
-	FIt("correct mock", func() {
+	It("correct mock", func() {
 		/* Create orderer mocks and make sure they can access the ledger */
 		var err error
 
@@ -172,7 +178,7 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 			Eventually(proc.Wait(), network.EventuallyTimeout).Should(Receive())
 		}
 
-		ledgerArray = ExtractLedger(network, network.Orderers[0])
+		ledgerArray = extractLedger(network, network.Orderers[0], channel)
 
 		for _, orderer := range network.Orderers {
 			serverTLSCerts := map[string][]byte{}
@@ -196,8 +202,6 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 			mocksArray[len(mocksArray)-1].logger.Infof("Mock orderer started at port %v", network.OrdererAddress(orderer, nwo.ListenPort))
 		}
 
-		channel := "testchannel1"
-
 		/* Create peer */
 		By("Create a peer and join to channel")
 		p0 := network.Peers[0]
@@ -214,9 +218,9 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 		nwo.WaitUntilEqualLedgerHeight(network, channel, 11, network.Peers[0])
 	})
 
-	FIt("block censorship", func() {
+	It("block censorship peer", func() {
 		var err error
-		
+
 		/* Shut down the orderers */
 		By("Taking down all the orderers")
 		for _, proc := range ordererProcesses {
@@ -224,7 +228,7 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 			Eventually(proc.Wait(), network.EventuallyTimeout).Should(Receive())
 		}
 
-		ledgerArray = ExtractLedger(network, network.Orderers[0])
+		ledgerArray = extractLedger(network, network.Orderers[0], channel)
 
 		for _, orderer := range network.Orderers {
 			serverTLSCerts := map[string][]byte{}
@@ -248,11 +252,8 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 			mocksArray[len(mocksArray)-1].logger.Infof("Mock orderer started at port %v", network.OrdererAddress(orderer, nwo.ListenPort))
 		}
 
-		channel := "testchannel1"
-
 		for _, mock := range mocksArray {
-			mock.censorDataMode = true
-			mock.stopDeliveryChannel = make(chan struct{})
+			mock.StartCensoring(5)
 		}
 
 		/* Create peer */
@@ -268,29 +269,70 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 			ClientAuth: network.ClientAuthRequired,
 		})
 		Expect(err).NotTo(HaveOccurred())
+		// After delivery is stopped due to censoring, stop the censoring on all the other mocks
+		var censoringOrderer *MockOrderer
+	censoringMockDetection:
+		for {
+			for _, mock := range mocksArray {
+				if mock.StopDelivery {
+					censoringOrderer = mock
+					for _, otherMock := range mocksArray {
+						if mock.address == otherMock.address {
+							continue
+						}
+						otherMock.StopCensoring()
+					}
+					break censoringMockDetection
+				}
+			}
+			time.Sleep(time.Second)
+		}
 		Eventually(peerRunner.Err(), network.EventuallyTimeout).Should(gbytes.Say("Block censorship detected"))
 		nwo.WaitUntilEqualLedgerHeight(network, channel, 11, network.Peers[0])
 
-		for _, mock := range mocksArray {
-			close(mock.stopDeliveryChannel)
-		}
+		close(censoringOrderer.StopDeliveryChannel)
 	})
 
-	It("block censorship 2", func() {
+	It("block censorship orderer", func() {
 		var err error
-		
-		/* Shut down the orderers */
-		By("Taking down last orderer")
-		proc := ordererProcesses[len(ordererProcesses)-1]
-		proc.Signal(syscall.SIGINT)
-		Eventually(proc.Wait(), network.EventuallyTimeout).Should(Receive())
-		
-		By("Send 10 more block in network")
 
+		By("Taking down the last orderer")
+		o4Proc := ordererProcesses[3]
+		o4Proc.Signal(syscall.SIGINT)
+		Eventually(o4Proc.Wait(), network.EventuallyTimeout).Should(Receive())
 
-		ledgerArray = ExtractLedger(network, network.Orderers[0])
+		By("Send 10 more blocks in network")
+		for i := 0; i < 10; i++ {
+			buff := make([]byte, 8)
+			binary.BigEndian.PutUint64(buff, uint64(i))
 
-		for _, orderer := range network.Orderers {
+			/* Create transaction envelope which will be broadcast to all orderers */
+			env := ordererclient.CreateBroadcastEnvelope(network, network.Orderers[0], channel, buff, common.HeaderType_ENDORSER_TRANSACTION)
+
+			for i, stream := range allStreams {
+				if i == 3 {
+					continue
+				}
+				err := stream.Send(env)
+				Expect(err).NotTo(HaveOccurred())
+			}
+		}
+		assertBlockReception(map[string]int{channel: 20}, network.Orderers[:3], peer, network)
+
+		By("Stop the rest of the orderers")
+		for i, proc := range ordererProcesses {
+			if i == 3 {
+				continue
+			}
+			proc.Signal(syscall.SIGINT)
+			Eventually(proc.Wait(), network.EventuallyTimeout).Should(Receive())
+		}
+
+		By("Copy ledger of the first orderer")
+		ledgerArray = extractLedger(network, network.Orderers[0], channel)
+
+		By("Create mocks instead of orderers 1-3")
+		for _, orderer := range network.Orderers[:3] {
 			serverTLSCerts := map[string][]byte{}
 			tlsCertPath := filepath.Join(network.OrdererLocalTLSDir(orderer), "server.crt")
 			serverTLSCerts[tlsCertPath], err = os.ReadFile(tlsCertPath)
@@ -306,37 +348,47 @@ var _ = Describe("Smart BFT Block Deliverer", func() {
 				Certificate: serverTLSCerts[tlsCertPath],
 				Key:         serverTLSKeys[tlsKeyPath],
 			}
-			mo, err := NewMockOrderer(network.OrdererAddress(orderer, nwo.ListenPort), ledgerArray, secOpts)
+			ordererAddress := network.OrdererAddress(orderer, nwo.ListenPort)
+			mo, err := NewMockOrderer(ordererAddress, ledgerArray, secOpts)
 			Expect(err).NotTo(HaveOccurred())
 			mocksArray = append(mocksArray, mo)
-			mocksArray[len(mocksArray)-1].logger.Infof("Mock orderer started at port %v", network.OrdererAddress(orderer, nwo.ListenPort))
+			mocksArray[len(mocksArray)-1].logger.Infof("Mock orderer started at port %v", ordererAddress)
 		}
 
-		channel := "testchannel1"
-
+		By("Making mocks of orderers 1-3 censor blocks")
 		for _, mock := range mocksArray {
-			mock.censorDataMode = true
-			mock.stopDeliveryChannel = make(chan struct{})
+			mock.StartCensoring(5)
 		}
 
-		/* Create peer */
-		By("Create a peer and join to channel")
-		p0 := network.Peers[0]
-		peerRunner := network.PeerRunner(p0)
-		peerRunner.Command.Env = append(peerRunner.Command.Env, "FABRIC_LOGGING_SPEC=debug")
-		peerProcesses = ifrit.Invoke(peerRunner)
-		Eventually(peerProcesses.Ready(), network.EventuallyTimeout).Should(BeClosed())
+		By("Bring up the last orderer")
+		o4Runner := network.OrdererRunner(network.Orderers[3], "FABRIC_LOGGING_SPEC=debug")
+		o4Proc = ifrit.Invoke(o4Runner)
+		Eventually(o4Proc.Ready(), network.EventuallyTimeout).Should(BeClosed())
 
-		_, err = network.PeerAdminSession(p0, commands.ChannelJoin{
-			BlockPath:  network.OutputBlockPath(channel),
-			ClientAuth: network.ClientAuthRequired,
-		})
-		Expect(err).NotTo(HaveOccurred())
-		Eventually(peerRunner.Err(), network.EventuallyTimeout).Should(gbytes.Say("Block censorship detected"))
-		nwo.WaitUntilEqualLedgerHeight(network, channel, 11, network.Peers[0])
-
-		for _, mock := range mocksArray {
-			close(mock.stopDeliveryChannel)
+		By("Assert block censorship detected")
+		// After delivery is stopped due to censoring, stop the censoring on all the other mocks
+		var censoringOrderer *MockOrderer
+	censoringMockDetection:
+		for {
+			for _, mock := range mocksArray {
+				if mock.StopDelivery {
+					censoringOrderer = mock
+					for _, otherMock := range mocksArray {
+						if mock.address == otherMock.address {
+							continue
+						}
+						otherMock.StopCensoring()
+					}
+					break censoringMockDetection
+				}
+			}
+			time.Sleep(time.Second)
 		}
+		Eventually(o4Runner.Err(), network.EventuallyTimeout).Should(gbytes.Say("Block censorship detected"))
+
+		By("Assert all block are received")
+		assertBlockReception(map[string]int{channel: 20}, []*nwo.Orderer{network.Orderers[3]}, peer, network)
+
+		close(censoringOrderer.StopDeliveryChannel)
 	})
 })
