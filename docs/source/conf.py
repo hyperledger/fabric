016344c62bcf247c440c421becc0273a563fbb32
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2024-10-17 17:05:11 +0100

    Update app dev tutorial to include Go and Java
    This change uses (linked) tabs to display code snippets and
    language-specific commands for the user's desired programming language.
    
    In order to use the sphinx-tabs extension, the version of Python used to
    build the docs had to be updated. Since the `n42org/tox` Docker image
    used had a very old Python version baked in, and there were no more
    recent versions available, the `make docs` target now uses the official
    Python image. In addition to providing a much more current Python
    environment, this image natively supports arm64 in addition to amd64,
    allowing docs to be built on Apple Silicon Macs.
    
    Signed-off-by: Mark S. Lewis <Mark.S.Lewis@outlook.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,29 +10,30 @@
 import os
 import sys
 from os import environ
-sys.path.insert(0, os.path.abspath('.'))
+
+sys.path.insert(0, os.path.abspath("."))
 
 import sphinx_rtd_theme
 
-rtd_tag = 'latest'
-if environ.get('READTHEDOCS_VERSION') is not None:
-    rtd_tag = os.environ['READTHEDOCS_VERSION']
+rtd_tag = "latest"
+if environ.get("READTHEDOCS_VERSION") is not None:
+    rtd_tag = os.environ["READTHEDOCS_VERSION"]
 
 
 placeholder_replacements = {
     "{BRANCH}": "main",
-    "{BRANCH_DOC}" : "latest", # Used to target the correct ReadTheDocs distribution version
-    "{RTD_TAG}": rtd_tag
+    "{BRANCH_DOC}": "latest",  # Used to target the correct ReadTheDocs distribution version
+    "{RTD_TAG}": rtd_tag,
 }
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = u'Hyperledger Fabric Docs'
-copyright = u'2017-2024, Hyperledger Foundation'
-author = u'Hyperledger Foundation'
-release = u'main'
-version = u'main'
+project = "Hyperledger Fabric Docs"
+copyright = "2017-2024, Hyperledger Foundation"
+author = "Hyperledger Foundation"
+release = "main"
+version = "main"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -46,43 +47,43 @@ version = u'main'
 # Sphinx projects.
 # Installed version as per directive in docs/requirement.txt
 source_parsers = {
-    '.md': 'recommonmark.parser.CommonMarkParser',
+    ".md": "recommonmark.parser.CommonMarkParser",
 }
 
-# The file extensions of source files. Sphinx considers the files with this suffix as sources. 
+# The file extensions of source files. Sphinx considers the files with this suffix as sources.
 # The value can be a dictionary mapping file extensions to file types. For example:
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown'
-}
+source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # Used to be "master_doc"
 # The main toctree document
-root_doc = 'index'
+root_doc = "index"
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'myst_parser',
-    'sphinxcontrib.jquery']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.imgmath",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+    "sphinxcontrib.jquery",
+    "sphinx_tabs.tabs",
+]
 
 # -- Special API Accesses -------------------------------------------------
 # They create an instance of the Sphinx object, documented here
@@ -91,26 +92,28 @@ extensions = ['sphinx.ext.autodoc',
 #
 # We then call it to perform special/specific customizations.
 
+
 def placeholderReplace(app, docname, source):
     result = source[0]
     for key in app.config.placeholder_replacements:
         result = result.replace(key, app.config.placeholder_replacements[key])
     source[0] = result
 
+
 def setup(app):
-    app.add_css_file('css/custom.css')
-    app.add_config_value('placeholder_replacements', {}, True)
-    app.connect('source-read', placeholderReplace)
+    app.add_css_file("css/custom.css")
+    app.add_config_value("placeholder_replacements", {}, True)
+    app.connect("source-read", placeholderReplace)
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_add_permalinks = True
 
