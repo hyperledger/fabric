a55f8b8fd310fd9b49eab664fc0232f91ba1ef68
Dave Enyeart <enyeart@us.ibm.com>
2025-01-27 13:18:55 -0500

    Release commit v2.5.11 (#5125)
    * Bump Go to 1.23.5 (release-2.5)
    
    Bump Go to 1.23.5 (release-2.5)
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>
    
    * Release commit for v2.5.11
    
    Release commit for v2.5.11.
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>
    
    ---------
    
    Signed-off-by: David Enyeart <enyeart@us.ibm.com>

--- docs/source/whatsnew.rst
+++ docs/source/whatsnew.rst
@@ -344,6 +344,7 @@ Specifically, take a look at the changes and deprecations.
 * `Fabric v2.5.8 release notes <https://github.com/hyperledger/fabric/releases/tag/v2.5.8>`_.
 * `Fabric v2.5.9 release notes <https://github.com/hyperledger/fabric/releases/tag/v2.5.9>`_.
 * `Fabric v2.5.10 release notes <https://github.com/hyperledger/fabric/releases/tag/v2.5.10>`_.
+* `Fabric v2.5.11 release notes <https://github.com/hyperledger/fabric/releases/tag/v2.5.11>`_.
 
 .. Licensed under Creative Commons Attribution 4.0 International License
    https://creativecommons.org/licenses/by/4.0/
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,9 +6,9 @@
 #
 
 # if version not passed in, default to latest released version
-VERSION=2.5.10
+VERSION=2.5.11
 # if ca version not passed in, default to latest released version
-CA_VERSION=1.5.13
+CA_VERSION=1.5.14
 ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m |sed 's/x86_64/amd64/g')" |sed 's/darwin-arm64/darwin-amd64/g')
 MARCH=$(uname -m)
 
@@ -21,8 +21,8 @@ printHelp() {
     echo "-s : bypass fabric-samples repo clone"
     echo "-b : bypass download of platform-specific binaries"
     echo
-    echo "e.g. bootstrap.sh 2.5.10 1.5.13 -s"
-    echo "will download docker images and binaries for Fabric v2.5.10 and Fabric CA v1.5.13"
+    echo "e.g. bootstrap.sh 2.5.11 1.5.14 -s"
+    echo "will download docker images and binaries for Fabric v2.5.11 and Fabric CA v1.5.14"
 }
 
 # dockerPull() pulls docker images from fabric and chaincode repositories
