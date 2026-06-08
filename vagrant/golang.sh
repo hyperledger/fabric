dffc087346ddc69a770b277088c03ef185dbdfb0
Fedor Partanskiy <fredprtnsk@gmail.com>
2026-06-08 19:20:49 +0300

    2.5 bump go to 1.26.4
    Signed-off-by: Fedor Partanskiy <fredprtnsk@gmail.com>

--- vagrant/golang.sh
+++ vagrant/golang.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 GOROOT='/opt/go'
-GO_VERSION=1.26.3
+GO_VERSION=1.26.4
 
 # ----------------------------------------------------------------
 # Install Golang
